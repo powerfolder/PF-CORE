588ede23ab8bbc8e29bb4e13feae60fbdb1bbdc5
Christian Sprajc <sprajc@powerfolder.com>
2006-06-25 11:14:44 +0000

    JNI source and DLL for the network helper class

---
+++ src/main/de/dal33t/powerfolder/util/os/Win32/make_netuil.bat
@@ -0,0 +1,4 @@
+javah -classpath ..\..\..\..\..\..\ de.dal33t.powerfolder.util.os.Win32.NetworkUtilImpl
+gcc -Wall -D_JNI_IMPLEMENTATION_ -IC:\JNI\include -IC:\JNI\include\win32 -c netutil.c
+gcc -Wl,--kill-at netutil.o -shared -o netutil.dll -lIphlpapi
+strip --strip-all netutil.dll
