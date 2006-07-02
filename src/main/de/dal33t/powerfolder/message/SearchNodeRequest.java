88cbeb1056b7d20a8255545a4ff4c391905e9f27
Christian Sprajc <sprajc@powerfolder.com>
2006-07-02 15:23:02 +0000

    Extracted nodeSearching from findfriendspanel with junit test

--- src/main/de/dal33t/powerfolder/message/SearchNodeRequest.java
+++ src/main/de/dal33t/powerfolder/message/SearchNodeRequest.java
@@ -1,6 +1,7 @@
 // $Id: SearchNodeRequest.java,v 1.1 2006/03/06 22:40:31 bytekeeper Exp $
 package de.dal33t.powerfolder.message;
 
+
 /**
  * A search request for nodes.
  * 
@@ -11,14 +12,11 @@ public class SearchNodeRequest extends Message {
     private static final long serialVersionUID = 100L;
 
     public String searchString;
-    
+
     public SearchNodeRequest(String searchString) {
         this.searchString = searchString;
     }
 
-    public SearchNodeRequest() {
-    }
-    
     public String toString() {
         return "Search for '" + searchString + "'";
     }
