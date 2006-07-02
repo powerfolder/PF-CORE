fae46b2b111290534d0756a22fe1e0463fa78e6e
Christian Sprajc <sprajc@powerfolder.com>
2006-07-02 15:31:30 +0000

    Test for nodemanagerTablemodel

---
+++ src/test/de/dal33t/powerfolder/test/ui/NodeTableModelTest.java
@@ -0,0 +1,148 @@
+/* $Id$
+ */
+package de.dal33t.powerfolder.test.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+
+import com.jgoodies.binding.list.ObservableList;
+
+import de.dal33t.powerfolder.Member;
+import de.dal33t.powerfolder.light.MemberInfo;
+import de.dal33t.powerfolder.test.ControllerTestCase;
+import de.dal33t.powerfolder.ui.model.NodeTableModel;
+import de.dal33t.powerfolder.util.IdGenerator;
+
+/**
+ * Tests the node table model.
+ * 
+ * @see de.dal33t.powerfolder.ui.model.NodeTableModel
+ * @author <a href="mailto:sprajc@riege.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public class NodeTableModelTest extends ControllerTestCase {
+    private NodeTableModel model;
+    private MyTableModelListener listener;
+    private Member moe;
+    private Member homer;
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        moe = getController().getNodeManager().addNode(
+            new MemberInfo("Moe", IdGenerator.makeId()));
+        homer = getController().getNodeManager().addNode(
+            new MemberInfo("Homer", IdGenerator.makeId()));
+
+        model = new NodeTableModel(getController());
+        listener = new MyTableModelListener();
+        model.addTableModelListener(listener);
+    }
+
+    /**
+     * Tests the direct mutation of the node table model via its own
+     * modification methods.
+     */
+    public void testDirectChanges() {
+        // Test = No users found
+        assertEquals(1, model.getRowCount());
+        assertTrue(model.getValueAt(0, 0) instanceof String);
+
+        model.add(getController().getMySelf());
+        assertEquals(1, model.getRowCount());
+        assertEquals(getController().getMySelf(), model.getDataAt(0));
+        assertEquals(1, listener.events.size());
+
+        model.add(getController().getMySelf());
+        assertEquals(2, model.getRowCount());
+        assertEquals(getController().getMySelf(), model.getDataAt(1));
+        assertEquals(2, listener.events.size());
+
+        model.add(moe);
+        assertEquals(3, model.getRowCount());
+        assertEquals(moe, model.getValueAt(2, 1));
+        assertEquals(3, listener.events.size());
+        assertTrue(model.contains(moe));
+
+        model.remove(getController().getMySelf());
+        assertEquals(2, model.getRowCount());
+        assertEquals(moe, model.getValueAt(1, 2));
+        assertEquals(moe, model.getDataAt(1));
+        assertEquals(4, listener.events.size());
+        assertTrue(model.contains(getController().getMySelf()));
+
+        model.clear();
+        assertEquals(1, model.getRowCount());
+        assertTrue(model.getDataAt(0) instanceof String);
+        assertEquals(5, listener.events.size());
+        assertEquals(TableModelEvent.UPDATE, listener.lastEvent.getType());
+
+        model.add(homer);
+        assertEquals(1, model.getRowCount());
+        assertEquals(homer, model.getValueAt(0, 0));
+        assertEquals(6, listener.events.size());
+        assertTrue(model.contains(homer));
+    }
+
+    /**
+     * Tests the mutation of the node table model via its listmodel.
+     */
+    public void testListModelChanges() {
+        ObservableList list = model.getListModel();
+        // Test = No users found
+        assertEquals(1, model.getRowCount());
+        assertTrue(model.getValueAt(0, 0) instanceof String);
+
+        list.add(getController().getMySelf());
+        assertEquals(1, model.getRowCount());
+        assertEquals(getController().getMySelf(), model.getDataAt(0));
+        assertEquals(1, listener.events.size());
+
+        list.add(getController().getMySelf());
+        assertEquals(2, model.getRowCount());
+        assertEquals(getController().getMySelf(), model.getDataAt(1));
+        assertEquals(2, listener.events.size());
+
+        list.add(moe);
+        assertEquals(3, model.getRowCount());
+        assertEquals(moe, model.getValueAt(2, 1));
+        assertEquals(3, listener.events.size());
+        assertTrue(model.contains(moe));
+
+        list.remove(getController().getMySelf());
+        assertEquals(2, model.getRowCount());
+        assertEquals(moe, model.getValueAt(1, 2));
+        assertEquals(moe, model.getDataAt(1));
+        assertEquals(4, listener.events.size());
+        assertTrue(model.contains(getController().getMySelf()));
+
+        list.clear();
+        assertEquals(1, model.getRowCount());
+        assertTrue(model.getDataAt(0) instanceof String);
+        assertEquals(5, listener.events.size());
+        assertEquals(TableModelEvent.UPDATE, listener.lastEvent.getType());
+
+        list.add(homer);
+        assertEquals(1, model.getRowCount());
+        assertEquals(homer, model.getValueAt(0, 0));
+        assertEquals(6, listener.events.size());
+        assertTrue(model.contains(homer));
+    }
+
+    private static final class MyTableModelListener implements
+        TableModelListener
+    {
+        public List<TableModelEvent> events = new ArrayList<TableModelEvent>();
+        public TableModelEvent lastEvent;
+
+        public void tableChanged(TableModelEvent e) {
+            events.add(e);
+            lastEvent = e;
+        }
+    }
+}
