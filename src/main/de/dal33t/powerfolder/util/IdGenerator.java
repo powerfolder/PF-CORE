2ef6c13108005efabf3153e60d6049c006560443
Christian Sprajc <sprajc@powerfolder.com>
2006-07-02 15:30:16 +0000

    Completely rewritten. now uses UUID from java 1.5

--- src/main/de/dal33t/powerfolder/util/IdGenerator.java
+++ src/main/de/dal33t/powerfolder/util/IdGenerator.java
@@ -2,190 +2,56 @@
  */
 package de.dal33t.powerfolder.util;
 
-import java.math.BigInteger;
-import java.net.InetAddress;
+import java.util.UUID;
 
 /**
- * Simple mechanismn to generate a unique id in space and time
+ * Simple mechanism to generate a unique id in space and time
  * 
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
  * @version $Revision: 1.2 $
  */
 public class IdGenerator {
-    private static String addressID;
-    private static String timeID;
-    private static short loopCounter = 0;
-
-    private static boolean isInitialized = false;
 
     /**
-     * Encode the information in base64.
-     *
-     * Base64 Alphabet (0-9, 10=A, 35=Z, 36=a, 61=z, 62='+', 63='#'). 
-     *
-     * @param bytes byte Byte array to be base64-encoded
-     * @return java.lang.String Base64-encoded String of the input
-     */
-    private static String encodeBytesInBase64(byte[] bytes) {
-
-        // max. 8 Ziffern
-        char[] chars = { '_', '_', '_', '_', '_', '_', '_', '_' };
-
-        // 8 x
-        for (int i = 7; i >= 0; i--) {
-
-            int z = bytes[5] & 0x3F;
-            // z in Ziffer umwandeln
-            if (z < 10) {
-                chars[i] = (char) ('0' + z);
-            } else if (z < 36) {
-                chars[i] = (char) ('A' + z - 10);
-            } else if (z < 62) {
-                chars[i] = (char) ('a' + z - 36);
-            } else if (z == 62) {
-                chars[i] = '+';
-            } else {
-                chars[i] = '#';
-            }
-
-            // Shiften...
-            for (int j = 5; j > 0; j--) {
-                bytes[j] =
-                    (byte) ((bytes[j] >> 6) + ((bytes[j - 1] & 0x3F) * 4));
-            }
-        }
-        return new String(chars);
-    }
-
-    /**
-     * Get the counter value as a signed short. 
-     * @return short
-     */
-    private static synchronized short getLoopCounter() {
-        return loopCounter++;
-    }
-
-    /**
-     * Initializes the per-JVM static part of the Object ID.
-     */
-    private static void initialize() {
-
-        try {
-            // Prepare a buffer to hold the 6 bytes for the addressID 
-            byte[] bytes6 = new byte[6];
-
-            // Get the local IP address as a byte array 
-            byte[] bytesIP = InetAddress.getLocalHost().getAddress();
-
-            // Copy the 2 less significant bytes of the IP address 
-            bytes6[5] = bytesIP[2];
-            bytes6[4] = bytesIP[3];
-
-            // Get the memory address for this object 
-            int memAdr = System.identityHashCode(bytesIP);
-
-            // Prepare a buffer to hold the 4 less significant bytes of the addr 
-            byte[] bytes4 = new byte[4];
-
-            // Convert the memory address into a byte array 
-            toFixSizeByteArray(new BigInteger(String.valueOf(memAdr)), bytes4);
-            bytes6[3] = bytes4[0];
-            bytes6[2] = bytes4[1];
-            bytes6[1] = bytes4[2];
-            bytes6[0] = bytes4[3];
-
-            // Encode the information in base64 
-            addressID = encodeBytesInBase64(bytes6);
-
-            isInitialized = true;
-        } catch (Exception ignore) {
-        }
-    }
-
-    /**
-     * Generates a new Object ID.
+     * Generates a randomly unique, base64 encoded id for a UUID. The UUID is
+     * 128 bits (16 bytes) strong. The returned string is 22 characters long.
      * 
-     * @return A new (globally unique) Object ID.
+     * @see UUID
+     * @return the base64 encoded uuid
      */
     public static String makeId() {
-        // Initialsierung
-        if (!isInitialized) {
-            initialize();
-        }
-
-        // Prepare a buffer to hold the 6 bytes for the timeID 
-        byte[] bytes6 = new byte[6];
-
-        // Get the current time 
-        long timeNow = System.currentTimeMillis();
-
-        // Ignore the most 4 significant bytes 
-        timeNow = (int) timeNow & 0xFFFFFFFF;
-
-        // Prepare a buffer to hold the 4 less significant bytes of the time 
-        byte[] bytes4 = new byte[4];
-
-        // Convert the memory address into a byte array 
-        toFixSizeByteArray(new BigInteger(String.valueOf(timeNow)), bytes4);
-        bytes6[5] = bytes4[0];
-        bytes6[4] = bytes4[1];
-        bytes6[3] = bytes4[2];
-        bytes6[2] = bytes4[3];
-
-        // Get the current counter reading 
-        short counter = getLoopCounter();
-
-        // Prepare a buffer to hold the 2 bytes of the counter 
-        byte[] bytes2 = new byte[2];
-
-        // Convert the counter into a byte array 
-        toFixSizeByteArray(new BigInteger(String.valueOf(counter)), bytes2);
-        bytes6[1] = bytes2[0];
-        bytes6[0] = bytes2[1];
-
-        // Encode the information in base64 
-        timeID = encodeBytesInBase64(bytes6);
-
-        // Return the unique ID 
-        return timeID + addressID;
-
+        String id = Base64.encodeBytes(makeIdBytes());
+        // Remove the last == at the end
+        return id.substring(0, id.length() - 2);
     }
 
     /**
-     * This method transforms Java BigInteger type into a fix size byte array 
-     * containing the two's-complement representation of the integer. 
-     * The byte array will be in big-endian byte-order: the most significant 
-     * byte is in the zeroth element. 
-     * If the destination array is shorter then the BigInteger.toByteArray(), 
-     * the the less significant bytes will be copy only. 
-     * If the destination array is longer then the BigInteger.toByteArray(), 
-     * destination will be left padded with zeros. 
-     * 
-     * @param bigInt java.math.BigInteger
-     * @param destination byte[]
+     * @return a random UUID as byte array (16 bytes strong)
      */
-    private static void toFixSizeByteArray(
-        java.math.BigInteger bigInt,
-        byte[] destination) {
+    public static byte[] makeIdBytes() {
+        UUID uid = UUID.randomUUID();
+        byte[] arr = new byte[16];
 
-        // Prepare the destination 
-        for (int i = 0; i < destination.length; i++) {
-            destination[i] = 0x00;
-        }
+        long msb = uid.getMostSignificantBits();
+        arr[0] = (byte) ((msb >> 56) & 0xFF);
+        arr[1] = (byte) ((msb >> 48) & 0xFF);
+        arr[2] = (byte) ((msb >> 40) & 0xFF);
+        arr[3] = (byte) ((msb >> 32) & 0xFF);
+        arr[4] = (byte) ((msb >> 24) & 0xFF);
+        arr[5] = (byte) ((msb >> 16) & 0xFF);
+        arr[6] = (byte) ((msb >> 8) & 0xFF);
+        arr[7] = (byte) (msb & 0xFF);
 
-        // Convert the BigInt to a byte array 
-        byte[] source = bigInt.toByteArray();
+        long lsb = uid.getLeastSignificantBits();
+        arr[8] = (byte) ((lsb >> 56) & 0xFF);
+        arr[9] = (byte) ((lsb >> 48) & 0xFF);
+        arr[10] = (byte) ((lsb >> 40) & 0xFF);
+        arr[11] = (byte) ((lsb >> 32) & 0xFF);
+        arr[12] = (byte) ((lsb >> 24) & 0xFF);
+        arr[13] = (byte) ((lsb >> 16) & 0xFF);
+        arr[14] = (byte) ((lsb >> 8) & 0xFF);
+        arr[15] = (byte) (lsb & 0xFF);
 
-        // Copy only the fix size length 
-        if (source.length <= destination.length) {
-            for (int i = 0; i < source.length; i++) {
-                destination[destination.length - source.length + i] = source[i];
-            }
-        } else {
-            for (int i = 0; i < destination.length; i++) {
-                destination[i] = source[source.length - destination.length + i];
-            }
-        }
+        return arr;
     }
-
 }
