5b49ab3921ec9882d6c6da82b7e40348b20698f2
Christian Sprajc <sprajc@powerfolder.com>
2006-07-02 15:21:43 +0000

    added docs

--- src/main/de/dal33t/powerfolder/message/MessageListener.java
+++ src/main/de/dal33t/powerfolder/message/MessageListener.java
@@ -5,15 +5,21 @@ package de.dal33t.powerfolder.message;
 import de.dal33t.powerfolder.Member;
 
 /**
- * A message listener, which can by added to a member
+ * A message listener, which can by added to a member.
+ * <p>
+ * TODO Make this extend CoreListener
+ * 
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
  * @version $Revision: 1.1 $
  */
 public interface MessageListener {
-	/**
-	 * Handles the received message
-	 * @param source the node where it came from
-     * @param message the message received
+    /**
+     * Handles the received message.
+     * 
+     * @param source
+     *            the node where it came from
+     * @param message
+     *            the message received
      */
     public void handleMessage(Member source, Message message);
 }
