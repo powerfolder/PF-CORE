70f64e1edb647ab19a63f156e0bcf76e64ec97e8
Christian Sprajc <sprajc@powerfolder.com>
2006-08-11 21:54:39 +0000

    more time ;)

--- src/test/de/dal33t/powerfolder/test/transfer/FileTransferTest.java
+++ src/test/de/dal33t/powerfolder/test/transfer/FileTransferTest.java
@@ -106,7 +106,7 @@ public class FileTransferTest extends TwoControllerTestCase {
         folder1.scanLocalFiles(true);
 
         // Give them time to copy
-        TestHelper.waitMilliSeconds(500);
+        TestHelper.waitMilliSeconds(5000);
 
         // Test ;)
         assertEquals(1, folder2.getFilesCount());
