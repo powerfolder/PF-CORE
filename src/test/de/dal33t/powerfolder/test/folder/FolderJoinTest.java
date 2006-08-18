bfb59c3aa8640bb35a29c9226b13dda494fe1067
Christian Sprajc <sprajc@powerfolder.com>
2006-08-18 20:08:42 +0000

    Added more tests

--- src/test/de/dal33t/powerfolder/test/folder/FolderJoinTest.java
+++ src/test/de/dal33t/powerfolder/test/folder/FolderJoinTest.java
@@ -22,29 +22,81 @@ import de.dal33t.powerfolder.util.IdGenerator;
  */
 public class FolderJoinTest extends TwoControllerTestCase {
 
-    private static final String BASEDIR1 = "build/test/controllerBart/testFolder";
-    private static final String BASEDIR2 = "build/test/controllerLisa/testFolder";
+    private static final String BASEDIR1 = "build/test/controllerBart";
+    private static final String BASEDIR2 = "build/test/controllerLisa";
+
+    private static final String FOLDERDIR1 = "build/test/controllerBart/testFolder";
+    private static final String FOLDERDIR2 = "build/test/controllerLisa/testFolder";
 
     @Override
     protected void setUp() throws Exception
     {
         // Remove directries
-        FileUtils.deleteDirectory(new File(BASEDIR1));
-        FileUtils.deleteDirectory(new File(BASEDIR2));
+        FileUtils.deleteDirectory(new File(FOLDERDIR1));
+        FileUtils.deleteDirectory(new File(FOLDERDIR2));
 
         super.setUp();
     }
 
-    public void testJoinByID() {
+    public void testJoinSecretFolder() {
         // Join on testfolder
         FolderInfo testFolder = new FolderInfo("testFolder", IdGenerator
             .makeId(), true);
-        joinFolder(testFolder, new File(BASEDIR1), new File(BASEDIR2));
+        joinFolder(testFolder, new File(FOLDERDIR1), new File(FOLDERDIR2));
 
         assertEquals(2, getContollerBart().getFolderRepository().getFolder(
             testFolder).getMembersCount());
         assertEquals(2, getContollerLisa().getFolderRepository().getFolder(
             testFolder).getMembersCount());
+    }
+
+    public void testJoinPublicFolder() {
+        // Join on testfolder
+        FolderInfo testFolder = new FolderInfo("testFolder", IdGenerator
+            .makeId(), false);
+        joinFolder(testFolder, new File(FOLDERDIR1), new File(FOLDERDIR2));
+
+        assertEquals(2, getContollerBart().getFolderRepository().getFolder(
+            testFolder).getMembersCount());
+        assertEquals(2, getContollerLisa().getFolderRepository().getFolder(
+            testFolder).getMembersCount());
+    }
+
+    public void testJoinMultipleFolders() {
+        for (int i = 0; i < 10; i++) {
+            FolderInfo testFolder = createRandomFolder("s-" + i, true);
+            File folderDirBart = new File(BASEDIR1 + "/" + testFolder.name);
+            File folderDirLisa = new File(BASEDIR2 + "/" + testFolder.name);
+            joinFolder(testFolder, folderDirBart, folderDirLisa);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            FolderInfo testFolder = createRandomFolder("p-" + i, false);
+            File folderDirBart = new File(BASEDIR1 + "/" + testFolder.name);
+            File folderDirLisa = new File(BASEDIR2 + "/" + testFolder.name);
+            joinFolder(testFolder, folderDirBart, folderDirLisa);
+        }
+        
+        Folder[] bartsFolders = getContollerBart().getFolderRepository().getFolders();
+        Folder[] lisasFolders = getContollerLisa().getFolderRepository().getFolders();
+       
+        assertEquals(20, getContollerBart().getFolderRepository().getFoldersCount());
+        assertEquals(20, getContollerLisa().getFolderRepository().getFoldersCount());
+        assertEquals(20, bartsFolders.length);
+        assertEquals(20, lisasFolders.length);
+        
+        for (Folder folder : lisasFolders) {
+            assertEquals(2, folder.getMembersCount());
+        }
+        
+        for (Folder folder : bartsFolders) {
+            assertEquals(2, folder.getMembersCount());
+        }
+    }
+
+    private FolderInfo createRandomFolder(String nameSuffix, boolean secret) {
+        String folderName = "testFolder-" + nameSuffix;
+        return new FolderInfo(folderName, IdGenerator.makeId(), secret);
     }
 
     /**
@@ -60,16 +112,16 @@ public class FolderJoinTest extends TwoControllerTestCase {
             .makeId(), true);
 
         // Prepare folder on "host" Bart.
-        TestHelper.createRandomFile(new File(BASEDIR1));
-        TestHelper.createRandomFile(new File(BASEDIR1));
-        TestHelper.createRandomFile(new File(BASEDIR1));
+        TestHelper.createRandomFile(new File(FOLDERDIR1));
+        TestHelper.createRandomFile(new File(FOLDERDIR1));
+        TestHelper.createRandomFile(new File(FOLDERDIR1));
 
         getContollerBart().getFolderRepository().createFolder(testFolder,
-            new File(BASEDIR1));
+            new File(FOLDERDIR1));
 
         // Now let lisa join with auto-download
         final Folder folderLisa = getContollerLisa().getFolderRepository()
-            .createFolder(testFolder, new File(BASEDIR2),
+            .createFolder(testFolder, new File(FOLDERDIR2),
                 SyncProfile.AUTO_DOWNLOAD_FROM_ALL, false);
 
         TestHelper.waitForCondition(20, new TestHelper.Condition() {
@@ -95,11 +147,11 @@ public class FolderJoinTest extends TwoControllerTestCase {
             .makeId(), true);
         // Prepare folder on "host" Bart.
         Folder folderBart = getContollerBart().getFolderRepository()
-            .createFolder(testFolder, new File(BASEDIR1));
+            .createFolder(testFolder, new File(FOLDERDIR1));
 
-        TestHelper.createRandomFile(new File(BASEDIR1));
-        TestHelper.createRandomFile(new File(BASEDIR1));
-        TestHelper.createRandomFile(new File(BASEDIR1));
+        TestHelper.createRandomFile(new File(FOLDERDIR1));
+        TestHelper.createRandomFile(new File(FOLDERDIR1));
+        TestHelper.createRandomFile(new File(FOLDERDIR1));
         folderBart.scanLocalFiles(true);
 
         // Set lisa in silent mode
@@ -107,7 +159,7 @@ public class FolderJoinTest extends TwoControllerTestCase {
 
         // Now let lisa join with auto-download
         final Folder folderLisa = getContollerLisa().getFolderRepository()
-            .createFolder(testFolder, new File(BASEDIR2),
+            .createFolder(testFolder, new File(FOLDERDIR2),
                 SyncProfile.AUTO_DOWNLOAD_FROM_ALL, false);
 
         TestHelper.waitForCondition(50, new TestHelper.Condition() {
