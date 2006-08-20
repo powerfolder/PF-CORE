e32e5b3859dfb11231adbec91f4ea464b795a457
Christian Sprajc <sprajc@powerfolder.com>
2006-08-20 12:28:56 +0000

    Added method to parse invitation from powerfolder link

--- src/main/de/dal33t/powerfolder/message/Invitation.java
+++ src/main/de/dal33t/powerfolder/message/Invitation.java
@@ -2,8 +2,13 @@
  */
 package de.dal33t.powerfolder.message;
 
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
 import de.dal33t.powerfolder.light.FolderInfo;
 import de.dal33t.powerfolder.light.MemberInfo;
+import de.dal33t.powerfolder.util.Logger;
+import de.dal33t.powerfolder.util.Reject;
 import de.dal33t.powerfolder.util.Util;
 
 /**
@@ -14,6 +19,9 @@ import de.dal33t.powerfolder.util.Util;
  */
 public class Invitation extends FolderRelatedMessage {
     private static final long serialVersionUID = 101L;
+    // The prefix for pf links
+    private static final String POWERFOLDER_LINK_PREFIX = "powerfolder://";
+    private static final Logger LOG = Logger.getLogger(Invitation.class);
 
     // Added invitor to invitation
     public MemberInfo invitor;
@@ -34,6 +42,63 @@ public class Invitation extends FolderRelatedMessage {
         return "PowerFolder://|folder|" + Util.endcodeForURL(folder.name) + "|"
             + (folder.secret ? "S" : "P") + "|" + Util.endcodeForURL(folder.id)
             + "|" + folder.bytesTotal + "|" + folder.filesCount;
+    }
+
+    /**
+     * Creates a powerfolder invitation from a powerfolder link.
+     * 
+     * @param link
+     *            the link as string.
+     * @return the invitation parsed from the link. or null if link was not
+     *         parsable.
+     */
+    public static Invitation fromPowerFolderLink(String link) {
+        Reject.ifBlank(link, "Link is empty");
+        String plainLink = link.substring(POWERFOLDER_LINK_PREFIX.length());
+
+        // Chop off ending /
+        if (plainLink.endsWith("/")) {
+            plainLink = plainLink.substring(1, plainLink.length() - 1);
+        }
+
+        try {
+            // Parse link
+            StringTokenizer nizer = new StringTokenizer(plainLink, "|");
+            // Get type
+            String type = nizer.nextToken();
+
+            if (!"folder".equalsIgnoreCase(type)) {
+                // Illegal, not supported
+                LOG.error("Link not supported: " + link);
+                return null;
+            }
+
+            // Decode the url form
+            String name = Util.decodeFromURL(nizer.nextToken());
+            boolean secret = nizer.nextToken().equalsIgnoreCase("s");
+            String id = Util.decodeFromURL(nizer.nextToken());
+            FolderInfo folder = new FolderInfo(name, id, secret);
+
+            // Parse optional folder infos
+            if (nizer.hasMoreElements()) {
+                try {
+                    folder.bytesTotal = Long.parseLong(nizer.nextToken());
+                    if (nizer.hasMoreElements()) {
+                        folder.filesCount = Integer.parseInt(nizer.nextToken());
+                    }
+                } catch (NumberFormatException e) {
+                    LOG
+                        .error("Unable to parse additonal folder info from link. "
+                            + link);
+                }
+            }
+
+            return new Invitation(folder, null);
+        } catch (NoSuchElementException e) {
+            LOG.error("Illegal link '" + link + "'");
+        }
+        return null;
+
     }
 
     public String toString() {
