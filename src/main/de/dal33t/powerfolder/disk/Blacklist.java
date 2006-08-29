008d7f7dc6dd55d1f6a4eb2445a9ec0365eddd21
Christian Sprajc <sprajc@powerfolder.com>
2006-08-29 21:55:13 +0000

    javadoced

--- src/main/de/dal33t/powerfolder/disk/Blacklist.java
+++ src/main/de/dal33t/powerfolder/disk/Blacklist.java
@@ -17,7 +17,8 @@ import de.dal33t.powerfolder.light.FileInfo;
  * </TR>
  * <TR>
  * <TD valign=top>*thumbs.db</TD>
- * <TD>Will filter the file thumbs.db in any subdirectory or filename that ends with thumbs.db</TD>
+ * <TD>Will filter the file thumbs.db in any subdirectory or filename that ends
+ * with thumbs.db</TD>
  * </TR>
  * <TR>
  * <TD valign=top>images/*thumbs.db </TD>
@@ -38,15 +39,39 @@ public class Blacklist {
 
     // using MAPs to get fast access. (not using HashSet because that one does
     // excactly the same)
+    /** The FileInfos that are specificaly marked to not automaticaly download */
     private Map<FileInfo, Object> doNotAutoDownload;
+
+    /**
+     * The FileInfos that are specificaly marked as do not share, other clients
+     * will never see this file
+     */
     private Map<FileInfo, Object> doNotShare;
 
+    /**
+     * The patterns as Strings that may match files so that files wont be
+     * downloaded (See class definition for explanation of the patterns)
+     */
+
     private List<String> doNotAutoDownloadStringPatterns;
+    /**
+     * The patterns as Strings that may match files so that files wont be shared
+     * (See class definition for explanation of the patterns)
+     */
     private List<String> doNotShareStringPatterns;
 
+    /**
+     * The patterns as Pattern objects that may match files so that files wont
+     * be downloaded (See class definition for explanation of the patterns)
+     */
     private List<Pattern> doNotAutoDownloadPatterns;
+    /**
+     * The patterns as Pattern objects that may match files so that files wont
+     * be shared (See class definition for explanation of the patterns)
+     */
     private List<Pattern> doNotSharePatterns;
 
+    /** creates a Blacklist creates all Maps */
     public Blacklist() {
         doNotAutoDownload = Collections
             .synchronizedMap(new HashMap<FileInfo, Object>(2));
@@ -64,51 +89,89 @@ public class Blacklist {
 
     // Mutators of blacklist **************************************************
 
+    /**
+     * add 1 or more FileInfos to the list in FileInfos that should not be
+     * automacicaly downloaded
+     */
     public void addToDoNotAutoDownload(FileInfo... fileInfos) {
         addToDoNotAutoDownload(Arrays.asList(fileInfos));
     }
 
+    /**
+     * add a Collection of FileInfos to the list in FileInfos that should not be
+     * automacicaly downloaded
+     */
     public void addToDoNotAutoDownload(Collection<FileInfo> fileInfos) {
         for (FileInfo fileInfo : fileInfos) {
             doNotAutoDownload.put(fileInfo, DUMMY);
         }
     }
 
+    /**
+     * Remove 1 or more FileInfos from the list of files that won't be
+     * automaticaly downloaded
+     */
     public void removeFromDoNotAutoDownload(FileInfo... fileInfos) {
         removeFromDoNotAutoDownload(Arrays.asList(fileInfos));
     }
 
+    /**
+     * Remove a Collection of FileInfos from the list of files that won't be
+     * automaticaly downloaded
+     */
     public void removeFromDoNotAutoDownload(Collection<FileInfo> fileInfos) {
         for (FileInfo fileInfo : fileInfos) {
             doNotAutoDownload.remove(fileInfo);
         }
     }
 
+    /**
+     * add 1 or more FileInfos to the list in FileInfos that should not be
+     * shared
+     */
     public void addToDoNotShare(FileInfo... fileInfos) {
         addToDoNotShare(Arrays.asList(fileInfos));
     }
 
+    /**
+     * add a Collection of FileInfos to the list in FileInfos that should not be
+     * shared
+     */
     public void addToDoNotShare(Collection<FileInfo> fileInfos) {
         for (FileInfo fileInfo : fileInfos) {
             doNotShare.put(fileInfo, DUMMY);
         }
     }
 
+    /**
+     * Remove 1 or more FileInfos from the list of files that won't be shared
+     */
     public void removeFromDoNotShare(FileInfo... fileInfos) {
         removeFromDoNotShare(Arrays.asList(fileInfos));
     }
 
+    /**
+     * Remove a Collection of FileInfos from the list of files that won't be
+     * shared
+     */
     public void removeFromDoNotShare(Collection<FileInfo> fileInfos) {
         for (FileInfo fileInfo : fileInfos) {
             doNotShare.remove(fileInfo);
         }
     }
 
+    /**
+     * Add a pattern to the list of patterns that will filter FileInfos so they
+     * won't be auto downloaded when matching this pattern
+     */
     public void addDoNotAutoDownloadPattern(String pattern) {
         doNotAutoDownloadStringPatterns.add(pattern);
         doNotAutoDownloadPatterns.add(Pattern.compile(convert(pattern)));
     }
 
+    /**
+     * Remove a pattern from the list of patterns that will filter FileInfos
+     */
     public void removeDoNotAutoDownloadPattern(String strPattern) {
         doNotAutoDownloadStringPatterns.remove(strPattern);
         String converted = convert(strPattern);
@@ -125,11 +188,18 @@ public class Blacklist {
         doNotAutoDownloadPatterns.remove(toRemove);
     }
 
+    /**
+     * Add a pattern to the list of patterns that will filter FileInfos so they
+     * won't be shared when matching this pattern
+     */
     public void addDoNotSharePattern(String pattern) {
         doNotShareStringPatterns.add(pattern);
         doNotSharePatterns.add(Pattern.compile(convert(pattern)));
     }
 
+    /**
+     * Remove a pattern from the list of patterns that will filter FileInfos
+     */
     public void removeDoNotSharePattern(String strPattern) {
         doNotShareStringPatterns.remove(strPattern);
         String converted = convert(strPattern);
@@ -148,6 +218,12 @@ public class Blacklist {
 
     // Accessors **************************************************************
 
+    /**
+     * Will check if this FileInfo is in the list of files that should not be
+     * auto download, or matches a pattern.
+     * 
+     * @return true if allowed to auto download, false if not
+     */
     public boolean isAllowedToAutoDownload(FileInfo fileInfo) {
         if (doNotAutoDownload.containsKey(fileInfo)) {
             return false;
@@ -162,6 +238,12 @@ public class Blacklist {
         return true;
     }
 
+    /**
+     * Will check if this FileInfo is in the list of files that should not be
+     * shared, or matches a pattern.
+     * 
+     * @return true if allowed to share, false if not
+     */
     public boolean isAllowedToShare(FileInfo fileInfo) {
         if (doNotShare.containsKey(fileInfo)) {
             return false;
@@ -175,18 +257,31 @@ public class Blacklist {
         return true;
     }
 
+    /**
+     * @return the list of files that are explicilt marked as not to download
+     *          atomaticaly
+     */
     public List<FileInfo> getDoNotAutodownload() {
         return new ArrayList<FileInfo>(doNotAutoDownload.keySet());
     }
 
+    /** @return the list of files that are explicilt marked as not share */
     public List<FileInfo> getDoNotShared() {
         return new ArrayList<FileInfo>(doNotShare.keySet());
     }
 
+    /**
+     * @return the list of patterns that may match files that should not be
+     *          auto downloaded
+     */
     public List<String> getDoNotAutoDownloadPatterns() {
         return new ArrayList<String>(doNotAutoDownloadStringPatterns);
     }
 
+    /**
+     * @return the list of patterns that may match files that should not be
+     *          shared
+     */
     public List<String> getDoNotSharePatterns() {
         return new ArrayList<String>(doNotShareStringPatterns);
     }
