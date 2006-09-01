ed600a278623cfaadc8badf741205b3229b770f0
Christian Sprajc <sprajc@powerfolder.com>
2006-09-01 23:11:00 +0000

    Added mixed case filename testings

--- src/test/de/dal33t/powerfolder/test/folder/TestBlacklist.java
+++ src/test/de/dal33t/powerfolder/test/folder/TestBlacklist.java
@@ -13,6 +13,8 @@ public class TestBlacklist extends TestCase {
         FileInfo fileInfo = new FileInfo(folderInfo, "thumbs.db");
         FileInfo fileInfo2 = new FileInfo(folderInfo, "thumbs.db");
         FileInfo fileInfo3 = new FileInfo(folderInfo, "somefile.txt");
+        FileInfo fileInfo4 = new FileInfo(folderInfo,
+            "A_UPPER_case_FILENAME.xxx");
         blacklist.add(fileInfo);
         assertTrue(blacklist.isIgnored(fileInfo));
         // other instance but equals
@@ -22,42 +24,45 @@ public class TestBlacklist extends TestCase {
         // after remove allow download again
         blacklist.remove(fileInfo);
         assertFalse(blacklist.isIgnored(fileInfo));
-
-       
-
+        // Mix-case filename test
+        blacklist.add(fileInfo4);
+        assertTrue(blacklist.isIgnored(fileInfo4));
     }
 
     public void testBlacklistPatterns() {
         Blacklist blacklist = new Blacklist();
         FolderInfo folderInfo = new FolderInfo("foldername", "id", true);
         blacklist.addPattern("*thumbs.db");
-        
+        blacklist.addPattern("*THAMBS.db");
 
-        assertTrue(blacklist.isIgnored(new FileInfo(folderInfo,
-            "thumbs.db")));
+        assertTrue(blacklist.isIgnored(new FileInfo(folderInfo, "thumbs.db")));
         assertTrue(blacklist.isIgnored(new FileInfo(folderInfo,
             "somewhere/in/a/sub/thumbs.db")));
-        assertFalse(blacklist.isIgnored(new FileInfo(folderInfo,
-            "thusssmbs.db")));
-        
+        assertFalse(blacklist
+            .isIgnored(new FileInfo(folderInfo, "thusssmbs.db")));
+
         blacklist.removePattern("*thumbs.db");
-      
-        assertFalse(blacklist.isIgnored(new FileInfo(folderInfo,
-            "thumbs.db")));
+
+        assertFalse(blacklist.isIgnored(new FileInfo(folderInfo, "thumbs.db")));
         assertFalse(blacklist.isIgnored(new FileInfo(folderInfo,
             "somewhere/in/a/sub/thumbs.db")));
-        assertFalse(blacklist.isIgnored(new FileInfo(folderInfo,
-            "thusssmbs.db")));
-      
+        assertFalse(blacklist
+            .isIgnored(new FileInfo(folderInfo, "thusssmbs.db")));
+
         Blacklist blacklist2 = new Blacklist();
         blacklist2.addPattern("images/*thumbs.db");
-      
-        assertFalse(blacklist2.isIgnored(new FileInfo(folderInfo,
-            "thumbs.db")));
+
+        assertFalse(blacklist2.isIgnored(new FileInfo(folderInfo, "thumbs.db")));
         assertTrue(blacklist2.isIgnored(new FileInfo(folderInfo,
             "images/thumbs.db")));
         assertTrue(blacklist2.isIgnored(new FileInfo(folderInfo,
             "images/deepinimages/thumbs.db")));
-      
+
+        // Mixed case pattern. Should match!
+        assertTrue(blacklist2.isIgnored(new FileInfo(folderInfo,
+            "images/deepinimages/THUMBS.db")));
+        assertTrue(blacklist2.isIgnored(new FileInfo(folderInfo,
+            "images/deepinimages/thambs.db")));
+
     }
 }
