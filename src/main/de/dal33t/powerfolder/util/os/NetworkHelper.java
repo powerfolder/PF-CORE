307c75da032b7d8f8bcc18cb6fa1176150524bf2
Christian Sprajc <sprajc@powerfolder.com>
2006-09-05 15:23:58 +0000

    isSupported added, formatted

--- src/main/de/dal33t/powerfolder/util/os/NetworkHelper.java
+++ src/main/de/dal33t/powerfolder/util/os/NetworkHelper.java
@@ -10,47 +10,67 @@ import de.dal33t.powerfolder.util.OSUtil;
 import de.dal33t.powerfolder.util.net.NetworkAddress;
 import de.dal33t.powerfolder.util.os.Win32.NetworkHelperImpl;
 
-public abstract class NetworkHelper {
-	private static NetworkHelper instance;
-	
-	/**
-	 * Returns an instance of a subclass of this class.
-	 * 
-	 * @return a NetworkUtil instance or null if there is none for
-	 * 		the underlying operating system
-	 */
-	public static NetworkHelper getInstance() {
-		if (instance == null) {
-			if (OSUtil.isWindowsSystem()) {
-				if (NetworkHelperImpl.loadLibrary())
-					instance = new NetworkHelperImpl(); 
-			}
-		}
-		return instance;
-	}
-	
-	/**
-	 * Returns a list of IP addresses and subnet masks.
-	 * The returned list contains String-arrays of size 2 with
-	 * index 0 containing an IP address and index 1 containing the
-	 * associated subnet mask.
-	 * @return
-	 */
-	public abstract Collection<String[]> getInterfaceAddresses();
-	
-	public Collection<NetworkAddress> getNetworkAddresses() {
-		Collection<String[]> addr = getInterfaceAddresses();
-		Collection<NetworkAddress> result = new ArrayList<NetworkAddress>(addr.size());
-		
-		for (String s[]: addr) {
-			try {
-				result.add(new NetworkAddress((Inet4Address) InetAddress.getByName(s[0]),
-						(Inet4Address) InetAddress.getByName(s[1])));
-			} catch (UnknownHostException e) {
-				// Should never happen!
-				throw new RuntimeException(e);
-			}
-		}
-		return result;
-	}
+public class NetworkHelper {
+    private static NetworkHelper instance;
+    private static boolean isWinLibLoaded = false;
+
+    static {
+        if (OSUtil.isWindowsSystem()) {
+            isWinLibLoaded = NetworkHelperImpl.loadLibrary();
+        }
+    }
+
+    /** @return true on windows and if lib loaded succesfully */
+    public static boolean isSupported() {
+        return OSUtil.isWindowsSystem() && isWinLibLoaded;
+
+    }
+
+    /**
+     * Returns an instance of a subclass of this class.
+     * 
+     * @return a NetworkUtil instance or null if there is none for the
+     *         underlying operating system
+     */
+    public static NetworkHelper getInstance() {
+        if (instance == null) {
+            if (OSUtil.isWindowsSystem()) {
+                if (NetworkHelperImpl.loadLibrary())
+                    instance = new NetworkHelperImpl();
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * Returns a list of IP addresses and subnet masks. The returned list
+     * contains String-arrays of size 2 with index 0 containing an IP address
+     * and index 1 containing the associated subnet mask.
+     * 
+     * @return
+     */
+    public Collection<String[]> getInterfaceAddresses() {
+        if (!isSupported()) {
+            throw new IllegalStateException("not supported");
+        }
+        return instance.getInterfaceAddresses();
+    }
+
+    public Collection<NetworkAddress> getNetworkAddresses() {
+        Collection<String[]> addr = getInterfaceAddresses();
+        Collection<NetworkAddress> result = new ArrayList<NetworkAddress>(addr
+            .size());
+
+        for (String s[] : addr) {
+            try {
+                result.add(new NetworkAddress((Inet4Address) InetAddress
+                    .getByName(s[0]), (Inet4Address) InetAddress
+                    .getByName(s[1])));
+            } catch (UnknownHostException e) {
+                // Should never happen!
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
 }
