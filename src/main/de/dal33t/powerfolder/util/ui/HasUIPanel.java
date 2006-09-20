4b96c4bf8bc7569b0a7861d370a476ad649e74a9
Christian Sprajc <sprajc@powerfolder.com>
2006-09-20 22:54:08 +0000

    Added lazy initalization of all panels

---
+++ src/main/de/dal33t/powerfolder/util/ui/HasUIPanel.java
@@ -0,0 +1,18 @@
+/* $Id$
+ * 
+ * Copyright (c) 2006 Riege Software. All rights reserved.
+ * Use is subject to license terms.
+ */
+package de.dal33t.powerfolder.util.ui;
+
+import java.awt.Component;
+
+/**
+ * Simple interface for items that provide a ui component.
+ * 
+ * @author <a href="mailto:sprajc@riege.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public interface HasUIPanel {
+    Component getUIComponent();
+}
