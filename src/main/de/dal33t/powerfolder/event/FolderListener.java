e6f8b01dec65c2c033923c244b77c04c919e72a9
Christian Sprajc <sprajc@powerfolder.com>
2006-09-21 11:33:20 +0000

    javadoced

--- src/main/de/dal33t/powerfolder/event/FolderListener.java
+++ src/main/de/dal33t/powerfolder/event/FolderListener.java
@@ -1,9 +1,22 @@
 package de.dal33t.powerfolder.event;
 
-
+/** Fired by Folder if the status of the Folder changes */
 public interface FolderListener extends CoreListener {
+    /**
+     * The statistics gets calculated delayed after a folder changed or the
+     * remote contents changed
+     */
     public void statisticsCalculated(FolderEvent folderEvent);
-    public void folderChanged(FolderEvent folderEvent);   
+
+    /** Contents on disk of the folder have changed */
+    public void folderChanged(FolderEvent folderEvent);
+
+    /** The synchronization profile changed */
     public void syncProfileChanged(FolderEvent folderEvent);
+
+    /**
+     * The remote contents of a Folder changed (e.g. a file was added remote or
+     * deleted)
+     */
     public void remoteContentsChanged(FolderEvent folderEvent);
 }
