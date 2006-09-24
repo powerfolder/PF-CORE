0a942bfbf64df1a07812bf1cb3854cd119991808
Christian Sprajc <sprajc@powerfolder.com>
2006-09-24 23:49:26 +0000

    -Extraced statusbar -Added warning icon/text to status bar about limited connectivity

---
+++ src/main/de/dal33t/powerfolder/ui/StatusBar.java
@@ -0,0 +1,123 @@
+package de.dal33t.powerfolder.ui;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.TimerTask;
+
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.PFUIComponent;
+import de.dal33t.powerfolder.util.Translation;
+import de.dal33t.powerfolder.util.ui.ComplexComponentFactory;
+import de.dal33t.powerfolder.util.ui.DialogFactory;
+import de.dal33t.powerfolder.util.ui.HasUIPanel;
+import de.dal33t.powerfolder.util.ui.LimitedConnectivityChecker;
+
+/**
+ * The status bar on the lower side of the main window.
+ * 
+ * @author <a href="mailto:sprajc@riege.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public class StatusBar extends PFUIComponent implements HasUIPanel {
+    private Component comp;
+
+    /** Online state info field */
+    private JLabel onlineStateInfo, limitedConnectivityLabel, upStats,
+        downStats;
+
+    protected StatusBar(Controller controller) {
+        super(controller);
+    }
+
+    public Component getUIComponent() {
+        if (comp == null) {
+            initComponents();
+
+            FormLayout layout = new FormLayout(
+                "pref, 3dlu, pref, fill:pref:grow, pref, 3dlu, pref, 3dlu, pref",
+                "pref");
+            DefaultFormBuilder b = new DefaultFormBuilder(layout);
+            b.setBorder(Borders.createEmptyBorder("0, 1dlu, 0, 2dlu"));
+
+            CellConstraints cc = new CellConstraints();
+            b.add(onlineStateInfo, cc.xy(1, 1));
+
+            b.add(limitedConnectivityLabel, cc.xy(3, 1));
+
+            JSeparator sep1 = new JSeparator(SwingConstants.VERTICAL);
+            sep1.setPreferredSize(new Dimension(2, 12));
+
+            b.add(downStats, cc.xy(5, 1));
+            b.add(sep1, cc.xy(7, 1));
+            b.add(upStats, cc.xy(9, 1));
+            return b.getPanel();
+        }
+        return comp;
+    }
+
+    private void initComponents() {
+        // Create online state info
+        onlineStateInfo = ComplexComponentFactory
+            .createOnlineStateLabel(getController());
+        // Add behavior
+        onlineStateInfo.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                // open connect dialog
+                getUIController().getConnectAction().actionPerformed(null);
+            }
+        });
+
+        upStats = ComplexComponentFactory
+            .createTransferCounterLabel(getController(), Translation
+                .getTranslation("status.upload"), getController()
+                .getTransferManager().getTotalUploadTrafficCounter());
+
+        downStats = ComplexComponentFactory.createTransferCounterLabel(
+            getController(), Translation.getTranslation("status.download"),
+            getController().getTransferManager()
+                .getTotalDownloadTrafficCounter());
+
+        limitedConnectivityLabel = new JLabel();
+        limitedConnectivityLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e)
+            {
+                if (getController().hasLimitedConnectivity()) {
+                    DialogFactory.showWarningDialog(getUIController()
+                        .getMainFrame().getUIComponent(), Translation
+                        .getTranslation("limitedconnection.title"), Translation
+                        .getTranslation("limitedconnection.text"));
+                }
+            }
+        });
+        getController().scheduleAndRepeat(new MyConnectivityChecker(),
+            LimitedConnectivityChecker.TEST_CONNECTIVITY_DELAY * 1000,
+            LimitedConnectivityChecker.TEST_CONNECTIVITY_DELAY * 1000);
+    }
+
+    private class MyConnectivityChecker extends TimerTask {
+        @Override
+        public void run()
+        {
+            if (getController().hasLimitedConnectivity()) {
+                limitedConnectivityLabel.setText(Translation
+                    .getTranslation("limitedconnection.title"));
+                limitedConnectivityLabel.setIcon(Icons.WARNING);
+            } else {
+                limitedConnectivityLabel.setText("");
+                limitedConnectivityLabel.setIcon(null);
+            }
+        }
+    }
+}
