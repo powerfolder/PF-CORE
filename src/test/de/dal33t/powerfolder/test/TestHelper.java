6ff7fc2250e204a409197778fa3a526a2766a513
Christian Sprajc <sprajc@powerfolder.com>
2006-10-07 18:25:33 +0000

    orger imports

--- src/test/de/dal33t/powerfolder/test/TestHelper.java
+++ src/test/de/dal33t/powerfolder/test/TestHelper.java
@@ -9,7 +9,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
