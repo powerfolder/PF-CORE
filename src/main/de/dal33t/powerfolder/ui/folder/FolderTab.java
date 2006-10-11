f91a9784e7cc3ed53a8e5534109a2024da805d62
Christian Sprajc <sprajc@powerfolder.com>
2006-10-11 15:35:54 +0000

    FolderTab isa HasUIPanel

--- src/main/de/dal33t/powerfolder/ui/folder/FolderTab.java
+++ src/main/de/dal33t/powerfolder/ui/folder/FolderTab.java
@@ -1,11 +1,9 @@
 package de.dal33t.powerfolder.ui.folder;
 
-import javax.swing.JComponent;
-
 import de.dal33t.powerfolder.disk.Folder;
+import de.dal33t.powerfolder.util.ui.HasUIPanel;
 
-public interface FolderTab {
+public interface FolderTab extends HasUIPanel {
     public String getTitle();
-    public void setFolder(Folder folder);
-    public JComponent getUIComponent(); 
+    public void setFolder(Folder folder);     
 }
