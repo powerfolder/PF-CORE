70a08b71e599a1d50b8e50ccc62469234d12c630
Christian Sprajc <sprajc@powerfolder.com>
2006-10-24 09:45:06 +0000

    confirm renamings with #17 on linux

--- src/test/de/dal33t/powerfolder/test/folder/FileNameProblemLinuxTest.java
+++ src/test/de/dal33t/powerfolder/test/folder/FileNameProblemLinuxTest.java
@@ -1,5 +1,6 @@
 package de.dal33t.powerfolder.test.folder;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -19,32 +20,42 @@ import de.dal33t.powerfolder.util.OSUtil;
  * on windows.
  */
 public class FileNameProblemLinuxTest extends ControllerTestCase {
-    
+
     FolderScanner folderScanner;
-   
+
     private int handlerCalledCount = 0;
 
     protected void setUp() throws Exception {
         if (OSUtil.isLinux()) {
             System.out.println("running linux specific Filename problem test");
             super.setUp();
-    
+
             setupTestFolder(SyncProfile.MANUAL_DOWNLOAD);
             folderScanner = new FolderScanner(getController());
             folderScanner.start();
 
             getController().getFolderRepository().setFileNameProblemHandler(
                 new FileNameProblemHandler() {
-
                     public void fileNameProblemsDetected(
                         FileNameProblemEvent fileNameProblemEvent)
                     {
                         handlerCalledCount++;
                         Map<FileInfo, List<FilenameProblem>> problems = fileNameProblemEvent
-                            .getScanResult().getProblemFiles();                        
+                            .getScanResult().getProblemFiles();
                         assertEquals(10, problems.size());
+                        for (FileInfo problemFileInfo : problems.keySet()) {
+                            List<FilenameProblem> problemList = problems.get(problemFileInfo);
+                            
+                            for (FilenameProblem problem : problemList) {
+                                //solve it
+                                FileInfo solved = problem.solve(getController());
+                                if (!FilenameProblem.hasProblems(solved.getFilenameOnly())) {
+                                    break;
+                                }
+                            }
+                            
+                        }
                     }
-
                 });
         }
     }
@@ -66,13 +77,16 @@ public class FileNameProblemLinuxTest extends ControllerTestCase {
             TestHelper.createRandomFile(getFolder().getLocalBase(), "xLPT1");
             // valid on windows
             TestHelper.createRandomFile(getFolder().getLocalBase(), "xAUX.txt");
-            // not valid on windows, but this results in a directory 'test' with file 'test' in it
-            
-            //TestHelper.createRandomFile(folder.getLocalBase(), "test/test");
+            // not valid on windows, but this results in a directory 'test' with
+            // file 'test' in it
+
+            // TestHelper.createRandomFile(folder.getLocalBase(), "test/test");
             // not valid on windows (4)
-            
-            //our test fails on this file, because we regard a \ a directory symbol
-            //TestHelper.createRandomFile(folder.getLocalBase(), "part1\\part2");
+
+            // our test fails on this file, because we regard a \ a directory
+            // symbol
+            // TestHelper.createRandomFile(folder.getLocalBase(),
+            // "part1\\part2");
             // not valid on windows (4)
             TestHelper.createRandomFile(getFolder().getLocalBase(), "?hhh");
             // not valid on windows (5)
@@ -80,17 +94,34 @@ public class FileNameProblemLinuxTest extends ControllerTestCase {
             // not valid on windows (6)
             TestHelper.createRandomFile(getFolder().getLocalBase(), "<hhf");
             // not valid on windows (7)
-            TestHelper.createRandomFile(getFolder().getLocalBase(), "hj\"gfgfg");
+            TestHelper
+                .createRandomFile(getFolder().getLocalBase(), "hj\"gfgfg");
             // not valid on windows (8)
             TestHelper.createRandomFile(getFolder().getLocalBase(), ":sds");
             // not valid on windows (9)
             TestHelper.createRandomFile(getFolder().getLocalBase(), "gfgf>");
             // not valid on windows (10)
-            TestHelper.createRandomFile(getFolder().getLocalBase(), "ssdffd<");
+            TestHelper.createRandomFile(getFolder().getLocalBase(), "gfgf<");
 
             ScanResult result = folderScanner.scanFolder(getFolder());
             assertEquals(12, result.getNewFiles().size());
             assertEquals(1, handlerCalledCount);
+            
+            File folderBaseDir = getFolder().getLocalBase();
+            
+            assertTrue(new File(folderBaseDir, "AUX-1").exists());
+            assertTrue(new File(folderBaseDir, "AUX-1.txt").exists());
+            assertTrue(new File(folderBaseDir, "LPT1-1").exists());
+            assertTrue(new File(folderBaseDir, "xLPT1").exists());
+            assertTrue(new File(folderBaseDir, "xAUX.txt").exists());
+            assertTrue(new File(folderBaseDir, "hhh").exists());
+            assertTrue(new File(folderBaseDir, "ddfgd").exists());
+            assertTrue(new File(folderBaseDir, "hhf").exists());
+            assertTrue(new File(folderBaseDir, "hjgfgfg").exists());
+            assertTrue(new File(folderBaseDir, "sds").exists());
+            assertTrue(new File(folderBaseDir, "gfgf").exists());
+            assertTrue(new File(folderBaseDir, "gfgf-1").exists());            
+           
         }
     }
 
