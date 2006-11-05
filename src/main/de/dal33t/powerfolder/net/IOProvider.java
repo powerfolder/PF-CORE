88d5e51cfb89d8a40467ff6a76a3e3ff7b4ca6d5
Christian Sprajc <sprajc@powerfolder.com>
2006-11-05 12:45:27 +0000

    #295

--- src/main/de/dal33t/powerfolder/net/IOProvider.java
+++ src/main/de/dal33t/powerfolder/net/IOProvider.java
@@ -33,6 +33,8 @@ public class IOProvider extends PFComponent {
 
     public IOProvider(Controller controller) {
         super(controller);
+        // Create default connection factory. not set this in 
+        conHanFactory = new ConnectionHandlerFactory();
     }
 
     public void start() {
