13e21c03d8f3ce32fa74f2f57d117670cbff2a3a
Christian Sprajc <sprajc@powerfolder.com>
2006-11-19 21:18:17 +0000

    #364

--- src/main/de/dal33t/powerfolder/light/FolderInfo.java
+++ src/main/de/dal33t/powerfolder/light/FolderInfo.java
@@ -4,8 +4,8 @@ package de.dal33t.powerfolder.light;
 
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
-
-import org.apache.commons.codec.digest.DigestUtils;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.disk.Folder;
@@ -53,7 +53,7 @@ public class FolderInfo implements Serializable, Cloneable, Comparable {
      * Returns the folder details for this folder
      * 
      * @param controller
-     * @return
+     * @return the folder details for this folder
      */
     public FolderDetails getFolderDetails(Controller controller) {
         return controller.getFolderRepository().getFolderDetails(this);
@@ -91,7 +91,7 @@ public class FolderInfo implements Serializable, Cloneable, Comparable {
      * Calculates the secure Id for this folder with magicid from remote
      * 
      * @param magicId
-     * @return
+     * @return the secure Id for this folder with magicid from remote
      */
     public String calculateSecureId(String magicId) {
         // Do the magic...
@@ -105,7 +105,7 @@ public class FolderInfo implements Serializable, Cloneable, Comparable {
             System.arraycopy(fId, 0, hexId, mId.length - 1, fId.length);
             System.arraycopy(mId, 0, hexId, mId.length + fId.length - 2,
                 mId.length);
-            return DigestUtils.md5Hex(hexId);
+            return new String(encodeHex(md5(hexId)));
         } catch (UnsupportedEncodingException e) {
             throw (IllegalStateException) new IllegalStateException(
                 "Fatal problem: UTF-8 encoding not found").initCause(e);
@@ -138,7 +138,7 @@ public class FolderInfo implements Serializable, Cloneable, Comparable {
         return false;
     }
 
-    //used for sorting ignores case
+    // used for sorting ignores case
     public int compareTo(Object other) {
         FolderInfo otherFolderInfo = (FolderInfo) other;
         return name.compareToIgnoreCase(otherFolderInfo.name);
@@ -146,5 +146,79 @@ public class FolderInfo implements Serializable, Cloneable, Comparable {
 
     public String toString() {
         return "Folder '" + name + "'";
+    }
+
+    // Encoding stuff *********************************************************
+
+    /**
+     * Used building output as Hex
+     */
+    private static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    /**
+     * Calculates the MD5 digest and returns the value as a 16 element
+     * <code>byte[]</code>.
+     * 
+     * @param data
+     *            Data to digest
+     * @return MD5 digest
+     */
+    private static byte[] md5(byte[] data) {
+        return getMd5Digest().digest(data);
+    }
+
+    /**
+     * Returns a MessageDigest for the given <code>algorithm</code>.
+     * 
+     * @param algorithm
+     *            The MessageDigest algorithm name.
+     * @return An MD5 digest instance.
+     * @throws RuntimeException
+     *             when a {@link java.security.NoSuchAlgorithmException} is
+     *             caught,
+     */
+    private static MessageDigest getDigest(String algorithm) {
+        try {
+            return MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * Returns an MD5 MessageDigest.
+     * 
+     * @return An MD5 digest instance.
+     * @throws RuntimeException
+     *             when a {@link java.security.NoSuchAlgorithmException} is
+     *             caught,
+     */
+    private static MessageDigest getMd5Digest() {
+        return getDigest("MD5");
+    }
+
+    /**
+     * Converts an array of bytes into an array of characters representing the
+     * hexidecimal values of each byte in order. The returned array will be
+     * double the length of the passed array, as it takes two characters to
+     * represent any given byte.
+     * 
+     * @param data
+     *            a byte[] to convert to Hex characters
+     * @return A char[] containing hexidecimal characters
+     */
+    private static char[] encodeHex(byte[] data) {
+        int l = data.length;
+
+        char[] out = new char[l << 1];
+
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS[0x0F & data[i]];
+        }
+
+        return out;
     }
 }
