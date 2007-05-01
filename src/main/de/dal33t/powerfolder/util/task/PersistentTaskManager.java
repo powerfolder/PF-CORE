53180a44221611fe329829329315dd5045ba6b88
Christian Sprajc <sprajc@powerfolder.com>
2007-05-01 23:25:54 +0000

    Formatted

--- src/main/de/dal33t/powerfolder/util/task/PersistentTaskManager.java
+++ src/main/de/dal33t/powerfolder/util/task/PersistentTaskManager.java
@@ -21,165 +21,178 @@ import de.dal33t.powerfolder.PFComponent;
  * @version $Revision$
  */
 public class PersistentTaskManager extends PFComponent {
-	private List<PersistentTask> tasks;
-	/**
-	 * Pending tasks that await initialization.
-	 */
-	private volatile int pendingTasks;
-	private boolean shuttingDown = false;
-	
-	public PersistentTaskManager(Controller controller) {
-		super(controller);
-	}
+    private List<PersistentTask> tasks;
+    /**
+     * Pending tasks that await initialization.
+     */
+    private volatile int pendingTasks;
+    private boolean shuttingDown = false;
 
-	/**
-	 * Returns the file which represents the persistent store of tasks.
-	 * @return the tasklist-file
-	 */
-	private File getTaskFile() {
-		String filename = getController().getConfigName() + ".tasks";
-		return new File(Controller.getMiscFilesLocation(), 
-				filename);
-		
-	}
-	
-	/**
-	 * Starts this manager.
-	 */
-	@SuppressWarnings("unchecked")
-	public synchronized void start() {
-		shuttingDown = false;
-		File taskfile = getTaskFile();
-		if (taskfile.exists()) {
-			log().info("Loading taskfile");
-			try {
-				ObjectInputStream oin = new ObjectInputStream(
-						new FileInputStream(taskfile));
-				tasks = (List<PersistentTask>) oin.readObject();
-				oin.close();
-				log().info("Loaded " + tasks.size() + " tasks.");
-			} catch (FileNotFoundException e) {
-				log().error(e);
-			} catch (IOException e) {
-				log().error(e);
-			} catch (ClassNotFoundException e) {
-				log().error(e); 
-			} catch (ClassCastException e) {
-				log().error(e);
-			}
-		} else {
-			log().info("No taskfile found - probably first start of PF.");
-		}
-		// If no taskfile was found or errors occured while loading it
-		if (tasks == null) {
-			tasks = new LinkedList<PersistentTask>();
-		}
-		for (PersistentTask t: tasks.toArray(new PersistentTask[0])) {
-			t.init(this);
-		}
-	}
-	
-	/**
-	 * Shuts down the manager.
-	 * Saves all remaining tasks - they'll continue execution once the manager has been restarted (Not 
-	 * neccesarrily in this session of PowerFolder)
-	 */
-	public synchronized void shutdown() {
-		shuttingDown = true;
-		waitForPendingTasks();
-		for (PersistentTask t: tasks) {
-			t.shutdown();
-		}
-		File taskFile = getTaskFile();
-		try {
-			log().info("There are " + tasks.size() + " tasks not completed yet.");
-			ObjectOutputStream oout = new ObjectOutputStream(
-					new FileOutputStream(taskFile));
-			oout.writeUnshared(tasks);
-			oout.close();
-			tasks = null;
-		} catch (FileNotFoundException e) {
-			log().error(e);
-		} catch (IOException e) {
-			log().error(e);
-		}
-	}
-	
-	/**
-	 * Schedules a new task.
-	 * The given task will be started as soon as possible by the shared Timer of the Controller class.
-	 * @param task the task to start
-	 */
-	public synchronized void scheduleTask(final PersistentTask task) {
-		if (!tasks.contains(task) && !shuttingDown) {
-			tasks.add(task);
-			pendingTasks++;
-			getController().schedule(
-					new TimerTask() {
-						@Override
-						public void run() {
-							task.init(PersistentTaskManager.this);
-							pendingTasks--;
-							synchronized (PersistentTaskManager.this) {
-								PersistentTaskManager.this.notify();
-							}
-						}
-					}, 0);
-		}
-	}
-	
-	/**
-	 * Shuts down and removes a given task.
-	 * This method will block until all tasks are properly initialized before removing the
-	 * given task.
-	 * @param task the task to remove
-	 */
-	public synchronized void removeTask(PersistentTask task) {
-		shuttingDown = true;
-		waitForPendingTasks();
-		task.shutdown();
-		tasks.remove(task);
-		shuttingDown = false;
-	}
-	
-	/**
-	 * Removes all pending tasks.
-	 * This is useful for tests or to clear all tasks in case some are errornous.
-	 * This method will block until all tasks are properly initialized.
-	 */
-	public synchronized void purgeAllTasks() {
-		shuttingDown = true;
-		waitForPendingTasks();
-		while (!tasks.isEmpty()) {
-			tasks.remove(0).shutdown();
-		}
-		shuttingDown = false;
-	}
-	
-	/**
-	 * Returns if there are any pending tasks.
-	 * @return true if there are 1 or more active tasks
-	 */
-	public synchronized boolean hasTasks() {
-		return !tasks.isEmpty();
-	}
-	
-	/**
-	 * Returns the number of active tasks
-	 * @return the active task count
-	 */
-	public synchronized int activeTaskCount() {
-		return tasks.size();
-	}
+    public PersistentTaskManager(Controller controller) {
+        super(controller);
+    }
 
-	/** Assumes the caller to have locked the manager */
-	private void waitForPendingTasks() {
-		while (pendingTasks > 0) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				log().error(e);
-			}
-		}
-	}
+    /**
+     * Returns the file which represents the persistent store of tasks.
+     * 
+     * @return the tasklist-file
+     */
+    private File getTaskFile() {
+        String filename = getController().getConfigName() + ".tasks";
+        return new File(Controller.getMiscFilesLocation(), filename);
+
+    }
+
+    /**
+     * Starts this manager.
+     */
+    @SuppressWarnings("unchecked")
+    public synchronized void start()
+    {
+        shuttingDown = false;
+        File taskfile = getTaskFile();
+        if (taskfile.exists()) {
+            log().info("Loading taskfile");
+            try {
+                ObjectInputStream oin = new ObjectInputStream(
+                    new FileInputStream(taskfile));
+                tasks = (List<PersistentTask>) oin.readObject();
+                oin.close();
+                log().info("Loaded " + tasks.size() + " tasks.");
+            } catch (FileNotFoundException e) {
+                log().error(e);
+            } catch (IOException e) {
+                log().error(e);
+            } catch (ClassNotFoundException e) {
+                log().error(e);
+            } catch (ClassCastException e) {
+                log().error(e);
+            }
+        } else {
+            log().info("No taskfile found - probably first start of PF.");
+        }
+        // If no taskfile was found or errors occured while loading it
+        if (tasks == null) {
+            tasks = new LinkedList<PersistentTask>();
+        }
+        for (PersistentTask t : tasks.toArray(new PersistentTask[0])) {
+            t.init(this);
+        }
+    }
+
+    /**
+     * Shuts down the manager. Saves all remaining tasks - they'll continue
+     * execution once the manager has been restarted (Not neccesarrily in this
+     * session of PowerFolder)
+     */
+    public synchronized void shutdown() {
+        shuttingDown = true;
+        waitForPendingTasks();
+        for (PersistentTask t : tasks) {
+            t.shutdown();
+        }
+        File taskFile = getTaskFile();
+        try {
+            log().info(
+                "There are " + tasks.size() + " tasks not completed yet.");
+            ObjectOutputStream oout = new ObjectOutputStream(
+                new FileOutputStream(taskFile));
+            oout.writeUnshared(tasks);
+            oout.close();
+            tasks = null;
+        } catch (FileNotFoundException e) {
+            log().error(e);
+        } catch (IOException e) {
+            log().error(e);
+        }
+    }
+
+    /**
+     * Schedules a new task. The given task will be started as soon as possible
+     * by the shared Timer of the Controller class.
+     * 
+     * @param task
+     *            the task to start
+     */
+    public synchronized void scheduleTask(final PersistentTask task) {
+        if (tasks == null) {
+            log().error(
+                "Unable to shedule task, taskmanager not initialized! Task: "
+                    + task, new RuntimeException("here"));
+            return;
+        }
+        if (!tasks.contains(task) && !shuttingDown) {
+            tasks.add(task);
+            pendingTasks++;
+            getController().schedule(new TimerTask() {
+                @Override
+                public void run()
+                {
+                    task.init(PersistentTaskManager.this);
+                    pendingTasks--;
+                    synchronized (PersistentTaskManager.this) {
+                        PersistentTaskManager.this.notify();
+                    }
+                }
+            }, 0);
+        }
+    }
+
+    /**
+     * Shuts down and removes a given task. This method will block until all
+     * tasks are properly initialized before removing the given task.
+     * 
+     * @param task
+     *            the task to remove
+     */
+    public synchronized void removeTask(PersistentTask task) {
+        shuttingDown = true;
+        waitForPendingTasks();
+        task.shutdown();
+        tasks.remove(task);
+        shuttingDown = false;
+    }
+
+    /**
+     * Removes all pending tasks. This is useful for tests or to clear all tasks
+     * in case some are errornous. This method will block until all tasks are
+     * properly initialized.
+     */
+    public synchronized void purgeAllTasks() {
+        shuttingDown = true;
+        waitForPendingTasks();
+        while (!tasks.isEmpty()) {
+            tasks.remove(0).shutdown();
+        }
+        shuttingDown = false;
+    }
+
+    /**
+     * Returns if there are any pending tasks.
+     * 
+     * @return true if there are 1 or more active tasks
+     */
+    public synchronized boolean hasTasks() {
+        return !tasks.isEmpty();
+    }
+
+    /**
+     * Returns the number of active tasks
+     * 
+     * @return the active task count
+     */
+    public synchronized int activeTaskCount() {
+        return tasks.size();
+    }
+
+    /** Assumes the caller to have locked the manager */
+    private void waitForPendingTasks() {
+        while (pendingTasks > 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                log().error(e);
+            }
+        }
+    }
 }
