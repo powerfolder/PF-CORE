3fbcbc6447b435b561c18c00e22fc97693e55c86
Christian Sprajc <sprajc@powerfolder.com>
2007-05-06 00:50:59 +0000

    Abstraction

---
+++ src/main/de/dal33t/powerfolder/util/PFUIPanel.java
@@ -0,0 +1,19 @@
+package de.dal33t.powerfolder.util;
+
+import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.PFUIComponent;
+import de.dal33t.powerfolder.util.ui.HasUIPanel;
+
+/**
+ * A panel of powerfolder. basically provides access methods to
+ * <code>Controller</code> and <code>UIController</code>.
+ * 
+ * @author <a href="mailto:sprajc@riege.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public abstract class PFUIPanel extends PFUIComponent implements HasUIPanel {
+
+    protected PFUIPanel(Controller controller) {
+        super(controller);
+    }
+}
