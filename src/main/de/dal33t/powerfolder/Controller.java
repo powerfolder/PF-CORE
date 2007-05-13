23dd0d0fe2d8815ecc2fd7cc0c9623f41933a4a2
Christian Sprajc <sprajc@powerfolder.com>
2007-05-13 22:49:42 +0000

    Ready for liftoff

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -79,7 +79,7 @@ public class Controller extends PFComponent {
     /**
      * program version. include "devel" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "1.3 dev";
+    public static final String PROGRAM_VERSION = "1.3.0";
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
