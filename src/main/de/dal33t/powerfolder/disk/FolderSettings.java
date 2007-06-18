a09f021712b91559d3092747b4f4f4479ee1db41
Harry Glasgow <glasgow@powerfolder.com>
2007-06-18 23:38:05 +0000

    - add api doco to FolderSettings

--- src/main/de/dal33t/powerfolder/disk/FolderSettings.java
+++ src/main/de/dal33t/powerfolder/disk/FolderSettings.java
@@ -2,19 +2,53 @@ package de.dal33t.powerfolder.disk;
 
 import java.io.File;
 
+/**
+ * Class to consolidate the settings for creating a folder.
+ * Used as constructor arg for the Folder class.
+ *
+ */
 public class FolderSettings {
 
+    /**
+     * Base location of files in the folder.
+     */
     private File localBaseDir;
+
+    /**
+     * The synchronization profile for the folder (manual, backup, etc)
+     */
     private SyncProfile syncProfile;
+
+    /**
+     * Whether an invitation file should be created at the time the folder is constructed.
+     */
     private boolean createInvitationFile;
+
+    /**
+     * Whether the folder move deleted items to the recycle bin.
+     */
     private boolean useRecycleBin;
 
-    public FolderSettings(File localBaseDir, SyncProfile syncProfile, boolean createInvitationFile, boolean useRecycleBin) {
+    /**
+     * Constructor. Creates a new FolderSettings object.
+     * @param localBaseDir
+     * @param syncProfile
+     * @param createInvitationFile
+     * @param useRecycleBin
+     */
+    public FolderSettings(File localBaseDir,
+                          SyncProfile syncProfile,
+                          boolean createInvitationFile,
+                          boolean useRecycleBin) {
         this.localBaseDir = localBaseDir;
         this.syncProfile = syncProfile;
         this.createInvitationFile = createInvitationFile;
         this.useRecycleBin = useRecycleBin;
     }
+
+    ///////////////
+    // Accessors //
+    ///////////////
 
     public File getLocalBaseDir() {
         return localBaseDir;
