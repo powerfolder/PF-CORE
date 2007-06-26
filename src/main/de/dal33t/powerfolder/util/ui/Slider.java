7bf5fecf14a43cde39af42e260b0bdf37f1dda1e
Christian Sprajc <sprajc@powerfolder.com>
2007-06-26 22:19:16 +0000

    fixed by dimitry

--- src/main/de/dal33t/powerfolder/util/ui/Slider.java
+++ src/main/de/dal33t/powerfolder/util/ui/Slider.java
@@ -13,25 +13,25 @@ import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JWindow;
-import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 /**
- * Creates an animated view that slides out of the bottom-right corner of the screen.
- * The user of this class supplies the contents for sliding. Also, the view automatically closes
- * itself after predetermined amount of time, which is currently set to 15 seconds.<br>
- * This class is based on code and ideas from <i>Swing Hacks</i> book by Joshua Marinacci and Chris Adamson.<br>
- *  
+ * Creates an animated view that slides out of the bottom-right corner of the
+ * screen. The user of this class supplies the contents for sliding. Also, the
+ * view automatically closes itself after predetermined amount of time, which is
+ * currently set to 15 seconds.<br>
+ * This class is based on code and ideas from <i>Swing Hacks</i> book by Joshua
+ * Marinacci and Chris Adamson.<br>
+ * 
  * @author <a href="mailto:magapov@gmail.com">Maxim Agapov</a>
- * @version $Revision: 1.1 $
- *
+ * @version $Revision: 1.2 $
  */
 public class Slider implements ActionListener {
 
     /**
      * Default animation time
      */
-    public static final int ANIMATION_TIME = 500;
+    public static final int ANIMATION_TIME = 700;
 
     /**
      * Default delay between animation frames
@@ -46,7 +46,7 @@ public class Slider implements ActionListener {
     private JWindow window;
 
     private JComponent contents;
-    
+
     private Timer dismissTimer;
 
     /**
@@ -57,22 +57,6 @@ public class Slider implements ActionListener {
     public Slider(JComponent contents) {
         super();
         this.contents = contents;
-    }
-
-    /**
-     * Close and dispose the animated window.
-     */
-    public void close() {
-        //stop the dismissal timer
-        if (dismissTimer != null) {
-            dismissTimer.stop();
-            dismissTimer =null;
-        }
-        //close the window
-        if (window != null) {
-            window.dispose();
-            window = null;
-        }
     }
 
     public JComponent getContents() {
@@ -88,79 +72,83 @@ public class Slider implements ActionListener {
         // System.out.println("max window bounds = " + desktopBounds);
     }
 
+    /**
+     * Create a window with an animating sheet copy over its contents from the
+     * temp window animate it when done, remove animating sheet and add real
+     * contents.
+     */
     public void show() {
         if (window != null) {
             return;
         }
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                showInUI();
-            }
-        });
-    }
-
-    /*
-     * create a window with an animating sheet copy over its contents from the
-     * temp window animate it when done, remove animating sheet and add real
-     * contents.
-     */
-    private void showInUI() {
         window = new JWindow();
-        AnimatingSheet animatingSheet = new AnimatingSheet();
+        final AnimatingSheet animatingSheet = new AnimatingSheet();
         animatingSheet.setSource(contents);
-        window.add(animatingSheet);
+        window.getContentPane().add(animatingSheet);
 
-        Dimension contentsSize = contents.getSize();
+        final Dimension contentsSize = contents.getSize();
         Rectangle desktopBounds = initDesktopBounds();
-        int showX = desktopBounds.width - contentsSize.width;
-        int startY = desktopBounds.y + desktopBounds.height;
+        final int showX = desktopBounds.width - contentsSize.width;
+        final int startY = desktopBounds.y + desktopBounds.height;
 
-        long animationStart = System.currentTimeMillis();
-        long elapsed = 0;
+        final long animationStart = System.currentTimeMillis();
 
-        while (elapsed < ANIMATION_TIME) {
-            log("elapsed=" + elapsed);
-            float progress = (float) elapsed / ANIMATION_TIME;
-            log(", progress=" + progress);
-
-            // get height to show
-            int animatingHeight = (int) (progress * contentsSize.getHeight());
-
-            animatingHeight = Math.max(animatingHeight, 1);
-            animatingSheet.setAnimatingHeight(animatingHeight);
-            window.pack();
-            window.setLocation(showX, startY - window.getHeight());
-            log(", X=" + showX + ", Y=" + (startY - window.getHeight()));
-            window.setVisible(true);
-            window.repaint();
-            try {
-                //this is executed in the Swing's event dispatcher thread
-                Thread.sleep(ANIMATION_DELAY);
-            } catch (InterruptedException e1) {
-            }
-            elapsed = System.currentTimeMillis() - animationStart;
-        }
-
-        // put real contents in window and show
-        window.getContentPane().removeAll();
-        window.getContentPane().add(contents);
-        window.pack();
-        window.setLocation(showX, startY - window.getHeight());
-        window.setVisible(true);
-        // window.repaint();
-        
-        //start the timer to autodismiss the view
-        dismissTimer = new Timer(DISMISS_DELAY, this);
-        dismissTimer.setRepeats(false);
-        dismissTimer.start();
+        // this is the main animation timer
+        final Timer animationTimer = new Timer(ANIMATION_DELAY,
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    long elapsed = System.currentTimeMillis() - animationStart;
+                    if (elapsed < ANIMATION_TIME) {
+                        log("elapsed=" + elapsed);
+                        float progress = (float) elapsed / ANIMATION_TIME;
+                        log(", progress=" + progress);
+                        // get height to show
+                        int animatingHeight = (int) (progress * contentsSize.height);
+                        animatingHeight = Math.max(animatingHeight, 1);
+                        animatingSheet.setAnimatingHeight(animatingHeight);
+                        window.pack();
+                        window.setLocation(showX, startY - window.getHeight());
+                        log(", X=" + showX + ", Y="
+                            + (startY - window.getHeight()));
+                        window.setVisible(true);
+                        // window.repaint();
+                    } else {
+                        // first thing to do is to stop the animation timer
+                        Timer myTimer = (Timer) e.getSource();
+                        myTimer.stop();
+                        // put real contents in window and show
+                        window.getContentPane().removeAll();
+                        window.getContentPane().add(contents);
+                        window.pack();
+                        window.setLocation(showX, startY - window.getHeight());
+                        window.setVisible(true);
+                        // window.repaint();
+                        // start the timer to autodismiss the view
+                        initDismissTimer();
+                    }
+                }
+            });
+        animationTimer.setInitialDelay(0);
+        animationTimer.setCoalesce(true);
+        animationTimer.start();
     }
 
     private void log(String msg) {
         // System.out.print(msg);
     }
 
+    /*
+     * initializes the dismiss timer 
+     */
+    private void initDismissTimer() {
+        dismissTimer = new Timer(DISMISS_DELAY, this);
+        dismissTimer.setRepeats(false);
+        dismissTimer.start();
+    }
+
     /**
      * Event listener method - handles dismiss timer event
+     * 
      * @param e
      */
     public void actionPerformed(ActionEvent e) {
@@ -168,7 +156,23 @@ public class Slider implements ActionListener {
             close();
         }
     }
-    
+
+    /**
+     * Close and dispose the animated window.
+     */
+    public void close() {
+        // stop the dismissal timer
+        if (dismissTimer != null) {
+            dismissTimer.stop();
+            dismissTimer = null;
+        }
+        // close the window
+        if (window != null) {
+            window.dispose();
+            window = null;
+        }
+    }
+
     // AnimatingSheet inner class
     private class AnimatingSheet extends JPanel {
 
@@ -185,14 +189,23 @@ public class Slider implements ActionListener {
             setOpaque(true);
         }
 
+        /* (non-Javadoc)
+         * @see javax.swing.JComponent#getMaximumSize()
+         */
         public Dimension getMaximumSize() {
             return animatingSize;
         }
 
+        /* (non-Javadoc)
+         * @see javax.swing.JComponent#getMinimumSize()
+         */
         public Dimension getMinimumSize() {
             return animatingSize;
         }
 
+        /* (non-Javadoc)
+         * @see javax.swing.JComponent#getPreferredSize()
+         */
         public Dimension getPreferredSize() {
             return animatingSize;
         }
@@ -213,18 +226,19 @@ public class Slider implements ActionListener {
             source.paint(offscreenGraphics);
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see javax.swing.JComponent#paint(java.awt.Graphics)
+        /**
+         * Paints the component by drawing image on top of it
+         * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
          */
-        public void paint(Graphics g) {
-            // get the top-most n pixels of source and
-            // paint them into g, where n is height
-            // (different from sheet example, which used bottom-most)
-            BufferedImage fragment = offscreenImage.getSubimage(0, 0, source
-                .getWidth(), animatingSize.height);
-            g.drawImage(fragment, 0, 0, this);
+        protected void paintComponent(Graphics g) {
+            // We'll do our own painting, so leave out
+            // a call to the superclass behavior
+            // We're telling Swing that we're opaque, and
+            // we'll honor this by always filling our
+            // our entire bounds with image.
+            g.drawImage(offscreenImage, 0, 0, source.getWidth(),
+                animatingSize.height, 0, 0, source.getWidth(),
+                animatingSize.height, this);
         }
 
         public void setAnimatingHeight(int height) {
@@ -236,17 +250,6 @@ public class Slider implements ActionListener {
             this.source = source;
             animatingSize.width = source.getWidth();
             makeOffscreenImage(source);
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see javax.swing.JComponent#update(java.awt.Graphics)
-         */
-        public void update(Graphics g) {
-            // override to eliminate flicker from
-            // unnecessary clear
-            paint(g);
         }
     }
 }
