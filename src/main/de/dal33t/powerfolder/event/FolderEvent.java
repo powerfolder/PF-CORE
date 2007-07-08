2b65bb3a4727ab9f24f9c310b30e449d553bbfef
Christian Sprajc <sprajc@powerfolder.com>
2007-07-08 16:12:03 +0000

    - added event "commitedScanResult"

--- src/main/de/dal33t/powerfolder/event/FolderEvent.java
+++ src/main/de/dal33t/powerfolder/event/FolderEvent.java
@@ -3,14 +3,25 @@ package de.dal33t.powerfolder.event;
 import java.util.EventObject;
 
 import de.dal33t.powerfolder.disk.Folder;
+import de.dal33t.powerfolder.disk.ScanResult;
 
 public class FolderEvent extends EventObject {
+    private ScanResult scanResult;
 
     public FolderEvent(Folder source) {
         super(source);
     }
+    
+    public FolderEvent(Folder source, ScanResult sr) {
+        super(source);
+        this.scanResult = sr;
+    }
 
     public Folder getFolder() {
         return (Folder) getSource();
+    }
+    
+    public ScanResult getScanResult() {
+        return scanResult;
     }
 }
--- src/main/de/dal33t/powerfolder/event/FolderListener.java
+++ src/main/de/dal33t/powerfolder/event/FolderListener.java
@@ -2,21 +2,41 @@ package de.dal33t.powerfolder.event;
 
 /** Fired by Folder if the status of the Folder changes */
 public interface FolderListener extends CoreListener {
+
     /**
      * The statistics gets calculated delayed after a folder changed or the
      * remote contents changed
+     * 
+     * @param folderEvent
      */
-    public void statisticsCalculated(FolderEvent folderEvent);
+    void statisticsCalculated(FolderEvent folderEvent);
 
-    /** Contents on disk of the folder have changed */
-    public void folderChanged(FolderEvent folderEvent);
+    /**
+     * Contents on disk of the folder have changed
+     * 
+     * @param folderEvent
+     */
+    void folderChanged(FolderEvent folderEvent);
 
-    /** The synchronization profile changed */
-    public void syncProfileChanged(FolderEvent folderEvent);
+    /**
+     * The synchronization profile changed
+     * 
+     * @param folderEvent
+     */
+    void syncProfileChanged(FolderEvent folderEvent);
 
     /**
      * The remote contents of a Folder changed (e.g. a file was added remote or
      * deleted)
+     * 
+     * @param folderEvent
      */
-    public void remoteContentsChanged(FolderEvent folderEvent);
+    void remoteContentsChanged(FolderEvent folderEvent);
+
+    /**
+     * Fired when a scanresult was commited (=scan processed finished)
+     * 
+     * @param folderEvent
+     */
+    void scanResultCommited(FolderEvent folderEvent);
 }
