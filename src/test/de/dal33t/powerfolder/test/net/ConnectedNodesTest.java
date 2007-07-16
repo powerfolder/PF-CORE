2f92f9eeac4c5733a2d3ac925d7353b89ebab720
Christian Sprajc <sprajc@powerfolder.com>
2007-07-16 20:43:12 +0000

    New test added

---
+++ src/test/de/dal33t/powerfolder/test/net/ConnectedNodesTest.java
@@ -0,0 +1,18 @@
+
+package de.dal33t.powerfolder.test.net;
+
+import de.dal33t.powerfolder.test.TwoControllerTestCase;
+
+public class ConnectedNodesTest extends TwoControllerTestCase {
+
+    public void testConnectedNodes() {
+        for (int i = 0; i < 20; i++) {
+            assertTrue("Connected nodes (@Bart): " + getContollerBart().getNodeManager().getConnectedNodes(), getContollerBart().getNodeManager().getConnectedNodes().isEmpty());
+            assertTrue("Connected nodes (@Lisa): " + getContollerLisa().getNodeManager().getConnectedNodes(), getContollerLisa().getNodeManager().getConnectedNodes().isEmpty());
+            connectBartAndLisa();
+            assertEquals(1, getContollerBart().getNodeManager().getConnectedNodes().size());
+            assertEquals(1, getContollerLisa().getNodeManager().getConnectedNodes().size());
+            disconnectBartAndLisa();
+        }
+    }
+}
