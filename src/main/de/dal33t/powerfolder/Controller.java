4c9fc21313833cf3216910c1d7d9e58d393ec334
Christian Sprajc <sprajc@powerfolder.com>
2007-08-05 17:58:40 +0000

    Increases program version

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -79,7 +79,7 @@ public class Controller extends PFComponent {
     /**
      * program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "2.0.0";
+    public static final String PROGRAM_VERSION = "2.0.1 dev";
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
