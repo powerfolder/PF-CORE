bc1951603567b45aec624115eafbab4db8c01587
Christian Sprajc <sprajc@powerfolder.com>
2007-08-26 22:35:37 +0000

    inc

--- src/test/de/dal33t/powerfolder/test/folder/ScanFolderTest.java
+++ src/test/de/dal33t/powerfolder/test/folder/ScanFolderTest.java
@@ -32,7 +32,7 @@ public class ScanFolderTest extends ControllerTestCase {
     }
     
     public void testScanSingleFileMulti() throws Exception {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 40; i++) {
             testScanSingleFile();
             tearDown();
             setUp();
