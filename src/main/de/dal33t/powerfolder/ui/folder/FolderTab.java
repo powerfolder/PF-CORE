5a9ef62d046a8ee3789c8416c55456c18062e9a0
Christian Sprajc <sprajc@powerfolder.com>
2007-10-28 12:58:56 +0000

    #621

--- src/main/de/dal33t/powerfolder/ui/folder/FolderTab.java
+++ src/main/de/dal33t/powerfolder/ui/folder/FolderTab.java
@@ -1,9 +1,9 @@
 package de.dal33t.powerfolder.ui.folder;
 
 import de.dal33t.powerfolder.disk.Folder;
-import de.dal33t.powerfolder.util.ui.HasUIPanel;
+import de.dal33t.powerfolder.util.ui.UIPanel;
 
-public interface FolderTab extends HasUIPanel {
+public interface FolderTab extends UIPanel {
     public String getTitle();
     public void setFolder(Folder folder);     
 }
