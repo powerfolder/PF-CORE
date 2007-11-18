9b5a166415a5fae896155348f8285e7ab2e8e1c1
Christian Sprajc <sprajc@powerfolder.com>
2007-11-18 06:49:56 +0000

    Selected enabled/disabled of features for tests

---
+++ src/main/de/dal33t/powerfolder/Feature.java
@@ -0,0 +1,40 @@
+package de.dal33t.powerfolder;
+
+/**
+ * Available features to enable/disable. Primary for testing.
+ * 
+ * @author <a href="mailto:sprajc@riege.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public enum Feature {
+
+    OS_CLIENT, EXIT_ON_SHUTDOWN,
+
+    /**
+     * If disabled all peers will be detected as on lan.
+     */
+    CORRECT_LAN_DETECTION;
+
+    public void disable() {
+        System.setProperty("powerfolder.feature." + name(), "XXX");
+    }
+
+    public void enable() {
+        System.setProperty("powerfolder.feature." + name(), "OK");
+    }
+
+    public boolean isDisabled() {
+        return "XXX"
+            .equals(System.getProperty("powerfolder.feature." + name()));
+    }
+
+    public boolean isEnabled() {
+        return !isDisabled();
+    }
+
+    public static void disableAll() {
+        for (Feature feature : values()) {
+            feature.disable();
+        }
+    }
+}
