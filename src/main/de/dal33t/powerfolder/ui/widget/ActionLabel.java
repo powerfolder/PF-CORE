b847bea65cdc97f646198110d5809761763d7fab
Christian Sprajc <sprajc@powerfolder.com>
2007-11-26 23:29:19 +0000

    Action instead of actionlistener

--- src/main/de/dal33t/powerfolder/ui/widget/ActionLabel.java
+++ src/main/de/dal33t/powerfolder/ui/widget/ActionLabel.java
@@ -4,26 +4,27 @@ package de.dal33t.powerfolder.ui.widget;
 
 import java.awt.Cursor;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.Action;
 
 import de.dal33t.powerfolder.util.Reject;
 
 /**
- * A Label which executes the actions listener when clicked.
+ * A Label which executes the action when clicked.
  * 
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc </a>
  * @version $Revision: 1.4 $
  */
 public class ActionLabel extends AntialiasedLabel {
-    public ActionLabel(String aText, final ActionListener listener) {
-        super("<html><font color=\"#00000\"><a href=\"#\">" + aText
-            + "</a></font></html>");
-        Reject.ifNull(listener, "Action listener is null");
+    public ActionLabel(final Action action) {
+        super("<html><font color=\"#00000\"><a href=\"#\">"
+            + action.getValue(Action.NAME) + "</a></font></html>");
+        Reject.ifNull(action, "Action listener is null");
         addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                listener.actionPerformed(new ActionEvent(e.getSource(), 0,
+                action.actionPerformed(new ActionEvent(e.getSource(), 0,
                     "clicked"));
             }
         });
