064441bba24eee3284223609657d923f284943b7
Christian Sprajc <sprajc@powerfolder.com>
2007-12-29 20:18:12 +0000

    Incomplete - # 692: Remove potential bugs found by static code analysis (FindBugs) http://dev.powerfolder.com/projects/powerfolder/ticket/692
    The last "fix" here was ... "don't code if you're tooooo tired"

--- src/main/de/dal33t/powerfolder/util/Format.java
+++ src/main/de/dal33t/powerfolder/util/Format.java
@@ -13,198 +13,197 @@ import java.util.Formatter;
  * @version $Revision: 1.6 $
  */
 public class Format {
-    private static final DateFormat TIME_ONLY_DATE_FOMRAT = new SimpleDateFormat(
-        "[HH:mm:ss]");
-    private static final DateFormat DETAILED_TIME_FOMRAT = new SimpleDateFormat(
-        "[HH:mm:ss:SS]");
-    private static final DateFormat FULL_DATE_FOMRAT = new SimpleDateFormat(
-        "dd.MM.yyyy HH:mm:ss");
-    // format of the added by date
-    private static final DateFormat FILE_DATE_FORMAT = new SimpleDateFormat(
-        "dd.MM.yyyy HH:mm");
-    private static final DateFormat FILE_DATE_FORMAT_HOURS = new SimpleDateFormat(
-        "HH:mm");
+	private static final DateFormat TIME_ONLY_DATE_FORMAT =
+		new SynchronizedDateFormat(new SimpleDateFormat(
+				"[HH:mm:ss]"));
+	private static final DateFormat DETAILED_TIME_FORMAT = 
+		new SynchronizedDateFormat(new SimpleDateFormat(
+				"[HH:mm:ss:SS]"));
+	private static final DateFormat FULL_DATE_FORMAT = 
+		new SynchronizedDateFormat(new SimpleDateFormat(
+				"dd.MM.yyyy HH:mm:ss"));
+	// format of the added by date
+	private static final DateFormat FILE_DATE_FORMAT = 
+		new SynchronizedDateFormat(new SimpleDateFormat(
+				"dd.MM.yyyy HH:mm"));
+	private static final DateFormat FILE_DATE_FORMAT_HOURS = 
+		new SynchronizedDateFormat(new SimpleDateFormat(
+				"HH:mm"));
 
-    // default number format for all numbers
-    public static final DecimalFormat NUMBER_FORMATS = new DecimalFormat(
-        "#,###,###,###.##");
+	// default number format for all numbers
+	public static final DecimalFormat NUMBER_FORMATS = new DecimalFormat(
+	"#,###,###,###.##");
 
-    public static final DecimalFormat LONG_FORMATS = new DecimalFormat(
-        "#,###,###,###");
+	public static final DecimalFormat LONG_FORMATS = new DecimalFormat(
+	"#,###,###,###");
 
-    /**
-     * Returns a count of bytes in a string
-     * 
-     * @param bytes
-     * @return
-     */
-    public static String formatBytes(long bytes) {
-        double number = bytes;
-        String suffix = "Bytes";
+	/**
+	 * Returns a count of bytes in a string
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static String formatBytes(long bytes) {
+		double number = bytes;
+		String suffix = "Bytes";
 
-        if (number > 800) {
-            number = number / 1024;
-            suffix = "KBytes";
-        }
-        if (number > 800) {
-            number = number / 1024;
-            suffix = "MBytes";
-        }
-        if (number > 800) {
-            number = number / 1024;
-            suffix = "GBytes";
-        }
-        String str = NUMBER_FORMATS.format(number);
-        return str + " " + suffix;
-    }
+		if (number > 800) {
+			number = number / 1024;
+			suffix = "KBytes";
+		}
+		if (number > 800) {
+			number = number / 1024;
+			suffix = "MBytes";
+		}
+		if (number > 800) {
+			number = number / 1024;
+			suffix = "GBytes";
+		}
+		String str = NUMBER_FORMATS.format(number);
+		return str + " " + suffix;
+	}
 
-    /**
-     * Returns a count of bytes in a string
-     * 
-     * @param bytes
-     * @return
-     */
-    public static String formatBytesShort(long bytes) {
-        double number = bytes;
+	/**
+	 * Returns a count of bytes in a string
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static String formatBytesShort(long bytes) {
+		double number = bytes;
 
-        number = number / 1024;
-        String suffix = "KB";
-        if (number > 800) {
-            number = number / 1024;
-            suffix = "MB";
-        }
-        if (number > 800) {
-            number = number / 1024;
-            suffix = "GB";
-        }
-        String str = NUMBER_FORMATS.format(number);
-        return str + " " + suffix;
-    }
+		number = number / 1024;
+		String suffix = "KB";
+		if (number > 800) {
+			number = number / 1024;
+			suffix = "MB";
+		}
+		if (number > 800) {
+			number = number / 1024;
+			suffix = "GB";
+		}
+		String str = NUMBER_FORMATS.format(number);
+		return str + " " + suffix;
+	}
 
-    /**
-     * Formats a date
-     * 
-     * @param date
-     * @return
-     */
-    public static String formatDate(Date date) {
-        if (date == null) {
-            return null;
-        }
-        Calendar calDate = Calendar.getInstance();
-        calDate.setTime(date);
-        Calendar calNow = Calendar.getInstance();
-        if (calDate.get(Calendar.YEAR) == calNow.get(Calendar.YEAR)) {
-            int dayDiffer = calDate.get(Calendar.DAY_OF_YEAR)
-                - calNow.get(Calendar.DAY_OF_YEAR);
-            if (dayDiffer == 0) {
-                return Translation.getTranslation("general.today") + " "
-                    + getFileDateFormatHours().format(date);
-            } else if (dayDiffer == -1) {
-                return Translation.getTranslation("general.yesterday") + " "
-                    + getFileDateFormatHours().format(date);
-            }
+	/**
+	 * Formats a date
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String formatDate(Date date) {
+		if (date == null) {
+			return null;
+		}
+		Calendar calDate = Calendar.getInstance();
+		calDate.setTime(date);
+		Calendar calNow = Calendar.getInstance();
+		if (calDate.get(Calendar.YEAR) == calNow.get(Calendar.YEAR)) {
+			int dayDiffer = calDate.get(Calendar.DAY_OF_YEAR)
+			- calNow.get(Calendar.DAY_OF_YEAR);
+			if (dayDiffer == 0) {
+				return Translation.getTranslation("general.today") + " "
+				+ getFileDateFormatHours().format(date);
+			} else if (dayDiffer == -1) {
+				return Translation.getTranslation("general.yesterday") + " "
+				+ getFileDateFormatHours().format(date);
+			}
 
-        }
-        // otherwise use default format
-        return getFileDateFormat().format(date);
-    }
+		}
+		// otherwise use default format
+		return getFileDateFormat().format(date);
+	}
 
-    /**
-     * Formats numbers
-     * 
-     * @param n
-     * @return
-     */
-    public static String formatNumber(double n) {
-        synchronized (NUMBER_FORMATS) {
-            return NUMBER_FORMATS.format(n);
-        }
+	/**
+	 * Formats numbers
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static String formatNumber(double n) {
+		synchronized (NUMBER_FORMATS) {
+			return NUMBER_FORMATS.format(n);
+		}
 
-    }
+	}
 
-    public static String formatLong(long n) {
-        synchronized (LONG_FORMATS) {
-            return LONG_FORMATS.format(n);
-        }
-    }
+	public static String formatLong(long n) {
+		synchronized (LONG_FORMATS) {
+			return LONG_FORMATS.format(n);
+		}
+	}
 
-    /**
-     * Translates a "how much time remaining" value into a string.
-     * 
-     * @param dt
-     *            The time in milliseconds
-     * @return the formatted string. Examples: "102 days", "10:30:23"
-     */
-    public static String formatDeltaTime(long dt) {
-        Formatter f = new Formatter();
-        long days = dt / 1000 / 60 / 60 / 24;
-        long hours = dt / 1000 / 60 / 60;
-        if (days > 1) { // Two days or more
-            f.format(Translation.getTranslation("general.days", days) + ", ");
-            hours %= 24;
-        }
-        long minutes = (dt / 1000 / 60) % 60;
-        long seconds = (dt / 1000) % 60;
-        return f.format(Translation.getTranslation("general.time"), hours,
-            minutes, seconds).out().toString();
-    }
+	/**
+	 * Translates a "how much time remaining" value into a string.
+	 * 
+	 * @param dt
+	 *            The time in milliseconds
+	 * @return the formatted string. Examples: "102 days", "10:30:23"
+	 */
+	public static String formatDeltaTime(long dt) {
+		Formatter f = new Formatter();
+		long days = dt / 1000 / 60 / 60 / 24;
+		long hours = dt / 1000 / 60 / 60;
+		if (days > 1) { // Two days or more
+			f.format(Translation.getTranslation("general.days", days) + ", ");
+			hours %= 24;
+		}
+		long minutes = (dt / 1000 / 60) % 60;
+		long seconds = (dt / 1000) % 60;
+		return f.format(Translation.getTranslation("general.time"), hours,
+				minutes, seconds).out().toString();
+	}
 
-    /*
-     * The reason for the following methods: The javadoc of DateFormat states,
-     * that that class (and subclasses mention the same) is not thread safe.
-     * Actually they recommend to create an instance per thread. (But since this
-     * is a general purpose class, we won't do that).
-     */
+	/*
+	 * The reason for the following methods: The javadoc of DateFormat states,
+	 * that that class (and subclasses mention the same) is not thread safe.
+	 * Actually they recommend to create an instance per thread. (But since this
+	 * is a general purpose class, we won't do that).
+	 */
 
-    /**
-     * See #692
-     * 
-     * @return the tIME_ONLY_DATE_FOMRAT
-     */
-    public static DateFormat getTimeOnlyDateFormat() {
-        synchronized (TIME_ONLY_DATE_FOMRAT) {
-            return TIME_ONLY_DATE_FOMRAT;
-        }
-    }
+	/**
+	 * See #692
+	 * 
+	 * @return the tIME_ONLY_DATE_FOMRAT
+	 */
+	public static DateFormat getTimeOnlyDateFormat() {
+		return TIME_ONLY_DATE_FORMAT;
+	}
 
-    /**
-     * See #692
-     * 
-     * @return the dETAILED_TIME_FOMRAT
-     */
-    public static DateFormat getDetailedTimeFormat() {
-        synchronized (DETAILED_TIME_FOMRAT) {
-            return DETAILED_TIME_FOMRAT;
-        }
-    }
+	/**
+	 * See #692
+	 * 
+	 * @return the dETAILED_TIME_FOMRAT
+	 */
+	public static DateFormat getDetailedTimeFormat() {
+		return DETAILED_TIME_FORMAT;
+	}
 
-    /**
-     * See #692
-     * 
-     * @return the fULL_DATE_FOMRAT
-     */
-    public static DateFormat getFullDateFormat() {
-        synchronized (FULL_DATE_FOMRAT) {
-            return FULL_DATE_FOMRAT;
-        }
-    }
+	/**
+	 * See #692
+	 * 
+	 * @return the fULL_DATE_FOMRAT
+	 */
+	public static DateFormat getFullDateFormat() {
+		return FULL_DATE_FORMAT;
+	}
 
-    /**
-     * See #692
-     * 
-     * @return the fILE_DATE_FORMAT
-     */
-    private static DateFormat getFileDateFormat() {
-        return FILE_DATE_FORMAT;
-    }
+	/**
+	 * See #692
+	 * 
+	 * @return the fILE_DATE_FORMAT
+	 */
+	private static DateFormat getFileDateFormat() {
+		return FILE_DATE_FORMAT;
+	}
 
-    /**
-     * See #692
-     * 
-     * @return the fILE_DATE_FORMAT_HOURS
-     */
-    private static DateFormat getFileDateFormatHours() {
-        return FILE_DATE_FORMAT_HOURS;
-    }
+	/**
+	 * See #692
+	 * 
+	 * @return the fILE_DATE_FORMAT_HOURS
+	 */
+	private static DateFormat getFileDateFormatHours() {
+		return FILE_DATE_FORMAT_HOURS;
+	}
 }
---
+++ src/main/de/dal33t/powerfolder/util/SynchronizedDateFormat.java
@@ -0,0 +1,113 @@
+package de.dal33t.powerfolder.util;
+
+import java.text.AttributedCharacterIterator;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Helper class which wraps a DateFormat and synchronizes all its methods.
+ * 
+ * 
+ * @author Dennis "Bytekeeper" Waldherr
+ *
+ */
+public class SynchronizedDateFormat extends DateFormat {
+	private static final long serialVersionUID = -4583523279891619161L;
+	private DateFormat format;
+
+	public SynchronizedDateFormat(DateFormat wrap) {
+		format = wrap;
+	}
+	
+	@Override
+	public synchronized Object clone() {
+		return format.clone();
+	}
+
+	@Override
+	public synchronized boolean equals(Object obj) {
+		return format.equals(obj);
+	}
+
+	@Override
+	public synchronized StringBuffer format(Date date, StringBuffer toAppendTo,
+			FieldPosition fieldPosition) {
+		return format.format(date, toAppendTo, fieldPosition);
+	}
+
+	@Override
+	public synchronized Calendar getCalendar() {
+		return format.getCalendar();
+	}
+
+	@Override
+	public synchronized NumberFormat getNumberFormat() {
+		return format.getNumberFormat();
+	}
+
+	@Override
+	public synchronized TimeZone getTimeZone() {
+		return format.getTimeZone();
+	}
+
+	@Override
+	public synchronized int hashCode() {
+		return format.hashCode();
+	}
+
+	@Override
+	public synchronized boolean isLenient() {
+		return format.isLenient();
+	}
+
+	@Override
+	public synchronized Date parse(String source, ParsePosition pos) {
+		return format.parse(source, pos);
+	}
+
+	@Override
+	public synchronized Date parse(String source) throws ParseException {
+		return format.parse(source);
+	}
+
+	@Override
+	public synchronized Object parseObject(String source, ParsePosition pos) {
+		return format.parseObject(source, pos);
+	}
+
+	@Override
+	public synchronized void setCalendar(Calendar newCalendar) {
+		format.setCalendar(newCalendar);
+	}
+
+	@Override
+	public synchronized void setLenient(boolean lenient) {
+		format.setLenient(lenient);
+	}
+
+	@Override
+	public synchronized void setNumberFormat(NumberFormat newNumberFormat) {
+		format.setNumberFormat(newNumberFormat);
+	}
+
+	@Override
+	public synchronized void setTimeZone(TimeZone zone) {
+		format.setTimeZone(zone);
+	}
+
+	@Override
+	public synchronized AttributedCharacterIterator formatToCharacterIterator(Object obj) {
+		return format.formatToCharacterIterator(obj);
+	}
+
+	@Override
+	public synchronized Object parseObject(String source) throws ParseException {
+		return format.parseObject(source);
+	}
+}
