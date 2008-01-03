7612fc66087f00e63ec177c800dd0b69aa25f052
Christian Sprajc <sprajc@powerfolder.com>
2008-01-03 14:24:00 +0000

    #706

--- src/main/de/dal33t/powerfolder/util/NamedThreadFactory.java
+++ src/main/de/dal33t/powerfolder/util/NamedThreadFactory.java
@@ -11,6 +11,7 @@ public class NamedThreadFactory implements ThreadFactory {
     private static final Logger LOG = Logger
         .getLogger(NamedThreadFactory.class);
 
+    private final UncaughtExceptionHandler exceptionHandler = new ExceptionHandler();
     final AtomicInteger threadNumber = new AtomicInteger(1);
     final String namePrefix;
 
@@ -20,16 +21,18 @@ public class NamedThreadFactory implements ThreadFactory {
 
     public Thread newThread(Runnable r) {
         Thread t = new Thread(r, namePrefix + threadNumber.getAndIncrement());
-        t.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
-            public void uncaughtException(Thread t1, Throwable e) {
-                e.printStackTrace();
-                LOG.error("Exception in " + t1 + ": " + e.toString(), e);
-            }
-        });
+        t.setUncaughtExceptionHandler(exceptionHandler);
         if (t.isDaemon())
             t.setDaemon(false);
         if (t.getPriority() != Thread.NORM_PRIORITY)
             t.setPriority(Thread.NORM_PRIORITY);
         return t;
+    }
+
+    private final class ExceptionHandler implements UncaughtExceptionHandler {
+        public void uncaughtException(Thread t1, Throwable e) {
+            e.printStackTrace();
+            LOG.error("Exception in " + t1 + ": " + e.toString(), e);
+        }
     }
 }
