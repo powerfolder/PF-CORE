b08539287ff921ecdbe83c5fd8ff8c3e13fa7f4a
Christian Sprajc <sprajc@powerfolder.com>
2008-01-27 21:40:50 +0000

    Disabled public network test

--- src/test/de/dal33t/powerfolder/test/net/ConnectedNodesTest.java
+++ src/test/de/dal33t/powerfolder/test/net/ConnectedNodesTest.java
@@ -1,6 +1,8 @@
 package de.dal33t.powerfolder.test.net;
 
 import de.dal33t.powerfolder.Member;
+import de.dal33t.powerfolder.NetworkingMode;
+import de.dal33t.powerfolder.net.ConnectionException;
 import de.dal33t.powerfolder.net.InvalidIdentityException;
 import de.dal33t.powerfolder.util.test.ConditionWithMessage;
 import de.dal33t.powerfolder.util.test.FiveControllerTestCase;
@@ -142,5 +144,31 @@ public class ConnectedNodesTest extends FiveControllerTestCase {
         } catch (InvalidIdentityException e) {
             // OK!
         }
+    }
+    
+    public void xtestPublicInfrastructureConnect() {
+        getContollerBart().setNetworkingMode(NetworkingMode.PRIVATEMODE);
+        for (int i = 0; i < 500; i++) {
+            try {
+                getContollerBart().connect("server.powerfolder.com");
+                final Member battlestar = getContollerBart().getNodeManager()
+                    .getNode("INFRASTRUCTURE01");
+                TestHelper.waitForCondition(10, new ConditionWithMessage() {
+                    public String message() {
+                        return "Unable to connect to battlestar";
+                    }
+
+                    public boolean reached() {
+                        return battlestar.isCompleteyConnected();
+                    }
+                });
+                assertTrue(battlestar.isCompleteyConnected());
+                battlestar.shutdown();
+            } catch (ConnectionException e) {
+                e.printStackTrace();
+                fail(e.toString());
+            }
+        }
+
     }
 }
