554d8ba51d0be4248dfa1a09bd8ca18da2e30bd5
Christian Sprajc <sprajc@powerfolder.com>
2008-01-27 22:16:20 +0000

    Ready up

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -54,7 +54,7 @@ public class Controller extends PFComponent {
     /**
      * program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "2.2.0 RC1";
+    public static final String PROGRAM_VERSION = "2.2.0";
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
