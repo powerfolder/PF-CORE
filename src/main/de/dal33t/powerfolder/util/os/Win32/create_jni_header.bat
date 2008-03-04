c21011fefcf60ede65bda6df10622d83bee756ae
Christian Sprajc <sprajc@powerfolder.com>
2008-03-04 10:43:12 +0000

    Removed remaining native stuff

--- src/main/de/dal33t/powerfolder/util/os/Win32/create_jni_header.bat
+++
@@ -1,2 +0,0 @@
-javah -jni -classpath ..\..\..\..\..\..\..\bin\ de.dal33t.powerfolder.util.os.Win32.RecycleDeleteImpl
-pause
--- src/main/de/dal33t/powerfolder/util/os/Win32/createdll.bat
+++
@@ -1,2 +0,0 @@
-cl -IC:\JavaPrograms\jdk1.5.0_01\include -IC:\JavaPrograms\jdk1.5.0_01\include\win32 -I"C:\Program Files\Microsoft Visual Studio\VC98\INCLUDE" -I"C:\Program Files\Microsoft Visual Studio\VC98\MFC\SRC" -I"C:\Program Files\Microsoft Visual Studio\VC98\ATL\SRC" -LD deleter.cpp -Fedelete.dll /link "C:\Progra~1\Micros~3\VC98\Lib\SHELL32.LIB"
-pause
