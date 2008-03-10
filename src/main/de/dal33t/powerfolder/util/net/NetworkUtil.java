c1879c339a91796dc89aba5214bd4cd5a753100b
Christian Sprajc <sprajc@powerfolder.com>
2008-03-10 00:26:21 +0000

    #591
    InetSocketAddress with null address problem fixed.

--- src/main/de/dal33t/powerfolder/util/net/NetworkUtil.java
+++ src/main/de/dal33t/powerfolder/util/net/NetworkUtil.java
@@ -76,7 +76,8 @@ public class NetworkUtil {
     public static void setupSocket(UDTSocket socket, InetSocketAddress inetSocketAddress) throws IOException {
         Reject.ifNull(socket, "Socket is null");
         
-        boolean onLan = inetSocketAddress != null ? isOnLanOrLoopback(inetSocketAddress.getAddress()) : false;
+        boolean onLan = (inetSocketAddress != null && inetSocketAddress.getAddress() != null) 
+            ? isOnLanOrLoopback(inetSocketAddress.getAddress()) : false;
 
         socket.setSoUDPReceiverBufferSize(onLan
             ? LAN_SOCKET_BUFFER_SIZE
