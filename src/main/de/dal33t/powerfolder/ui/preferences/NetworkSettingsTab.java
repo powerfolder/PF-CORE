73861e022581b768ceb75cf5b759cb334a167e8e
Christian Sprajc <sprajc@powerfolder.com>
2008-03-15 04:58:27 +0000

    Cosmetics

--- src/main/de/dal33t/powerfolder/ui/preferences/NetworkSettingsTab.java
+++ src/main/de/dal33t/powerfolder/ui/preferences/NetworkSettingsTab.java
@@ -176,7 +176,7 @@ public class NetworkSettingsTab extends PFComponent implements PreferenceTab {
         if (panel == null) {
             FormLayout layout = new FormLayout(
                 "right:100dlu, 3dlu, 30dlu, 3dlu, 15dlu, 10dlu, 30dlu, 30dlu, pref, 0:grow",
-                "pref, 3dlu, pref, 7dlu, top:pref, 7dlu, top:pref, 7dlu, top:pref, 7dlu, top:pref, 3dlu, top:pref:grow, 3dlu");
+                "pref, 3dlu, pref, 3dlu, pref, 7dlu, top:pref, 7dlu, top:pref, 7dlu, top:pref, 7dlu, top:pref, 3dlu, top:pref:grow");
             PanelBuilder builder = new PanelBuilder(layout);
             builder.setBorder(Borders
                 .createEmptyBorder("3dlu, 0dlu, 0dlu, 0dlu"));
