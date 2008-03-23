9abbc7c6e84aaaab6fd59e7742fcc1588edbf7a8
Christian Sprajc <sprajc@powerfolder.com>
2008-03-23 01:40:09 +0000

    Refactorings

--- src/main/de/dal33t/powerfolder/util/ui/FolderCreateWorker.java
+++ src/main/de/dal33t/powerfolder/util/ui/FolderCreateWorker.java
@@ -1,14 +1,12 @@
 package de.dal33t.powerfolder.util.ui;
 
-import java.io.File;
-
-import de.dal33t.powerfolder.Controller;
+import static de.dal33t.powerfolder.disk.Folder.DESKTOP_INI_FILENAME;
+import static de.dal33t.powerfolder.disk.Folder.THUMBS_DB;
 import de.dal33t.powerfolder.ConfigurationEntry;
+import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.disk.Folder;
 import de.dal33t.powerfolder.disk.FolderException;
-import de.dal33t.powerfolder.disk.SyncProfile;
 import de.dal33t.powerfolder.disk.FolderSettings;
-import static de.dal33t.powerfolder.disk.Folder.*;
 import de.dal33t.powerfolder.light.FolderInfo;
 import de.dal33t.powerfolder.ui.widget.ActivityVisualizationWorker;
 import de.dal33t.powerfolder.util.Logger;
@@ -34,37 +32,29 @@ import de.dal33t.powerfolder.util.os.OSUtil;
  * dialog.
  */
 public abstract class FolderCreateWorker extends ActivityVisualizationWorker {
-    private static final Logger LOG = Logger.getLogger(FolderCreateWorker.class);
+    private static final Logger LOG = Logger
+        .getLogger(FolderCreateWorker.class);
 
     private Controller controller;
     private FolderInfo foInfo;
-    private File localBase;
-    private SyncProfile syncProfile;
-    private boolean storeInvitation;
+    private FolderSettings folderSettings;
     private FolderException exception;
     private Folder folder;
     private boolean createShortcut;
-    private boolean useRecycleBin;
-    private boolean previewOnly;
 
-    public FolderCreateWorker(Controller theController,
-                              FolderInfo folderInfo,
-                              FolderSettings folderSettings,
-                              boolean createShortcut)
+    public FolderCreateWorker(Controller theController, FolderInfo folderInfo,
+        FolderSettings folderSettings, boolean createShortcut)
     {
         super(theController.getUIController().getMainFrame().getUIComponent());
         Reject.ifNull(folderInfo, "FolderInfo is null");
         Reject.ifNull(folderSettings, "FolderSettings is null");
-        Reject.ifNull(folderSettings.getLocalBaseDir(), "Folder local basedir is null");
+        Reject.ifNull(folderSettings.getLocalBaseDir(),
+            "Folder local basedir is null");
         Reject.ifNull(folderSettings.getSyncProfile(), "Syncprofile is null");
-        controller = theController;
-        foInfo = folderInfo;
-        localBase = folderSettings.getLocalBaseDir();
-        syncProfile = folderSettings.getSyncProfile();
-        storeInvitation = folderSettings.isCreateInvitationFile();
+        this.controller = theController;
+        this.foInfo = folderInfo;
+        this.folderSettings = folderSettings;
         this.createShortcut = createShortcut;
-        useRecycleBin = folderSettings.isUseRecycleBin();
-        previewOnly = folderSettings.isPreviewOnly();
     }
 
     /**
@@ -84,49 +74,40 @@ public abstract class FolderCreateWorker extends ActivityVisualizationWorker {
     // Overiding stuff ********************************************************
 
     @Override
-    protected String getTitle()
-    {
+    protected String getTitle() {
         return Translation.getTranslation("folder_create.progress.text",
             foInfo.name);
     }
 
     @Override
-    protected String getWorkingText()
-    {
+    protected String getWorkingText() {
         return Translation.getTranslation("folder_create.progress.text",
             foInfo.name);
     }
 
     @Override
-    public Object construct()
-    {
+    public Object construct() {
         try {
-            FolderSettings folderSettings =
-                    new FolderSettings(localBase, syncProfile, storeInvitation,
-                            true, previewOnly);
             folder = controller.getFolderRepository().createFolder(foInfo,
-                    folderSettings);
+                folderSettings);
             if (createShortcut) {
                 folder.setDesktopShortcut(true);
             }
             if (OSUtil.isWindowsSystem()) {
                 // Add thumbs to ignore pattern on windows systems
                 // Don't duplicate thumbs (like when moving a preview folder)
-                if (!folder.getBlacklist().getPatterns().contains(THUMBS_DB))
-                {
+                if (!folder.getBlacklist().getPatterns().contains(THUMBS_DB)) {
                     folder.getBlacklist().addPattern(THUMBS_DB);
                 }
 
                 // Add desktop.ini to ignore pattern on windows systems
-                if (!OSUtil.isWindowsVistaSystem() &&
-                        ConfigurationEntry.USE_PF_ICON
+                if (!OSUtil.isWindowsVistaSystem()
+                    && ConfigurationEntry.USE_PF_ICON
                         .getValueBoolean(controller))
                 {
                     folder.getBlacklist().addPattern(DESKTOP_INI_FILENAME);
                 }
-
             }
-            folder.setUseRecycleBin(useRecycleBin);
         } catch (FolderException ex) {
             exception = ex;
             LOG.error("Unable to create new folder " + foInfo, ex);
