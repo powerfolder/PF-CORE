c7ecc48b517ccf7c7a5ecc5fe5b5c9e572dd8759
Christian Sprajc <sprajc@powerfolder.com>
2008-03-24 04:37:01 +0000

    Namings

--- src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
+++ src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
@@ -7,8 +7,8 @@ package de.dal33t.powerfolder.os;
  * @version $Revision: 1.5 $
  */
 public enum OnlineStorageSubscriptionType {
-    TRIAL("OS-T", "1 GB Trial (30 days)", 1, true),
-    TRIAL_PRO("OS-TP", "DONTUSE: 1 GB Trial (60 days)", 1, true), 
+    TRIAL("OS-T", "1 GB (30 days trial)", 1, true),
+    TRIAL_PRO("OS-TP", "DONTUSE: 1 GB (60 days trial)", 1, true), 
     STARTER("OS-S", "1 GB", 1, false),
     BASIC("OS-B", "5 GB", 5, false),
     ADVANCED("OS-A", "10 GB", 10, false),
