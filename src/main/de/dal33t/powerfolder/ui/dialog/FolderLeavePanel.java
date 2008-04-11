356e711ec6d4c5b61654328d7f2df2924167da80
Christian Sprajc <sprajc@powerfolder.com>
2008-04-11 23:05:00 +0000

    #847

--- src/main/de/dal33t/powerfolder/ui/dialog/FolderLeavePanel.java
+++ src/main/de/dal33t/powerfolder/ui/dialog/FolderLeavePanel.java
@@ -21,7 +21,7 @@ import java.awt.event.ActionListener;
 
 /**
  * Panel displayed when wanting to leave a folder
- *
+ * 
  * @author <a href="mailto:hglasgow@powerfolder.com">Harry Glasgow</a>
  * @version $Revision: 2.00 $
  */
@@ -33,17 +33,20 @@ public class FolderLeavePanel extends BaseDialog {
     private JButton leaveButton;
     private JButton cancelButton;
     private JLabel messageLabel;
-    private JCheckBox cbDeleteSystemSubFolder;
-    private JCheckBox cbConvertToPreview;
+    private JCheckBox deleteSystemSubFolderBox;
+    private JCheckBox convertToPreviewBox;
+    private JCheckBox removeFromServerBox;
 
     /**
      * Contructor when used on choosen folder
-     *
+     * 
      * @param action
      * @param controller
      * @param foInfo
      */
-    public FolderLeavePanel(FolderLeaveAction action, Controller controller, Folder folder) {
+    public FolderLeavePanel(FolderLeaveAction action, Controller controller,
+        Folder folder)
+    {
         super(controller, true);
         this.action = action;
         this.folder = folder;
@@ -61,30 +64,38 @@ public class FolderLeavePanel extends BaseDialog {
         String folerLeaveText;
         if (syncFlag) {
             folerLeaveText = Translation.getTranslation(
-                    "folder_leave.dialog.text", folder.getInfo().name)
-                    + '\n'
-                    + Translation
+                "folder_leave.dialog.text", folder.getInfo().name)
+                + '\n'
+                + Translation
                     .getTranslation("folder_leave.dialog.sync_warning");
         } else {
             folerLeaveText = Translation.getTranslation(
-                    "folder_leave.dialog.text", folder.getInfo().name);
+                "folder_leave.dialog.text", folder.getInfo().name);
         }
         messageLabel = new JLabel(folerLeaveText);
 
-        cbDeleteSystemSubFolder = SimpleComponentFactory.createCheckBox(Translation
+        deleteSystemSubFolderBox = SimpleComponentFactory
+            .createCheckBox(Translation
                 .getTranslation("folder_leave.dialog.delete"));
 
-        cbConvertToPreview = SimpleComponentFactory.createCheckBox(Translation
-                .getTranslation("folder_leave.dialog.preview"));
-        cbConvertToPreview.addActionListener(new ConvertActionListener());
+        convertToPreviewBox = SimpleComponentFactory.createCheckBox(Translation
+            .getTranslation("folder_leave.dialog.preview"));
+        convertToPreviewBox.addActionListener(new ConvertActionListener());
 
+        removeFromServerBox = SimpleComponentFactory.createCheckBox(Translation
+            .getTranslation("folder_leave.dialog.remove_from_os"));
+        removeFromServerBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getUIController().getOnlineStorageClientModel().checkAndSetupAccount();
+            }});
 
         // Buttons
         createLeaveButton(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 leaveButton.setEnabled(false);
-                action.confirmedFolderLeave(cbDeleteSystemSubFolder.isSelected(),
-                        cbConvertToPreview.isSelected());
+                action.confirmedFolderLeave(deleteSystemSubFolderBox
+                    .isSelected(), convertToPreviewBox.isSelected(),
+                    removeFromServerBox.isSelected());
                 close();
             }
         });
@@ -99,16 +110,16 @@ public class FolderLeavePanel extends BaseDialog {
     private void createLeaveButton(ActionListener listener) {
         leaveButton = new JButton(Translation
             .getTranslation("folder_leave.button.name"));
-        leaveButton.setMnemonic(Translation
-                .getTranslation("folder_leave.button.key").trim().charAt(0));       
+        leaveButton.setMnemonic(Translation.getTranslation(
+            "folder_leave.button.key").trim().charAt(0));
         leaveButton.addActionListener(listener);
     }
 
     // Methods for BaseDialog *************************************************
 
     public String getTitle() {
-        return Translation.getTranslation(
-                "folder_leave.dialog.title", folder.getInfo().name);
+        return Translation.getTranslation("folder_leave.dialog.title", folder
+            .getInfo().name);
     }
 
     protected Icon getIcon() {
@@ -118,18 +129,22 @@ public class FolderLeavePanel extends BaseDialog {
     protected Component getContent() {
         initComponents();
 
-        FormLayout layout = new FormLayout(
-            "pref:grow, 5dlu, pref:grow",
-            "pref, 5dlu, pref, 5dlu, pref");
+        FormLayout layout = new FormLayout("pref:grow, 5dlu, pref:grow",
+            "pref, 3dlu, pref, 3dlu, pref, 3dlu, pref");
         PanelBuilder builder = new PanelBuilder(layout);
 
         CellConstraints cc = new CellConstraints();
 
         builder.add(messageLabel, cc.xyw(1, 1, 3));
 
-        builder.add(cbDeleteSystemSubFolder, cc.xyw(1, 3, 3));
+        builder.add(deleteSystemSubFolderBox, cc.xyw(1, 3, 3));
 
-        builder.add(cbConvertToPreview, cc.xyw(1, 5, 3));
+        builder.add(convertToPreviewBox, cc.xyw(1, 5, 3));
+
+        boolean showRemoveFromServer = !getController().isLanOnly() && getController().getOSClient().hasJoined(folder);
+        if (showRemoveFromServer) {
+            builder.add(removeFromServerBox, cc.xyw(1, 7, 3));
+        }
 
         return builder.getPanel();
     }
@@ -140,7 +155,8 @@ public class FolderLeavePanel extends BaseDialog {
 
     private class ConvertActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            cbDeleteSystemSubFolder.setEnabled(!cbConvertToPreview.isSelected());
+            deleteSystemSubFolderBox.setEnabled(!convertToPreviewBox
+                .isSelected());
         }
     }
 }
