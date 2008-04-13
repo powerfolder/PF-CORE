22f5d3b9836d5cee60a311aaa6c3c408f9bf0cf6
Christian Sprajc <sprajc@powerfolder.com>
2008-04-13 23:44:50 +0000

    Disabled UDT tests on non-UDT systems

--- src/test/de/dal33t/powerfolder/test/net/UDTTest.java
+++ src/test/de/dal33t/powerfolder/test/net/UDTTest.java
@@ -9,16 +9,21 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 
 import junit.framework.TestCase;
+import de.dal33t.powerfolder.util.net.NetworkUtil;
 import de.dal33t.powerfolder.util.net.UDTSocket;
+import de.dal33t.powerfolder.util.os.OSUtil;
 
 public class UDTTest extends TestCase {
-	private class ThreadHelper<T> {
-		volatile T value;
-	}
-	
-	public void testUDTSO() throws IOException {
-	    UDTSocket sock = new UDTSocket();
-        
+    private class ThreadHelper<T> {
+        volatile T value;
+    }
+
+    public void testUDTSO() throws IOException {
+        if (!NetworkUtil.isUDTSupported()) {
+            return;
+        }
+        UDTSocket sock = new UDTSocket();
+
         sock.setSoSenderBufferLimit(1000 * 1024);
         assertTrue(Math.abs(1000 * 1024 - sock.getSoSenderBufferLimit()) < 1000);
         sock.setSoReceiverBufferLimit(1000 * 1024);
@@ -27,9 +32,12 @@ public class UDTTest extends TestCase {
         assertTrue(Math.abs(1000 * 1024 - sock.getSoUDPSenderBufferSize()) < 1000);
         sock.setSoUDPReceiverBufferSize(1000 * 1024);
         assertTrue(Math.abs(1000 * 1024 - sock.getSoUDPReceiverBufferSize()) < 1000);
-	}
-	
-	public void testClosing() {
+    }
+
+    public void testClosing() {
+        if (!NetworkUtil.isUDTSupported()) {
+            return;
+        }
         final UDTSocket c1 = new UDTSocket();
         final UDTSocket c2 = new UDTSocket();
         connectRendezvous(c1, new Runnable() {
@@ -45,7 +53,7 @@ public class UDTTest extends TestCase {
                     throw new Error(e);
                 }
             }
-            
+
         }, c2, new Runnable() {
 
             public void run() {
@@ -57,206 +65,225 @@ public class UDTTest extends TestCase {
                     throw new Error(e);
                 }
             }
-            
+
         });
-	}
-	
-	public void testSocket() throws IOException, InterruptedException {
-		final ThreadHelper<Boolean> tmp = new ThreadHelper<Boolean>();
-		tmp.value = false;
-		final UDTSocket serv = new UDTSocket();
-		int prt = bindSocket(serv);
-		Thread t = new Thread(new Runnable() {
-			public void run() {
-				try {
-					serv.listen(10);
-					UDTSocket cl = serv.accept();
-					PrintWriter w = new PrintWriter(cl.getOutputStream());
-					w.println("Hello World!");
-					w.close();
-					cl.close();
-					serv.close();
-					tmp.value = true;
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-		assertFalse(serv.isConnected());
-		assertFalse(serv.isClosed());
-		t.start();
-		UDTSocket other = new UDTSocket();
-		assertFalse(other.isConnected());
-		assertFalse(other.isClosed());
-		
-		other.connect(new InetSocketAddress("127.0.0.1", prt));
-		
-		assertTrue(other.isConnected());
-		assertFalse(serv.isClosed());
-		assertFalse(other.isClosed());
-		BufferedReader in = new BufferedReader(new InputStreamReader(other.getInputStream()));
-		assertEquals("Hello World!", in.readLine());
-		in.close();
-		
-		other.close();
-		assertFalse(other.isConnected());
-		assertTrue(other.isClosed());
-		
-		t.join(1000);
-		assertTrue(tmp.value);
-		assertFalse(serv.isConnected());
-		assertTrue(serv.isClosed());
-	}
-	
-	/**
-	 * Potential NAT traversal test.
-	 */
-	public void testRendezvous() throws IOException {
-		final UDTSocket c1 = new UDTSocket();
-		final UDTSocket c2 = new UDTSocket();
-		connectRendezvous(c1, new Runnable() {
+    }
 
-			public void run() {
-				try {
-					PrintWriter w = new PrintWriter(c1.getOutputStream());
-					w.println("Hello World!");
-					w.close();
-					c1.close();
-				} catch (IOException e) {
-					throw new Error(e);
-				}
-			}
-			
-		}, c2, new Runnable() {
+    public void testSocket() throws IOException, InterruptedException {
+        if (!NetworkUtil.isUDTSupported()) {
+            return;
+        }
+        final ThreadHelper<Boolean> tmp = new ThreadHelper<Boolean>();
+        tmp.value = false;
+        final UDTSocket serv = new UDTSocket();
+        int prt = bindSocket(serv);
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    serv.listen(10);
+                    UDTSocket cl = serv.accept();
+                    PrintWriter w = new PrintWriter(cl.getOutputStream());
+                    w.println("Hello World!");
+                    w.close();
+                    cl.close();
+                    serv.close();
+                    tmp.value = true;
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+        assertFalse(serv.isConnected());
+        assertFalse(serv.isClosed());
+        t.start();
+        UDTSocket other = new UDTSocket();
+        assertFalse(other.isConnected());
+        assertFalse(other.isClosed());
 
-			public void run() {
-				try {
-					BufferedReader in = new BufferedReader(new InputStreamReader(c2.getInputStream()));
-					assertEquals("Hello World!", in.readLine());
-					in.close();
-					c2.close();
-				} catch (IOException e) {
-					throw new Error(e);
-				}
-			}
-			
-		});
-	}
-	
-	public void testLargeTransfer() {
-		final UDTSocket c1 = new UDTSocket();
-		final UDTSocket c2 = new UDTSocket();
-		connectRendezvous(c1, new Runnable() {
+        other.connect(new InetSocketAddress("127.0.0.1", prt));
 
-			public void run() {
-				try {
-					byte b[] = new byte[32768];
-					OutputStream out = c1.getOutputStream();
-					System.err.println("Sending");
-					long time = System.currentTimeMillis();
-					for (int i = 0; i < 10000; i++) {
-						out.write(b);
-					}
-					c1.close();
-					System.err.println("Done sending with " + (b.length * 10000000L / (System.currentTimeMillis() - time)) + " bytes/sec");
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
-			
-		}, c2, new Runnable() {
+        assertTrue(other.isConnected());
+        assertFalse(serv.isClosed());
+        assertFalse(other.isClosed());
+        BufferedReader in = new BufferedReader(new InputStreamReader(other
+            .getInputStream()));
+        assertEquals("Hello World!", in.readLine());
+        in.close();
 
-			public void run() {
-				try {
-					byte b[] = new byte[32768];
-					InputStream in = c2.getInputStream();
-					System.err.println("Receiving");
-					long count = 0;
-					int read = 0;
-					long time = System.currentTimeMillis();
-					while ((read = in.read(b)) >= 0) {
-						count += read;
-					}
-					c2.close();
-					assertEquals(b.length * 10000, count);
-					System.err.println("Done receiving with " + (b.length * 10000000L / (System.currentTimeMillis() - time)) + " bytes/sec");
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
-			
-		});
-	}
-	
-	private int bindSocket(UDTSocket a) {
-		for (int i = 10000; i < 10000 + 50; i++)  {
-			try {
-				a.bind(new InetSocketAddress(i));
-			} catch (IOException e) {
-//				e.printStackTrace();
-//				System.err.println("Trying next port...");
-				continue;
-			}
-			return i;
-		}
-		return -1;
-	}
-	
-	private void connectRendezvous(final UDTSocket a, final Runnable workerA, final UDTSocket b, final Runnable workerB) {
+        other.close();
+        assertFalse(other.isConnected());
+        assertTrue(other.isClosed());
+
+        t.join(1000);
+        assertTrue(tmp.value);
+        assertFalse(serv.isConnected());
+        assertTrue(serv.isClosed());
+    }
+
+    /**
+     * Potential NAT traversal test.
+     */
+    public void testRendezvous() throws IOException {
+        if (!NetworkUtil.isUDTSupported()) {
+            return;
+        }
+        final UDTSocket c1 = new UDTSocket();
+        final UDTSocket c2 = new UDTSocket();
+        connectRendezvous(c1, new Runnable() {
+
+            public void run() {
+                try {
+                    PrintWriter w = new PrintWriter(c1.getOutputStream());
+                    w.println("Hello World!");
+                    w.close();
+                    c1.close();
+                } catch (IOException e) {
+                    throw new Error(e);
+                }
+            }
+
+        }, c2, new Runnable() {
+
+            public void run() {
+                try {
+                    BufferedReader in = new BufferedReader(
+                        new InputStreamReader(c2.getInputStream()));
+                    assertEquals("Hello World!", in.readLine());
+                    in.close();
+                    c2.close();
+                } catch (IOException e) {
+                    throw new Error(e);
+                }
+            }
+
+        });
+    }
+
+    public void testLargeTransfer() {
+        if (!NetworkUtil.isUDTSupported()) {
+            return;
+        }
+        final UDTSocket c1 = new UDTSocket();
+        final UDTSocket c2 = new UDTSocket();
+        connectRendezvous(c1, new Runnable() {
+
+            public void run() {
+                try {
+                    byte b[] = new byte[32768];
+                    OutputStream out = c1.getOutputStream();
+                    System.err.println("Sending");
+                    long time = System.currentTimeMillis();
+                    for (int i = 0; i < 10000; i++) {
+                        out.write(b);
+                    }
+                    c1.close();
+                    System.err
+                        .println("Done sending with "
+                            + (b.length * 10000000L / (System
+                                .currentTimeMillis() - time)) + " bytes/sec");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }, c2, new Runnable() {
+
+            public void run() {
+                try {
+                    byte b[] = new byte[32768];
+                    InputStream in = c2.getInputStream();
+                    System.err.println("Receiving");
+                    long count = 0;
+                    int read = 0;
+                    long time = System.currentTimeMillis();
+                    while ((read = in.read(b)) >= 0) {
+                        count += read;
+                    }
+                    c2.close();
+                    assertEquals(b.length * 10000, count);
+                    System.err
+                        .println("Done receiving with "
+                            + (b.length * 10000000L / (System
+                                .currentTimeMillis() - time)) + " bytes/sec");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        });
+    }
+
+    private int bindSocket(UDTSocket a) {
+        for (int i = 10000; i < 10000 + 50; i++) {
+            try {
+                a.bind(new InetSocketAddress(i));
+            } catch (IOException e) {
+                // e.printStackTrace();
+                // System.err.println("Trying next port...");
+                continue;
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    private void connectRendezvous(final UDTSocket a, final Runnable workerA,
+        final UDTSocket b, final Runnable workerB)
+    {
         a.setSoRendezvous(true);
         b.setSoRendezvous(true);
 
         final int pa = bindSocket(a);
-		final int pb = bindSocket(b);
-		System.err.println(pa + " " + pb);
-		assertTrue(pa > 0);
-		assertTrue(pb > 0);
-		assertTrue(a.getSoRendezvous());
-		assertTrue(b.getSoRendezvous());
-		
-		final ThreadHelper<Error> rethrower = new ThreadHelper<Error>();
-		Thread tA, tB;
-		tA = new Thread(new Runnable() {
-			public void run() {
-				try {
-					a.connect(new InetSocketAddress("localhost", pb));
-					workerA.run();
-				} catch (Error t) {
-					rethrower.value = t;
-				} catch (IOException e) {
-					rethrower.value = new Error(e);
-				} 
-			}
-		}, "Worker A");
-		tB = new Thread(new Runnable() {
-			public void run() {
-				try {
-					b.connect(new InetSocketAddress("localhost", pa));
-					workerB.run();
-				} catch (Error t) {
-					rethrower.value = t;
-				} catch (IOException e) {
-					rethrower.value = new Error(e);
-				} 
-			}
-		}, "Worker B");
-		tB.start();
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		tA.start();
-		try {
-			tA.join();
-			tB.join();
-		} catch (InterruptedException e) {
-			fail(e.toString());
-		}
-		if (rethrower.value != null) {
-			throw rethrower.value;
-		}
-	}
+        final int pb = bindSocket(b);
+        System.err.println(pa + " " + pb);
+        assertTrue(pa > 0);
+        assertTrue(pb > 0);
+        assertTrue(a.getSoRendezvous());
+        assertTrue(b.getSoRendezvous());
+
+        final ThreadHelper<Error> rethrower = new ThreadHelper<Error>();
+        Thread tA, tB;
+        tA = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    a.connect(new InetSocketAddress("localhost", pb));
+                    workerA.run();
+                } catch (Error t) {
+                    rethrower.value = t;
+                } catch (IOException e) {
+                    rethrower.value = new Error(e);
+                }
+            }
+        }, "Worker A");
+        tB = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    b.connect(new InetSocketAddress("localhost", pa));
+                    workerB.run();
+                } catch (Error t) {
+                    rethrower.value = t;
+                } catch (IOException e) {
+                    rethrower.value = new Error(e);
+                }
+            }
+        }, "Worker B");
+        tB.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        tA.start();
+        try {
+            tA.join();
+            tB.join();
+        } catch (InterruptedException e) {
+            fail(e.toString());
+        }
+        if (rethrower.value != null) {
+            throw rethrower.value;
+        }
+    }
 }
