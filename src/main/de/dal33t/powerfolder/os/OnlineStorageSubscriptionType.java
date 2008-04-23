5cb0a7e8366e0ae096d09ccf2bb24647c4068c81
Christian Sprajc <sprajc@powerfolder.com>
2008-04-23 22:24:37 +0000

    New OS pricing

--- src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
+++ src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
@@ -9,18 +9,22 @@ package de.dal33t.powerfolder.os;
 public enum OnlineStorageSubscriptionType {
     TRIAL("OS-T", "1 GB (30 days trial)", 1, true),
     TRIAL_PRO("OS-TP", "DONTUSE: 1 GB (60 days trial)", 1, true), 
-    STARTER("OS-S", "1 GB", 1, false),
-    BASIC("OS-B", "5 GB", 5, false),
-    ADVANCED("OS-A", "10 GB", 10, false),
+    STARTER("OS-1", "1 GB", 1, false),
+    BASIC("OS-5", "5 GB", 5, false),
+    ADVANCED("OS-10", "10 GB", 10, false),
 
     /**
      * For JUNIT testing only
      */
     TEST(),
 
-    SMALL_ENTERPRISE("OS-SE", "20 GB", 20, false),
+    SMALL_ENTERPRISE("OS-20", "20 GB", 20, false),
     UNLIMITED("OS-U", "Unlimited", 9999, false),
-    NONE("OS-N", "None", 0, false);
+    NONE("OS-N", "None", 0, false),
+    GB50("OS-50", "50 GB", 50, false),
+    GB100("OS-100", "100 GB", 100, false),
+    GB250("OS-250", "250 GB", 250, false),
+    TRIAL_5GB("OS-T-5", "5 GB (30 days trial)", 5, true);
 
     private String articleNo;
     private String description;
