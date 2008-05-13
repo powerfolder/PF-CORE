d944e02d27a73c55a4dfd0025f0c74a79975f693
Christian Sprajc <sprajc@powerfolder.com>
2008-05-13 21:46:12 +0000

    Incomplete - # 1007: Add test for AddressRange http://dev.powerfolder.com/projects/powerfolder/ticket/1007
    - Added test for the given example (and additional checks)

--- src/test/de/dal33t/powerfolder/test/util/NetUtilTest.java
+++ src/test/de/dal33t/powerfolder/test/util/NetUtilTest.java
@@ -3,6 +3,7 @@ package de.dal33t.powerfolder.test.util;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.text.ParseException;
 
 import junit.framework.TestCase;
 import de.dal33t.powerfolder.util.net.AddressRange;
@@ -24,5 +25,19 @@ public class NetUtilTest extends TestCase {
 				(Inet4Address) InetAddress.getByName("128.127.127.127")));
 		assertFalse(ar.contains(
 				(Inet4Address) InetAddress.getByName("0.0.0.1")));
+	}
+	
+	public void testExamples() throws ParseException, UnknownHostException {
+	    AddressRange r = AddressRange.parseRange("195.145.13.0-195.145.13.255");
+	    assertTrue(r.contains((Inet4Address) Inet4Address.getByName("195.145.13.84")));
+	    for (int j = 1; j < 255; j++) {
+    	    for (int i = 1; i < 255; i++) {
+    	        if (j == 13) {
+    	            assertTrue(r.contains((Inet4Address) Inet4Address.getByName("195.145." + j + "." + i)));
+    	        } else { 
+    	            assertFalse(r.contains((Inet4Address) Inet4Address.getByName("195.145." + j + "." + i)));
+    	        }
+    	    }
+	    }
 	}
 }
