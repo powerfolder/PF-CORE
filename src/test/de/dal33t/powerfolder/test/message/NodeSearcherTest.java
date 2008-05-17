cb473e1a4aaabf3b8afbbe9ec02fba9f465d8b42
Christian Sprajc <sprajc@powerfolder.com>
2008-05-17 05:17:12 +0000

    Fixed, added OS

--- src/test/de/dal33t/powerfolder/test/message/NodeSearcherTest.java
+++ src/test/de/dal33t/powerfolder/test/message/NodeSearcherTest.java
@@ -164,7 +164,7 @@ public class NodeSearcherTest extends TwoControllerTestCase {
         searcher.cancelSearch();
         assertFalse(searchResultModel.isEmpty());
         // baRt, homeR and maRge, ned flendeRs
-        assertEquals(searchResultModel.toString(), 4, searchResultModel.size());
+        assertEquals(searchResultModel.toString(), 5, searchResultModel.size());
 
         // Search for "127.0.0.1"
         searcher = new NodeSearcher(getContollerLisa(), "127.0.0.1",
