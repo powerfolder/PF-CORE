465c4b23a21a80b4c8a5787f9f92d66bc24c81d2
Christian Sprajc <sprajc@powerfolder.com>
2008-06-02 23:21:11 +0000

    Wiki links

--- src/main/de/dal33t/powerfolder/ui/preferences/DynDnsSettingsTab.java
+++ src/main/de/dal33t/powerfolder/ui/preferences/DynDnsSettingsTab.java
@@ -150,8 +150,8 @@ public class DynDnsSettingsTab extends PFComponent implements PreferenceTab {
             builder.add(myDnsLabel, cc.xy(1, row));
             builder.add(myDnsField, cc.xywh(3, row, 1, 1));
             // FIXME correct URL
-            builder.add(Help.createHelpLinkLabel(Translation
-                .getTranslation("general.what_is_this"), "node/guide_supernode"),
+            builder.add(Help.createWikiLinkLabel(Translation
+                .getTranslation("general.what_is_this"), "DYN-Dns"),
                 cc.xy(5, row));
 
             row += 2;
