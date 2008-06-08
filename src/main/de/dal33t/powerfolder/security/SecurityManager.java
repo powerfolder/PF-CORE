70a4919e0157347e6ff6879f32d46c342773fbef
Christian Sprajc <sprajc@powerfolder.com>
2008-06-08 00:28:52 +0000

    #958 - weblogin broken

--- src/main/de/dal33t/powerfolder/security/SecurityManager.java
+++ src/main/de/dal33t/powerfolder/security/SecurityManager.java
@@ -17,6 +17,14 @@ public interface SecurityManager {
     Account getSession();
 
     /**
+     * Associates the current thread with the given account
+     * 
+     * @param account
+     *            the account to set for the current thread.
+     */
+    void setSession(Account account);
+
+    /**
      * Clears the currently active session.
      */
     void destroySession();
@@ -27,17 +35,21 @@ public interface SecurityManager {
      * Saves a new or updates an old account. Afterwards the account is
      * persisted.
      * 
-     * @param login the identity to save
+     * @param login
+     *            the identity to save
      */
     void saveIdentity(Account login);
 
     /**
+     * Authenticates the user. If successful the session is set afterwards.
+     * 
+     * @see #setSession(Account)
      * @param username
      *            the username of the login
      * @param password
      *            the password of the login
-     * @return the account if acces is possible, null if user could not be logged
-     *         in.
+     * @return the account if acces is possible, null if user could not be
+     *         logged in.
      */
     Account authenticate(String username, String password);
 
