0dba36a12be7952e7c9be94446ac9fba4e70ee09
Christian Sprajc <sprajc@powerfolder.com>
2008-06-15 02:07:45 +0000

    #1049 change default OS server

--- src/main/de/dal33t/powerfolder/ui/webservice/OpenWebServiceInBrowserAction.java
+++ src/main/de/dal33t/powerfolder/ui/webservice/OpenWebServiceInBrowserAction.java
@@ -3,6 +3,9 @@ package de.dal33t.powerfolder.ui.webservice;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
+import org.apache.commons.lang.StringUtils;
+
+import de.dal33t.powerfolder.ConfigurationEntry;
 import de.dal33t.powerfolder.Constants;
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.ui.action.BaseAction;
@@ -16,7 +19,20 @@ public class OpenWebServiceInBrowserAction extends BaseAction {
 
     public void actionPerformed(ActionEvent e) {
         try {
-            BrowserLauncher.openURL(Constants.ONLINE_STORAGE_URL);
+            String host = ConfigurationEntry.SERVER_HOST
+                .getValue(getController());
+            if (!StringUtils.isBlank(host)) {
+                // FIXME: Does not work with port extensions
+                int i = host.indexOf(":");
+                if (i > 0) {
+                    host = host.substring(0, i);
+                }
+                BrowserLauncher.openURL("http://" + host);
+            } else {
+                // Default
+                BrowserLauncher.openURL(Constants.ONLINE_STORAGE_URL);
+            }
+
         } catch (IOException e1) {
             log().error(e1);
         }
