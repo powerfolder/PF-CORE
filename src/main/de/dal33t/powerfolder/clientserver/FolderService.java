21deb3d2d409a6720392b380fa5bd994cca78b90
Christian Sprajc <sprajc@powerfolder.com>
2008-06-21 23:58:37 +0000

    Removed unused method

--- src/main/de/dal33t/powerfolder/clientserver/FolderService.java
+++ src/main/de/dal33t/powerfolder/clientserver/FolderService.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.clientserver;
 
 import de.dal33t.powerfolder.disk.FolderException;
@@ -59,7 +59,7 @@ public interface FolderService {
      *            the name of the user to be invited
      * @param foInfo
      *            the folder to be invited to
-     * @throws FolderException 
+     * @throws FolderException
      */
     void inviteUser(FolderInfo foInfo, String user) throws FolderException;
 
@@ -83,14 +83,14 @@ public interface FolderService {
      */
     void setDefaultSynchronizedFolder(FolderInfo foInfo);
 
-    /**
-     * Grants the currently logged in user access to folder. the folder is NOT
-     * setup on the remote server.
-     * 
-     * @param foInfos
-     * @see #createFolder(FolderInfo, SyncProfile)
-     */
-    void grantAdmin(FolderInfo... foInfos);
+    // /**
+    // * Grants the currently logged in user access to folder. the folder is NOT
+    // * setup on the remote server.
+    // *
+    // * @param foInfos
+    // * @see #createFolder(FolderInfo, SyncProfile)
+    // */
+    // void grantAdmin(FolderInfo... foInfos);
 
     /**
      * Revokes the currently logged in user access to folder. the folder is NOT
