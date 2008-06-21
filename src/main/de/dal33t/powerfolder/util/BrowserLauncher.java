baa6114084e10f4cee66e31fc82d674d0277863c
Christian Sprajc <sprajc@powerfolder.com>
2008-06-21 23:59:20 +0000

    minor

--- src/main/de/dal33t/powerfolder/util/BrowserLauncher.java
+++ src/main/de/dal33t/powerfolder/util/BrowserLauncher.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util;
 
 import java.awt.Desktop;
@@ -78,7 +78,7 @@ public class BrowserLauncher {
     }
 
     private static boolean java6impl(String url) throws IOException {
-        Logger log = Logger.getLogger(BrowserLauncher.class); 
+        Logger log = Logger.getLogger(BrowserLauncher.class);
         try {
             if (Desktop.isDesktopSupported()) {
                 log.debug("Using Java6 Desktop.browse()");
@@ -87,7 +87,8 @@ public class BrowserLauncher {
         } catch (LinkageError err) {
             log.verbose(err);
         } catch (URISyntaxException e) {
-            throw new IOException(e);
+            throw (IOException) new IOException("Error:" + e.toString())
+                .initCause(e);
         }
         return false;
     }
