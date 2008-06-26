7a3b47b3b8e77ee6efc4f6bdbcf98fd4fa36e1af
Christian Sprajc <sprajc@powerfolder.com>
2008-06-26 19:17:14 +0000

    refs #1083
    * Added JUnit test
    * Works for me

--- src/test/de/dal33t/powerfolder/test/util/PersistentTaskManagerTestCase.java
+++ src/test/de/dal33t/powerfolder/test/util/PersistentTaskManagerTestCase.java
@@ -1,29 +1,39 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
+ */
 package de.dal33t.powerfolder.test.util;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+
+import de.dal33t.powerfolder.Member;
 import de.dal33t.powerfolder.disk.SyncProfile;
+import de.dal33t.powerfolder.event.InvitationReceivedEvent;
+import de.dal33t.powerfolder.event.InvitationReceivedHandler;
 import de.dal33t.powerfolder.light.MemberInfo;
+import de.dal33t.powerfolder.message.Invitation;
+import de.dal33t.powerfolder.util.InvitationUtil;
 import de.dal33t.powerfolder.util.task.PersistentTaskManager;
 import de.dal33t.powerfolder.util.task.SendMessageTask;
-import de.dal33t.powerfolder.util.test.ControllerTestCase;
+import de.dal33t.powerfolder.util.test.Condition;
+import de.dal33t.powerfolder.util.test.TestHelper;
+import de.dal33t.powerfolder.util.test.TwoControllerTestCase;
 
 /**
  * Tests for the TaskManager and the possible tasks.
@@ -31,31 +41,75 @@ import de.dal33t.powerfolder.util.test.ControllerTestCase;
  * @author Dennis "Bytekeeper" Waldherr
  * @version $revision$
  */
-public class PersistentTaskManagerTestCase extends ControllerTestCase {
-	public void testTaskManager() throws InterruptedException {
-		PersistentTaskManager man = getController().getTaskManager();
-		man.purgeAllTasks();
-		assertFalse(man.hasTasks());
-		assertEquals(man.activeTaskCount(), 0);
-		
-		setupTestFolder(SyncProfile.AUTOMATIC_DOWNLOAD);
-		MemberInfo inf = new MemberInfo("Nobody", "0");
-		getController().getNodeManager().addNode(inf);
-		
-		man.scheduleTask(new SendMessageTask(
-				getFolder().createInvitation(), inf.id)); 
+public class PersistentTaskManagerTestCase extends TwoControllerTestCase {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
 
-		man.shutdown();
-		man.start();
-		assertTrue(man.hasTasks());
-		assertEquals(man.activeTaskCount(),1);
-		
-		man.purgeAllTasks();
-		assertFalse(man.hasTasks());
-		assertEquals(man.activeTaskCount(), 0);
-		
-		inf.getNode(getController()).setFriend(true, "");
-		assertEquals(man.activeTaskCount(), 1);
-		man.purgeAllTasks();
-	}
+        connectBartAndLisa();
+
+        joinTestFolder(SyncProfile.AUTOMATIC_DOWNLOAD);
+    }
+
+    public void testTaskManager() throws InterruptedException {
+        PersistentTaskManager man = getContollerBart().getTaskManager();
+        man.purgeAllTasks();
+        assertFalse(man.hasTasks());
+        assertEquals(man.activeTaskCount(), 0);
+
+        MemberInfo inf = new MemberInfo("Nobody", "0");
+        getContollerBart().getNodeManager().addNode(inf);
+
+        man.scheduleTask(new SendMessageTask(getFolderAtBart()
+            .createInvitation(), inf.id));
+
+        man.shutdown();
+        man.start();
+        assertTrue(man.hasTasks());
+        assertEquals(man.activeTaskCount(), 1);
+
+        man.purgeAllTasks();
+        assertFalse(man.hasTasks());
+        assertEquals(man.activeTaskCount(), 0);
+
+        inf.getNode(getContollerBart()).setFriend(true, "");
+        assertEquals(man.activeTaskCount(), 1);
+        man.purgeAllTasks();
+    }
+
+    public void testOfflineInvitation() {
+        Member lisaAtBart = getContollerBart().getNodeManager()
+            .getConnectedNodes().iterator().next();
+
+        disconnectBartAndLisa();
+
+        getContollerLisa().getFolderRepository().removeFolder(
+            getFolderAtLisa(), true);
+        Invitation inv = new Invitation(getFolderAtBart().getInfo(),
+            getContollerBart().getMySelf().getInfo());
+        InvitationUtil.invitationToNode(getContollerBart(), inv, lisaAtBart);
+        TestHelper.waitMilliSeconds(2000);
+        assertEquals(1, getContollerBart().getTaskManager().activeTaskCount());
+
+        Mockery mock = new Mockery();
+        final InvitationReceivedHandler handler = mock
+            .mock(InvitationReceivedHandler.class);
+        mock.checking(new Expectations() {
+            {
+                one(handler).invitationReceived(
+                    with(any(InvitationReceivedEvent.class)));
+            }
+        });
+        getContollerLisa().getFolderRepository().setInvitationReceivedHandler(
+            handler);
+        connectBartAndLisa();
+        TestHelper.waitForCondition(30, new Condition() {
+            public boolean reached() {
+                return getContollerBart().getTaskManager().activeTaskCount() == 0;
+            }
+        });
+
+        TestHelper.waitMilliSeconds(2000);
+        mock.assertIsSatisfied();
+    }
 }
