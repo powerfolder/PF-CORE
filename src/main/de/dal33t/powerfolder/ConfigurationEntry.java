99ce74aa5e871eda58ebfe94ab82deb6ec584269
Christian Sprajc <sprajc@powerfolder.com>
2008-06-28 03:15:21 +0000

    refs #994

--- src/main/de/dal33t/powerfolder/ConfigurationEntry.java
+++ src/main/de/dal33t/powerfolder/ConfigurationEntry.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder;
 
 import java.beans.PropertyChangeEvent;
@@ -233,7 +233,8 @@ public enum ConfigurationEntry {
     /**
      * Whether to show system notifications when minimized.
      */
-    SHOW_SYSTEM_NOTIFICATIONS("show.system.notifications", Boolean.TRUE.toString()),
+    SHOW_SYSTEM_NOTIFICATIONS("show.system.notifications", Boolean.TRUE
+        .toString()),
 
     /**
      * Whether to log verbose.
@@ -280,6 +281,8 @@ public enum ConfigurationEntry {
 
     /**
      * Enable/Disable relayed connections.
+     * <P>
+     * Currently disabled see #994
      */
     UDT_CONNECTIONS_ENABLED("connections.udt", Boolean.FALSE.toString()),
 
