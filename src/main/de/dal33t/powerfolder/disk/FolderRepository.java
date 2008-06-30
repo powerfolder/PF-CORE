2124965fd773ffc7fc7f482a96f274c5782f1d97
Christian Sprajc <sprajc@powerfolder.com>
2008-06-30 22:43:16 +0000

    minor

--- src/main/de/dal33t/powerfolder/disk/FolderRepository.java
+++ src/main/de/dal33t/powerfolder/disk/FolderRepository.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.disk;
 
 import static de.dal33t.powerfolder.disk.FolderSettings.FOLDER_SETTINGS_DIR;
@@ -271,13 +271,11 @@ public class FolderRepository extends PFComponent implements Runnable {
         }
 
         SyncProfile syncProfile;
-        if (syncProfConfig.equals(PRE_777_BACKUP_TARGET_FIELD_LIST)) {
-
+        if (PRE_777_BACKUP_TARGET_FIELD_LIST.equals(syncProfConfig)) {
             // Migration for #787 (backup target timeBetweenScans changed
             // from 0 to 60).
             syncProfile = SyncProfile.BACKUP_TARGET;
         } else {
-
             // Load profile from field list.
             syncProfile = SyncProfile.getSyncProfileByFieldList(syncProfConfig);
         }
