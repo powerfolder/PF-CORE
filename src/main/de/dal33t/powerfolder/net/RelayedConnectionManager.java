efc2a5f28bd8a50dafb41cc9c1e4bac3b2cbdd2e
Christian Sprajc <sprajc@powerfolder.com>
2008-06-30 02:23:42 +0000

    Reduced debug output

--- src/main/de/dal33t/powerfolder/net/RelayedConnectionManager.java
+++ src/main/de/dal33t/powerfolder/net/RelayedConnectionManager.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.net;
 
 import java.util.Collection;
@@ -535,7 +535,9 @@ public class RelayedConnectionManager extends PFComponent {
                             }
                         } catch (ConnectionException e) {
                             log().warn(
-                                "Unable to connect to relay: " + canidate, e);
+                                "Unable to connect to relay: " + canidate
+                                    + ": " + e);
+                            log().verbose(e);
                         }
                     }
                     tringToConnect = false;
