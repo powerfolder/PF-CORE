ad06308debadedbef965ec1e07d7caf69561382d
Christian Sprajc <sprajc@powerfolder.com>
2008-06-30 02:27:37 +0000

    Added check for join

--- src/main/de/dal33t/powerfolder/util/test/FiveControllerTestCase.java
+++ src/main/de/dal33t/powerfolder/util/test/FiveControllerTestCase.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util.test;
 
 import java.io.File;
@@ -204,26 +204,26 @@ public abstract class FiveControllerTestCase extends MultipleControllerTestCase
             profile);
         joinFolder(testFolder, TESTFOLDER_BASEDIR_MAGGIE, getContollerMaggie(),
             profile);
-        // try {
-        // // Give them time to join
-        // TestHelper.waitForCondition(5, new Condition() {
-        // public boolean reached() {
-        // return getFolderAtBart().getMembersCount() >= 5
-        // && getFolderAtHomer().getMembersCount() >= 5
-        // && getFolderAtMarge().getMembersCount() >= 5
-        // && getFolderAtLisa().getMembersCount() >= 5
-        // && getFolderAtMaggie().getMembersCount() >= 5;
-        // }
-        // });
-        // } catch (Exception e) {
-        // throw new IllegalStateException("Homer: "
-        // + getFolderAtHomer().getMembersCount() + ", Bart: "
-        // + getFolderAtBart().getMembersCount() + ", Marge: "
-        // + getFolderAtMarge().getMembersCount() + ", Lisa: "
-        // + getFolderAtLisa().getMembersCount() + ", Maggie: "
-        // + getFolderAtMaggie().getMembersCount() + ". Folder: "
-        // + testFolder + " id: " + testFolder.id);
-        // }
+        try {
+            // Give them time to join
+            TestHelper.waitForCondition(5, new Condition() {
+                public boolean reached() {
+                    return getFolderAtBart().getMembersCount() >= 5
+                        && getFolderAtHomer().getMembersCount() >= 5
+                        && getFolderAtMarge().getMembersCount() >= 5
+                        && getFolderAtLisa().getMembersCount() >= 5
+                        && getFolderAtMaggie().getMembersCount() >= 5;
+                }
+            });
+        } catch (Exception e) {
+            throw new IllegalStateException("Homer: "
+                + getFolderAtHomer().getMembersCount() + ", Bart: "
+                + getFolderAtBart().getMembersCount() + ", Marge: "
+                + getFolderAtMarge().getMembersCount() + ", Lisa: "
+                + getFolderAtLisa().getMembersCount() + ", Maggie: "
+                + getFolderAtMaggie().getMembersCount() + ". Folder: "
+                + testFolder + " id: " + testFolder.id);
+        }
     }
 
     protected void clearCompletedDownloads() {
