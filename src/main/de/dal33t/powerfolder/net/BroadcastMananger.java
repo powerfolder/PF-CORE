75446320b093f33c3acf640291aff99ff300b85e
Christian Sprajc <sprajc@powerfolder.com>
2008-06-30 22:43:40 +0000

    Skip broadcast sending if no connection listener was opened

--- src/main/de/dal33t/powerfolder/net/BroadcastMananger.java
+++ src/main/de/dal33t/powerfolder/net/BroadcastMananger.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.net;
 
 import java.io.IOException;
@@ -141,19 +141,22 @@ public class BroadcastMananger extends PFComponent implements Runnable {
         myThread.start();
         log().debug("Started");
 
-        getController().scheduleAndRepeat(new TimerTask() {
-            @Override
-            public void run() {
-                if (socket == null || socket.isClosed()) {
-                    return;
+        if (getController().getConnectionListener() != null) {
+            getController().scheduleAndRepeat(new TimerTask() {
+                @Override
+                public void run() {
+                    if (socket == null || socket.isClosed()) {
+                        return;
+                    }
+                    if (broadCastString == null) {
+                        log().warn("Not sending network broadcast");
+                        return;
+                    }
+                    getController().getIOProvider().startIO(
+                        new BroadcastSender());
                 }
-                if (broadCastString == null) {
-                    log().warn("Not sending network broadcast");
-                    return;
-                }
-                getController().getIOProvider().startIO(new BroadcastSender());
-            }
-        }, 10L * 1000);
+            }, 10L * 1000);
+        }
     }
 
     /**
