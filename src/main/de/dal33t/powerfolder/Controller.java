82c0e8c06f44a65cf6e5a278ad2c68a3b08ffe18
Christian Sprajc <sprajc@powerfolder.com>
2008-07-08 00:18:48 +0000

    Release canidate 1

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -107,7 +107,7 @@ public class Controller extends PFComponent {
     /**
      * program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "3.1.0 dev15";
+    public static final String PROGRAM_VERSION = "3.1.0 RC1";
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
