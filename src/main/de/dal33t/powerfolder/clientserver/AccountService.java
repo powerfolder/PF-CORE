1305b290d3ae03163b4f7bf89beeb1ca804be2df
Christian Sprajc <sprajc@powerfolder.com>
2008-07-21 15:27:23 +0000

    Support for trial key creation during activation

--- src/main/de/dal33t/powerfolder/clientserver/AccountService.java
+++ src/main/de/dal33t/powerfolder/clientserver/AccountService.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.clientserver;
 
 import java.util.Collection;
@@ -111,6 +111,14 @@ public interface AccountService {
      *            the username of the users to check.
      */
     void checkAccounts(String... usernames);
+
+    /**
+     * Adds a 31 days trial key to the given account.
+     * 
+     * @param account
+     * @return true if succeeded
+     */
+    boolean addTrialLicenseKey(Account account);
 
     /**
      * @return all license key content for this account. or null if no key was
