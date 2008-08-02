f1ff142f9b3d58bcfda9dbff9d6c98e6e36e2d62
Christian Sprajc <sprajc@powerfolder.com>
2008-08-02 00:48:37 +0000

    refs #278 new UI

--- src/main/de/dal33t/powerfolder/PFUIComponent.java
+++ src/main/de/dal33t/powerfolder/PFUIComponent.java
@@ -1,25 +1,26 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder;
 
 import de.dal33t.powerfolder.ui.UIController;
+import de.dal33t.powerfolder.ui.model.ApplicationModel;
 
 /**
  * A element which is owned by a ui controller
@@ -36,12 +37,19 @@ public abstract class PFUIComponent extends PFComponent {
     }
 
     /**
-     * Answers the ui controller gives acces to all user interface componenets *
+     * Answers the ui controller gives acces to all user interface componenets
      * 
      * @return The UIController.
      */
     protected UIController getUIController() {
         return getController().getUIController();
+    }
+
+    /**
+     * @return the central application model.
+     */
+    protected ApplicationModel getApplicationModel() {
+        return getUIController().getApplicationModel();
     }
 
 }
