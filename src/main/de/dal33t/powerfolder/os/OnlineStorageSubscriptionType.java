c9faab308d68fc48e2ca4c9e9154461b7045c0e7
Christian Sprajc <sprajc@powerfolder.com>
2008-09-11 21:07:40 +0000

    refs #1242 Clustering

--- src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
+++ src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
@@ -26,21 +26,24 @@ package de.dal33t.powerfolder.os;
  * @version $Revision: 1.5 $
  */
 public enum OnlineStorageSubscriptionType {
-    TRIAL("OS-T", "1 GB (30 days trial)", 1, true), TRIAL_PRO("OS-TP",
-        "DONTUSE: 1 GB (60 days trial)", 1, true), STARTER("OS-1", "1 GB", 1,
-        false), BASIC("OS-5", "5 GB", 5, false), ADVANCED("OS-10", "10 GB", 10,
-        false),
+    TRIAL("OS-T", "1 GB (30 days)", 1, true),
+    TRIAL_PRO("OS-TP", "DONTUSE: 1 GB (60)", 1, true), 
+    STARTER("OS-1", "1 GB", 1, false),
+    BASIC("OS-5", "5 GB", 5, false),
+    ADVANCED("OS-10", "10 GB", 10, false),
 
     /**
      * For JUNIT testing only
      */
     TEST(),
 
-    SMALL_ENTERPRISE("OS-20", "20 GB", 20, false), UNLIMITED("OS-U",
-        "Unlimited", 9999, false), NONE("OS-N", "None", 0, false), GB50(
-        "OS-50", "50 GB", 50, false), GB100("OS-100", "100 GB", 100, false), GB250(
-        "OS-250", "250 GB", 250, false), TRIAL_5GB("OS-T-5",
-        "5 GB (30 days trial)", 5, true);
+    SMALL_ENTERPRISE("OS-20", "20 GB", 20, false),
+    UNLIMITED("OS-U", "Unlimited", 9999, false),
+    NONE("OS-N", "None", 0, false),
+    GB50("OS-50", "50 GB", 50, false),
+    GB100("OS-100", "100 GB", 100, false),
+    GB250("OS-250", "250 GB", 250, false),
+    TRIAL_5GB("OS-T-5", "5 GB (30 days)", 5, true);
 
     private String articleNo;
     private String description;
