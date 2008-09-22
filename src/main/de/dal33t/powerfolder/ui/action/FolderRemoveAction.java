905377bfd247e4013835d834ef23b7e0e15d56ad
Christian Sprajc <sprajc@powerfolder.com>
2008-09-22 05:18:15 +0000

    Fixed not closing dialog

--- src/main/de/dal33t/powerfolder/ui/action/FolderRemoveAction.java
+++ src/main/de/dal33t/powerfolder/ui/action/FolderRemoveAction.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.ui.action;
 
 import de.dal33t.powerfolder.Controller;
@@ -87,22 +87,24 @@ public class FolderRemoveAction extends BaseAction {
      *            Storage
      */
     public void confirmedFolderLeave(boolean removeLocal,
-                                     boolean deleteSystemSubFolder,
-                                     boolean convertToPreview,
-                                     boolean removeFromOS) {
+        boolean deleteSystemSubFolder, boolean convertToPreview,
+        boolean removeFromOS)
+    {
 
         Folder folder = (Folder) actionSelectionModel.getSelection();
 
+        boolean removed = false;
         if (removeLocal) {
             if (convertToPreview) {
                 boolean converted = FolderPreviewHelper.convertFolderToPreview(
-                        getController(), folder, deleteSystemSubFolder);
+                    getController(), folder, deleteSystemSubFolder);
                 if (!converted) {
                     return;
                 }
             } else {
                 getController().getFolderRepository().removeFolder(folder,
                     deleteSystemSubFolder);
+                removed = true;
             }
         }
 
@@ -116,9 +118,13 @@ public class FolderRemoveAction extends BaseAction {
             }
             getController().getOSClient().refreshAccountDetails();
 
-            folderRepository = getController().getFolderRepository();
-            FolderSettings folderSettings = folderRepository.loadFolderSettings(folder.getName());
-            folderRepository.saveFolderConfig(folder.getInfo(), folderSettings, true);
+            if (!removed) {
+                folderRepository = getController().getFolderRepository();
+                FolderSettings folderSettings = folderRepository
+                    .loadFolderSettings(folder.getName());
+                folderRepository.saveFolderConfig(folder.getInfo(),
+                    folderSettings, true);
+            }
         }
     }
 }
