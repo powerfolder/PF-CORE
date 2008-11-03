05a7cd2f6fcd759f4744e2e35dca8aac1ecca40d
Christian Sprajc <sprajc@powerfolder.com>
2008-11-03 03:26:26 +0000

    Removed useless method

--- src/main/de/dal33t/powerfolder/security/FolderAdminPermission.java
+++ src/main/de/dal33t/powerfolder/security/FolderAdminPermission.java
@@ -1,26 +1,23 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.security;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 import de.dal33t.powerfolder.light.FolderInfo;
 import de.dal33t.powerfolder.util.Reject;
@@ -43,22 +40,6 @@ public class FolderAdminPermission implements Permission {
 
     public FolderInfo getFolder() {
         return folder;
-    }
-
-    /**
-     * @param account
-     * @return the folderinfo this account has admin permissions on.
-     */
-    public static Collection<FolderInfo> filter(Account account)
-    {
-        Reject.ifNull(account, "Account is null");
-        Collection<FolderInfo> l = new ArrayList<FolderInfo>();
-        for (Permission p : account.getPermissions()) {
-            if (p instanceof FolderAdminPermission) {
-                l.add(((FolderAdminPermission) p).getFolder());
-            }
-        }
-        return l;
     }
 
     @Override
