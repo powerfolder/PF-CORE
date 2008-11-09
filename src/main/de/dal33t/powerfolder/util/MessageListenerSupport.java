86acf705437fa41e467a3ef4dd4449dc7ae0ce1e
Christian Sprajc <sprajc@powerfolder.com>
2008-11-09 14:22:08 +0000

    Better multi threading support

--- src/main/de/dal33t/powerfolder/util/MessageListenerSupport.java
+++ src/main/de/dal33t/powerfolder/util/MessageListenerSupport.java
@@ -1,44 +1,58 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import de.dal33t.powerfolder.Member;
 import de.dal33t.powerfolder.message.Message;
 import de.dal33t.powerfolder.message.MessageListener;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
+import de.dal33t.powerfolder.util.logging.Loggable;
 
 /**
- * Helper class to handle message listener/firering
+ * Helper class to handle message listener/firing
  * 
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc </a>
  * @version $Revision: 1.8 $
  */
 public class MessageListenerSupport {
+    private Loggable source;
     // Listeners for incoming messages
-    private Map<Class, Collection<MessageListener>> messageListener;
+    private Map<Class<?>, CopyOnWriteArrayList<MessageListener>> messageListener;
+
+    /**
+     * Initializes the the message listener support with a logger from the
+     * parent
+     * 
+     * @param source
+     *            the source
+     */
+    public MessageListenerSupport(Loggable source) {
+        this.source = source;
+        if (this.source == null) {
+            throw new NullPointerException("Source in null");
+        }
+    }
 
     // Message listener de-/registering ***************************************
 
@@ -47,8 +61,8 @@ public class MessageListenerSupport {
      * 
      * @param aListener
      */
-    public synchronized void addMessageListener(MessageListener aListener) {
-        addMessageListener(null, aListener);
+    public void addMessageListener(MessageListener aListener) {
+        addMessageListener(All.class, aListener);
     }
 
     /**
@@ -58,45 +72,39 @@ public class MessageListenerSupport {
      * @param messageType
      * @param aListener
      */
-    public synchronized void addMessageListener(Class messageType,
+    public void addMessageListener(Class<?> messageType,
         MessageListener aListener)
     {
         if (aListener == null) {
             return;
         }
-        Collection<MessageListener> listeners = getListenersMap()
-            .get(messageType);
-        if (listeners == null) {
-            // Build new list for this type of message
-            listeners = Collections
-                .synchronizedSet(new HashSet<MessageListener>());
-            getListenersMap().put(messageType, listeners);
-        }
-
-        synchronized (listeners) {
-            // Check for duplicate listeners
-            if (!listeners.contains(aListener)) {
-                listeners.add(aListener);
+        synchronized (this) {
+            CopyOnWriteArrayList<MessageListener> listeners = getListenersMap()
+                .get(messageType);
+            if (listeners == null) {
+                // Build new list for this type of message
+                listeners = new CopyOnWriteArrayList<MessageListener>();
+                getListenersMap().put(messageType, listeners);
             }
+            listeners.addIfAbsent(aListener);
         }
-        // String msgType = (messageType == null) ? "all messages" : messageType
-        // .getName();
-        // source.logFiner(
-        // "Added message listener (" + aListener + ") for " + msgType);
     }
 
     /**
-     * Removes a message listener completely from the message listner support
+     * Removes a message listener completely from the message listener support
      * 
      * @param aListener
      */
-    public synchronized void removeMessageListener(MessageListener aListener) {
+    public void removeMessageListener(MessageListener aListener) {
         if (messageListener == null) {
             return;
         }
-        for (Iterator it = messageListener.values().iterator(); it.hasNext();) {
-            Collection listeners = (Collection) it.next();
-            listeners.remove(aListener);
+        synchronized (this) {
+            for (Collection<MessageListener> listeners : messageListener
+                .values())
+            {
+                listeners.remove(aListener);
+            }
         }
     }
 
@@ -109,10 +117,9 @@ public class MessageListenerSupport {
         }
         synchronized (this) {
             // Remove message listeners
-            for (Iterator it = messageListener.values().iterator(); it
-                .hasNext();)
+            for (Collection<MessageListener> listeners : messageListener
+                .values())
             {
-                Collection listeners = (Collection) it.next();
                 listeners.clear();
             }
         }
@@ -147,51 +154,42 @@ public class MessageListenerSupport {
         int lGenCount = 0;
         int lSpcCount = 0;
         // Fire general listener
-        synchronized (this) {
-            Collection<MessageListener> generalListeners = messageListener
-                .get(null);
-            if (generalListeners != null && !generalListeners.isEmpty()) {
-                MessageListener[] genListener;
-                synchronized (generalListeners) {
-                    genListener = new MessageListener[generalListeners.size()];
-                    generalListeners.toArray(genListener);
-                }
-                for (int i = 0; i < genListener.length; i++) {
-                    genListener[i].handleMessage(theSource, message);
-                    lGenCount++;
-                }
-            }
 
-            // Fire special listeners
-            Collection<MessageListener> specialListeners = messageListener
-                .get(message.getClass());
-            if (specialListeners != null && !specialListeners.isEmpty()) {
-                MessageListener[] specListener;
-                synchronized (specialListeners) {
-                    specListener = new MessageListener[specialListeners.size()];
-                    specialListeners.toArray(specListener);
-                }
-                for (int i = 0; i < specListener.length; i++) {
-                    specListener[i].handleMessage(theSource, message);
-                    lSpcCount++;
-                }
+        Collection<MessageListener> generalListeners = messageListener
+            .get(All.class);
+        if (generalListeners != null && !generalListeners.isEmpty()) {
+            for (MessageListener genListener : generalListeners) {
+                genListener.handleMessage(theSource, message);
+                lGenCount++;
+            }
+        }
+
+        // Fire special listeners
+        Collection<MessageListener> specialListeners = messageListener
+            .get(message.getClass());
+        if (specialListeners != null && !specialListeners.isEmpty()) {
+            for (MessageListener sepcListener : specialListeners) {
+                sepcListener.handleMessage(theSource, message);
+                lSpcCount++;
             }
         }
 
         if (lSpcCount > 0 || lGenCount > 0) {
-            // theSource.logFiner(
+            // theSource.getLogger().verbose(
             // "Deligated message (" + message.getClass().getName() + ") to "
             // + lGenCount + " general and " + lSpcCount
             // + " special message listener");
         }
     }
 
-    private synchronized Map<Class, Collection<MessageListener>> getListenersMap() {
+    private synchronized Map<Class<?>, CopyOnWriteArrayList<MessageListener>> getListenersMap()
+    {
         if (messageListener == null) {
-            messageListener = Collections
-                .synchronizedMap(new HashMap<Class, Collection<MessageListener>>(
-                    1));
+            messageListener = new ConcurrentHashMap<Class<?>, CopyOnWriteArrayList<MessageListener>>();
         }
         return messageListener;
+    }
+
+    private class All {
     }
 }
