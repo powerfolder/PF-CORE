5bb1bbde3dedb07d447e6ca1c3909e1e010c5652
Christian Sprajc <sprajc@powerfolder.com>
2008-11-24 02:30:45 +0000

    refs #1310 memory

--- src/main/de/dal33t/powerfolder/message/FolderList.java
+++ src/main/de/dal33t/powerfolder/message/FolderList.java
@@ -1,25 +1,26 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.message;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -54,29 +55,22 @@ public class FolderList extends Message {
      * @param remoteMagicId
      *            the magic id which was sent by the remote side
      */
-    public FolderList(FolderInfo[] allFolders, String remoteMagicId) {
+    public FolderList(Collection<FolderInfo> allFolders, String remoteMagicId) {
         // Split folderlist into secret and public list
         // Encrypt secret folder ids with magic id
-        List<FolderInfo> publicFos = new ArrayList<FolderInfo>(
-            allFolders.length);
-        List<FolderInfo> secretFos = new ArrayList<FolderInfo>(
-            allFolders.length);
-
-        for (int i = 0; i < allFolders.length; i++) {
+        List<FolderInfo> secretFos = new ArrayList<FolderInfo>(allFolders
+            .size());
+        for (FolderInfo folderInfo : allFolders) {
             if (!StringUtils.isBlank(remoteMagicId)) {
                 // Send secret folder infos if magic id is not empty
-
                 // Clone folderinfo
-                FolderInfo secretFolder = (FolderInfo) allFolders[i].clone();
-
+                FolderInfo secretFolder = (FolderInfo) folderInfo.clone();
                 // Set Id to secure Id
                 secretFolder.id = secretFolder.calculateSecureId(remoteMagicId);
-
                 // Secret folder, encrypt folder id with magic id
                 secretFos.add(secretFolder);
             }
         }
-
         this.secretFolders = new FolderInfo[secretFos.size()];
         secretFos.toArray(secretFolders);
     }
