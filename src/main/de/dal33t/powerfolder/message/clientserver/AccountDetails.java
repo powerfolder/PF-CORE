2d99b8a459e55ed0ec94659e65bbf0a9899e93e0
Christian Sprajc <sprajc@powerfolder.com>
2008-11-24 02:30:55 +0000

    Server monitoring

--- src/main/de/dal33t/powerfolder/message/clientserver/AccountDetails.java
+++ src/main/de/dal33t/powerfolder/message/clientserver/AccountDetails.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.message.clientserver;
 
 import java.io.Serializable;
@@ -36,12 +36,16 @@ public class AccountDetails implements Serializable {
     private Account user;
     private long spaceUsed;
     private int nFolders;
+    private long recycleBinSize;
 
-    public AccountDetails(Account user, long spaceUsed, int nFolders) {
+    public AccountDetails(Account user, long spaceUsed, int nFolders,
+        long recycleBinSize)
+    {
         super();
         this.user = user;
         this.spaceUsed = spaceUsed;
         this.nFolders = nFolders;
+        this.recycleBinSize = recycleBinSize;
     }
 
     public Account getAccount() {
@@ -54,6 +58,10 @@ public class AccountDetails implements Serializable {
 
     public int getNFolders() {
         return nFolders;
+    }
+
+    public long getRecycleBinSize() {
+        return recycleBinSize;
     }
 
     public String toString() {
