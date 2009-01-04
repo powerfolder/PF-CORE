0424a8a7ea965125dea342cf80fab8fc83a095e7
Harry Glasgow <glasgow@powerfolder.com>
2009-01-04 12:05:43 +0000

    #278 - OS sync details - web link

--- src/main/de/dal33t/powerfolder/ui/folders/OnlineStorageComponent.java
+++ src/main/de/dal33t/powerfolder/ui/folders/OnlineStorageComponent.java
@@ -21,10 +21,15 @@ package de.dal33t.powerfolder.ui.folders;
 
 import de.dal33t.powerfolder.PFUIComponent;
 import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.ui.action.BaseAction;
+import de.dal33t.powerfolder.ui.widget.JButtonMini;
 import de.dal33t.powerfolder.util.Translation;
+import de.dal33t.powerfolder.util.BrowserLauncher;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.CellConstraints;
@@ -37,6 +42,7 @@ public class OnlineStorageComponent extends PFUIComponent {
 
     private JPanel uiComponent;
     private JLabel syncLabel;
+    private JButton webButton;
 
     public OnlineStorageComponent(Controller controller) {
         super(controller);
@@ -53,23 +59,39 @@ public class OnlineStorageComponent extends PFUIComponent {
     private void initialize() {
         syncLabel = new JLabel(Translation.getTranslation(
                 "online_storage_component.online_storage_text", 0));
+        webButton = new JButtonMini(new MyOnlineStorageAction(getController()), true);
     }
 
     private void buildUI() {
-        FormLayout layout = new FormLayout("pref, pref:grow",
+        FormLayout layout = new FormLayout("pref, pref:grow, pref",
             "3dlu, pref, 3dlu, pref");
         PanelBuilder builder = new PanelBuilder(layout);
         CellConstraints cc = new CellConstraints();
 
-        builder.addSeparator(null, cc.xyw(1, 2, 2));
+        builder.addSeparator(null, cc.xyw(1, 2, 3));
         builder.add(syncLabel, cc.xy(1, 4));
+        builder.add(webButton, cc.xy(3, 4));
         uiComponent = builder.getPanel();
         uiComponent.setBackground(SystemColor.text);
-
     }
 
     public void setSyncPercentage(double serverSync) {
         syncLabel.setText(Translation.getTranslation(
                 "online_storage_component.online_storage_text", serverSync));
+    }
+
+    private class MyOnlineStorageAction extends BaseAction {
+
+        private MyOnlineStorageAction(Controller controller) {
+            super("action_online_storage", controller);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            try {
+                BrowserLauncher.openURL(getController().getOSClient().getWebURL());
+            } catch (IOException e1) {
+                logSevere(e1);
+            }
+        }
     }
 }
