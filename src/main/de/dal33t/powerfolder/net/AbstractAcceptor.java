66fcfe4d90f94f1d41312c5a1f0a8ed4f93e3514
Christian Sprajc <sprajc@powerfolder.com>
2009-01-21 23:37:27 +0000

    refs  #1390 (new feature request) Direct UDT connection optimized for high speed networks

---
+++ src/main/de/dal33t/powerfolder/net/AbstractAcceptor.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: Controller.java 6331 2009-01-03 18:40:42Z tot $
+ */
+package de.dal33t.powerfolder.net;
+
+import java.util.Date;
+
+import de.dal33t.powerfolder.Constants;
+import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.PFComponent;
+import de.dal33t.powerfolder.util.Profiling;
+import de.dal33t.powerfolder.util.ProfilingEntry;
+
+/**
+ * An acceptor is a task to fully handshake a new incoming connection.
+ * 
+ * @author Christian Sprajc
+ * @version $Revision$
+ */
+public abstract class AbstractAcceptor extends PFComponent implements Runnable {
+    private Date startTime;
+
+    protected AbstractAcceptor(Controller controller) {
+        super(controller);
+    }
+
+    /**
+     * Shuts the acceptor down and closes the socket
+     */
+    protected abstract void shutdown();
+
+    /**
+     * Overriding behavior. Performs the actual work. If Exception occurs the
+     * acceptor gets shut down.
+     */
+    protected  abstract void accept() throws ConnectionException;
+
+    /**
+     * @return if this acceptor has a timeout
+     */
+    boolean hasTimeout() {
+        if (startTime == null) {
+            // Not started yet
+            return false;
+        }
+        return (System.currentTimeMillis() > startTime.getTime()
+            + (Constants.INCOMING_CONNECTION_TIMEOUT * 1000));
+    }
+
+    public final void run() {
+        if (!getController().getNodeManager().isStarted()) {
+            logWarning(
+                "NodeManager already shut down. "
+                    + "Closing incoming connection attempt: " + this);
+            return;
+        }
+        ProfilingEntry pe = Profiling.start();
+        startTime = new Date();
+        try {
+            accept();
+        } catch (ConnectionException e) {
+            logFiner(
+                "Unable to accept incoming connection handler " + this, e);
+            shutdown();
+        } finally {
+            // Remove from acceptors list
+            getController().getNodeManager().acceptors.remove(this);
+            Profiling.end(pe);
+        }
+    }
+
+    /**
+     * Convenience method to accept the new Connection Handler.
+     * 
+     * @param handler
+     */
+    protected void acceptConnection(ConnectionHandler handler) {
+        try {
+            getController().getNodeManager().acceptConnection(handler);
+        } catch (ConnectionException e) {
+            logFiner(
+                "Unable to accept incoming connection handler " + handler, e);
+            handler.shutdown();
+            shutdown();
+            // Remove from acceptors list
+            getController().getNodeManager().acceptors.remove(this);
+        }
+    }
+}
