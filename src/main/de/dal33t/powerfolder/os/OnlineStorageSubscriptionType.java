fc1b3f930138b828ad5b8a30f0abba636312f2e8
Christian Sprajc <sprajc@powerfolder.com>
2009-01-21 23:39:36 +0000

    Hide non-active OS subscription types

--- src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
+++ src/main/de/dal33t/powerfolder/os/OnlineStorageSubscriptionType.java
@@ -26,37 +26,39 @@ package de.dal33t.powerfolder.os;
  * @version $Revision: 1.5 $
  */
 public enum OnlineStorageSubscriptionType {
-    TRIAL("OS-T", "1 GB (30 days)", 1, true),
-    TRIAL_PRO("OS-TP", "DONTUSE: 1 GB (60)", 1, true), 
-    STARTER("OS-1", "1 GB", 1, false),
-    BASIC("OS-5", "5 GB", 5, false),
-    ADVANCED("OS-10", "10 GB", 10, false),
+    TRIAL("OS-T", "1 GB (30 days)", 1, true, false),
+    TRIAL_PRO("OS-TP", "DONTUSE: 1 GB (60)", 1, true, false), 
+    STARTER("OS-1", "1 GB", 1, false, false),
+    BASIC("OS-5", "5 GB", 5, false, true),
+    ADVANCED("OS-10", "10 GB", 10, false, true),
 
     /**
      * For JUNIT testing only
      */
     TEST(),
 
-    SMALL_ENTERPRISE("OS-20", "20 GB", 20, false),
-    UNLIMITED("OS-U", "Unlimited", 9999, false),
-    NONE("OS-N", "None", 0, false),
-    GB50("OS-50", "50 GB", 50, false),
-    GB100("OS-100", "100 GB", 100, false),
-    GB250("OS-250", "250 GB", 250, false),
-    TRIAL_5GB("OS-T-5", "5 GB (30 days)", 5, true);
+    SMALL_ENTERPRISE("OS-20", "20 GB", 20, false, true),
+    UNLIMITED("OS-U", "Unlimited", 9999, false, true),
+    NONE("OS-N", "None", 0, false, true),
+    GB50("OS-50", "50 GB", 50, false, true),
+    GB100("OS-100", "100 GB", 100, false, true),
+    GB250("OS-250", "250 GB", 250, false, true),
+    TRIAL_5GB("OS-T-5", "5 GB (30 days)", 5, true, false);
 
     private String articleNo;
     private String description;
     private long storageSize;
     private boolean trial;
+    private boolean active;
 
     private OnlineStorageSubscriptionType(String articleNo, String description,
-        long gbs, boolean trial)
+        long gbs, boolean trial, boolean active)
     {
         this.articleNo = articleNo;
         this.description = description;
         this.storageSize = gbs * 1024 * 1024 * 1024;
         this.trial = trial;
+        this.active = active;
     }
 
     /**
@@ -67,8 +69,9 @@ public enum OnlineStorageSubscriptionType {
         this.description = "DONTUSE: 1 MB Test subscription";
         this.storageSize = 1 * 1024 * 1024;
         this.trial = true;
+        this.active = false;
     }
-
+    
     public static OnlineStorageSubscriptionType getByArticleNo(String articleNo)
     {
         for (OnlineStorageSubscriptionType type : values()) {
@@ -95,12 +98,16 @@ public enum OnlineStorageSubscriptionType {
     public long getStorageSize() {
         return storageSize;
     }
-    
+
     public int getStorageSizeGB() {
         return (int) (storageSize / 1024 / 1024 / 1024);
     }
 
     public boolean isTrial() {
         return trial;
+    }
+
+    public boolean isActive() {
+        return active;
     }
 }
