6fba56cad475e3f2ccb78fdafb352c6ab64b77b2
Christian Sprajc <sprajc@powerfolder.com>
2009-01-31 22:41:55 +0000

    refs #1427

--- src/main/de/dal33t/powerfolder/ui/dialog/SyncFolderPanel.java
+++ src/main/de/dal33t/powerfolder/ui/dialog/SyncFolderPanel.java
@@ -187,7 +187,7 @@ public class SyncFolderPanel extends BaseDialog {
                 folder.syncRemoteDeletedFiles(true);
                 // Request ALL files now modified by friends
                 getController().getFolderRepository().getFileRequestor()
-                    .requestMissingFiles(folder, true, true, true);
+                    .requestMissingFiles(folder, true, true, false);
             }
             return null;
         }
