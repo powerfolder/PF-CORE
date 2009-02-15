c01804e1135190958d8197d42f482e75ff2b94a8
Harry Glasgow <glasgow@powerfolder.com>
2009-02-15 05:16:59 +0000

    nicer error message box

--- src/jwf/jwf/ErrorMessageBox.java
+++ src/jwf/jwf/ErrorMessageBox.java
@@ -2,22 +2,17 @@
 package jwf;
 
 import de.dal33t.powerfolder.util.Translation;
+import de.dal33t.powerfolder.util.ui.UIUtil;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dialog;
-import java.awt.Frame;
-import java.awt.Window;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Iterator;
 import java.util.List;
 
-/** Displays a list of error messages and blocks until ok is pressed.
+/**
+ * Displays a list of error messages and blocks until ok is pressed.
  * @author Christopher Brind
  */
 public class ErrorMessageBox extends JDialog implements ActionListener {
@@ -30,41 +25,51 @@ public class ErrorMessageBox extends JDialog implements ActionListener {
 
     /** Construct a dialog with a frame parent. */
     public ErrorMessageBox(Frame frame) {
-        super(frame, "messages", true);
+        super(frame, Translation.getTranslation("general.messages"), true);
         init();
         center(frame);
     }
 
     /** Construct a dialog with a dialog parent. */
     public ErrorMessageBox(Dialog dialog) {
-        super(dialog, "messages", true);
+        super(dialog, Translation.getTranslation("general.messages"), true);
         init();
         center(dialog);
     }
 
     private void center(Window window) {
 
-        int x = window.getLocation().x + window.getSize().width / 2 - getSize().width / 2;
-        int y = window.getLocation().y + window.getSize().height / 2 - getSize().height / 2;
+        int x = window.getLocation().x +
+                window.getSize().width / 2 - getSize().width / 2;
+        int y = window.getLocation().y +
+                window.getSize().height / 2 - getSize().height / 2;
 
         setLocation(x, y);
     }
 
     private void init() {
-        Container c = getContentPane();
-        c.setLayout(new BorderLayout());
-        c.add(new JScrollPane(textPane), BorderLayout.CENTER);
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        JScrollPane scrollPane = new JScrollPane(textPane);
+        container.add(scrollPane, BorderLayout.CENTER);
+        UIUtil.removeBorder(scrollPane);
+        scrollPane.setBorder(BorderFactory.createEtchedBorder());
         JButton button = new JButton(Translation.getTranslation("general.ok") );
         button.addActionListener(this);
-        c.add(button, BorderLayout.SOUTH);
+        getRootPane().setDefaultButton(button);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(button);
+        container.add(buttonPanel, BorderLayout.SOUTH);
         textPane.setEditable(false);
-        setSize(200, 200);
+        textPane.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+        textPane.setOpaque(true);
+        textPane.setBackground(SystemColor.text);
+        setSize(300, 200);
     }
 
-
     /** Handles the ok press. */
-    public void actionPerformed(ActionEvent ae) {
-        if (Translation.getTranslation("general.ok").equals(ae.getActionCommand())) {
+    public void actionPerformed(ActionEvent e) {
+        if (Translation.getTranslation("general.ok").equals(e.getActionCommand())) {
             setVisible(false);
         }
     }
@@ -72,14 +77,14 @@ public class ErrorMessageBox extends JDialog implements ActionListener {
     /** Show a list of messages and block until ok is pressed.
      * @param list a List of String objects.
      */
-    public void showErrorMessages(List list) {
-        String theText = "";
-        Iterator iter = list.iterator();
+    public void showErrorMessages(List<String> list) {
+        StringBuilder builder = new StringBuilder();
+        Iterator<String> iter = list.iterator();
         while(iter.hasNext()) {
-            String s = (String)iter.next();
-            theText = theText + s + (iter.hasNext() ? "\n" : "");
+            String s = iter.next();
+            builder.append(builder + s + (iter.hasNext() ? "\n" : ""));
         }
-        textPane.setText(theText);
+        textPane.setText(builder.toString());
         setVisible(true);
     }
 
