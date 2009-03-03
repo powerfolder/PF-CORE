d1cb305ce8ec5b79b28f9bb57b00c562bc2fd470
Christian Sprajc <sprajc@powerfolder.com>
2009-03-03 01:36:29 +0000

    Fixed sorting

--- src/main/de/dal33t/powerfolder/DiskItem.java
+++ src/main/de/dal33t/powerfolder/DiskItem.java
@@ -30,7 +30,7 @@ import java.util.Date;
  */
 public interface DiskItem {
     String getExtension();
-    String getName();
+    String getFilenameOnly();
     String getLowerCaseName();
     long getSize();
     MemberInfo getModifiedBy();
