5ceac8124a8df21ca6524646a989e96dff245f64
Christian Sprajc <sprajc@powerfolder.com>
2009-03-17 00:45:43 +0000

    Added different landing page for client links and google anaytics champaign tracking

--- src/main/de/dal33t/powerfolder/ConfigurationEntry.java
+++ src/main/de/dal33t/powerfolder/ConfigurationEntry.java
@@ -67,30 +67,30 @@ public enum ConfigurationEntry {
     /**
      * URL of the PowerFolder homepage
      */
-    PROVIDER_URL("provider.url.main", "http://www.powerfolder.com"),
+    PROVIDER_URL(
+        "provider.url.main",
+        "http://www.powerfolder.com/home_client.html?utm_source=client&utm_medium=client&utm_campaign=Client"),
 
     /**
      * URL of the Online Storage features
      */
-    PROVIDER_ABOUT_URL("provider.url.about",
-        "http://www.powerfolder.com/online_storage_features.html"),
+    PROVIDER_ABOUT_URL(
+        "provider.url.about",
+        "http://www.powerfolder.com/online_storage_features.html?utm_source=client&utm_medium=client&utm_campaign=Client"),
 
     /**
      * Quickstart guides to PowerFolder
      */
-    PROVIDER_QUICKSTART_URL("provider.url.quickstart",
-        "http://www.powerfolder.com/quickstart.html"),
-
-    /**
-     * URL of the PowerFolder Wiki
-     */
-    PROVIDER_WIKI_URL("provider.url.wiki", "http://www.powerfolder.com/wiki"),
+    PROVIDER_QUICKSTART_URL(
+        "provider.url.quickstart",
+        "http://www.powerfolder.com/quickstart.html?utm_source=client&utm_medium=client&utm_campaign=Client"),
 
     /**
      * URL of the PowerFolder Support
      */
-    PROVIDER_SUPPORT_URL("provider.url.support",
-        "http://www.powerfolder.com/support.html"),
+    PROVIDER_SUPPORT_URL(
+        "provider.url.support",
+        "http://www.powerfolder.com/support.html?utm_source=client&utm_medium=client&utm_campaign=Client"),
 
     /**
      * URL where bugs or tickets can be filed.
@@ -103,13 +103,20 @@ public enum ConfigurationEntry {
      * URL of the PowerFolder Pro page
      */
     PROVIDER_BUY_URL("provider.url.buy",
-        "http://www.powerfolder.com/buynow.html"),
+        "http://www.powerfolder.com/buynow_client.html?utm_source=client&utm_medium=client&utm_campaign=Client"),
 
     /**
      * URL where the contact form resides
      */
-    PROVIDER_CONTACT_URL("provider.url.contact",
-        "http://www.powerfolder.com/contact.html"),
+    PROVIDER_CONTACT_URL(
+        "provider.url.contact",
+        "http://www.powerfolder.com/contact.html?utm_source=client&utm_medium=client&utm_campaign=Client"),
+
+    /**
+     * URL of the PowerFolder Wiki. ATTENTION: This URL gets extended by article
+     * URI from many help links
+     */
+    PROVIDER_WIKI_URL("provider.url.wiki", "http://www.powerfolder.com/wiki"),
 
     /**
      * HTTP RPC URL for
