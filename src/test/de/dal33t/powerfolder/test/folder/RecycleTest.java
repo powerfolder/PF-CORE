e830f488413b9b6b7becee1f671e7e45185bef8d
Christian Sprajc <sprajc@powerfolder.com>
2009-05-13 19:47:02 +0000

    refs #1294 Fixed use of FileInfos

--- src/test/de/dal33t/powerfolder/test/folder/RecycleTest.java
+++ src/test/de/dal33t/powerfolder/test/folder/RecycleTest.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
+ */
 package de.dal33t.powerfolder.test.folder;
 
 import java.io.File;
@@ -62,6 +62,7 @@ public class RecycleTest extends ControllerTestCase {
 
         TestHelper.waitMilliSeconds(1000);
         getFolder().removeFilesLocal(files);
+        fileInfo = getFolder().getKnowFilesAsArray()[0];
         assertFalse(file.exists());
         assertEquals(fileInfo.getModifiedBy(), getController().getMySelf()
             .getInfo());
@@ -69,6 +70,8 @@ public class RecycleTest extends ControllerTestCase {
             lastModified));
 
         assertTrue(bin.restoreFromRecycleBin(fileInfo));
+        fileInfo = getFolder().getKnowFilesAsArray()[0];
+
         assertTrue(file.exists());
         assertFileMatch(file, getFolder().getKnownFiles().iterator().next());
         assertTrue(
