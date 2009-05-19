30400dcf29828d2dffd33eb238bbe45672d4cc32
Harry Glasgow <glasgow@powerfolder.com>
2009-05-19 08:05:26 +0000

    Add Wiki constants.

---
+++ src/main/de/dal33t/powerfolder/ui/WikiLinks.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: WikiLinks.java 7764 2009-04-24 10:31:44Z tot $
+ */
+package de.dal33t.powerfolder.ui;
+
+/**
+ * Repository of links to Wiki pages.
+ * Any references to the PowerFolder Wiki should go here, so that we can ensure the Wiki is up-to-date.
+ */
+public interface WikiLinks {
+}
