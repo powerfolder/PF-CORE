a6e50409447fae1f1aa4c659f151956af173c17a
Christian Sprajc <sprajc@powerfolder.com>
2009-05-31 11:29:32 +0000

    Added todos

--- src/main/de/dal33t/powerfolder/disk/problem/Problem.java
+++ src/main/de/dal33t/powerfolder/disk/problem/Problem.java
@@ -1,31 +1,36 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: Problem.java 5483 2008-10-21 06:29:05Z harry $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: Problem.java 5483 2008-10-21 06:29:05Z harry $
+ */
 package de.dal33t.powerfolder.disk.problem;
 
 import java.util.Date;
 
 /**
  * Base of folder problems.
+ * <p>
+ * TODO Force implementation of {@link #equals(Object)} and
+ * {@link #equals(Object)} on interface impls.
  */
 public interface Problem {
     String getDescription();
+
     String getWikiLinkKey();
+
     Date getDate();
 }
