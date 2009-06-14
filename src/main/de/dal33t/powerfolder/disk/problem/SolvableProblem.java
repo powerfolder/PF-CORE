0ba8688f292745bc7fce36061f6ea383e4ce09bc
Harry Glasgow <glasgow@powerfolder.com>
2009-06-14 03:03:52 +0000

    Introduce concept of a solvable Problem class, refs #1578

---
+++ src/main/de/dal33t/powerfolder/disk/problem/SolvableProblem.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: Problem.java 5483 2008-10-21 06:29:05Z harry $
+ */
+package de.dal33t.powerfolder.disk.problem;
+
+/**
+ * Problem types that are solvable by PowerFolder.
+ */
+public interface SolvableProblem extends Problem {
+    Runnable solution();
+}
