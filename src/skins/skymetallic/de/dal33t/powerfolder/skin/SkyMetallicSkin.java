0a8fd2d779ea67f661b13229a5d6e3fab7485d8d
Harry Glasgow <glasgow@powerfolder.com>
2009-06-22 06:19:45 +0000

    copyright, refs #1320

--- src/skins/skymetallic/de/dal33t/powerfolder/skin/SkyMetallicSkin.java
+++ src/skins/skymetallic/de/dal33t/powerfolder/skin/SkyMetallicSkin.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: SkyMetallicSkin.java 8103 2009-05-27 23:56:11Z tot $
+ */
 package de.dal33t.powerfolder.skin;
 
 import de.javasoft.plaf.synthetica.SyntheticaSkyMetallicLookAndFeel;
