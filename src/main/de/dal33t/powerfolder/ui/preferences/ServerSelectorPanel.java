79fc991af88605843ca69d44ada05e0a44abda27
Christian Sprajc <sprajc@powerfolder.com>
2009-09-08 00:16:27 +0000

    refs  #1687 : Also cleanup of server selector

--- src/main/de/dal33t/powerfolder/ui/preferences/ServerSelectorPanel.java
+++ src/main/de/dal33t/powerfolder/ui/preferences/ServerSelectorPanel.java
@@ -19,49 +19,49 @@
  */
 package de.dal33t.powerfolder.ui.preferences;
 
-import com.jgoodies.binding.adapter.BasicComponentFactory;
-import com.jgoodies.binding.list.SelectionInList;
-import com.jgoodies.binding.value.ValueModel;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetSocketAddress;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
-import de.dal33t.powerfolder.Controller;
-import de.dal33t.powerfolder.Member;
-import de.dal33t.powerfolder.PFUIComponent;
-import de.dal33t.powerfolder.ui.Icons;
-import de.dal33t.powerfolder.ui.friends.FindComputersDialog;
-import de.dal33t.powerfolder.util.Reject;
-import de.dal33t.powerfolder.util.Translation;
-import de.dal33t.powerfolder.util.ui.UIPanel;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.PFUIComponent;
+import de.dal33t.powerfolder.net.ConnectionListener;
+import de.dal33t.powerfolder.ui.Icons;
+import de.dal33t.powerfolder.ui.widget.JButtonMini;
+import de.dal33t.powerfolder.util.ProUtil;
+import de.dal33t.powerfolder.util.Translation;
+import de.dal33t.powerfolder.util.net.NetworkUtil;
+import de.dal33t.powerfolder.util.ui.UIPanel;
 
 public class ServerSelectorPanel extends PFUIComponent implements UIPanel {
 
     private JPanel panel;
-    private JComboBox selectBox;
+    private JTextField addressField;
     private JButton searchButton;
-    private ValueModel serverModel;
 
-    ServerSelectorPanel(Controller controller, ValueModel serverModel) {
+    ServerSelectorPanel(Controller controller) {
         super(controller);
-        Reject.ifNull(serverModel, "Server model is null");
-        this.serverModel = serverModel;
     }
 
     public Component getUIComponent() {
         if (panel == null) {
             initComponent();
 
-            FormLayout layout = new FormLayout("0:grow", "pref, 3dlu, pref");
+            FormLayout layout = new FormLayout("0:grow, 3dlu, pref", "pref");
             PanelBuilder builder = new PanelBuilder(layout);
             CellConstraints cc = new CellConstraints();
 
-            builder.add(selectBox, cc.xy(1, 1));
-            builder.add(searchButton, cc.xy(1, 3));
+            builder.add(addressField, cc.xy(1, 1));
+            builder.add(searchButton, cc.xy(3, 1));
 
             panel = builder.getPanel();
         }
@@ -69,62 +69,22 @@ public class ServerSelectorPanel extends PFUIComponent implements UIPanel {
     }
 
     private void initComponent() {
-        SelectionInList<Member> selectModel = new SelectionInList<Member>(getUIController()
-                .getApplicationModel().getNodeManagerModel().getFriendsListModel(),
-                serverModel);
-        selectBox = BasicComponentFactory.createComboBox(selectModel);
-        selectBox.setRenderer(new MemberListCellRenderer());
-
-        searchButton = new JButton(Translation.getTranslation("general.search"));
+        InetSocketAddress addr = getController().getOSClient().getServer() != null
+            ? getController().getOSClient().getServer().getReconnectAddress()
+            : null;
+        String addrStr = addr != null ? NetworkUtil
+            .getHostAddressNoResolve(addr.getAddress()) : "n/a";
+        if (addr != null && addr.getPort() != ConnectionListener.DEFAULT_PORT) {
+            addrStr += ":" + addr.getPort();
+        }
+        addressField = new JTextField(addrStr);
+        addressField.setEditable(false);
+        searchButton = new JButtonMini(Icons.getIconById(Icons.EDIT),
+            Translation.getTranslation("general.search"));
         searchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                FindComputersDialog dialog = new FindComputersDialog(getController(),
-                    true);
-                dialog.open();
+                ProUtil.openConfigLoaderDialog(getController());
             }
         });
     }
-
-    private static class MemberListCellRenderer extends DefaultListCellRenderer {
-
-        public Component getListCellRendererComponent(JList list, Object value,
-            int index, boolean isSelected, boolean cellHasFocus)
-        {
-
-            Member member = (Member) value;
-            if (member == null) {
-                setText("- n/a -");
-                return this;
-            }
-            String newValue = member.getNick();
-            if (member.isMySelf()) {
-                newValue += " (" + Translation.getTranslation("member.me")
-                    + ')';
-            }
-            super.getListCellRendererComponent(list, newValue, index,
-                isSelected, cellHasFocus);
-            setIcon(Icons.getIconFor(member));
-            String tooltipText = "";
-
-            if (member.isMySelf()) {
-                tooltipText += Translation.getTranslation("member.me");
-            } else if (member.isFriend()) {
-                tooltipText += Translation
-                    .getTranslation("member.non_mutual_friend");
-            }
-
-            if (member.isSupernode()) {
-                tooltipText += " ("
-                    + Translation.getTranslation("member.supernode") + ')';
-            }
-            if (member.isOnLAN()) {
-                tooltipText += " ("
-                    + Translation.getTranslation("member.onlan") + ')';
-            }
-            setToolTipText(tooltipText);
-            return this;
-        }
-
-    }
-
 }
