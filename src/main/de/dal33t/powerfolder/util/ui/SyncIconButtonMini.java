be136a563d82c09f4163ef3cce8495221a56809d
Christian Sprajc <sprajc@powerfolder.com>
2009-09-18 20:50:34 +0000

    refs #1703

---
+++ src/main/de/dal33t/powerfolder/util/ui/SyncIconButtonMini.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2004 - 2009 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: ExpandableFolderView.java 5495 2008-10-24 04:59:13Z harry $
+ */
+package de.dal33t.powerfolder.util.ui;
+
+import java.awt.EventQueue;
+import java.util.TimerTask;
+
+import javax.swing.Action;
+import javax.swing.Icon;
+
+import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.ui.Icons;
+import de.dal33t.powerfolder.ui.widget.JButtonMini;
+
+/**
+ * Displays a rotating sync icon when visible.
+ * 
+ * @author sprajc
+ */
+public class SyncIconButtonMini extends JButtonMini {
+
+    private static final long ROTATION_STEP_DELAY = 100L;
+    private int state;
+
+    public SyncIconButtonMini(Controller controller) {
+        super(Icons.getIconById(Icons.SYNC_ANIMATION[0]), "");
+        state = 0;
+        controller.scheduleAndRepeat(new MyUpdateTask(), ROTATION_STEP_DELAY);
+    }
+
+    private void rotate() {
+        state++;
+        if (state >= Icons.SYNC_ANIMATION.length) {
+            state = 0;
+        }
+        Icon icon = Icons.getIconById(Icons.SYNC_ANIMATION[state]);
+        setIcon(icon);
+    }
+
+    private class MyUpdateTask extends TimerTask {
+        public void run() {
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    if (isVisible()) {
+                        rotate();
+                    }
+                }
+            });
+        }
+    }
+}
--- src/main/de/dal33t/powerfolder/util/ui/SyncProfileUtil.java
+++ src/main/de/dal33t/powerfolder/util/ui/SyncProfileUtil.java
@@ -1,27 +1,24 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util.ui;
 
-import javax.swing.Icon;
-
-import de.dal33t.powerfolder.ui.Icons;
 import de.dal33t.powerfolder.util.Format;
 import de.dal33t.powerfolder.util.Translation;
 
@@ -41,31 +38,10 @@ public class SyncProfileUtil {
      */
     public static final String renderSyncPercentage(double syncPercentage) {
         if (syncPercentage >= 0) {
-            return Translation.getTranslation("percent.place.holder",
-                    Format.getNumberFormat().format(syncPercentage));
+            return Translation.getTranslation("percent.place.holder", Format
+                .getNumberFormat().format(syncPercentage));
         }
         return Translation.getTranslation("percent.place.holder", "?");
     }
 
-    /**
-     * Returns the icon for the sync status percentage.
-     * 
-     * @param syncPercentage
-     *            in % between 0 and 100. Below 0 unkown sync status icons is
-     *            returned
-     * @return
-     */
-    public static Icon getSyncIcon(double syncPercentage) {
-        if (syncPercentage < 0) {
-            return Icons.getIconById(Icons.FOLDER_SYNC_UNKNOWN);
-        } else if (syncPercentage <= 20) {
-            return Icons.getIconById(Icons.FOLDER_SYNC_0);
-        } else if (syncPercentage <= 50) {
-            return Icons.getIconById(Icons.FOLDER_SYNC_1);
-        } else if (syncPercentage <= 80) {
-            return Icons.getIconById(Icons.FOLDER_SYNC_2);
-        } else {
-            return Icons.getIconById(Icons.FOLDER_SYNC_3);
-        }
-    }
 }
