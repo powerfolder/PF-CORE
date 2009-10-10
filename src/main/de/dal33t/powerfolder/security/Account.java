7a41f67f0fddfb279dff9b520bad775a74a5244a
Christian Sprajc <sprajc@powerfolder.com>
2009-10-10 15:02:01 +0000

    Corrected handling of folder owner

--- src/main/de/dal33t/powerfolder/security/Account.java
+++ src/main/de/dal33t/powerfolder/security/Account.java
@@ -484,10 +484,11 @@ public class Account extends Model implements Serializable {
         Reject.ifNull(controller, "Controller is null");
         int n = 0;
         for (Permission p : getPermissions()) {
-            if (!(p instanceof FolderAdminPermission)) {
+            if (!(p instanceof FolderAdminPermission || p instanceof FolderOwnerPermission))
+            {
                 continue;
             }
-            FolderAdminPermission fp = (FolderAdminPermission) p;
+            FolderPermission fp = (FolderPermission) p;
             Folder f = fp.getFolder().getFolder(controller);
             if (f == null) {
                 continue;
