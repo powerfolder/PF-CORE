29651d8507184630e9c43f27e4b300ca7254dce1
Christian Sprajc <sprajc@powerfolder.com>
2009-10-28 15:14:06 +0000

    refs #1797

--- src/main/de/dal33t/powerfolder/util/os/Win32/WinUtils.java
+++ src/main/de/dal33t/powerfolder/util/os/Win32/WinUtils.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util.os.Win32;
 
 import java.io.File;
@@ -28,9 +28,8 @@ import de.dal33t.powerfolder.util.logging.Loggable;
 import de.dal33t.powerfolder.util.os.OSUtil;
 
 /**
- * Utilities for windows.
- * http://vbnet.mvps.org/index.html?code/browse/csidl.htm
- *
+ * Utilities for windows. http://vbnet.mvps.org/index.html?code/browse/csidl.htm
+ * 
  * @author <A HREF="mailto:bytekeeper@powerfolder.com">Dennis Waldherr</A>
  * @version $Revision$
  */
@@ -73,15 +72,13 @@ public class WinUtils extends Loggable {
 
     // e.g. C:\Windows
     public static final int CSIDL_WINDOWS = 0x0024;
-    
-    /**
-     * Very common app data
-     */
-    public static final int CSIDL_COMMON_APPDATA = 0x0023;
 
-    // Program files
+    // Application Data of current user. Roaming and Local
     public static final int CSIDL_APP_DATA = 0x001A;
-    public static final int CSIDL_LOCAL_SETTINGS_APP_DATA = 0x001C;
+    public static final int CSIDL_LOCAL_APP_DATA = 0x001C;
+    
+    // Application Data of ALL USERs.
+    public static final int CSIDL_COMMON_APP_DATA = 0x0023;
 
     /*
      * Other CSLIDs:
@@ -92,39 +89,48 @@ public class WinUtils extends Loggable {
     private static boolean error = false;
 
     private WinUtils() {
-	}
+    }
 
-	public static synchronized WinUtils getInstance() {
-		if (instance == null && !error) {
-			if (OSUtil.loadLibrary(WinUtils.class, "desktoputils")) {
-				instance = new WinUtils();
-				instance.init();
-			} else {
-				error = true;
-			}
-		}
-		return instance;
-	}
+    public static synchronized WinUtils getInstance() {
+        if (instance == null && !error) {
+            if (OSUtil.loadLibrary(WinUtils.class, "desktoputils")) {
+                instance = new WinUtils();
+                instance.init();
+            } else {
+                error = true;
+            }
+        }
+        return instance;
+    }
 
-	/**
-	 * Retrieve a path from Windows.
-	 * @param id the path-id to retrieve
-	 * @param defaultPath if true, returns the default path location instead of the current
-	 * @return
-	 */
-	public native String getSystemFolderPath(int id, boolean defaultPath);
-	public native void createLink(ShellLink link, String lnkTarget) throws IOException;
-	private native void init();
+    /**
+     * Retrieve a path from Windows.
+     * 
+     * @param id
+     *            the path-id to retrieve
+     * @param defaultPath
+     *            if true, returns the default path location instead of the
+     *            current
+     * @return
+     */
+    public native String getSystemFolderPath(int id, boolean defaultPath);
+
+    public native void createLink(ShellLink link, String lnkTarget)
+        throws IOException;
+
+    private native void init();
 
     /**
      * Create a 'PowerFolders' link in Links, pointing to the PowerFolder base
      * dir.
+     * 
      * @param setup
      * @param baseDir
      * @throws IOException
      */
-    public void setPFFavorite(boolean setup, Controller controller) throws
-            IOException {
+    public void setPFFavorite(boolean setup, Controller controller)
+        throws IOException
+    {
         String userHome = System.getProperty("user.home");
         File linksDir = new File(userHome, "Links");
         if (!linksDir.exists()) {
@@ -134,41 +140,42 @@ public class WinUtils extends Loggable {
         File shortCut = new File(linksDir, "PowerFolders.lnk");
         if (setup) {
             File baseDir = new File(controller.getFolderRepository()
-                    .getFoldersBasedir());
-            ShellLink link = new ShellLink(null, "PowerFolders",
-                    baseDir.getAbsolutePath(), null);
+                .getFoldersBasedir());
+            ShellLink link = new ShellLink(null, "PowerFolders", baseDir
+                .getAbsolutePath(), null);
             createLink(link, shortCut.getAbsolutePath());
         } else {
             shortCut.delete();
         }
     }
 
-	public void setPFStartup(boolean setup) throws IOException {
-		File pfile = new File(
-			new File(System.getProperty("java.class.path")).getParentFile(),
-			"PowerFolder.exe");
-		if (!pfile.exists()) {
-			logSevere("Couldn't find PowerFolder executable! "
-					+ "Note: Setting up a shortcut only works "
-					+ "when PowerFolder was started by PowerFolder.exe");
-			return;
-		}
-		logFiner("Found " + pfile.getAbsolutePath());
-		File pflnk = new File(getSystemFolderPath(CSIDL_STARTUP, false), SHORTCUTNAME);
+    public void setPFStartup(boolean setup) throws IOException {
+        File pfile = new File(new File(System.getProperty("java.class.path"))
+            .getParentFile(), "PowerFolder.exe");
+        if (!pfile.exists()) {
+            logSevere("Couldn't find PowerFolder executable! "
+                + "Note: Setting up a shortcut only works "
+                + "when PowerFolder was started by PowerFolder.exe");
+            return;
+        }
+        logFiner("Found " + pfile.getAbsolutePath());
+        File pflnk = new File(getSystemFolderPath(CSIDL_STARTUP, false),
+            SHORTCUTNAME);
         if (setup) {
             ShellLink sl = new ShellLink("--minimized", Translation
-                    .getTranslation("winutils.shortcut.description"), pfile
-                    .getAbsolutePath(), pfile.getParent());
+                .getTranslation("winutils.shortcut.description"), pfile
+                .getAbsolutePath(), pfile.getParent());
             logFiner("Creating startup link: " + pflnk.getAbsolutePath());
             createLink(sl, pflnk.getAbsolutePath());
         } else {
             logFiner("Deleting startup link.");
             pflnk.delete();
         }
-	}
+    }
 
-	public boolean isPFStartup() {
-		File pflnk = new File(getSystemFolderPath(CSIDL_STARTUP, false), SHORTCUTNAME);
-		return pflnk.exists();
-	}
+    public boolean isPFStartup() {
+        File pflnk = new File(getSystemFolderPath(CSIDL_STARTUP, false),
+            SHORTCUTNAME);
+        return pflnk.exists();
+    }
 }
