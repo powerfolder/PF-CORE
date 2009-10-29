54cf89978a61eafe65900545db4b7cc46453c578
Christian Sprajc <sprajc@powerfolder.com>
2009-10-29 16:19:04 +0000

    refs #1799

---
+++ src/test/de/dal33t/powerfolder/test/util/ConfigurationLoadTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2004 - 2009 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: CleanupTranslationFiles.java 4282 2008-06-16 03:25:09Z tot $
+ */
+package de.dal33t.powerfolder.test.util;
+
+import de.dal33t.powerfolder.ConfigurationEntry;
+import de.dal33t.powerfolder.Member;
+import de.dal33t.powerfolder.NetworkingMode;
+import de.dal33t.powerfolder.message.ConfigurationLoadRequest;
+import de.dal33t.powerfolder.net.ConnectionException;
+import de.dal33t.powerfolder.util.test.ConditionWithMessage;
+import de.dal33t.powerfolder.util.test.TestHelper;
+import de.dal33t.powerfolder.util.test.TwoControllerTestCase;
+
+/**
+ * Test the loading of the config.
+ * 
+ * @author sprajc
+ */
+public class ConfigurationLoadTest extends TwoControllerTestCase {
+    private static final String TEST_CONFIG_URL = "download.powerfolder.com/development/junit";
+    private Member lisaAtBart;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        makeFriends();
+        connectBartAndLisa();
+        Member bartAtLisa = getContollerLisa().getNodeManager().getNode(
+            getContollerBart().getMySelf().getInfo());
+        bartAtLisa.setServer(true);
+
+        lisaAtBart = getContollerBart().getNodeManager().getNode(
+            getContollerLisa().getMySelf().getInfo());
+    }
+
+    public void testRestartOnConfigReload() throws ConnectionException {
+        ConfigurationLoadRequest r = new ConfigurationLoadRequest(
+            TEST_CONFIG_URL, false, true);
+        lisaAtBart.sendMessage(r);
+
+        TestHelper.waitForCondition(10, new ConditionWithMessage() {
+            public String message() {
+                return "Lisa started? " + getContollerLisa().isStarted();
+            }
+
+            public boolean reached() {
+                return !getContollerLisa().isStarted()
+                    && getContollerLisa().isRestartRequested();
+            }
+        });
+    }
+
+    public void testReloadConfig() throws ConnectionException {
+        getContollerLisa().setNetworkingMode(NetworkingMode.LANONLYMODE);
+
+        ConfigurationLoadRequest r = new ConfigurationLoadRequest("http://"
+            + TEST_CONFIG_URL, false, false);
+        lisaAtBart.sendMessage(r);
+
+        TestHelper.waitMilliSeconds(1000);
+        assertEquals(NetworkingMode.LANONLYMODE.toString(),
+            ConfigurationEntry.NETWORKING_MODE.getValue(getContollerLisa()));
+        // Non existing should have been added.
+        assertTrue(ConfigurationEntry.USER_INTERFACE_LOCKED
+            .getValueBoolean(getContollerLisa()));
+
+        // Now overwrite values.
+        r = new ConfigurationLoadRequest(TEST_CONFIG_URL + "/", true, false);
+        lisaAtBart.sendMessage(r);
+
+        TestHelper.waitForCondition(10, new ConditionWithMessage() {
+            public String message() {
+                return "Lisas networking mode: "
+                    + ConfigurationEntry.NETWORKING_MODE
+                        .getValue(getContollerLisa());
+            }
+
+            public boolean reached() {
+                return ConfigurationEntry.NETWORKING_MODE.getValue(
+                    getContollerLisa()).equals(
+                    NetworkingMode.SERVERONLYMODE.toString());
+            }
+        });
+    }
+
+}
