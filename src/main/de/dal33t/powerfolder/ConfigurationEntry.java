5599e0b81d8a6d5f3b0a769db9ba7b708ef6e5a4
Christian Sprajc <sprajc@powerfolder.com>
2009-11-06 13:51:03 +0000

    Replaced email address

--- src/main/de/dal33t/powerfolder/ConfigurationEntry.java
+++ src/main/de/dal33t/powerfolder/ConfigurationEntry.java
@@ -38,7 +38,7 @@ import de.dal33t.powerfolder.util.os.Win32.WinUtils;
  * Refelects a entry setting in the configuration file. Provides basic method
  * for accessing and setting the configuration.
  * 
- * @author <a href="mailto:sprajc@riege.com">Christian Sprajc</a>
+ * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
  * @author Dennis "Bytekeeper" Waldherr
  * @version $Revision: 1.5 $
  */
