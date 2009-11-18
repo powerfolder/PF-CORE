5a9a177c21642eee96ecbdee5bcd63ccea4d8c51
Christian Sprajc <sprajc@powerfolder.com>
2009-11-18 14:43:27 +0000

    refs #1823

--- src/main/de/dal33t/powerfolder/transfer/TransferManager.java
+++ src/main/de/dal33t/powerfolder/transfer/TransferManager.java
@@ -1191,11 +1191,14 @@ public class TransferManager extends PFComponent {
         boolean fileInSyncWithDisk = diskFile != null
             && dl.file.inSyncWithDisk(diskFile);
         if (!fileInSyncWithDisk) {
-            // This should free up an otherwise waiting for download partner
-            folder.recommendScanOnNextMaintenance();
             logWarning("File not in sync with disk: '"
                 + dl.file.toDetailString() + "', should be modified at "
                 + diskFile.lastModified());
+            // This should free up an otherwise waiting for download partner
+            if (folder.scanAllowedNow()) {
+                folder.scanChangedFile(dl.file);
+            }
+            // folder.recommendScanOnNextMaintenance();
             return null;
         }
 
