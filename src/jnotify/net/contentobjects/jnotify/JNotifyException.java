be2c5d2c1cebc24d6935f21c86e5afd5ae37543d
Christian Sprajc <sprajc@powerfolder.com>
2009-11-19 13:09:08 +0000

    refs #711

---
+++ src/jnotify/net/contentobjects/jnotify/JNotifyException.java
@@ -0,0 +1,27 @@
+package net.contentobjects.jnotify;
+
+import java.io.IOException;
+
+public abstract class JNotifyException extends IOException
+{
+	public final static int ERROR_UNSPECIFIED = 1;
+	public final static int ERROR_WATCH_LIMIT_REACHED = 2;
+	public final static int ERROR_PERMISSION_DENIED = 3;
+	public final static int ERROR_NO_SUCH_FILE_OR_DIRECTORY = 4;
+	
+	
+	protected final int _systemErrorCode;
+	
+	public JNotifyException(String s, int systemErrorCode)
+	{
+		super(s);
+		_systemErrorCode = systemErrorCode;
+	}
+	
+	public int getSystemError()
+	{
+		return _systemErrorCode;
+	}
+	
+	public abstract int getErrorCode();
+}
