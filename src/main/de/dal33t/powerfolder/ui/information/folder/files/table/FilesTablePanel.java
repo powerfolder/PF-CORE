0083994f8487207cedc3e63291433044204cdf75
Christian Sprajc <sprajc@powerfolder.com>
2009-11-30 15:34:55 +0000

    minor

--- src/main/de/dal33t/powerfolder/ui/information/folder/files/table/FilesTablePanel.java
+++ src/main/de/dal33t/powerfolder/ui/information/folder/files/table/FilesTablePanel.java
@@ -19,13 +19,27 @@
  */
 package de.dal33t.powerfolder.ui.information.folder.files.table;
 
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-import javax.swing.*;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
@@ -39,8 +53,8 @@ import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
 import de.dal33t.powerfolder.Controller;
-import de.dal33t.powerfolder.PFUIComponent;
 import de.dal33t.powerfolder.DiskItem;
+import de.dal33t.powerfolder.PFUIComponent;
 import de.dal33t.powerfolder.disk.Directory;
 import de.dal33t.powerfolder.disk.Folder;
 import de.dal33t.powerfolder.disk.FolderRepository;
@@ -48,8 +62,6 @@ import de.dal33t.powerfolder.light.FileInfo;
 import de.dal33t.powerfolder.transfer.DownloadManager;
 import de.dal33t.powerfolder.transfer.TransferManager;
 import de.dal33t.powerfolder.ui.Icons;
-import de.dal33t.powerfolder.ui.wizard.PFWizard;
-import de.dal33t.powerfolder.ui.wizard.MultiFileRestorePanel;
 import de.dal33t.powerfolder.ui.action.BaseAction;
 import de.dal33t.powerfolder.ui.information.HasDetailsPanel;
 import de.dal33t.powerfolder.ui.information.folder.files.DirectoryFilterListener;
@@ -60,6 +72,8 @@ import de.dal33t.powerfolder.ui.information.folder.files.FilteredDirectoryModel;
 import de.dal33t.powerfolder.ui.information.folder.files.tree.DirectoryTreeNodeUserObject;
 import de.dal33t.powerfolder.ui.information.folder.files.versions.FileVersionsPanel;
 import de.dal33t.powerfolder.ui.widget.ActivityVisualizationWorker;
+import de.dal33t.powerfolder.ui.wizard.MultiFileRestorePanel;
+import de.dal33t.powerfolder.ui.wizard.PFWizard;
 import de.dal33t.powerfolder.util.FileUtils;
 import de.dal33t.powerfolder.util.Translation;
 import de.dal33t.powerfolder.util.os.OSUtil;
@@ -298,10 +312,9 @@ public class FilesTablePanel extends PFUIComponent implements HasDetailsPanel,
                             folder.removeFilesLocal(fileInfo);
                         } else if (diskItem instanceof Directory) {
                             Directory directory = (Directory) diskItem;
-                            List<FileInfo> fileInfoList = directory.getFileInfos();
+                            Collection<FileInfo> fileInfoList = directory.getFileInfos();
                             Folder folder = directory.getRootFolder();
-                            folder.removeFilesLocal(fileInfoList.toArray(
-                                    new FileInfo[fileInfoList.size()]));
+                            folder.removeFilesLocal(fileInfoList);
                         }
                     } catch (Exception e) {
                         logSevere(e);
