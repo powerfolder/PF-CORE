6901e3c0dc20399a525c9cb7a50443bcf0cd2968
Christian Sprajc <sprajc@powerfolder.com>
2009-11-30 14:19:37 +0000

    refs #711

--- src/test/de/dal33t/powerfolder/test/transfer/FileTransferTest.java
+++ src/test/de/dal33t/powerfolder/test/transfer/FileTransferTest.java
@@ -433,8 +433,8 @@ public class FileTransferTest extends TwoControllerTestCase {
 
     public void testMultipleFilesCopyWithFolderWatcher() {
         // Register listeners
-        ConfigurationEntry.WATCH_FILESYSTEM.setValue(getContollerLisa(), true);
-        ConfigurationEntry.WATCH_FILESYSTEM.setValue(getContollerBart(), true);
+        ConfigurationEntry.FOLDER_WATCH_FILESYSTEM.setValue(getContollerLisa(), true);
+        ConfigurationEntry.FOLDER_WATCH_FILESYSTEM.setValue(getContollerBart(), true);
         LoggingManager.setConsoleLogging(Level.WARNING);
         getFolderAtBart().setSyncProfile(
             SyncProfile.AUTOMATIC_SYNCHRONIZATION_10MIN);
