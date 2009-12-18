13146e8f8fcc3f7b638a2b0c3ad454ac6073dc6d
Christian Sprajc <sprajc@powerfolder.com>
2009-12-18 00:39:12 +0000

    refs #1826 Completely exctracted overall sync calculation into FolderRepoModel. Also using a more heuristic approach to detect "in sync" state: Simple is in sync when no synchronization action is running (upload, download or scan)

--- src/main/de/dal33t/powerfolder/event/OverallFolderStatEvent.java
+++ src/main/de/dal33t/powerfolder/event/OverallFolderStatEvent.java
@@ -1,50 +1,38 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: OverallFolderStatEvent.java 5457 2008-10-17 14:25:41Z harry $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: OverallFolderStatEvent.java 5457 2008-10-17 14:25:41Z harry $
+ */
 package de.dal33t.powerfolder.event;
 
-import java.util.Date;
 
 /**
  * Event with overall stats about the state of folders.
  */
 public class OverallFolderStatEvent {
 
-    /** True if all folders are in sync. */
-    private boolean allInSync;
+    /** True if any folder is synchronizing. */
+    private boolean synchronizing;
 
-    /**
-     * Date all folders were last in sync
-     * OR date when all folders are expected to be synchronized.
-     */
-    private Date syncDate;
-
-    public OverallFolderStatEvent(boolean allInSync, Date syncDate) {
-        this.allInSync = allInSync;
-        this.syncDate = syncDate;
+    public OverallFolderStatEvent(boolean synchronizing) {
+        this.synchronizing = synchronizing;
     }
 
-    public boolean isAllInSync() {
-        return allInSync;
-    }
-
-    public Date getSyncDate() {
-        return syncDate;
+    public boolean isSyncing() {
+        return synchronizing;
     }
 }
