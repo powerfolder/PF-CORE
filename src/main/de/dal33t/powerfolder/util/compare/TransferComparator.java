b2304d585892e45e3e6d2517a46eff935aba49d8
Christian Sprajc <sprajc@powerfolder.com>
2009-12-19 05:23:28 +0000

    refs #1866 Considering completed date

--- src/main/de/dal33t/powerfolder/util/compare/TransferComparator.java
+++ src/main/de/dal33t/powerfolder/util/compare/TransferComparator.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util.compare;
 
 import de.dal33t.powerfolder.transfer.Transfer;
@@ -25,7 +25,7 @@ import java.util.Comparator;
 
 /**
  * Comparator for FileInfo
- *
+ * 
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
  * @version $Revision: 1.15 $
  */
@@ -55,7 +55,13 @@ public class TransferComparator implements Comparator<Transfer> {
                 return o1.getFile().getFilenameOnly().compareToIgnoreCase(
                     o2.getFile().getFilenameOnly());
             case BY_PROGRESS :
-                return o1.getState().compareTo(o2.getState());
+                int comp = o1.getState().compareTo(o2.getState());
+                if (comp == 0 && o1.getCompletedDate() != null
+                    && o2.getCompletedDate() != null)
+                {
+                    return o1.getCompletedDate().compareTo(
+                        o2.getCompletedDate());
+                }
             case BY_SIZE :
                 long s1 = o1.getFile().getSize();
                 long s2 = o2.getFile().getSize();
@@ -65,11 +71,11 @@ public class TransferComparator implements Comparator<Transfer> {
                     return s1 - s2 > 0 ? 1 : -1;
                 }
             case BY_FOLDER :
-                return o1.getFile().getFolderInfo().name.compareTo(
-                        o2.getFile().getFolderInfo().name);
+                return o1.getFile().getFolderInfo().name.compareTo(o2.getFile()
+                    .getFolderInfo().name);
             case BY_MEMBER :
                 return o1.getPartner().getNick().compareTo(
-                        o2.getPartner().getNick());
+                    o2.getPartner().getNick());
         }
         return 0;
     }
