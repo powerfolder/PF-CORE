8353dce76a48472f6f3840dad7edb91a49a5f169
Christian Sprajc <sprajc@powerfolder.com>
2009-12-20 22:33:47 +0000

    refs #1866 todo

--- src/main/de/dal33t/powerfolder/security/SecurityManagerClient.java
+++ src/main/de/dal33t/powerfolder/security/SecurityManagerClient.java
@@ -176,6 +176,7 @@ public class SecurityManagerClient extends PFComponent implements
         }
         AccountInfo aInfo;
         try {
+            // TODO Check if really required
             synchronized (requestLock) {
                 // After we are request lock owner. Check if other thread
                 // probably has refreshed the session we are looking for.
