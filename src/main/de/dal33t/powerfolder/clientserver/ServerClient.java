fee9fe757d56deb5c1530164af234ccf8990ccce
Christian Sprajc <sprajc@powerfolder.com>
2010-01-25 13:53:31 +0000

    Reduced debug load

--- src/main/de/dal33t/powerfolder/clientserver/ServerClient.java
+++ src/main/de/dal33t/powerfolder/clientserver/ServerClient.java
@@ -792,7 +792,7 @@ public class ServerClient extends PFComponent {
     }
 
     private boolean setServerHTTPTunnelURLInConfig(String newTunnelURL) {
-        logWarning("New tunnel URL: " + newTunnelURL);
+        logFine("New tunnel URL: " + newTunnelURL);
         String oldUrl = ConfigurationEntry.SERVER_HTTP_TUNNEL_RPC_URL
             .getValue(getController());
         if (Util.equals(oldUrl, newTunnelURL)) {
