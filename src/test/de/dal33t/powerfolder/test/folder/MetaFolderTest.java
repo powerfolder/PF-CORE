1b79fa4273006d354f87b975e894d8a3d36b654b
Harry Glasgow <glasgow@powerfolder.com>
2010-04-15 13:11:26 +0000

    MetaFolder test case, refs #1548

--- src/test/de/dal33t/powerfolder/test/folder/MetaFolderTest.java
+++ src/test/de/dal33t/powerfolder/test/folder/MetaFolderTest.java
@@ -24,8 +24,10 @@ import java.io.File;
 import de.dal33t.powerfolder.disk.Folder;
 import de.dal33t.powerfolder.disk.SyncProfile;
 import de.dal33t.powerfolder.util.test.TwoControllerTestCase;
+import de.dal33t.powerfolder.util.test.TestHelper;
 import de.dal33t.powerfolder.Feature;
 import de.dal33t.powerfolder.Constants;
+import de.dal33t.powerfolder.Controller;
 
 /**
  * Test cases for MetaFolder synchronization.
@@ -43,7 +45,7 @@ public class MetaFolderTest extends TwoControllerTestCase {
      */
     public void testMetaFolderSync() {
         if (Feature.META_FOLDER.isEnabled()) {
-            joinTestFolder(SyncProfile.MANUAL_SYNCHRONIZATION);
+            joinTestFolder(SyncProfile.AUTOMATIC_SYNCHRONIZATION);
 
             Folder bartFolder = getFolderAtBart();
 
@@ -60,13 +62,53 @@ public class MetaFolderTest extends TwoControllerTestCase {
 
             // Check the mata folder was created.
             localBase = lisaFolder.getLocalBase();
-            systemSubdir = new File(localBase, Constants.POWERFOLDER_SYSTEM_SUBDIR);
-            assertTrue("lisa system subdir does not exists", systemSubdir.exists());
-            metaFolderDir = new File(systemSubdir, Constants.METAFOLDER_SUBDIR);
-            assertTrue("lisa metaFolder dir does not exists", metaFolderDir.exists());
-            metaFolderSystemSubdir = new File(metaFolderDir, Constants.POWERFOLDER_SYSTEM_SUBDIR);
-            assertTrue("lisa metaFolder systemsubdir does not exists", metaFolderSystemSubdir.exists());
+            systemSubdir = new File(localBase,
+                    Constants.POWERFOLDER_SYSTEM_SUBDIR);
+            assertTrue("lisa system subdir does not exists",
+                    systemSubdir.exists());
+            metaFolderDir = new File(systemSubdir,
+                    Constants.METAFOLDER_SUBDIR);
+            assertTrue("lisa metaFolder dir does not exists",
+                    metaFolderDir.exists());
+            metaFolderSystemSubdir = new File(metaFolderDir,
+                    Constants.POWERFOLDER_SYSTEM_SUBDIR);
+            assertTrue("lisa metaFolder systemsubdir does not exists",
+                    metaFolderSystemSubdir.exists());
 
+            // Check folders are in repo
+            Controller contollerBart = getContollerBart();
+            Folder bartMetaFolder = contollerBart.getFolderRepository()
+                    .getMetaFolder(bartFolder.getInfo());
+            assertNotNull("No bart meta folder", bartMetaFolder);
+
+            Folder lisaMetaFolder = contollerBart.getFolderRepository()
+                    .getMetaFolder(lisaFolder.getInfo());
+            assertNotNull("No lisa meta folder", lisaMetaFolder);
+
+            // Check sync between bart and lisa still works.
+            int lisaOriginalCount = lisaFolder.getKnownFiles().size();
+            TestHelper.createRandomFile(bartFolder.getLocalBase(),
+                    "TestFile.txt");
+            scanFolder(bartFolder);
+            TestHelper.waitMilliSeconds(1000);
+            assertEquals("lisa file count wrong", lisaOriginalCount + 1,
+                    lisaFolder.getKnownFiles().size());
+            Controller contollerLisa = getContollerLisa();
+            assertTrue("lisa file does not exist", lisaFolder.getKnownFiles()
+                    .iterator().next().diskFileExists(contollerLisa));
+
+            // Check sync between bart and lisa metafolders works.
+            int lisaOriginalMetaCount = lisaMetaFolder.getKnownFiles().size();
+            TestHelper.createRandomFile(bartMetaFolder.getLocalBase(),
+                    "MetaTestFile.txt");
+            scanFolder(bartMetaFolder);
+            TestHelper.waitMilliSeconds(1000);
+            assertEquals("lisa metafolder file count wrong",
+                    lisaOriginalMetaCount + 1,
+                    lisaMetaFolder.getKnownFiles().size());
+            assertTrue("lisa metafolder file does not exist",
+                    lisaMetaFolder.getKnownFiles().iterator().next()
+                            .diskFileExists(contollerLisa));
         }
     }
 }
