fcc9c94db0e2bb6879057cc6f34d62534085e92d
Christian Sprajc <sprajc@powerfolder.com>
2010-04-20 11:54:26 +0000

    HotFix for server

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -131,7 +131,7 @@ public class Controller extends PFComponent {
     /**
      * program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "4.2.2 - 1.7.0.5"; // 1.7.0.4 / RC2
+    public static final String PROGRAM_VERSION = "4.2.2 - 1.7.0.6"; // 1.7.0.6 / RC2
     /**
      * the (java beans like) property, listen to changes of the networking mode
      * by calling addPropertyChangeListener with this as parameter
