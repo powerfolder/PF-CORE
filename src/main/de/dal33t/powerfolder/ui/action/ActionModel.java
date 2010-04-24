79443a4c9c7039f72b4e71acdc3ad19f409cf0be
Christian Sprajc <sprajc@powerfolder.com>
2010-04-24 02:10:03 +0000

    refs #1979 client permissions

--- src/main/de/dal33t/powerfolder/ui/action/ActionModel.java
+++ src/main/de/dal33t/powerfolder/ui/action/ActionModel.java
@@ -1,35 +1,34 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: ActionModel.java 5419 2008-09-29 12:18:20Z harry $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: ActionModel.java 5419 2008-09-29 12:18:20Z harry $
+ */
 package de.dal33t.powerfolder.ui.action;
 
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.PFComponent;
+import de.dal33t.powerfolder.security.ChangePreferencesPermission;
+import de.dal33t.powerfolder.security.FolderCreatePermission;
 
 /**
  * Holder of all simple singleton actions.
  */
 public class ActionModel extends PFComponent {
 
-    /**
-     * Constructor.
-     */
     public ActionModel(Controller controller) {
         super(controller);
     }
@@ -50,6 +49,7 @@ public class ActionModel extends PFComponent {
     public NewFolderAction getNewFolderAction() {
         if (newFolderAction == null) {
             newFolderAction = new NewFolderAction(getController());
+            newFolderAction.allowWith(FolderCreatePermission.INSTANCE);
         }
         return newFolderAction;
     }
@@ -64,20 +64,24 @@ public class ActionModel extends PFComponent {
     public OpenPreferencesAction getOpenPreferencesAction() {
         if (openPreferencesAction == null) {
             openPreferencesAction = new OpenPreferencesAction(getController());
+            openPreferencesAction
+                .allowWith(ChangePreferencesPermission.INSTANCE);
         }
         return openPreferencesAction;
     }
 
     public OpenDownloadsInformationAction getOpenDownloadsInformationAction() {
         if (openDownloadsInformationAction == null) {
-            openDownloadsInformationAction = new OpenDownloadsInformationAction(getController());
+            openDownloadsInformationAction = new OpenDownloadsInformationAction(
+                getController());
         }
         return openDownloadsInformationAction;
     }
 
     public OpenUploadsInformationAction getOpenUploadsInformationAction() {
         if (openUploadsInformationAction == null) {
-            openUploadsInformationAction = new OpenUploadsInformationAction(getController());
+            openUploadsInformationAction = new OpenUploadsInformationAction(
+                getController());
         }
         return openUploadsInformationAction;
     }
@@ -96,12 +100,12 @@ public class ActionModel extends PFComponent {
         return connectAction;
     }
 
-    public OpenInvitationReceivedWizardAction
-    getOpenInvitationReceivedWizardAction() {
+    public OpenInvitationReceivedWizardAction getOpenInvitationReceivedWizardAction()
+    {
 
         if (openInvitationReceivedWizardAction == null) {
-            openInvitationReceivedWizardAction =
-                    new OpenInvitationReceivedWizardAction(getController());
+            openInvitationReceivedWizardAction = new OpenInvitationReceivedWizardAction(
+                getController());
         }
         return openInvitationReceivedWizardAction;
     }
@@ -122,14 +126,16 @@ public class ActionModel extends PFComponent {
 
     public OpenDebugInformationAction getOpenDebugInformationAction() {
         if (openDebugInformationAction == null) {
-        	openDebugInformationAction = new OpenDebugInformationAction(getController());
+            openDebugInformationAction = new OpenDebugInformationAction(
+                getController());
         }
         return openDebugInformationAction;
     }
 
     public SingleFileTransferOfferAction getSingleFileTransferOfferAction() {
         if (singleFileTransferOfferAction == null) {
-        	singleFileTransferOfferAction = new SingleFileTransferOfferAction(getController());
+            singleFileTransferOfferAction = new SingleFileTransferOfferAction(
+                getController());
         }
         return singleFileTransferOfferAction;
     }
