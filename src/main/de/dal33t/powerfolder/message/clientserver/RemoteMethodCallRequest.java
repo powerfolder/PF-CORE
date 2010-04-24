8653e0359ead69e63c597b6eff884b116ea1d5c6
Christian Sprajc <sprajc@powerfolder.com>
2010-04-24 02:07:32 +0000

    refs #1987 RPC

--- src/main/de/dal33t/powerfolder/message/clientserver/RemoteMethodCallRequest.java
+++ src/main/de/dal33t/powerfolder/message/clientserver/RemoteMethodCallRequest.java
@@ -1,25 +1,30 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.message.clientserver;
 
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.Serializable;
 import java.lang.reflect.Method;
+
+import de.dal33t.powerfolder.util.ByteSerializer;
 
 /**
  * Request to execute a method on a service registred in the
@@ -35,6 +40,7 @@ public class RemoteMethodCallRequest extends Request {
     private String methodName;
     private Class<?>[] methodParamTypes;
     private Object[] args;
+    private byte[][] argsSerialized;
 
     public RemoteMethodCallRequest(String serviceId, Method method,
         Object... args)
@@ -46,6 +52,38 @@ public class RemoteMethodCallRequest extends Request {
         this.args = args;
     }
 
+    /**
+     * @return the
+     * @throws IOException
+     */
+    public RemoteMethodCallRequest toSerzializedForm() throws IOException {
+        if (args == null) {
+            argsSerialized = null;
+            return this;
+        }
+        if (args.length == 0) {
+            argsSerialized = new byte[0][0];
+            return this;
+        }
+        argsSerialized = new byte[args.length][0];
+        for (int i = 0; i < args.length; i++) {
+            Object arg = args[i];
+            if (arg == null) {
+                argsSerialized[i] = null;
+                continue;
+            }
+            if (arg instanceof Serializable) {
+                argsSerialized[i] = ByteSerializer.serializeStatic(
+                    (Serializable) arg, false);
+            } else {
+                throw new NotSerializableException(
+                    "Argument not serializable: " + arg);
+            }
+        }
+        args = null;
+        return this;
+    }
+
     public String getServiceId() {
         return serviceId;
     }
@@ -53,17 +91,33 @@ public class RemoteMethodCallRequest extends Request {
     public Method getMethod(Object target) throws NoSuchMethodException {
         return target.getClass().getMethod(methodName, methodParamTypes);
     }
-    
+
     public String getMethodName() {
         return methodName;
     }
 
-    public Object[] getArgs() {
+    public Class<?>[] getArgsTypes() {
+        return methodParamTypes;
+    }
+
+    public Object[] getArgs() throws IOException, ClassNotFoundException {
+        if (args == null && argsSerialized != null) {
+            args = new Object[argsSerialized.length];
+            for (int i = 0; i < argsSerialized.length; i++) {
+                byte[] buf = argsSerialized[i];
+                if (buf == null) {
+                    args[i] = null;
+                    continue;
+                }
+                args[i] = ByteSerializer.deserializeStatic(buf, false);
+            }
+        }
         return args;
     }
 
     public String toString() {
         return "RemoteCall on '" + serviceId + "', Method " + methodName
-            + ", args " + (args != null ? args.length : "n/a");
+            + ", args "
+            + (methodParamTypes != null ? methodParamTypes.length : "n/a");
     }
 }
