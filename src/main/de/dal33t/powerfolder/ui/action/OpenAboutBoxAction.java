845a68e00c4e43f3143ad21d746b5c7e06220906
Christian Sprajc <sprajc@powerfolder.com>
2010-05-07 23:54:17 +0000

    refs #2002

--- src/main/de/dal33t/powerfolder/ui/action/OpenAboutBoxAction.java
+++ src/main/de/dal33t/powerfolder/ui/action/OpenAboutBoxAction.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.ui.action;
 
 import de.dal33t.powerfolder.Controller;
@@ -29,8 +29,7 @@ import java.awt.event.ActionEvent;
  * 
  * @author <a href=mailto:xsj@users.sourceforge.net">Daniel Harabor</a>
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc </a>
- * 
- * @version 1.0 	Last Modified: 10/04/05
+ * @version 1.0 Last Modified: 10/04/05
  */
 public class OpenAboutBoxAction extends BaseAction {
     public OpenAboutBoxAction(Controller controller) {
@@ -38,7 +37,6 @@ public class OpenAboutBoxAction extends BaseAction {
     }
 
     public void actionPerformed(ActionEvent e) {
-        AboutDialog aboutbox = new AboutDialog(getController());
-        aboutbox.open();
+        new AboutDialog(getController()).open();
     }
 }
