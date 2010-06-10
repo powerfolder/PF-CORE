5eeb78bc5ff939e1fcf71f9631d7316598dbd943
Christian Sprajc <sprajc@powerfolder.com>
2010-06-10 17:21:25 +0000

    refs #1422

---
+++ src/test/de/dal33t/powerfolder/test/folder/dao/FileInfoDAOHashTest.java
@@ -0,0 +1,48 @@
+package de.dal33t.powerfolder.test.folder.dao;
+
+import java.util.logging.Level;
+
+import de.dal33t.powerfolder.disk.DiskItemFilter;
+import de.dal33t.powerfolder.disk.dao.FileInfoDAO;
+import de.dal33t.powerfolder.disk.dao.FileInfoDAOHashMapImpl;
+import de.dal33t.powerfolder.util.logging.LoggingManager;
+import de.dal33t.powerfolder.util.test.TestHelper;
+
+public class FileInfoDAOHashTest extends FileInfoDAOTestCase {
+    private FileInfoDAO dao;
+    private DiskItemFilter filter;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        TestHelper.cleanTestDir();
+        filter = new DiskItemFilter();
+        dao = new FileInfoDAOHashMapImpl("ME", filter);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        dao.stop();
+        super.tearDown();
+    }
+
+    public void testStats() {
+        testStats(dao, filter, 1);
+        testStats(dao, filter, 1000);
+        testStats(dao, filter, 5000);
+    }
+
+    public void testIndexFileInfo() {
+        testIndexFileInfo(dao);
+    }
+
+    public void testFindNewestVersion() {
+        testFindNewestVersion(dao);
+    }
+
+    public void testFindAll() {
+        LoggingManager.setConsoleLogging(Level.SEVERE);
+        testFindAll(dao, 5000);
+    }
+
+}
