e3935b16fa9339b8d641146dc0c748e4342b90ca
Christian Sprajc <sprajc@powerfolder.com>
2010-06-14 14:55:20 +0000

    refs #1521

--- src/test/de/dal33t/powerfolder/test/folder/SendFileListTest.java
+++ src/test/de/dal33t/powerfolder/test/folder/SendFileListTest.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
+ */
 package de.dal33t.powerfolder.test.folder;
 
 import java.util.ArrayList;
@@ -42,8 +42,7 @@ public class SendFileListTest extends TwoControllerTestCase {
     private MyMessageListener lisasListener;
 
     @Override
-    protected void setUp() throws Exception
-    {
+    protected void setUp() throws Exception {
         super.setUp();
         connectBartAndLisa();
         makeFriends();
@@ -59,7 +58,8 @@ public class SendFileListTest extends TwoControllerTestCase {
                 return !lisasListener.messages.isEmpty();
             }
         });
-        assertEquals(1, lisasListener.messages.size());
+        assertEquals("Received: " + lisasListener.messages, 1,
+            lisasListener.messages.size());
         assertTrue(lisasListener.messages.get(0) instanceof FileList);
         FileList list = (FileList) lisasListener.messages.get(0);
         assertEquals(0, list.nFollowingDeltas);
