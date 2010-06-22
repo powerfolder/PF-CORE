885191d0e675807deafc37781065f1f099d60488
Christian Sprajc <sprajc@powerfolder.com>
2010-06-22 09:11:47 +0000

    PowerFolder server = WEBSERVICE ID

--- src/main/de/dal33t/powerfolder/distribution/AbstractDistribution.java
+++ src/main/de/dal33t/powerfolder/distribution/AbstractDistribution.java
@@ -72,12 +72,12 @@ public abstract class AbstractDistribution extends Loggable implements
      *         (non inhouse server).
      */
     public static boolean isPowerFolderServer(Controller c) {
-        String host = ConfigurationEntry.SERVER_HOST.getValue(c);
-        if (host != null) {
-            if (host.toLowerCase().contains("powerfolder.com")) {
-                return true;
-            }
-        }
+        // String host = ConfigurationEntry.SERVER_HOST.getValue(c);
+        // if (host != null) {
+        // if (host.toLowerCase().contains("powerfolder.com")) {
+        // return true;
+        // }
+        // }
         String nodeId = ConfigurationEntry.SERVER_NODEID.getValue(c);
         if (nodeId != null) {
             if (nodeId.toLowerCase().contains("WEBSERVICE")) {
