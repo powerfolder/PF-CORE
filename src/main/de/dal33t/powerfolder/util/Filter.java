e08a4d2bbfc483f034f45b8ca7728b23bd2070a7
Christian Sprajc <sprajc@powerfolder.com>
2010-06-24 13:33:11 +0000

    refs #2038

---
+++ src/main/de/dal33t/powerfolder/util/Filter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2004 - 2010 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: NodeManager.java 12576 2010-06-14 14:28:23Z tot $
+ */
+package de.dal33t.powerfolder.util;
+
+/**
+ * General filter for items
+ * 
+ * @author sprajc
+ * @param <T>
+ *            the item class
+ */
+public interface Filter<T> {
+    /**
+     * @param item
+     * @return if the item should be accepted
+     */
+    boolean accept(T item);
+}
