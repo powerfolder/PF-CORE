a2aec7bbf32da7e9260c5f5fe6bb9fd2472b769d
Christian Sprajc <sprajc@powerfolder.com>
2010-07-21 16:01:47 +0000

    added toString

--- src/main/de/dal33t/powerfolder/message/StartUpload.java
+++ src/main/de/dal33t/powerfolder/message/StartUpload.java
@@ -1,46 +1,49 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.message;
 
 import de.dal33t.powerfolder.light.FileInfo;
 
 /**
- * Message to indicate that the upload can be started.
- * This message is sent by the uploader.
- * The remote side should send PartRequests or PartinfoRequests.
+ * Message to indicate that the upload can be started. This message is sent by
+ * the uploader. The remote side should send PartRequests or PartinfoRequests.
  * 
  * @author Dennis "Dante" Waldherr
  * @version $Revision$
- */ 
+ */
 public class StartUpload extends Message {
-	private static final long serialVersionUID = 100L;
-	private FileInfo fileInfo;
+    private static final long serialVersionUID = 100L;
+    private FileInfo fileInfo;
 
-	public StartUpload() {
-	}
+    public StartUpload() {
+    }
 
-	public StartUpload(FileInfo fInfo) {
-		fileInfo = fInfo;
-	}
-	
-	public FileInfo getFile() {
-		return fileInfo;
-	}
+    public StartUpload(FileInfo fInfo) {
+        fileInfo = fInfo;
+    }
+
+    public FileInfo getFile() {
+        return fileInfo;
+    }
+
+    public String toString() {
+        return "StartUpload of " + fileInfo.toDetailString();
+    }
 }
