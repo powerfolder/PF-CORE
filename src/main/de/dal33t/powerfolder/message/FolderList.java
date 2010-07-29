981e9ad3218bcf99ab9a457263c32963a76511b6
Christian Sprajc <sprajc@powerfolder.com>
2010-07-29 00:52:24 +0000

    refs #2059

--- src/main/de/dal33t/powerfolder/message/FolderList.java
+++ src/main/de/dal33t/powerfolder/message/FolderList.java
@@ -19,13 +19,28 @@
  */
 package de.dal33t.powerfolder.message;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
 
+import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.Feature;
+import de.dal33t.powerfolder.Member;
 import de.dal33t.powerfolder.light.FolderInfo;
+import de.dal33t.powerfolder.util.ByteSerializer;
+import de.dal33t.powerfolder.util.Convert;
+import de.dal33t.powerfolder.util.FileUtils;
 import de.dal33t.powerfolder.util.Reject;
+import de.dal33t.powerfolder.util.StreamUtils;
+import de.dal33t.powerfolder.util.Util;
 
 /**
  * List of available folders
@@ -35,6 +50,7 @@ import de.dal33t.powerfolder.util.Reject;
  */
 public class FolderList extends Message {
     private static final long serialVersionUID = 101L;
+    private static Logger LOG = Logger.getLogger(FolderList.class.getName());
 
     /** List of public folders. LEFT for backward compatibility */
     public FolderInfo[] folders = new FolderInfo[0];
@@ -90,6 +106,107 @@ public class FolderList extends Message {
             }
         }
         return false;
+    }
+
+    /**
+     * @param member
+     *            the {@link Member} to store this {@link FolderList} relative
+     *            to.
+     * @return true if this {@link FolderList} could be stored.
+     * @see #load(Member)
+     */
+    public synchronized boolean store(Member member) {
+        String idPath = new String(Util.encodeHex(Util.md5(member.getId()
+            .getBytes(Convert.UTF8))));
+        File file = new File(Controller.getMiscFilesLocation(), "nodes/"
+            + idPath + ".FolderList");
+        return store(file);
+    }
+
+    /**
+     * @param file
+     *            the file to store this {@link FolderList} in.
+     * @return true if this {@link FolderList} could be stored to that file.
+     *@see #load(File)
+     */
+    public synchronized boolean store(File file) {
+        Reject.ifNull(file, "File");
+        if (file.getParentFile() != null) {
+            file.getParentFile().mkdirs();
+        }
+        try {
+            byte[] buf = ByteSerializer.serializeStatic(this, false);
+            FileUtils.copyFromStreamToFile(new ByteArrayInputStream(buf), file);
+            return true;
+        } catch (Exception e) {
+            LOG.warning("Unable to store to " + file + ". " + e + ". " + this);
+            return false;
+        }
+    }
+
+    /**
+     * @param member
+     *            the {@link Member} to load a {@link FolderList} previously
+     *            stored with {@link #store(Member)} from
+     * @return the loaded {@link FolderList} or null if failed or not existing.
+     */
+    public static FolderList load(Member member) {
+        String idPath = new String(Util.encodeHex(Util.md5(member.getId()
+            .getBytes(Convert.UTF8))));
+        File file = new File(Controller.getMiscFilesLocation(), "nodes/"
+            + idPath + ".FolderList");
+        return load(file);
+    }
+
+    /**
+     * @param file
+     *            the file to load a {@link FolderList} previously stored with
+     *            {@link #store(File)} from
+     * @return the loaded {@link FolderList} or null if failed or not existing.
+     */
+    public static FolderList load(File file) {
+        Reject.ifNull(file, "File");
+        InputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(file));
+            byte[] buf = StreamUtils.readIntoByteArray(in);
+            return (FolderList) ByteSerializer.deserializeStatic(buf, false);
+        } catch (Exception e) {
+            LOG.warning("Unable to load to " + file + ". " + e);
+            return null;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (joinedMetaFolders ? 1231 : 1237);
+        result = prime * result + Arrays.hashCode(secretFolders);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FolderList other = (FolderList) obj;
+        if (joinedMetaFolders != other.joinedMetaFolders)
+            return false;
+        if (!Arrays.equals(secretFolders, other.secretFolders))
+            return false;
+        return true;
     }
 
     public String toString() {
