7598b28c2e2275b72fa2fbf8fe491ccfb47d2cc6
Christian Sprajc <sprajc@powerfolder.com>
2010-07-30 14:15:42 +0000

    refs #1928

--- src/main/de/dal33t/powerfolder/security/FolderSecuritySettings.java
+++ src/main/de/dal33t/powerfolder/security/FolderSecuritySettings.java
@@ -26,7 +26,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.Type;
 
