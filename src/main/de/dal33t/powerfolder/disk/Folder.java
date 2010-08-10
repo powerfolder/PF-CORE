bbd1e324681e9277838a03156e8de1d329797efa
Christian Sprajc <sprajc@powerfolder.com>
2010-08-10 15:20:38 +0000

    refs #1227

--- src/main/de/dal33t/powerfolder/disk/Folder.java
+++ src/main/de/dal33t/powerfolder/disk/Folder.java
@@ -1427,7 +1427,7 @@ public class Folder extends PFComponent {
         loadFolderDB();
         loadLastSyncDate();
         diskItemFilter.loadPatternsFrom(new File(getSystemSubDir(),
-            DiskItemFilter.PATTERNS_FILENAME));
+            DiskItemFilter.PATTERNS_FILENAME), false);
     }
 
     /**
@@ -2303,7 +2303,7 @@ public class Folder extends PFComponent {
 
         File syncPatternsFile = metaFolder.getDiskFile(fileInfo);
         logFine("Reading syncPatterns " + syncPatternsFile);
-        diskItemFilter.loadPatternsFrom(syncPatternsFile);
+        diskItemFilter.loadPatternsFrom(syncPatternsFile, true);
     }
 
     public DirectoryInfo getBaseDirectoryInfo() {
