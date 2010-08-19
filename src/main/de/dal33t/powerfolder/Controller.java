006a41c798d8fcf3b76b36b84bdd81ac2db25466
Christian Sprajc <sprajc@powerfolder.com>
2010-08-19 16:12:17 +0000

    RC5

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -131,7 +131,7 @@ public class Controller extends PFComponent {
     /**
      * Program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "4.3.0 RC4"; // 1.7.1.35 /
+    public static final String PROGRAM_VERSION = "4.3.0 RC5"; // 1.7.1.35 /
                                                               // RC3
 
     /**
