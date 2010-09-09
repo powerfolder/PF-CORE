000cdf552aae31197b64f82d72e4494aa310d505
Christian Sprajc <sprajc@powerfolder.com>
2010-09-09 00:29:37 +0000

    refs 2075

--- src/main/de/dal33t/powerfolder/util/MemoryMonitor.java
+++ src/main/de/dal33t/powerfolder/util/MemoryMonitor.java
@@ -19,6 +19,9 @@
  */
 package de.dal33t.powerfolder.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -70,10 +73,9 @@ public class MemoryMonitor implements Runnable {
      * Add a warning event for the user.
      */
     private void addWarning() {
-        WarningNotice notice = new WarningNotice(
-                Translation.getTranslation("warning_notice.title"),
-                Translation.getTranslation("warning_notice.low_memory"),
-                new Runnable() {
+        WarningNotice notice = new WarningNotice(Translation
+            .getTranslation("warning_notice.title"), Translation
+            .getTranslation("warning_notice.low_memory"), new Runnable() {
             public void run() {
                 if (OSUtil.isWindowsSystem() && !OSUtil.isWebStart()) {
                     int response = DialogFactory
@@ -101,14 +103,25 @@ public class MemoryMonitor implements Runnable {
                 }
             }
         });
-        controller.getUIController().getApplicationModel()
-            .getNoticesModel().addNotice(notice);
+        controller.getUIController().getApplicationModel().getNoticesModel()
+            .addNotice(notice);
     }
 
     /**
-     * Reconfigure ini from (initial) 54M to 256M max memory.
+     * Reconfigure ini from (initial) 512M to 1024M max memory.
      */
     private void increaseAvailableMemory() {
+        if (OSUtil.isMacOS()) {
+            increaseAvailableMemoryMac();
+        } else {
+            increaseAvailableMemoryWin();
+        }
+    }
+
+    /**
+     * Reconfigure ini from (initial) 512M to 1024M max memory.
+     */
+    private void increaseAvailableMemoryWin() {
 
         // Read the current ini file.
         boolean wroteNewIni = false;
@@ -159,6 +172,69 @@ public class MemoryMonitor implements Runnable {
 
         // Show a response
         if (wroteNewIni) {
+            DialogFactory.genericDialog(controller, Translation
+                .getTranslation("low_memory.title"), Translation
+                .getTranslation("low_memory.configure_success"),
+                GenericDialogType.INFO);
+        } else {
+            DialogFactory.genericDialog(controller, Translation
+                .getTranslation("low_memory.title"), Translation
+                .getTranslation("low_memory.configure_failure"),
+                GenericDialogType.WARN);
+        }
+    }
+
+    /**
+     * Reconfigure Info.plist from (initial) 512M to 1024M max memory.
+     */
+    private void increaseAvailableMemoryMac() {
+        boolean success = false;
+        PrintWriter pw = null;
+        BufferedReader br = null;
+        try {
+            log.fine("Looking for Contents/Info.plist...");
+            File orig = new File("Contents/Info.plist");
+            File temp = new File("Contents/Info.plist.temp");
+            File back = new File("Contents/Info.plist.backup");
+            br = new BufferedReader(new FileReader(orig));
+            pw = new PrintWriter(new FileWriter(temp));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.trim().startsWith("<string>-Xm")
+                    && !line.trim().contains("-Xmx1024m"))
+                {
+                    pw
+                        .write("      <string>-Xms16m -Xmx1024m -XX:MaxPermSize=256m "
+                            + "-XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10</string>");
+                    success = true;
+                } else {
+                    pw.write(line);
+                }
+                pw.write('\n');
+            }
+            FileUtils.copyFile(orig, back);
+            FileUtils.copyFile(temp, orig);
+        } catch (IOException e) {
+            log.log(Level.WARNING,
+                "Problem reconfiguring Contents/Info.plist: " + e.getMessage(),
+                e);
+            success = false;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    // Ignore
+                }
+            }
+            if (pw != null) {
+                pw.close();
+            }
+
+        }
+
+        // Show a response
+        if (success) {
             DialogFactory.genericDialog(controller, Translation
                 .getTranslation("low_memory.title"), Translation
                 .getTranslation("low_memory.configure_success"),
