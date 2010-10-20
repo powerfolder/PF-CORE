511db8ede487dd9460bd539ea7afe5caddb60d25
Christian Sprajc <sprajc@powerfolder.com>
2010-10-20 12:04:21 +0000

    Corrected test

--- src/test/de/dal33t/powerfolder/test/net/UDTConnectionTest.java
+++ src/test/de/dal33t/powerfolder/test/net/UDTConnectionTest.java
@@ -1,30 +1,35 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc, Dennis Waldherr. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
+ */
 package de.dal33t.powerfolder.test.net;
 
 import java.net.InetSocketAddress;
+import java.util.logging.Level;
 
+import de.dal33t.powerfolder.Member;
 import de.dal33t.powerfolder.NetworkingMode;
 import de.dal33t.powerfolder.disk.SyncProfile;
 import de.dal33t.powerfolder.net.ConnectionException;
 import de.dal33t.powerfolder.net.ConnectionHandler;
+import de.dal33t.powerfolder.net.NodeManager;
+import de.dal33t.powerfolder.net.RelayFinder;
+import de.dal33t.powerfolder.util.logging.LoggingManager;
 import de.dal33t.powerfolder.util.net.NetworkUtil;
 import de.dal33t.powerfolder.util.test.ConditionWithMessage;
 import de.dal33t.powerfolder.util.test.FiveControllerTestCase;
@@ -62,8 +67,17 @@ public class UDTConnectionTest extends FiveControllerTestCase {
         getContollerBart().getMySelf().getInfo().setConnectAddress(
             new InetSocketAddress("192.168.0.4", 10001));
 
+        getContollerMarge().getIOProvider().getRelayedConnectionManager()
+            .setRelayFiner(new RelayFinder() {
+                public Member findRelay(NodeManager nodeManager) {
+                    return nodeManager.getNode(getContollerBart().getMySelf()
+                        .getInfo());
+                }
+            });
+
         ConnectionHandler conHan = getContollerMarge().getIOProvider()
-            .getUDTSocketConnectionManager().initRendezvousUDTConnectionHandler(
+            .getUDTSocketConnectionManager()
+            .initRendezvousUDTConnectionHandler(
                 getContollerLisa().getMySelf().getInfo());
         assertTrue(conHan.isConnected());
         getContollerMarge().getNodeManager().acceptConnection(conHan);
