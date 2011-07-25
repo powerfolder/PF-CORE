f6cb16af0b3257e2b4081ad58651fbc36c0729d5
Christoph Kappel <kappel@powerfolder.com>
2011-07-25 16:13:57 +0000

    Refs #2308: Added TranslationWrapper class

---
+++ src/main/de/dal33t/powerfolder/util/TranslationWrapper.java
@@ -0,0 +1,19 @@
+package de.dal33t.powerfolder.util;
+
+/** TranslationWrapper
+ * Wrapper to access static classes from Velocity templates.
+ * @author <a href="mailto:kappel@powerfolder">Christoph Kappel</a>
+ */
+
+public class TranslationWrapper {
+    
+    /** TranslationWrapper.TranslationWrapper
+     * Get translation of given string id
+     * @param  id  Translation string id
+     */
+    
+    public String get(String id)
+    {
+        return Translation.getTranslation(id);
+    }
+}
