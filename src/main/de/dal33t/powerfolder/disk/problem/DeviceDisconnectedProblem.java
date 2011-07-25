e15b66bb9af6081e86ad2258524eda5e8727dd24
Christian Sprajc <sprajc@powerfolder.com>
2011-07-25 12:09:11 +0000

    refs #2329

--- src/main/de/dal33t/powerfolder/disk/problem/DeviceDisconnectedProblem.java
+++ src/main/de/dal33t/powerfolder/disk/problem/DeviceDisconnectedProblem.java
@@ -1,9 +1,19 @@
 package de.dal33t.powerfolder.disk.problem;
 
+import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.disk.Folder;
+import de.dal33t.powerfolder.light.FolderInfo;
 import de.dal33t.powerfolder.ui.WikiLinks;
+import de.dal33t.powerfolder.util.Reject;
 import de.dal33t.powerfolder.util.Translation;
 
-public class DeviceDisconnectedProblem extends Problem {
+public class DeviceDisconnectedProblem extends ResolvableProblem {
+    private final FolderInfo folderInfo;
+
+    public DeviceDisconnectedProblem(FolderInfo folderInfo) {
+        Reject.ifNull(folderInfo, "Folder");
+        this.folderInfo = folderInfo;
+    }
 
     @Override
     public String getDescription() {
@@ -13,6 +23,26 @@ public class DeviceDisconnectedProblem extends Problem {
     @Override
     public String getWikiLinkKey() {
         return WikiLinks.PROBLEM_DEVICE_DISCONNECTED;
+    }
+
+    @Override
+    public Runnable resolution(final Controller controller) {
+        return new Runnable() {
+            public void run() {
+                Folder folder = controller.getFolderRepository().getFolder(
+                    folderInfo);
+                if (folder != null) {
+                    controller.getFolderRepository()
+                        .removeFolder(folder, false);
+                }
+            }
+        };
+    }
+
+    @Override
+    public String getResolutionDescription() {
+        return Translation
+            .getTranslation("folder_problem.device_disconnected.remove_folder");
     }
 
 }
