02553c37bc68b5721cc77086344054b30e444bcb
Christian Sprajc <sprajc@powerfolder.com>
2011-07-31 23:48:13 +0000

    refs #2165

--- src/test/de/dal33t/powerfolder/test/transfer/BandwidthLimitTest.java
+++ src/test/de/dal33t/powerfolder/test/transfer/BandwidthLimitTest.java
@@ -1,39 +1,42 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
+ */
 package de.dal33t.powerfolder.test.transfer;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import junit.framework.TestCase;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
 
-import de.dal33t.powerfolder.transfer.*;
+import de.dal33t.powerfolder.transfer.BandwidthLimiter;
+import de.dal33t.powerfolder.transfer.BandwidthProvider;
+import de.dal33t.powerfolder.transfer.BandwidthStat;
+import de.dal33t.powerfolder.transfer.BandwidthStatsListener;
+import de.dal33t.powerfolder.transfer.LimitedInputStream;
+import de.dal33t.powerfolder.transfer.LimitedOutputStream;
 
 public class BandwidthLimitTest extends TestCase {
-    BandwidthProvider provider = new BandwidthProvider(Executors
-        .newScheduledThreadPool(1));
+    BandwidthProvider provider = new BandwidthProvider();
 
     public void testUnlimited() {
         BandwidthLimiter bl = BandwidthLimiter.LAN_INPUT_BANDWIDTH_LIMITER;
@@ -79,12 +82,12 @@ public class BandwidthLimitTest extends TestCase {
         }
         assertTrue("Exceeded amount", amount == 0);
     }
-    
+
     public void testLimiteds() {
         BandwidthLimiter bl = BandwidthLimiter.LAN_INPUT_BANDWIDTH_LIMITER;
         bl.setAvailable(10000);
         byte b[] = new byte[1000];
-        
+
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
         LimitedOutputStream out = new LimitedOutputStream(bl, bout);
         try {
@@ -92,7 +95,8 @@ public class BandwidthLimitTest extends TestCase {
         } catch (IOException e) {
             fail(e.toString());
         }
-        assertTrue("Wrong amount left/write", bl.getAvailable() == 10000 - b.length);
+        assertTrue("Wrong amount left/write",
+            bl.getAvailable() == 10000 - b.length);
         assertTrue("Wrong amount written", bout.size() == b.length);
         ByteArrayInputStream bin = new ByteArrayInputStream(bout.toByteArray());
         bl.setAvailable(10000);
@@ -106,7 +110,7 @@ public class BandwidthLimitTest extends TestCase {
         assertTrue("Wrong amount left/read", bl.getAvailable() == 10000 - read);
         assertTrue("Wrong amount read", bin.available() == b.length - read);
     }
-    
+
     public void testProvider() {
         BandwidthLimiter bl = BandwidthLimiter.LAN_INPUT_BANDWIDTH_LIMITER;
         bl.setAvailable(0);
@@ -119,19 +123,17 @@ public class BandwidthLimitTest extends TestCase {
         }
         provider.removeLimiter(bl);
         provider.shutdown();
-        assertEquals(
-            1000, bl.getAvailable());
+        assertEquals(1000, bl.getAvailable());
     }
-    
+
     public static class ReaderThread implements Runnable {
         public LimitedInputStream in;
         public long amount;
-        
+
         public ReaderThread(LimitedInputStream in, long amount) {
             this.in = in;
             this.amount = amount;
         }
-
 
         public void run() {
             byte b[] = new byte[1024];
@@ -144,33 +146,32 @@ public class BandwidthLimitTest extends TestCase {
             }
         }
     }
-    
+
     public void testHeavyLoad() {
         BandwidthLimiter bl = BandwidthLimiter.LAN_INPUT_BANDWIDTH_LIMITER;
         bl.setAvailable(0);
         provider.start();
         provider.setLimitBPS(bl, 1024 * 100);
-        LimitedInputStream in = new LimitedInputStream(bl, 
-            new InputStream() {
-                @Override
-                public int read() throws IOException {
-                    return 0;
-                }
-            });
+        LimitedInputStream in = new LimitedInputStream(bl, new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 0;
+            }
+        });
         Thread pool[] = new Thread[400];
-        for (int i = 0; i < pool.length; i++) 
+        for (int i = 0; i < pool.length; i++)
             pool[i] = new Thread(new ReaderThread(in, 1000));
-        
+
         for (int i = 0; i < pool.length; i++)
             pool[i].start();
-        
+
         try {
             Thread.sleep(6000);
             pool[0].join(1);
         } catch (InterruptedException e) {
             fail(e.toString());
         }
-   
+
         provider.shutdown();
         assertEquals(Thread.State.TERMINATED, pool[0].getState());
         for (int i = 0; i < pool.length; i++) {
