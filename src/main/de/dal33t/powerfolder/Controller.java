ecb4938797939b23ca9a6a2fc7480ce842377479
Christian Sprajc <sprajc@powerfolder.com>
2011-08-23 12:26:24 +0000

    Near 5.0

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -105,7 +105,7 @@ public class Controller extends PFComponent {
     /**
      * Program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "4.8.18"; // 3.5.16
+    public static final String PROGRAM_VERSION = "4.9.0"; // 3.5.16
 
     /**
      * the (java beans like) property, listen to changes of the networking mode
