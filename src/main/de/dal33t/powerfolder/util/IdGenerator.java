75d2de8f6f0d4c418891082467f3a7e5e944dff2
Christian Sprajc <sprajc@powerfolder.com>
2011-11-26 00:54:53 +0000

    ID no special chars

--- src/main/de/dal33t/powerfolder/util/IdGenerator.java
+++ src/main/de/dal33t/powerfolder/util/IdGenerator.java
@@ -1,22 +1,22 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.util;
 
 import java.util.UUID;
@@ -39,7 +39,8 @@ public class IdGenerator {
     public static String makeId() {
         String id = Base64.encodeBytes(makeIdBytes());
         // Remove the last == at the end
-        return id.substring(0, id.length() - 2);
+        return FileUtils.removeInvalidFilenameChars(
+            id.substring(0, id.length() - 2)).replace("+", "");
     }
 
     /**
