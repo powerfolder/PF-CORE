8d6e206e14d3f09f0796729e916d46d5fa450773
Christian Sprajc <sprajc@powerfolder.com>
2012-01-12 02:57:56 +0000

    Version GWDG

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -131,7 +131,7 @@ public class Controller extends PFComponent {
     /**
      * Program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "5.5.7 - 4.0.22"; // 4.0.22
+    public static final String PROGRAM_VERSION = "5.6.2 - 4.5.4"; // 4.5.4
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
