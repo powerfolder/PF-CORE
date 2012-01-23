1aa034dba832617e70d7ed1b14ee8c4eaab347ea
Christian Sprajc <sprajc@powerfolder.com>
2012-01-23 16:35:13 +0000

    No shutdown allowed during running shutdown

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -143,7 +143,7 @@ public class Controller extends PFComponent {
     /**
      * Program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "5.6.7"; // 4.5.9
+    public static final String PROGRAM_VERSION = "5.6.7"; // 4.5.10
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
@@ -1595,6 +1595,9 @@ public class Controller extends PFComponent {
      * Shuts down all activities of this controller
      */
     public synchronized void shutdown() {
+        if (shuttingDown || !started) {
+            return;
+        }
         shuttingDown = true;
         logInfo("Shutting down...");
         setFirstStart(false);
