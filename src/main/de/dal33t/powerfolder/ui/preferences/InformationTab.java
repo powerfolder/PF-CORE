6c882b7490c241f236f14ee8df0b4d93b5f6f95d
Harry Glasgow <glasgow@powerfolder.com>
2012-02-04 06:20:56 +0000

    More information preference tab, refs #2610

--- src/main/de/dal33t/powerfolder/ui/preferences/InformationTab.java
+++ src/main/de/dal33t/powerfolder/ui/preferences/InformationTab.java
@@ -19,56 +19,43 @@
  */
 package de.dal33t.powerfolder.ui.preferences;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import java.awt.*;
 import java.io.File;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
-import java.util.StringTokenizer;
-import java.util.List;
+import java.util.Date;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.jar.Attributes;
+import java.text.SimpleDateFormat;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
-import javax.swing.text.PlainDocument;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 
-import com.jgoodies.binding.adapter.BasicComponentFactory;
-import com.jgoodies.binding.value.ValueHolder;
-import com.jgoodies.binding.value.ValueModel;
-import com.jgoodies.forms.builder.ButtonBarBuilder;
 import com.jgoodies.forms.builder.PanelBuilder;
-import com.jgoodies.forms.factories.Borders;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
-import de.dal33t.powerfolder.ConfigurationEntry;
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.PFComponent;
+import de.dal33t.powerfolder.Member;
+import de.dal33t.powerfolder.ConfigurationEntry;
 import de.dal33t.powerfolder.disk.Folder;
-import de.dal33t.powerfolder.net.ConnectionListener;
-import de.dal33t.powerfolder.ui.Icons;
-import de.dal33t.powerfolder.ui.util.DialogFactory;
-import de.dal33t.powerfolder.ui.util.LANList;
-import de.dal33t.powerfolder.ui.util.GenericDialogType;
-import de.dal33t.powerfolder.ui.util.SimpleComponentFactory;
-import de.dal33t.powerfolder.ui.widget.JButtonMini;
+import de.dal33t.powerfolder.ui.util.*;
+import de.dal33t.powerfolder.ui.widget.LinkLabel;
 import de.dal33t.powerfolder.util.StringUtils;
 import de.dal33t.powerfolder.util.Translation;
-import de.dal33t.powerfolder.util.os.Win32.FirewallUtil;
+import de.dal33t.powerfolder.util.JavaVersion;
+import de.dal33t.powerfolder.util.Format;
+import de.dal33t.powerfolder.util.os.OSUtil;
 
 public class InformationTab extends PFComponent implements PreferenceTab {
+
+    private static final int HEADER_FONT_SIZE = 16;
+
+    private String buildDate;
+    private String buildTime;
 
     private JPanel panel;
 
@@ -93,7 +80,244 @@ public class InformationTab extends PFComponent implements PreferenceTab {
         return true;
     }
 
+    private static JPanel createTeamPanel() {
+    return createTextBox(
+        Translation.getTranslation("about_dialog.team"),
+        "Bernhard Rutkowsky\nCecilia Saltori\nChristian Sprajc\nDennis Waldherr\nFlorian Lahr\nHarry Glasgow\n");
+    }
+
+
+
     private void initComponents() {
+        readDateTimeFromJar();
+        FormLayout layout = new FormLayout(
+            "pref:grow, pref:grow, pref:grow, pref:grow",
+            "fill:pref:grow, fill:pref:grow");
+        PanelBuilder builder = new PanelBuilder(layout);
+        CellConstraints cc = new CellConstraints();
+        builder.add(createGeneralBox(), cc.xywh(1, 1, 2, 1));
+        builder.add(createPowerFolderBox(), cc.xy(1, 2));
+        builder.add(createSystemBox(), cc.xy(2, 2));
+        if (getController().getDistribution().showCredentials()) {
+            builder.add(createTeamPanel(), cc.xy(3, 1));
+            builder.add(createTranslators(), cc.xy(3, 2));
+        }
+
+        panel = builder.getPanel();
+//        panel.setBackground(Color.WHITE);
+    }
+
+    private static JPanel createTranslators() {
+        return createTextBox(
+            Translation.getTranslation("about_dialog.translators"),
+            "Bayan El Ameen\n" + "Cecilia Saltori\n" + "Javier Isassi\n"
+                + "Keblo\n" + "Olle Wikstrom\n" + "Zhang Jia\n ");
+    }
+
+    /**
+     * Reads the date and time from the jarfile manifest "BuildDateTime"
+     * property. This is set by build.xml when ant creates the jar file
+     *
+     * @see #buildDate
+     * @see #buildTime
+     */
+    private void readDateTimeFromJar() {
+        try {
+            File jar = new File(getController().getJARName());
+            JarFile file = new JarFile(jar);
+            Manifest mf = file.getManifest();
+            Attributes attr = mf.getMainAttributes();
+
+            logFine(attr.getValue("BuildDateTime"));
+
+            String buildDateTimeString = attr.getValue("BuildDateTime");
+            SimpleDateFormat parser = new SimpleDateFormat();
+            // must match the format in build.xml
+            parser.applyPattern("MMMM/dd/yyyy hh:mm:ss a, z");
+            Date buildDateTime = parser.parse(buildDateTimeString);
+
+            SimpleDateFormat localizedFormatter = new SimpleDateFormat(
+                "HH:mm:ss z", Translation.getActiveLocale());
+
+            buildTime = localizedFormatter.format(buildDateTime);
+            // localizedFormatter.applyPattern("dd MMMM yyyy");
+            localizedFormatter.applyPattern(Translation
+                .getTranslation("general.localized_date_format"));
+            buildDate = localizedFormatter.format(buildDateTime);
+
+            file.close();
+        } catch (Exception e) {
+            logInfo("Build date/time works only from jar.");
+            buildTime = "n/a";
+            buildDate = "n/a";
+        }
+    }
+
+    private long calculateTotalLocalSharedSize() {
+        long totalSize = 0;
+        for (Folder folder : getController().getFolderRepository().getFolders())
+        {
+            totalSize += folder.getStatistic().getLocalSize();
+        }
+        return totalSize;
+    }
+
+    private JPanel createSystemBox() {
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+        long dbSize = 0;
+        for (Folder folder : getController().getFolderRepository().getFolders(
+            true))
+        {
+            for (Member member : folder.getMembersAsCollection()) {
+                dbSize += folder.getDAO().count(member.getId(), true, false);
+            }
+        }
+
+        return createTextBox(
+            Translation.getTranslation("about_dialog.your_system.title"),
+            Translation.getTranslation("about_dialog.your_system.java_version",
+                JavaVersion.systemVersion().toString())
+                + '\n'
+                + Translation.getTranslation("about_dialog.your_system.os",
+                    System.getProperty("os.name"))
+                + " ("
+                + (OSUtil.is64BitPlatform() ? "64bit" : "32bit")
+                + ')'
+                + '\n'
+                + Translation.getTranslation("about_dialog.your_system.screen",
+                    String.valueOf(dim.width), String.valueOf(dim.height))
+                + '\n'
+                + Translation.getTranslation(
+                    "about_dialog.power_folder.max",
+                    String
+                        .valueOf(Runtime.getRuntime().maxMemory() / 1024 / 1024))
+                + '\n'
+                + Translation.getTranslation(
+                    "about_dialog.power_folder.used",
+                    String
+                        .valueOf(Runtime.getRuntime().totalMemory() / 1024 / 1024))
+                + '\n'
+                + Translation.getTranslation(
+                    "about_dialog.power_folder.datasize",
+                    Format.formatBytesShort(calculateTotalLocalSharedSize()))
+                + '\n'
+                + Translation.getTranslation(
+                    "about_dialog.power_folder.dbsize", String.valueOf(dbSize)));
+    }
+
+    private String readLicense() {
+    Object licKey = getController().getUIController().getApplicationModel().getLicenseModel()
+        .getLicenseKeyModel().getValue();
+    return licKey != null ? Translation.getTranslation(
+        "about_dialog.power_folder.license", licKey.toString()) : "";
+    }
+
+    private JPanel createPowerFolderBox() {
+        return createTextBox(
+                Translation.getTranslation("general.application.name"),
+                Translation.getTranslation("about_dialog.power_folder.text",
+                        Controller.PROGRAM_VERSION)
+                        + '\n'
+                        + Translation.getTranslation(
+                        "about_dialog.power_folder.build_date", buildDate)
+                        + '\n'
+                        + Translation.getTranslation(
+                        "about_dialog.power_folder.build_time", buildTime)
+                        + '\n'
+                        + Translation.getTranslation(
+                        "about_dialog.power_folder.distribution",
+                        getController().getDistribution().getName()) + '\n' +
+                        readLicense());
+    }
+
+    private static JPanel createTextBox(String title, String contents) {
+        String[] contentsArray = contents.split("\n");
+
+        FormLayout contentsForm = new FormLayout("pref");
+        PanelBuilder builder = new PanelBuilder(contentsForm);
+
+        TitledBorder titledBorder = new TitledBorder(title);
+        titledBorder.setTitleColor(Color.BLACK);
+        builder.setBorder(new CompoundBorder(titledBorder, new EmptyBorder(2,
+            2, 2, 2)));
+        // split into tokens
+        int row = 1;
+        CellConstraints cc = new CellConstraints();
+
+        for (String lineText : contentsArray) {
+            if (StringUtils.isEmpty(lineText.trim())) {
+                // Add gap
+                builder.appendRow("3dlu");
+            } else {
+                builder.appendRow("pref");
+                builder.add(new JLabel("<HTML><BODY>" + lineText
+                    + "</BODY></HTML>"), cc.xy(1, row));
+            }
+            row += 1;
+        }
+        JPanel textBoxPanel = builder.getPanel();
+        textBoxPanel.setBackground(Color.WHITE);
+        return textBoxPanel;
+    }
+
+    private JPanel createGeneralBox() {
+        FormLayout layout = new FormLayout("pref",
+            "pref, 15dlu, pref, 3dlu, pref, 3dlu, pref");
+        PanelBuilder builder = new PanelBuilder(layout);
+        CellConstraints cc = new CellConstraints();
+
+        builder.add(TextLinesPanelBuilder.createTextPanel(
+            Translation.getTranslation("about_dialog.app_description"),
+            HEADER_FONT_SIZE), cc.xy(1, 1));
+        builder.add(createHomeLink().getUIComponent(), cc.xy(1, 3));
+        builder.add(createDocLink().getUIComponent(), cc.xy(1, 5));
+        builder.add(createSupportLink().getUIComponent(), cc.xy(1, 7));
+
+        TitledBorder titledBorder = new TitledBorder(
+            Translation.getTranslation("about_dialog.general_information"));
+        titledBorder.setTitleColor(Color.BLACK);
+        builder.setBorder(new CompoundBorder(titledBorder, new EmptyBorder(2,
+            2, 2, 2)));
+
+        JPanel generalPanel = builder.getPanel();
+        generalPanel.setBackground(Color.WHITE);
+        return generalPanel;
+    }
+
+    private LinkLabel createHomeLink() {
+        LinkLabel homeLink =  new LinkLabel(getController(),
+            Translation.getTranslation("about_dialog.home_page"),
+            ConfigurationEntry.PROVIDER_URL.getValue(getController()));
+        SimpleComponentFactory.setFontSize(homeLink.getUIComponent(),
+            SimpleComponentFactory.BIG_FONT_SIZE);
+        return homeLink;
+    }
+
+    private LinkLabel createDocLink() {
+        String docLinkStr = ConfigurationEntry.PROVIDER_WIKI_URL
+            .getValue(getController());
+        if (StringUtils.isBlank(docLinkStr)) {
+            docLinkStr = ConfigurationEntry.PROVIDER_QUICKSTART_URL
+                .getValue(getController());
+        }
+        LinkLabel docLink = new LinkLabel(getController(),
+            Translation.getTranslation("about_dialog.documentation"),
+            docLinkStr);
+        docLink.setVisible(StringUtils.isNotBlank(docLinkStr));
+        SimpleComponentFactory.setFontSize(docLink.getUIComponent(),
+            SimpleComponentFactory.BIG_FONT_SIZE);
+        return docLink;
+    }
+
+    private LinkLabel createSupportLink() {
+        LinkLabel supportLink = new LinkLabel(getController(),
+            Translation.getTranslation("about_dialog.support"),
+            ConfigurationEntry.PROVIDER_SUPPORT_URL.getValue(getController()));
+        SimpleComponentFactory.setFontSize(supportLink.getUIComponent(),
+            SimpleComponentFactory.BIG_FONT_SIZE);
+        return supportLink;
     }
 
     /**
@@ -102,31 +326,17 @@ public class InformationTab extends PFComponent implements PreferenceTab {
      * @return the created panel
      */
     public JPanel getUIPanel() {
-        if (panel == null) {
-            String rows = "pref, 3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu, pref,  3dlu, pref, "
-                + "3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu, pref";
-            if (FirewallUtil.isFirewallAccessible()) {
-                rows = "pref, 3dlu, " + rows;
-            }
-
-            FormLayout layout = new FormLayout(
-                "right:pref, 3dlu, 140dlu, pref:grow", rows);
-            PanelBuilder builder = new PanelBuilder(layout);
-            builder.setBorder(Borders
-                .createEmptyBorder("3dlu, 3dlu, 3dlu, 3dlu"));
-            CellConstraints cc = new CellConstraints();
-
-            int row = 1;
-
-            panel = builder.getPanel();
-        }
-        return panel;
+        // Put the panel in a panel with a pref:grow Y-axis.
+        // So it does not  s-t-r-e-a-c-h  vertically.
+        FormLayout layout = new FormLayout("pref:grow",
+            "pref, pref:grow");
+        PanelBuilder builder = new PanelBuilder(layout);
+        CellConstraints cc = new CellConstraints();
+        builder.add(panel, cc.xy(1, 1));
+        return builder.getPanel();
     }
 
-    /**
-     * Saves the advanced settings.
-     */
     public void save() {
-
+        // Nothing to do here.
     }
 }
