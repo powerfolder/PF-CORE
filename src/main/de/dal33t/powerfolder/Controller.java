279949a10e89e5421ba3471b6e83717b9f58f214
Christian Sprajc <sprajc@powerfolder.com>
2012-02-14 02:46:04 +0000

    First hotfix

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -135,7 +135,7 @@ public class Controller extends PFComponent {
     /**
      * Program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "5.6.15 - 5.0.0"; // 5.0.0
+    public static final String PROGRAM_VERSION = "5.6.15 - 5.0.1"; // 5.0.0
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
