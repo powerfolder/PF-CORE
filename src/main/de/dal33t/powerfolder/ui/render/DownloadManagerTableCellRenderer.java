c119850a2256c70b37fae6c0eb95cff772461cd4
Christian Sprajc <sprajc@powerfolder.com>
2012-04-12 17:18:58 +0000

    refs #2659 Luna finalizations

--- src/main/de/dal33t/powerfolder/ui/render/DownloadManagerTableCellRenderer.java
+++ src/main/de/dal33t/powerfolder/ui/render/DownloadManagerTableCellRenderer.java
@@ -1,35 +1,31 @@
 /*
-* Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
-*
-* This file is part of PowerFolder.
-*
-* PowerFolder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation.
-*
-* PowerFolder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
-*
-* $Id$
-*/
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
 package de.dal33t.powerfolder.ui.render;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.util.Collection;
 import java.util.Date;
 
 import javax.swing.JProgressBar;
 import javax.swing.JTable;
-import javax.swing.UIManager;
 import javax.swing.table.DefaultTableCellRenderer;
-
-import com.jgoodies.forms.factories.Borders;
 
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.light.FileInfo;
@@ -37,17 +33,17 @@ import de.dal33t.powerfolder.light.FolderInfo;
 import de.dal33t.powerfolder.transfer.Download;
 import de.dal33t.powerfolder.transfer.DownloadManager;
 import de.dal33t.powerfolder.transfer.Transfer;
-import de.dal33t.powerfolder.ui.util.Icons;
 import de.dal33t.powerfolder.ui.util.ColorUtil;
+import de.dal33t.powerfolder.ui.util.Icons;
+import de.dal33t.powerfolder.ui.util.UIUtil;
+import de.dal33t.powerfolder.util.EstimatedTime;
 import de.dal33t.powerfolder.util.Format;
 import de.dal33t.powerfolder.util.TransferCounter;
 import de.dal33t.powerfolder.util.Translation;
-import de.dal33t.powerfolder.util.EstimatedTime;
-import de.dal33t.powerfolder.ui.util.UIUtil;
 
 /**
  * Renderer for any transfer table
- *
+ * 
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc </a>
  * @version $Revision: 1.20 $
  */
@@ -60,21 +56,16 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
     /**
      * Initalizes the renderer for a transfertable. renderDownloads determines
      * if we are rendering ul or dls. Maybe split this class up into two
-     *
+     * 
      * @param controller
      */
     public DownloadManagerTableCellRenderer(Controller controller) {
         this.controller = controller;
 
-        // FIXME: Find a better way to set text color of progress bar string
-        UIManager.put("ProgressBar.selectionBackground", Color.BLACK);
-        UIManager.put("ProgressBar.selectionForeground", Color.BLACK);
-        // UIManager.put("ProgressBar.foreground", Color.WHITE);
-
         bar = new JProgressBar();
-        bar.setBorderPainted(false);
-        bar.setBorder(Borders.EMPTY_BORDER);
-        bar.setStringPainted(true);
+        // bar.setBorderPainted(false);
+        // bar.setBorder(Borders.EMPTY_BORDER);
+        // bar.setStringPainted(true);
 
         // Listen for ui l&f changes
         UIUtil.addUIChangeTask(new Runnable() {
@@ -85,17 +76,18 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
     }
 
     public Component getTableCellRendererComponent(JTable table, Object value,
-                                                   boolean isSelected, boolean hasFocus, int row, int column) {
+        boolean isSelected, boolean hasFocus, int row, int column)
+    {
         Component defaultComp = super.getTableCellRendererComponent(table,
-                value, isSelected, hasFocus, row, column);
+            value, isSelected, hasFocus, row, column);
 
         if (value instanceof DownloadManager) {
             DownloadManager downloadManager = (DownloadManager) value;
             TransferCounter counter = downloadManager.getCounter();
 
             // Show bar
-            bar.setValue((int) (Math.max(0,
-                    downloadManager.getState().getProgress()) * 100));
+            bar.setValue((int) (Math.max(0, downloadManager.getState()
+                .getProgress()) * 100));
             bar.setBackground(defaultComp.getBackground());
 
             Transfer.State state = downloadManager.getState();
@@ -105,27 +97,26 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
             }
 
             switch (state.getState()) {
-                case VERIFYING:
+                case VERIFYING :
                     bar.setString(Translation.getTranslation(state.getState()
-                            .getTranslationId()));
+                        .getTranslationId()));
                     bar.setValue(100);
                     break;
-                case MATCHING:
-                case FILERECORD_REQUEST:
-                case COPYING:
+                case MATCHING :
+                case FILERECORD_REQUEST :
+                case COPYING :
                     bar.setString(Translation.getTranslation(state.getState()
-                            .getTranslationId()));
+                        .getTranslationId()));
                     break;
-                case DOWNLOADING:
+                case DOWNLOADING :
                     EstimatedTime et = new EstimatedTime(downloadManager
-                            .getCounter().calculateEstimatedMillisToCompletion(),
-                            downloadManager.isCompleted() && downloadManager.isStarted());
-                    String kbs = Translation.getTranslation(
-                            "transfers.kbs", Format.formatDecimal(
-                                    counter.calculateCurrentKBS()));
-                    String text = (et.isActive() ? et.toString()
-                            + " - " : "")
-                            + kbs;
+                        .getCounter().calculateEstimatedMillisToCompletion(),
+                        downloadManager.isCompleted()
+                            && downloadManager.isStarted());
+                    String kbs = Translation.getTranslation("transfers.kbs",
+                        Format.formatDecimal(counter.calculateCurrentKBS()));
+                    String text = (et.isActive() ? et.toString() + " - " : "")
+                        + kbs;
                     bar.setString(text);
                     break;
 
@@ -142,7 +133,7 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
                             .getTranslation("transfers.started"));
                     } else {
                         bar.setString(Translation
-                                .getTranslation("transfers.requested"));
+                            .getTranslation("transfers.requested"));
                     }
             }
             return bar;
@@ -165,7 +156,6 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
         } else if (value instanceof FolderInfo) {
             FolderInfo foInfo = (FolderInfo) value;
             setText(foInfo.name);
-            setIcon(Icons.getIconById(Icons.FOLDER));
             setHorizontalAlignment(LEFT);
         } else if (value instanceof Collection) {
             Collection<Download> sources = (Collection<Download>) value;
@@ -181,7 +171,6 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
                     setText(Translation.getTranslation("transfers.swarm",
                         String.valueOf(sources.size())));
                 }
-                setIcon(Icons.getIconFor(primaryDownload.getPartner()));
                 setHorizontalAlignment(LEFT);
             }
         } else if (value instanceof EstimatedTime) {
@@ -201,8 +190,9 @@ public class DownloadManagerTableCellRenderer extends DefaultTableCellRenderer {
         }
 
         if (!isSelected) {
-            setBackground(row % 2 == 0 ? ColorUtil.EVEN_TABLE_ROW_COLOR
-                    : ColorUtil.ODD_TABLE_ROW_COLOR);
+            setBackground(row % 2 == 0
+                ? ColorUtil.EVEN_TABLE_ROW_COLOR
+                : ColorUtil.ODD_TABLE_ROW_COLOR);
         }
 
         return defaultComp;
