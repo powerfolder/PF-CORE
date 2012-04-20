5df203149b4179f5af1ae38301fb2817faa190dc
Christian Sprajc <sprajc@powerfolder.com>
2012-04-20 00:49:39 +0000

    refs #2485 silent mode / pause

--- src/main/de/dal33t/powerfolder/ui/dialog/PauseDialog.java
+++ src/main/de/dal33t/powerfolder/ui/dialog/PauseDialog.java
@@ -45,14 +45,16 @@ public class PauseDialog extends BaseDialog {
 
     static {
         PAUSE_RESUME_VALUES = new TreeMap<Integer, String>();
-        PAUSE_RESUME_VALUES.put(5 * 60, Translation.getTranslation(
-                "pause_dialog.pause.5minutes"));
-        PAUSE_RESUME_VALUES.put(3600, Translation.getTranslation(
-                "pause_dialog.pause.1hour"));
-        PAUSE_RESUME_VALUES.put(8 * 3660, Translation.getTranslation(
-                "pause_dialog.pause.8hours"));
-        PAUSE_RESUME_VALUES.put(Integer.MAX_VALUE, Translation.getTranslation(
-                "pause_dialog.pause.permanent"));
+        PAUSE_RESUME_VALUES.put(0,
+            Translation.getTranslation("pause_dialog.pause.user_inactive"));
+        PAUSE_RESUME_VALUES.put(5 * 60,
+            Translation.getTranslation("pause_dialog.pause.5minutes"));
+        PAUSE_RESUME_VALUES.put(3600,
+            Translation.getTranslation("pause_dialog.pause.1hour"));
+        PAUSE_RESUME_VALUES.put(8 * 3660,
+            Translation.getTranslation("pause_dialog.pause.8hours"));
+        PAUSE_RESUME_VALUES.put(Integer.MAX_VALUE,
+            Translation.getTranslation("pause_dialog.pause.permanent"));
     }
 
     private JButton pauseButton;
@@ -72,23 +74,23 @@ public class PauseDialog extends BaseDialog {
     }
 
     protected JComponent getContent() {
-        pauseButton = new JButton(Translation.getTranslation(
-                "pause_dialog.button.text"));
+        pauseButton = new JButton(
+            Translation.getTranslation("pause_dialog.button.text"));
         pauseButton.setMnemonic(Translation
-                .getTranslation("pause_dialog.button.text").trim().charAt(0));
+            .getTranslation("pause_dialog.button.text").trim().charAt(0));
         pauseButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 pause();
             }
         });
 
-        neverAskAgainCB = new JCheckBox(Translation.getTranslation(
-                "general.neverAskAgain"));
+        neverAskAgainCB = new JCheckBox(
+            Translation.getTranslation("general.neverAskAgain"));
         neverAskAgainCB.setMnemonic(Translation.getTranslation(
-                "general.neverAskAgain").charAt(0));
+            "general.neverAskAgain").charAt(0));
 
         int pauseResumeSeconds = ConfigurationEntry.PAUSE_RESUME_SECONDS
-                .getValueInt(getController());
+            .getValueInt(getController());
         pauseValuesCombo = new JComboBox();
         int i = 0;
         for (Map.Entry<Integer, String> entry : PAUSE_RESUME_VALUES.entrySet())
@@ -101,12 +103,13 @@ public class PauseDialog extends BaseDialog {
         }
 
         FormLayout layout = new FormLayout("pref, 3dlu, pref",
-                "pref, 3dlu, pref");
+            "pref, 3dlu, pref");
         PanelBuilder builder = new PanelBuilder(layout);
         CellConstraints cc = new CellConstraints();
 
-        builder.add(new JLabel(Translation.getTranslation(
-                "pause_dialog.pause_label.text")), cc.xy(1, 1));
+        builder.add(
+            new JLabel(Translation
+                .getTranslation("pause_dialog.pause_label.text")), cc.xy(1, 1));
         builder.add(pauseValuesCombo, cc.xy(3, 1));
 
         builder.add(neverAskAgainCB, cc.xyw(1, 3, 3));
@@ -115,21 +118,21 @@ public class PauseDialog extends BaseDialog {
 
     private void pause() {
         if (neverAskAgainCB.isSelected()) {
-            PreferencesEntry.SHOW_ASK_FOR_PAUSE.setValue(getController(),
-                    false);
+            PreferencesEntry.SHOW_ASK_FOR_PAUSE
+                .setValue(getController(), false);
         }
 
         int i = 0;
         for (Integer pauseTime : PAUSE_RESUME_VALUES.keySet()) {
             if (i == pauseValuesCombo.getSelectedIndex()) {
                 ConfigurationEntry.PAUSE_RESUME_SECONDS.setValue(
-                        getController(), pauseTime);
+                    getController(), pauseTime);
                 getController().saveConfig();
                 break;
             }
             i++;
         }
-        
+
         getController().setPaused(!getController().isPaused());
 
         dialog.dispose();
