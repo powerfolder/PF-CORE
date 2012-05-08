ca9d175f7a3c0fde8be68ef094994082bf76411b
Christian Sprajc <sprajc@powerfolder.com>
2012-05-08 16:41:18 +0000

    refs #2485

---
+++ src/main/de/dal33t/powerfolder/message/FileRequestCommand.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2004 - 2012 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: ScanCommand.java 10350 2009-11-06 13:53:13Z tot $
+ */
+package de.dal33t.powerfolder.message;
+
+import de.dal33t.powerfolder.light.FolderInfo;
+import de.dal33t.powerfolder.util.Reject;
+
+/**
+ * Commando to trigger the file requesting on a specified folder.
+ * 
+ * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public class FileRequestCommand extends FolderRelatedMessage {
+    private static final long serialVersionUID = 101L;
+
+    public FileRequestCommand(FolderInfo foInfo) {
+        Reject.ifNull(foInfo, "Folder info is null");
+        folder = foInfo;
+    }
+}
---
+++ src/main/de/dal33t/powerfolder/message/FolderDBMaintCommando.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004 - 2012 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: ScanCommand.java 10350 2009-11-06 13:53:13Z tot $
+ */
+package de.dal33t.powerfolder.message;
+
+import java.util.Date;
+
+import de.dal33t.powerfolder.light.FolderInfo;
+import de.dal33t.powerfolder.util.Reject;
+
+/**
+ * Commando to trigger the db maintenance on a specified folder.
+ * 
+ * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
+ * @version $Revision: 1.5 $
+ */
+public class FolderDBMaintCommando extends FolderRelatedMessage {
+    private static final long serialVersionUID = 101L;
+    private Date date;
+
+    public FolderDBMaintCommando(FolderInfo foInfo, Date date) {
+        Reject.ifNull(foInfo, "Folder info is null");
+        this.folder = foInfo;
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
