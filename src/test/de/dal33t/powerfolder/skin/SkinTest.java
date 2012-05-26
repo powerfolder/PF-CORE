a0ef58ca9c8c41a6e14f5b154e5e776633755431
Christian Sprajc <sprajc@powerfolder.com>
2012-05-26 22:24:15 +0000

    refs #2776

--- src/test/de/dal33t/powerfolder/skin/SkinTest.java
+++ src/test/de/dal33t/powerfolder/skin/SkinTest.java
@@ -38,7 +38,7 @@ public class SkinTest extends TestCase {
         for (Iterator<Skin> it = skinLoader.iterator(); it.hasNext();) {
             skins.add(it.next());
         }
-        assertEquals(2, skins.size());
+        assertEquals(1, skins.size());
     }
 
     public void testIcons() {
