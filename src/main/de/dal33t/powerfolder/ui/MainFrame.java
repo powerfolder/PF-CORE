4d9414fa8168ea529fd10be58fe13f672f3a12f2
Christian Sprajc <sprajc@powerfolder.com>
2012-06-03 01:02:25 +0000

    don't show rounded up 100%

--- src/main/de/dal33t/powerfolder/ui/MainFrame.java
+++ src/main/de/dal33t/powerfolder/ui/MainFrame.java
@@ -644,7 +644,7 @@ public class MainFrame extends PFUIComponent {
         } else if (paused) {
             // Paused
             String s;
-            if (overallSyncPercentage >= 0 && overallSyncPercentage < 100.0d) {
+            if (overallSyncPercentage >= 0 && overallSyncPercentage < 99.5d) {
                 s = Format.formatDecimal(overallSyncPercentage) + '%';
             } else {
                 s = "";
@@ -653,7 +653,7 @@ public class MainFrame extends PFUIComponent {
             synced = true;
         } else if (syncing) {
             String s;
-            if (overallSyncPercentage >= 0 && overallSyncPercentage < 100.0d) {
+            if (overallSyncPercentage >= 0 && overallSyncPercentage < 99.5d) {
                 s = Format.formatDecimal(overallSyncPercentage) + '%';
             } else {
                 s = "...";
