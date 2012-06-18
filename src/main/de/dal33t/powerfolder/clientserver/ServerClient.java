2c7a88b8a37500409fe3e189349be19af2f08d48
Christian Sprajc <sprajc@powerfolder.com>
2012-06-18 17:45:05 +0000

    Minor

--- src/main/de/dal33t/powerfolder/clientserver/ServerClient.java
+++ src/main/de/dal33t/powerfolder/clientserver/ServerClient.java
@@ -1360,7 +1360,7 @@ public class ServerClient extends PFComponent {
         if (username != null && StringUtils.isNotBlank(passwordObf)) {
             try {
                 login(username, passwordObf);
-                getController().schedule(new HostingServerRetriever(), 0);
+                getController().schedule(new HostingServerRetriever(), 50L);
             } catch (Exception ex) {
                 logWarning("Unable to login. " + ex);
                 logFine(ex);
