9b7c43d087bce51439f110b3de9ec9b192007e3e
Christian Sprajc <sprajc@powerfolder.com>
2012-08-24 13:50:09 +0000

    PFS-605 #close done

--- src/main/de/dal33t/powerfolder/security/Account.java
+++ src/main/de/dal33t/powerfolder/security/Account.java
@@ -120,6 +120,11 @@ public class Account implements Serializable {
     @JoinColumn(name = "lastLoginFrom_id")
     private MemberInfo lastLoginFrom;
     private boolean proUser;
+    
+    // PFS-605
+    private String custom1;
+    private String custom2;
+    private String custom3;
 
     @Column(length = 1024)
     private String notes;
@@ -517,6 +522,30 @@ public class Account implements Serializable {
 
     public void setNotes(String notes) {
         this.notes = notes;
+    }
+
+    public String getCustom1() {
+        return custom1;
+    }
+
+    public void setCustom1(String custom1) {
+        this.custom1 = custom1;
+    }
+
+    public String getCustom2() {
+        return custom2;
+    }
+
+    public void setCustom2(String custom2) {
+        this.custom2 = custom2;
+    }
+
+    public String getCustom3() {
+        return custom3;
+    }
+
+    public void setCustom3(String custom3) {
+        this.custom3 = custom3;
     }
 
     /**
