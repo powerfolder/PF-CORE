494c01a773401b4029746126147def48d4f76164
Christian Sprajc <sprajc@powerfolder.com>
2012-08-27 09:50:57 +0000

    Version 5.2.47

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -136,7 +136,7 @@ public class Controller extends PFComponent {
     /**
      * Program version. include "dev" if its a development version.
      */
-    public static final String PROGRAM_VERSION = "6.0.44"; // 5.2.44
+    public static final String PROGRAM_VERSION = "6.0.47 - 5.2.47"; // 5.2.47
 
     /** general wait time for all threads (5000 is a balanced value) */
     private static final long WAIT_TIME = 5000;
