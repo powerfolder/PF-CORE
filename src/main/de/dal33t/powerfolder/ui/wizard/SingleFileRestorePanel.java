f3d47fa4c79c37e4cd3e2834c7f9e0bab57d3a6f
Harry Glasgow <glasgow@powerfolder.com>
2012-10-14 06:35:32 +0000

    PFC-2202 - Single-file restore.

--- src/main/de/dal33t/powerfolder/ui/wizard/SingleFileRestorePanel.java
+++ src/main/de/dal33t/powerfolder/ui/wizard/SingleFileRestorePanel.java
@@ -24,33 +24,56 @@ import jwf.WizardPanel;
 import javax.swing.*;
 
 import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.util.Translation;
 import de.dal33t.powerfolder.disk.Folder;
 import de.dal33t.powerfolder.ui.information.folder.files.versions.FileInfoVersionTypeHolder;
 import de.dal33t.powerfolder.light.FileInfo;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.builder.PanelBuilder;
 
 /**
  * Call this class via PFWizard.
  */
 public class SingleFileRestorePanel extends PFWizardPanel {
 
-    public SingleFileRestorePanel(Controller controller, Folder folder, FileInfo fileInfoToRestore,
-                                  FileInfoVersionTypeHolder selectedInfo) {
-        super(controller);
-    }
+    private final Folder folder;
+    private final FileInfo fileInfoToRestore;
+    private final FileInfoVersionTypeHolder selectedInfo; // Note - this may be null.
+
+    private final JLabel infoLabel;
 
     public SingleFileRestorePanel(Controller controller, Folder folder, FileInfo fileInfoToRestore) {
         this(controller, folder, fileInfoToRestore, null);
     }
 
+    public SingleFileRestorePanel(Controller controller, Folder folder, FileInfo fileInfoToRestore,
+                                  FileInfoVersionTypeHolder selectedInfo) {
+        super(controller);
+        this.folder = folder;
+        this.fileInfoToRestore = fileInfoToRestore;
+        this.selectedInfo = selectedInfo; // Note - this may be null.
+
+        infoLabel = new JLabel();
+    }
+
     protected JComponent buildContent() {
-        return null;
+        FormLayout layout = new FormLayout("140dlu, pref:grow", "pref, 3dlu, pref, 3dlu, pref");
+
+        PanelBuilder builder = new PanelBuilder(layout);
+        CellConstraints cc = new CellConstraints();
+
+        builder.add(infoLabel, cc.xyw(1, 1, 2));
+
+        return builder.getPanel();
     }
 
     protected String getTitle() {
-        return null;
+        return Translation.getTranslation("wizard.single_file_restore.title");
     }
 
     protected void initComponents() {
+        infoLabel.setText("Work in progress - PFC2202");
     }
 
     public boolean hasNext() {
