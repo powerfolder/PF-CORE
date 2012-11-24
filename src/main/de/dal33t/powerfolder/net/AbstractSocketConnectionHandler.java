7dc4ebbc582092f652ded5c0aea8ec1b6151b924
Christian Sprajc <sprajc@powerfolder.com>
2012-11-24 19:47:08 +0000

    logging

--- src/main/de/dal33t/powerfolder/net/AbstractSocketConnectionHandler.java
+++ src/main/de/dal33t/powerfolder/net/AbstractSocketConnectionHandler.java
@@ -552,7 +552,7 @@ public abstract class AbstractSocketConnectionHandler extends PFComponent
         senderSpawnLock.lock();
         try {
             messagesToSendQueue.offer(message);
-            if (messagesToSendQueue.size() > 100 && isWarning()) {
+            if (messagesToSendQueue.size() > 500 && isWarning()) {
                 String msg = "Many messages in send queue: "
                     + messagesToSendQueue.size() + ": " + messagesToSendQueue;
                 if (msg.length() > 300) {
