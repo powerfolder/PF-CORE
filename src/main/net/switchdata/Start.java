a546855cfeaf50f823c4dc4baa28bb64da6a8e5c
Christian Sprajc <sprajc@powerfolder.com>
2013-02-05 12:57:59 +0000

    SwitchData Start

---
+++ src/main/net/switchdata/Start.java
@@ -0,0 +1,10 @@
+package net.switchdata;
+
+import de.dal33t.powerfolder.PowerFolder;
+
+public class Start {
+    public static void main(String[] args) {
+        // Start PF
+        PowerFolder.startPowerFolder(args);
+    }
+}
