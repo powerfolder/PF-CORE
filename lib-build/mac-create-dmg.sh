45e9b6c0df0cfd0edda6d11207318c46ad849a6d
Maximilian Krickl <krickl@powerfolder.com>
2013-03-18 10:35:14 +0000

    PFC-2250 #comment Compress the disk image after packing everything

--- lib-build/mac-create-dmg.sh
+++ lib-build/mac-create-dmg.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-hdiutil create -size 15m -type UDIF -fs HFS+ -volname $2 -attach $1/$2_v$3.dmg
+DMG_NAME=$2_v$3
 
+echo "Creating uncompressed disk image: "
+hdiutil create -size 160m -fs HFS+ -volname $2 $1/$DMG_NAME-temp.dmg
+
+echo "Mounting uncompressed disk image: "
+hdiutil attach $1/$DMG_NAME-temp.dmg -readwrite -mount required
+
+echo "Putting app into disk image: "
 cp -R $1/$2.app /Volumes/$2/
 ln -s /Applications /Volumes/$2/Applications
 
-umount /Volumes/$2
+echo "Unmounting uncompressed image: "
+hdiutil detach /Volumes/$2
+
+echo "Compressing image: "
+hdiutil convert $1/$DMG_NAME-temp.dmg -format UDZO -imagekey zlib-level=9 -o $1/$DMG_NAME.dmg
+
+echo "Removing temporary disk image: "
+rm $1/$DMG_NAME-temp.dmg
