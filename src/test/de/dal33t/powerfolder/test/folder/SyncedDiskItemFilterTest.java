85522f60380b2e0a216cf6b3857a2efd3a48e0ac
Christian Sprajc <sprajc@powerfolder.com>
2013-03-19 00:58:14 +0000

    Test for synced ignore patterns

---
+++ src/test/de/dal33t/powerfolder/test/folder/SyncedDiskItemFilterTest.java
@@ -0,0 +1,38 @@
+package de.dal33t.powerfolder.test.folder;
+
+import de.dal33t.powerfolder.disk.SyncProfile;
+import de.dal33t.powerfolder.util.pattern.DefaultExcludes;
+import de.dal33t.powerfolder.util.test.ConditionWithMessage;
+import de.dal33t.powerfolder.util.test.TestHelper;
+import de.dal33t.powerfolder.util.test.TwoControllerTestCase;
+
+public class SyncedDiskItemFilterTest extends TwoControllerTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        connectBartAndLisa();
+        joinTestFolder(SyncProfile.AUTOMATIC_SYNCHRONIZATION);
+        assertEquals(DefaultExcludes.values().length, getFolderAtBart()
+            .getDiskItemFilter().getPatterns().size());
+        assertEquals(DefaultExcludes.values().length, getFolderAtLisa()
+            .getDiskItemFilter().getPatterns().size());
+    }
+
+    public void testSyncExcludes() {
+        final String testPattern = "xxx";
+        getFolderAtBart().addPattern(testPattern);
+        TestHelper.waitForCondition(10, new ConditionWithMessage() {
+            public boolean reached() {
+                return getFolderAtLisa().getDiskItemFilter().getPatterns()
+                    .contains(testPattern);
+            }
+
+            public String message() {
+                return "Lisa did not sync ignore patterns: "
+                    + getFolderAtLisa().getDiskItemFilter().getPatterns();
+            }
+        });
+    }
+
+}
