f967b301bb795f1e83dccaa912fc062657669f33
Maximilian Krickl <krickl@powerfolder.com>
2013-08-09 11:23:13 +0200

    PFS-837 #comment GPL header

--- src/main/de/dal33t/powerfolder/clientserver/OrganizationFilterModel.java
+++ src/main/de/dal33t/powerfolder/clientserver/OrganizationFilterModel.java
@@ -1,5 +1,23 @@
 package de.dal33t.powerfolder.clientserver;
 
+/*
+ * Copyright 2004 - 2013 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import com.jgoodies.binding.beans.Model;
 
 public class OrganizationFilterModel extends Model {
