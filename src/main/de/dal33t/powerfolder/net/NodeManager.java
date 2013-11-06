bd70ebfa1fd9bf61524e865c2877deb0a712df11
Christian Sprajc <sprajc@powerfolder.com>
2013-11-06 21:42:14 +0100

    Logging

--- src/main/de/dal33t/powerfolder/net/NodeManager.java
+++ src/main/de/dal33t/powerfolder/net/NodeManager.java
@@ -1092,7 +1092,7 @@ public class NodeManager extends PFComponent {
             // Only actually connect to other clients if logged into server.
             if (!client.isLoggedIn() && !client.isPrimaryServer(handler)) {
                 handler.shutdown();
-                logWarning("Not logged in at server ("
+                logFine("Not logged in at server ("
                     + client.getServer().getNick() + ") yet. Disconnecting: "
                     + handler.getIdentity());
                 throw new ConnectionException("Not logged in at server ("
