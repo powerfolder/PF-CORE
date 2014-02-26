7e4ed533473e16e42f0c4937ca0d21d8cd1b553d
Maximilian Krickl <krickl@powerfolder.com>
2014-02-26 16:27:23 +0100

    Use System-specific API to set Startup Item and Sidebar link in OS X

--- src/main/de/dal33t/powerfolder/util/os/mac/MacUtils.java
+++ src/main/de/dal33t/powerfolder/util/os/mac/MacUtils.java
@@ -19,7 +19,9 @@ s * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
  */
 package de.dal33t.powerfolder.util.os.mac;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
@@ -27,7 +29,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import de.dal33t.powerfolder.Controller;
-import de.dal33t.powerfolder.util.Util;
+import de.dal33t.powerfolder.net.HTTPProxySettings;
 import de.dal33t.powerfolder.util.logging.Loggable;
 import de.dal33t.powerfolder.util.os.OSUtil;
 
@@ -35,22 +37,21 @@ import de.dal33t.powerfolder.util.os.OSUtil;
  * Utilities for Mac OS X
  * 
  * @author <A HREF="mailto:bytekeeper@powerfolder.com">Dennis Waldherr</A>
+ * @author <a href="mailto:krickl@powerfolder.com">Maximilian Krickl</a>
  * @version $Revision$
  */
 public class MacUtils extends Loggable {
 
     private static MacUtils instance;
-    private static boolean error = false;
-    private static String placesHelperPath;
-
+//    private static boolean error = false;
     private MacUtils() {
     }
 
     /**
-     * @return true if this platform supports the winutils helpers.
+     * @return true if this platform supports the macutils helpers.
      */
     public static boolean isSupported() {
-        return getInstance() != null && !error;
+        return getInstance() != null;
     }
 
     /**
@@ -61,43 +62,52 @@ public class MacUtils extends Loggable {
             return null;
         }
 
-        if (instance == null && !error) {
+        if (instance == null) {
             if (OSUtil.isMacOS()) {
                 instance = new MacUtils();
-                error = !instance.init();
-            } else {
-                error = true;
             }
         }
         return instance;
     }
 
-    public void createPlacesLink(String lnkTarget) throws IOException {
-        Runtime.getRuntime().exec(new String[] {placesHelperPath, "p", lnkTarget});
-    }
+    /**
+     * Parse the system proxy configuration.
+     */
+    public void parseProxyConfig(Controller controller) {
+        try {
+            Process proxyEthernet = Runtime.getRuntime().exec(
+                new String[]{"networksetup", "-getwebproxy", "Ethernet"});
 
-    private boolean init() {
-        String fileName = "placeshelper";
-        Path targetFile = Controller.getTempFilesLocation().resolve(fileName);
-        try {
-            Files.deleteIfExists(targetFile);
-        } catch (IOException ioe) {
-            logWarning("Unable to delete the file '"
-                + targetFile.toAbsolutePath() + "'");
-            return false;
-        }
-        Path file = Util.copyResourceTo("mac/" + fileName, null, targetFile,
-            false, false);
-        placesHelperPath = file.toAbsolutePath().toString();
-        try {
-            Runtime.getRuntime().exec(
-                new String[]{"chmod", "+x", placesHelperPath});
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(proxyEthernet.getInputStream()))) {
+                String line = br.readLine();
+                if (line != null && !line.startsWith("Enabled: Yes")) {
+                    throw new UnsupportedOperationException();
+                }
+
+                String proxyHost = null;
+                int proxyPort = 0;
+
+                while (line != null) {
+                    if (line.startsWith("Server: ")) {
+                        proxyHost = line.split(" ")[1];
+                    } else if (line.startsWith("Port: ")) {
+                        proxyPort = Integer.valueOf(line.split(" ")[1])
+                            .intValue();
+                    }
+
+                    line = br.readLine();
+                }
+
+                HTTPProxySettings.saveToConfig(controller, proxyHost,
+                    proxyPort, null, null);
+            } catch (IOException e) {
+                logWarning("Parsing of ethernet webproxy failed. " + e, e);
+            } catch (UnsupportedOperationException uoe) {
+                logFine("No proxy configuration found.");
+            }
         } catch (IOException e) {
-            logWarning("Unable to initialize mac helper files. " + e);
-            return false;
+            logWarning("Could not parse proxy settings. " + e, e);
         }
-        return Files.exists(file);
-
     }
 
     /**
@@ -111,54 +121,64 @@ public class MacUtils extends Loggable {
     public void setPFPlaces(boolean setup, Controller controller)
         throws IOException
     {
+        if (!de.dal33t.powerfolder.jni.osx.Util.loaded) {
+            logFine("JNI bindings not loaded");
+            return;
+        }
+
+        Path baseDir = controller.getFolderRepository()
+            .getFoldersBasedir();
+
         if (setup) {
-            Path baseDir = controller.getFolderRepository()
-                .getFoldersBasedir();
-            createPlacesLink(baseDir.toAbsolutePath().toString());
+            logInfo("Setting Favorite item: " + baseDir.toAbsolutePath().toString());
+            de.dal33t.powerfolder.jni.osx.Util.addFavorite(baseDir.toAbsolutePath().toString());
         } else {
-            // TODO Remove link
+            de.dal33t.powerfolder.jni.osx.Util.removeFavorite(baseDir.toAbsolutePath().toString());
         }
     }
 
     public void setPFStartup(boolean setup, Controller controller)
         throws IOException
     {
-        if (setup) {
-            String bundleLocation = null;
+        if (!de.dal33t.powerfolder.jni.osx.Util.loaded) {
+            logFine("JNI bindings not loaded");
+            return;
+        }
+        String bundleLocation = null;
 
-            try {
-                Class<?> c = Class.forName("com.apple.eio.FileManager");
-                Method getPathToAppBundle = c
-                    .getMethod("getPathToApplicationBundle");
-                bundleLocation = (String) getPathToAppBundle.invoke(null);
-            } catch (ClassNotFoundException | NoSuchMethodException
-                | SecurityException | IllegalAccessException
-                | IllegalArgumentException | InvocationTargetException e)
-            {
-                throw new IOException(
-                    "Enabling start up item is not supported on your system. Your system is a "
-                        + System.getProperty("os.name") + " "
-                        + System.getProperty("os.version"));
-            }
-
-            Path pfile = Paths.get(bundleLocation);
+        try {
+            Class<?> c = Class.forName("com.apple.eio.FileManager");
+            Method getPathToAppBundle = c
+                .getMethod("getPathToApplicationBundle");
+            bundleLocation = (String) getPathToAppBundle.invoke(null);
+        } catch (ClassNotFoundException | NoSuchMethodException
+            | SecurityException | IllegalAccessException
+            | IllegalArgumentException | InvocationTargetException e)
+        {
+            throw new IOException(
+                "Enabling start up item is not supported on your system. Your system is a "
+                    + System.getProperty("os.name") + " "
+                    + System.getProperty("os.version"));
+        }
+        
+        Path pfile = Paths.get(bundleLocation).toAbsolutePath();
+        if (Files.notExists(pfile)) {
+            logFine("Reset bundle path");
+            pfile = Paths.get(
+                controller.getDistribution().getBinaryName() + ".app")
+                .toAbsolutePath();
             if (Files.notExists(pfile)) {
-                pfile = Paths.get(
-                    controller.getDistribution().getBinaryName() + ".app")
-                    .toAbsolutePath();
-                if (Files.notExists(pfile)) {
-                    throw new IOException("Couldn't find executable! "
-                        + "Note: Setting up a startup shortcut only works "
-                        + "when "
-                        + controller.getDistribution().getBinaryName()
-                        + " was started by " + pfile.getFileName());
-                }
+                throw new IOException("Couldn't find executable! "
+                    + "Note: Setting up a startup shortcut only works "
+                    + "when "
+                    + controller.getDistribution().getBinaryName()
+                    + " was started by " + pfile.getFileName());
             }
-            Runtime.getRuntime().exec(
-                new String[]{placesHelperPath, "s",
-                    pfile.toAbsolutePath().toString()});
+        }
+        if (setup) {
+            de.dal33t.powerfolder.jni.osx.Util.addLoginItem(pfile.toAbsolutePath().toString());
         } else {
-            // TODO Remove link
+            de.dal33t.powerfolder.jni.osx.Util.removeLoginItem(pfile.toAbsolutePath().toString());
         }
     }
 }
