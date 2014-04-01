3201c165a7fd77e555e99ed57038b6a2b7fed5b6
Christian Sprajc <sprajc@powerfolder.com>
2014-04-01 18:17:46 +0200

    done hide invite if not folder admin

--- src/main/de/dal33t/powerfolder/ui/folders/ExpandableFolderView.java
+++ src/main/de/dal33t/powerfolder/ui/folders/ExpandableFolderView.java
@@ -1165,7 +1165,7 @@ public class ExpandableFolderView extends PFUIComponent implements
             if (!getController().isBackupOnly()) {
                 boolean addedSeparator = false;
                 if (ConfigurationEntry.SERVER_INVITE_ENABLED
-                    .getValueBoolean(getController()))
+                    .getValueBoolean(getController()) && admin)
                 {
                     contextMenu.addSeparator();
                     addedSeparator = true;
