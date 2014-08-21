1b7e8545a9207beef4f9ece2218a012c78884198
Maximilian Krickl <krickl@powerfolder.com>
2014-08-21 17:34:23 +0200

    PFC-2395 #comment prerequisite

---
+++ src/main/de/dal33t/powerfolder/transfer/SyncStatus.java
@@ -0,0 +1,31 @@
+/*
+* Copyright 2004 - 2014 Christian Sprajc. All rights reserved.
+*
+* This file is part of PowerFolder.
+*
+* PowerFolder is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation.
+*
+* PowerFolder is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.dal33t.powerfolder.transfer;
+
+/**
+ * Identify the icon overlay by index and name.
+ * 
+ * @author <a href="mailto:krickl@powerfolder.com">Maximilian Krickl</a>
+ */
+public enum SyncStatus {
+    SYNC_OK,
+    SYNCING,
+    IGNORED,
+    WARNING,
+    LOCKED;
+}
