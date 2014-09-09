e953cc42f157836c206c6f8dc3009889094ae63c
Christian Sprajc <sprajc@powerfolder.com>
2014-09-09 16:13:46 +0200

    9.4.157

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -148,7 +148,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 9;
     private static final int MINOR_VERSION = 4;
-    private static final int REVISION_VERSION = 156;
+    private static final int REVISION_VERSION = 157;
 
     /**
      * Program version.
