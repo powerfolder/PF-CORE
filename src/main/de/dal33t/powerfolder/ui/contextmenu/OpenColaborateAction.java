26f97e1bebe7a2f14727f92b01f1a422d2216838
Maximilian Krickl <krickl@powerfolder.com>
2014-10-28 15:03:25 +0100

    Added comments

--- src/main/de/dal33t/powerfolder/ui/contextmenu/OpenColaborateAction.java
+++ src/main/de/dal33t/powerfolder/ui/contextmenu/OpenColaborateAction.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2004 - 2014 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.dal33t.powerfolder.ui.contextmenu;
 
 import java.awt.Desktop;
@@ -11,6 +28,11 @@ import de.dal33t.powerfolder.SyncStatus;
 import de.dal33t.powerfolder.light.FileInfo;
 import de.dal33t.powerfolder.ui.util.UIUtil;
 
+/**
+ * Open the selected file with the standard application of the OS and lock it.
+ * 
+ * @author <a href="mailto:krickl@powerfolder.com">Maximilian Krickl</a>
+ */
 class OpenColaborateAction extends PFContextMenuAction {
 
     private static final Logger log = Logger
--- src/main/de/dal33t/powerfolder/ui/contextmenu/VersionHistoryAction.java
+++ src/main/de/dal33t/powerfolder/ui/contextmenu/VersionHistoryAction.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2004 - 2014 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.dal33t.powerfolder.ui.contextmenu;
 
 import java.util.List;
@@ -7,6 +24,11 @@ import de.dal33t.powerfolder.light.FileInfo;
 import de.dal33t.powerfolder.ui.util.UIUtil;
 import de.dal33t.powerfolder.ui.wizard.PFWizard;
 
+/**
+ * Open the restore wizard to show the version history of a file.
+ * 
+ * @author <a href="mailto:krickl@powerfolder.com">Maximilian Krickl</a>
+ */
 class VersionHistoryAction extends PFContextMenuAction {
 
     VersionHistoryAction(Controller controller) {
