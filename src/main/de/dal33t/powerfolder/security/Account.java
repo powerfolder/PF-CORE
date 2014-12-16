bd2c55e4f085c0bb007a346e402bffbe5e62d311
Maximilian Krickl <krickl@powerfolder.com>
2014-12-16 12:02:55 +0100

    PFS-1500 #comment Use Account#addEmail to avoid duplicate email addresses. #resolve

--- src/main/de/dal33t/powerfolder/security/Account.java
+++ src/main/de/dal33t/powerfolder/security/Account.java
@@ -913,7 +913,9 @@ public class Account implements Serializable {
             this.addEmail(account.getUsername());
         }
 
-        this.emails.addAll(account.emails);
+        for (String email : account.emails) {
+            this.addEmail(email);
+        }
 
         // Use the OSSubscription that has provides more space
         if (account.getOSSubscription().getStorageSizeGB() > this.osSubscription.getStorageSizeGB()) {
