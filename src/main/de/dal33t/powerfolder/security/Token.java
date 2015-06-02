7d9b420cce80297764afffffdc489753741848df
Christian Sprajc <sprajc@powerfolder.com>
2015-06-02 18:27:15 +0200

    License info

--- src/main/de/dal33t/powerfolder/security/Token.java
+++ src/main/de/dal33t/powerfolder/security/Token.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2004 - 2015 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: Constants.java 11478 2010-02-01 15:25:42Z tot $
+ */
 package de.dal33t.powerfolder.security;
 
 import java.nio.ByteBuffer;
