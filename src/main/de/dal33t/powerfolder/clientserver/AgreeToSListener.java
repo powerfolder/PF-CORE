80ab9b8807d80edd5a7d19b1635b4c4c29334dc4
Maximilian Krickl <krickl@powerfolder.com>
2015-06-16 12:50:50 +0200

    Add License comment

--- src/main/de/dal33t/powerfolder/clientserver/AgreeToSListener.java
+++ src/main/de/dal33t/powerfolder/clientserver/AgreeToSListener.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2004 - 2015 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.dal33t.powerfolder.clientserver;
 
 import java.io.BufferedReader;
@@ -14,6 +31,9 @@ import de.dal33t.powerfolder.ui.dialog.GenericDialogType;
 import de.dal33t.powerfolder.util.BrowserLauncher;
 import de.dal33t.powerfolder.util.Translation;
 
+/**
+ * @author <a href="mailto:krickl@powerfolder.com">Maximilian Krickl</a>
+ */
 public class AgreeToSListener extends PFComponent implements ServerClientListener {
 
     private boolean wasPaused = false;
@@ -26,7 +46,6 @@ public class AgreeToSListener extends PFComponent implements ServerClientListene
 
     @Override
     public boolean fireInEventDispatchThread() {
-        // TODO Auto-generated method stub
         return false;
     }
 
