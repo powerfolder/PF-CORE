6eda8b40e2641b6134ebbae6fc2a20627d9175cd
Maximilian Krickl <krickl@powerfolder.com>
2015-09-22 16:03:47 +0200

    252

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -151,7 +151,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 10;
     private static final int MINOR_VERSION = 4;
-    private static final int REVISION_VERSION = 251;
+    private static final int REVISION_VERSION = 252;
 
     /**
      * Program version.
