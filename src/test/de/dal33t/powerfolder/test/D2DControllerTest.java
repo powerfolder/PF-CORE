e35afd107fd756309603b47363044eeb1b1faa75
Christoph Kappel <kappel@powerfolder.com>
2015-10-02 15:15:35 +0200

    DDC-16 #comment Added initial test case for D2D

---
+++ src/test/de/dal33t/powerfolder/test/D2DControllerTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: AddLicenseHeader.java 4282 2008-06-16 03:25:09Z tot $
+ */
+package de.dal33t.powerfolder.test;
+
+import de.dal33t.powerfolder.ConfigurationEntry;
+import de.dal33t.powerfolder.util.test.TwoControllerTestCase;
+
+public class D2DControllerTest extends TwoControllerTestCase
+{
+    @Override
+    protected void setUp() throws Exception {
+        ConfigurationEntry.D2D_ENABLED.setDefaultValue(String.valueOf(true));
+
+        super.setUp();
+    }
+
+    public void testNothing() throws Exception {
+        tryToConnectBartAndLisa();
+    }
+}
