927ab3c5deca21387053fe2d23f8926d3b81e147
Christoph Kappel <kappel@powerfolder.com>
2015-10-08 15:48:50 +0200

    DDC-16 #comment Updated D2D test case

--- src/test/de/dal33t/powerfolder/test/D2DControllerTest.java
+++ src/test/de/dal33t/powerfolder/test/D2DControllerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004 - 2008 Christian Sprajc. All rights reserved.
+ * Copyright 2004 - 2015 Christian Sprajc. All rights reserved.
  *
  * This file is part of PowerFolder.
  *
@@ -19,19 +19,76 @@
  */
 package de.dal33t.powerfolder.test;
 
+import java.util.logging.Level;
+
 import de.dal33t.powerfolder.ConfigurationEntry;
+import de.dal33t.powerfolder.net.ConnectionListener;
+import de.dal33t.powerfolder.util.logging.LoggingManager;
+import de.dal33t.powerfolder.util.test.TestHelper;
 import de.dal33t.powerfolder.util.test.TwoControllerTestCase;
 
 public class D2DControllerTest extends TwoControllerTestCase
 {
     @Override
     protected void setUp() throws Exception {
-        ConfigurationEntry.D2D_ENABLED.setDefaultValue(String.valueOf(true));
-
         super.setUp();
+
+        LoggingManager.setConsoleLogging(Level.ALL);
     }
 
-    public void testNothing() throws Exception {
-        tryToConnectBartAndLisa();
+    @Override
+    protected void startControllerBart() {
+        controllerBart = createControllerBart();
+        controllerBart.initTranslation();
+        controllerBart.loadConfigFile("build/test/ControllerBart/PowerFolder");
+
+        //ConfigurationEntry.NET_BROADCAST.setValue(controllerBart, true);
+        ConfigurationEntry.D2D_ENABLED.setValue(controllerBart, true);
+        ConfigurationEntry.D2D_PORT.setValue(controllerBart, 7331);
+
+        controllerBart.start();
+
+        TestHelper.addStartedController(controllerBart);
+        waitForStart(controllerBart);
+        assertNotNull(controllerBart.getConnectionListener());
+    }
+
+    @Override
+    protected void startControllerLisa() {
+        controllerLisa = createControllerLisa();
+        controllerLisa.initTranslation();
+        controllerLisa.loadConfigFile("build/test/ControllerLisa/PowerFolder");
+
+        //ConfigurationEntry.NET_BROADCAST.setValue(controllerLisa, true);
+        ConfigurationEntry.D2D_ENABLED.setValue(controllerLisa, true);
+        ConfigurationEntry.D2D_PORT.setValue(controllerLisa, 7332);
+
+        controllerLisa.start();
+
+        TestHelper.addStartedController(controllerLisa);
+        waitForStart(controllerLisa);
+        assertNotNull("Connection listener of lisa is null",
+            controllerLisa.getConnectionListener());
+    }
+
+    public void testD2DConnection() throws Exception {
+
+        try {
+            /* Find D2D listener of Bart */
+            for(ConnectionListener cl :
+                    controllerBart.getAdditionalConnectionListeners()) {
+
+                /* Connect Lisa to Bart via D2D */
+                if(cl.getPort() ==
+                        ConfigurationEntry.D2D_PORT.getValueInt(controllerBart)) {
+                    controllerLisa.connect(cl.getAddress(), true);
+                }
+            }
+        } catch (Exception e) {
+        }
+
+        Thread.sleep(5000);
+
+        disconnectBartAndLisa();
     }
 }
