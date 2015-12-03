7cb95dc059e87aa70ae14ff28416624de49df720
Maximilian Krickl <krickl@powerfolder.com>
2015-12-03 14:05:01 +0100

    11.0.0

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -151,9 +151,9 @@ public class Controller extends PFComponent {
     private static final Logger log = Logger.getLogger(Controller.class
         .getName());
 
-    private static final int MAJOR_VERSION = 10;
-    private static final int MINOR_VERSION = 4;
-    private static final int REVISION_VERSION = 321;
+    private static final int MAJOR_VERSION = 11;
+    private static final int MINOR_VERSION = 0;
+    private static final int REVISION_VERSION = 0;
 
     /**
      * Program version.
