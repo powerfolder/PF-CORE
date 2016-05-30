400ca348419588d6946b52254b870a2c36072ea9
Jan Wiegmann <wiegmann@powerfolder.com>
2016-05-30 11:09:40 +0200

    New OO DocumentServer states + session handling 10.6.401

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -153,7 +153,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 10;
     private static final int MINOR_VERSION = 6;
-    private static final int REVISION_VERSION = 400;
+    private static final int REVISION_VERSION = 401;
 
     /**
      * Program version.
