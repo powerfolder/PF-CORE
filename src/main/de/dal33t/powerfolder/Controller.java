02d36ddfc94b5a475f7847ba6c6756e3248260be
Jan Wiegmann <wiegmann@powerfolder.com>
2016-07-22 13:05:03 +0200

    PFS-1973 11.0.155

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -153,7 +153,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 0;
-    private static final int REVISION_VERSION = 154;
+    private static final int REVISION_VERSION = 155;
 
     /**
      * Program version.
