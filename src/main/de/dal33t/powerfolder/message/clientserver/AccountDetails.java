4e96800226a70c5dbc83ee2c96139dacf23bf588
Christoph Kappel <kappel@powerfolder.com>
2016-10-04 11:29:04 +0200

    Added D2D methods to AccountDetails

--- src/main/de/dal33t/powerfolder/message/clientserver/AccountDetails.java
+++ src/main/de/dal33t/powerfolder/message/clientserver/AccountDetails.java
@@ -21,6 +21,14 @@ package de.dal33t.powerfolder.message.clientserver;
 
 import java.io.Serializable;
 
+import com.google.protobuf.AbstractMessage;
+
+import de.dal33t.powerfolder.d2d.D2DObject;
+import de.dal33t.powerfolder.light.FileInfo;
+import de.dal33t.powerfolder.protocol.AbortDownloadProto;
+import de.dal33t.powerfolder.protocol.AccountDetailsProto;
+import de.dal33t.powerfolder.protocol.AccountProto;
+import de.dal33t.powerfolder.protocol.FileInfoProto;
 import de.dal33t.powerfolder.security.Account;
 import de.dal33t.powerfolder.util.Format;
 
@@ -30,7 +38,7 @@ import de.dal33t.powerfolder.util.Format;
  * @author <a href="mailto:totmacher@powerfolder.com">Christian Sprajc</a>
  * @version $Revision: 1.5 $
  */
-public class AccountDetails implements Serializable {
+public class AccountDetails implements Serializable, D2DObject {
     private static final long serialVersionUID = 100L;
 
     private Account user;
@@ -81,5 +89,44 @@ public class AccountDetails implements Serializable {
     public String toString() {
         return "AccountDetails, " + user + ". "
             + Format.formatBytesShort(spaceUsed + recycleBinSize);
+    }
+
+    /** initFromD2DMessage
+     * Init from D2D message
+     * @author Christoph Kappel <kappel@powerfolder.com>
+     * @param  mesg  Message to use data from
+     **/
+
+    @Override
+    public void initFromD2D(AbstractMessage mesg) {
+      if(mesg instanceof AccountDetailsProto.AccountDetails) {
+          AccountDetailsProto.AccountDetails proto = 
+              (AccountDetailsProto.AccountDetails)mesg;
+
+          this.user            = new Account(proto.getAccount());
+          this.spaceUsed       = proto.getSpaceUsed();
+          this.needsToAgreeToS = proto.getNeedsToAgreeToS();
+          this.recycleBinSize  = proto.getRecycleBinSize();
+      }
+    }
+
+    /** toD2
+     * Convert to D2D message
+     * @author Christoph Kappel <kappel@powerfolder.com>
+     * @return Converted D2D message
+     **/
+
+    @Override
+    public AbstractMessage toD2D() {
+      AccountDetailsProto.AccountDetails.Builder builder = 
+          AccountDetailsProto.AccountDetails.newBuilder();
+
+      builder.setClazzName(this.getClass().getSimpleName());
+      builder.setAccount((AccountProto.Account)this.user.toD2D());
+      builder.setSpaceUsed(this.spaceUsed);
+      builder.setNeedsToAgreeToS(this.needsToAgreeToS);
+      builder.setRecycleBinSize(this.recycleBinSize);
+
+      return builder.build();
     }
 }
