a8e9d8a2f508ab579b217fbce07d14d6e27453c8
Christian Sprajc <sprajc@powerfolder.com>
2016-10-09 21:28:42 +0200

    release 11.2

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -86,8 +86,8 @@ public class Controller extends PFComponent {
         .getName());
 
     private static final int MAJOR_VERSION = 11;
-    private static final int MINOR_VERSION = 1;
-    private static final int REVISION_VERSION = 227;
+    private static final int MINOR_VERSION = 2;
+    private static final int REVISION_VERSION = 228;
 
     /**
      * Program version.
