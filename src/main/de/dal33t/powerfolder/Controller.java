7521a2df313402574faa550316fbea8d7f4f4c10
Christian Sprajc <sprajc@powerfolder.com>
2016-10-11 00:06:15 +0200

    PFS-2195

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -87,7 +87,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 2;
-    private static final int REVISION_VERSION = 228;
+    private static final int REVISION_VERSION = 229;
 
     /**
      * Program version.
