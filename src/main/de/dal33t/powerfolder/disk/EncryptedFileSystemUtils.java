baeb6c6af1b9dc0557a854b8fee4d86e84bed691
Jan Wiegmann <wiegmann@powerfolder.com>
2016-11-08 18:05:30 +0100

    PFS-1994 #comment Bug fixes.
    Conflicts:
    	src/main/de/dal33t/powerfolder/disk/EncryptedFileSystemUtils.java
    	src/test/de/dal33t/powerfolder/test/encryptedStorage/EncryptedStorageTest.java

--- src/main/de/dal33t/powerfolder/disk/EncryptedFileSystemUtils.java
+++ src/main/de/dal33t/powerfolder/disk/EncryptedFileSystemUtils.java
@@ -1,16 +1,40 @@
+/*
+ * Copyright 2004 - 2016 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: Controller.java 21251 2013-03-19 01:46:23Z sprajc $
+ */
+
 package de.dal33t.powerfolder.disk;
 
 import de.dal33t.powerfolder.ConfigurationEntry;
 import de.dal33t.powerfolder.Constants;
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.util.IdGenerator;
+import de.dal33t.powerfolder.util.Reject;
 import org.cryptomator.cryptofs.CryptoFileSystemProperties;
 import org.cryptomator.cryptofs.CryptoFileSystemProvider;
 import org.cryptomator.cryptofs.CryptoFileSystemUris;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.*;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Helper class for working with the encrypted FileSystem from Cryptomator.
@@ -18,19 +42,9 @@ import java.nio.file.*;
 
 public class EncryptedFileSystemUtils {
 
-    private static FileSystem initCryptoFileSystem(Controller controller, Path encDir) throws IOException {
-
-        return CryptoFileSystemProvider.newFileSystem(
-                encDir,
-                CryptoFileSystemProperties.cryptoFileSystemProperties()
-                        .withPassphrase(ConfigurationEntry.ENCRYPTED_STORAGE_PASSPHRASE.getValue(controller))
-                        .build());
-    }
-
     public static Path initCryptoFS(Controller controller, Path encDir) throws IOException {
-
-        try {
-            URI encFolderUri = CryptoFileSystemUris.createUri(encDir);
+       try {
+            URI encFolderUri = CryptoFileSystemUris.createUri(encDir, encDir.toString());
             encDir = FileSystems.getFileSystem(encFolderUri).provider().getPath(encFolderUri);
             if (!Files.exists(encDir)){
                 Files.createDirectories(encDir);
@@ -47,32 +61,51 @@ public class EncryptedFileSystemUtils {
     }
 
     public static boolean isEncryptionActivated(Controller controller){
-
+        Reject.ifNull(controller, "controller");
         return ConfigurationEntry.ENCRYPTED_STORAGE.getValueBoolean(controller);
     }
 
-    public static boolean isEncryptedPath (Path path){
-
-        return path.toString().endsWith(Constants.FOLDER_ENCRYPTION_SUFFIX);
+    public static boolean isEncryptedPath(Path path){
+        Reject.ifNull(path, "Path");
+        return isEncryptedPath(path.toString());
     }
 
-    public static boolean isEncryptedPath (String path){
-
-        return path.endsWith(Constants.FOLDER_ENCRYPTION_SUFFIX);
+    public static boolean isEncryptedPath(String path){
+        Reject.ifNull(path, "Path");
+        return path.contains(Constants.FOLDER_ENCRYPTION_SUFFIX);
     }
 
     public static boolean isCryptoPathInstance(Path path){
-
+        Reject.ifNull(path, "Path");
         return path.getFileSystem().provider() instanceof CryptoFileSystemProvider;
     }
 
     public static void setEncryptionPassphrase(Controller controller){
-
+        Reject.ifNull(controller, "controller");
         if (!ConfigurationEntry.ENCRYPTED_STORAGE_PASSPHRASE.hasValue(controller)) {
             ConfigurationEntry.ENCRYPTED_STORAGE_PASSPHRASE.setValue(controller,
                     IdGenerator.makeId() + IdGenerator.makeId() + IdGenerator.makeId() + IdGenerator.makeId());
             controller.saveConfig();
         }
+    }
+
+    public static boolean checkJCEinstalled() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        int keyLength = Cipher.getInstance("AES/CBC/PKCS5Padding").getMaxAllowedKeyLength("AES");
+        if (keyLength == 128) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Internal helper ********************************************************
+
+    private static FileSystem initCryptoFileSystem(Controller controller, Path encDir) throws IOException {
+        return CryptoFileSystemProvider.newFileSystem(
+                encDir,
+                CryptoFileSystemProperties.cryptoFileSystemProperties()
+                        .withPassphrase(ConfigurationEntry.ENCRYPTED_STORAGE_PASSPHRASE.getValue(controller))
+                        .build());
     }
 
 }
--- src/test/de/dal33t/powerfolder/test/encryptedStorage/EncryptedStorageTest.java
+++ src/test/de/dal33t/powerfolder/test/encryptedStorage/EncryptedStorageTest.java
@@ -1,32 +1,154 @@
 package de.dal33t.powerfolder.test.encryptedStorage;
 
-import de.dal33t.powerfolder.util.test.ControllerTestCase;
+import de.dal33t.powerfolder.ConfigurationEntry;
+import de.dal33t.powerfolder.clientserver.FolderService;
+import de.dal33t.powerfolder.disk.EncryptedFileSystemUtils;
+import de.dal33t.powerfolder.disk.Folder;
+import de.dal33t.powerfolder.disk.FolderRepository;
 import de.dal33t.powerfolder.disk.SyncProfile;
-import org.cryptomator.cryptofs.CryptoFileSystemProperties;
-import org.cryptomator.cryptofs.CryptoFileSystemProvider;
-import org.junit.Test;
+import de.dal33t.powerfolder.light.FileInfo;
+import de.dal33t.powerfolder.util.PathUtils;
+import de.dal33t.powerfolder.util.test.ControllerTestCase;
 
+import javax.imageio.spi.ServiceRegistry;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.FileSystem;
+import java.io.ObjectOutputStream;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * JUnit Test for file encryption with cryptomator lib and cryptofs.
+ * JUnit Test for file encryption with cryptomator lib, cryptofs and PowerFolder.
  *
- * @author Jan Wiegmann <wiegmann@de.dal33t.powerfolder.test.encryptedStorage.com>
+ * @author Jan Wiegmann <wiegmann@powerfolder.com>
  * @since <pre>Aug 23, 2016</pre>
  */
 
 public class EncryptedStorageTest extends ControllerTestCase {
 
-    public void testStartFolderRepository() {
+    private Folder testFolder1;
+    private Path testFolder2;
 
-        getController().getFolderRepository().start();
+    @Override
+    public void setUp() throws Exception {
 
-        // Setup a test folder.
-        setupTestFolder(SyncProfile.HOST_FILES);
+        super.setUp();
+
+        // Setup encrypted folder.
+        setupEncryptedFolder();
+    }
+
+    public void setupEncryptedFolder() {
+
+        // Setup a encrypted test testFolder1.
+        getController().setPaused(true);
+        ConfigurationEntry.ENCRYPTED_STORAGE.setValue(super.getController(), true);
+        setupEncryptedTestFolder(SyncProfile.HOST_FILES);
+        testFolder1 = getFolder();
+
+        Path localBase = testFolder1.getLocalBase();
+
+        assertTrue(EncryptedFileSystemUtils.isCryptoPathInstance(localBase));
+
+        // Create a test.txt file
+        try {
+
+            Path testFile = localBase.resolve("test.txt");
+            Files.deleteIfExists(testFile);
+            Files.createFile(testFile);
+
+            // Create a text2.txt file in the 'sub' testFolder.
+            Path sub = localBase.resolve("sub");
+            Files.createDirectory(sub);
+            assertTrue(Files.exists(sub));
+
+            Path testFile2 = sub.resolve("test2.txt");
+            Files.deleteIfExists(testFile2);
+            Files.createFile(testFile2);
+
+            Path emptySub = localBase.resolve("emptySub");
+            Files.createDirectory(emptySub);
+            assertTrue(Files.exists(emptySub));
+
+            // Write a test files.
+            BufferedWriter writer = Files.newBufferedWriter(testFile, Charset.forName("UTF-8"));
+
+            writer.write("This is the test text.\n\nl;fjk sdl;fkjs dfljkdsf ljds flsfjd lsjdf lsfjdoi;ureffd dshf" +
+                    "\nhjfkluhgfidgh kdfghdsi8yt ribnv.,jbnfd kljhfdlkghes98o jkkfdgh klh8iesyt");
+
+            writer.flush();
+
+            writer = Files.newBufferedWriter(testFile2, Charset.forName("UTF-8"));
+            writer.write("This is the test2 text.\n\nl;fjk sdl;fkjs dfljkdsf ljds flsfjd lsjdf lsfjdoi;ureffd dshf" +
+                    "\nhjfkluhgfidgh kdfghdsi8yt ribnv.,jbnfd kljhfdlkghes98o jkkfdgh osdjft");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        testFolder2 = Paths.get(testFolder1.getLocalBase().toAbsolutePath().toString().replace(".crypto", "2.crypto"));
+
+    }
+
+    public void testMoveEncryptedFolder() throws IOException {
+
+        FolderRepository repository = getController().getFolderRepository();
+
+        try {
+
+            System.out.println("Before moving:");
+
+            Files.walk(testFolder1.getLocalBase())
+                    .forEach(p -> System.out.println(p));
+
+            Path oldLocalBase = testFolder1.getLocalBase();
+
+            testFolder1 = repository.moveLocalFolder(testFolder1, testFolder2);
+
+            scanFolder(testFolder1);
+
+            System.out.println("After moving:");
+
+            Files.walk(testFolder1.getLocalBase())
+                    .forEach(p -> System.out.println(p));
+
+            // The testFolder should have the test files plus 2 subdirs
+            assertEquals(4, testFolder1.getKnownItemCount());
+
+            // Sub dir should contain one file; test2.txt
+            Files.walk(testFolder2)
+                    .filter(p -> p.getFileName().toString().equals("sub") && Files.isDirectory(p))
+                    .forEach(p -> assertEquals(1, PathUtils.getNumberOfSiblings(p)));
+
+            // Since moveFolder method is NOT removing the old directory, this has to be true:
+            assertTrue("Old location still existing!:  " + oldLocalBase, Files.exists(oldLocalBase));
+
+        } catch (IOException e) {
+            //fail(e);
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testDeleteEncryptedFolder() throws IOException {
+
+        boolean isDirectoryEmpty = false;
+
+        FolderRepository repo = getController().getFolderRepository();
+
+        repo.removeFolder(testFolder1, true);
+
+        try {
+            Files.list(testFolder1.getLocalBase());
+        } catch (NoSuchFileException nsf){
+            isDirectoryEmpty = true;
+        }
+
+        assertTrue(isDirectoryEmpty);
+
     }
 
 }
