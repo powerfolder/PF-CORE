a5690f9039c8ce462ca7647c0777c0a0f754a1c8
Jan Wiegmann <wiegmann@powerfolder.com>
2016-12-08 15:08:58 +0100

    PFS-2207 #comment Fixed download link for files.

--- src/main/de/dal33t/powerfolder/DocumentType.java
+++ src/main/de/dal33t/powerfolder/DocumentType.java
@@ -10,14 +10,14 @@ public enum DocumentType {
     SPREADSHEET("xls", "xlsx", "ods", "csv"),
     PRESENTATION("ppt", "pptx", "odp"),
     IMAGE("png", "jpg", "jpeg", "gif", "bmp"),
-    AUDIO("wav", "mp3", "ogg", "oga", "webma", "fla", "flac", "m3u8a", "rtmpa"),
+    AUDIO("wav", "mp3", "ogg", "oga", "webma", "fla", "flac", "m3u8a", "rtmpa", "djvu"),
     VIDEO("mp4", "flv", "rtmp", "rtmpv", "m4v", "ogv", "webmv", "m3uv", "m3u8v"),
     PDF("pdf"),
     TEXT("txt", "rtf");
 
     private Collection<String> extensions;
 
-    private DocumentType(String... extensions) {
+    DocumentType(String... extensions) {
         this.extensions = Collections.unmodifiableCollection(
             Arrays.asList(extensions));
     }
