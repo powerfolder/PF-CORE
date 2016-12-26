42e23cb7801f062749b8b4de52781f821cdf7797
Christian Sprajc <sprajc@powerfolder.com>
2016-12-26 19:22:59 +0100

    PFS-2232

--- src/main/de/dal33t/powerfolder/util/StackDump.java
+++ src/main/de/dal33t/powerfolder/util/StackDump.java
@@ -2,4 +2,12 @@ package de.dal33t.powerfolder.util;
 
 public class StackDump extends RuntimeException {
     private static final long serialVersionUID = 100L;
+
+    public StackDump() {
+        super();
+    }
+
+    public StackDump(String message) {
+        super(message);
+    }
 }
