919afa1c60e3c28704cf487a023f676e1a283e3a
Maximilian Krickl <krickl@powerfolder.com>
2016-12-29 13:40:40 +0100

    PFS-2188 #comment Extend JSON setter and getter

--- src/main/de/dal33t/powerfolder/security/Organization.java
+++ src/main/de/dal33t/powerfolder/security/Organization.java
@@ -25,6 +25,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Logger;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -62,6 +63,8 @@ import org.json.JSONObject;
 @Entity
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Organization implements Serializable {
+
+    private static final Logger LOG = Logger.getLogger(Organization.class.getName());
     private static final long serialVersionUID = 100L;
 
     public static final int UNLIMITED_USERS = 999999999;
@@ -223,7 +226,21 @@ public class Organization implements Serializable {
         if (jsonData == null) {
             return new JSONObject();
         }
-        return new JSONObject(jsonData);
+        try {
+            return new JSONObject(jsonData);
+        } catch (JSONException e) {
+            LOG.severe("Illegal JSON data for " + name + ": " + jsonData
+                + ". " + e);
+            return new JSONObject();
+        }
+    }
+
+    public void setJSONObject(JSONObject jsonObject) {
+        if (jsonObject == null) {
+            this.jsonData = null;
+            return;
+        }
+        this.jsonData = jsonObject.toString();
     }
 
     /**
