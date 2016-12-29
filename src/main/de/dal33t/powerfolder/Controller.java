ad3e9b1a7bb77ae8912ed30fc82f23beae05a764
Maximilian Krickl <krickl@powerfolder.com>
2016-12-29 22:19:40 +0100

    several tickets

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -153,7 +153,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 2;
-    private static final int REVISION_VERSION = 299;
+    private static final int REVISION_VERSION = 300;
 
     /**
      * Program version.
