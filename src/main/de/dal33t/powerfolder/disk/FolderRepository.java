24a1911233d44cb78a8c76bdccd45e3aff744698
Jan Wiegmann <wiegmann@powerfolder.com>
2017-02-02 14:39:30 +0100

    PFS-2026 PFS-2225 PFS-2227 #comment Fixed a big bug for owner permission handover to another user.
    Conflicts:
    	src/test/de/dal33t/powerfolder/util/test/TwoControllerTestCase.java

--- src/main/de/dal33t/powerfolder/disk/FolderRepository.java
+++ src/main/de/dal33t/powerfolder/disk/FolderRepository.java
@@ -2542,7 +2542,7 @@ public class FolderRepository extends PFComponent implements Runnable {
 
         newDirectory = PathUtils.removeInvalidFilenameChars(newDirectory);
 
-        if (Files.exists(newDirectory)) {
+        if (Files.exists(newDirectory) && !PathUtils.isEmptyDir(newDirectory)) {
             logSevere("Not moving folder " + folder + " to new directory "
                 + newDirectory.toString()
                 + ". The new directory already exists!");
