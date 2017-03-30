db9a22a7adfcbe7d738b53c51e3179dba4d963a4
Christian Sprajc <sprajc@powerfolder.com>
2017-03-31 00:39:40 +0200

    Better texts/documentation for networking mode

--- src/main/de/dal33t/powerfolder/NetworkingMode.java
+++ src/main/de/dal33t/powerfolder/NetworkingMode.java
@@ -19,49 +19,23 @@
  */
 package de.dal33t.powerfolder;
 
-/**
- * One of 3 networking modes.
- * <UL>
- * <LI>LANONLYMODE : Connect only to PowerFolder clients in the Local Area
- * Network.<BR>
- * The only connection out will be to the update check site.<BR>
- * Your firewall will maybe detect an outgoing connection to 224.0.0.1 or
- * ALL-SYSTEMS.MCAST.NET<BR>
- * We use that to detect the other PowerFolder clients in your LAN.</LI>
- * <LI>PRIVATEMODE : Disables public folder sharing. Restricts connectivity to
- * interesting users only.<BR>
- * Actually only connects to friends, users on LAN and people, who are on joined
- * folders.<BR>
- * Further PowerFolder connects to some other users so so the finding of your
- * friends in the network is posible.</LI>
- * <LI>PUBLICMODE : NO LONGER AVAILABLE<BR>
- * Private folders will always require an Invitation, regardless of the
- * networking mode.</LI>
- * </UL>
-  *
- * The names are used as configuration entry values, so should not be changed.
- */
 public enum NetworkingMode {
     /**
-     * Disables public folder sharing. Restricts connectivity to interesting
-     * users only.<BR>
-     * Actually only connects to friends, users on LAN and people, who are on
-     * joined folders.
+     * Connects to and synchronizes with server and other clients on LAN and internet.
      */
     PRIVATEMODE,
 
     /**
-     * Restricts connectivity to the server(s) only. This actually disables any
-     * peer-to-peer traffic and keeps the connection to the set server(s) only.
-     */
-    SERVERONLYMODE,
-
-    /**
-     * Connect only to PowerFolder clients in the Local Area Network.<BR>
-     * The only connection out will be to the update check site.<BR>
+     * Connects to and synchronizes with server and other clients on LAN only.
+     *
      * Your firewall will maybe detect an outgoing connection to 224.0.0.1 or
      * ALL-SYSTEMS.MCAST.NET<BR>
      * We use that to detect the other PowerFolder clients in your LAN.
      */
-    LANONLYMODE
+    LANONLYMODE,
+
+    /**
+     * Connects to and synchronizes with server only. Disables any peer-to-peer connections.
+     */
+    SERVERONLYMODE
 }
