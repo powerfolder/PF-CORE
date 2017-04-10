d21fcd4ab5348580abeacbf5f31036964760a693
Christian Sprajc <sprajc@powerfolder.com>
2017-04-10 15:21:12 +0200

    MD5 speed test

--- src/test/de/dal33t/powerfolder/test/util/UtilTest.java
+++ src/test/de/dal33t/powerfolder/test/util/UtilTest.java
@@ -19,8 +19,11 @@
  */
 package de.dal33t.powerfolder.test.util;
 
+import de.dal33t.powerfolder.util.IdGenerator;
 import junit.framework.TestCase;
 import de.dal33t.powerfolder.util.Util;
+
+import java.io.UnsupportedEncodingException;
 
 public class UtilTest extends TestCase {
 
@@ -33,4 +36,32 @@ public class UtilTest extends TestCase {
         assertFalse(Util.betweenVersion("8.1.0", "8.2.1", "8.2.0"));
     }
 
+    public void testMD5Multiple() {
+        int n = 250000;
+        for (int i = 0; i < n; i++) {
+            testMD5();
+        }
+    }
+
+    public void testMD5() {
+        String magicId = IdGenerator.makeId();
+        String id = IdGenerator.makeFolderId();
+
+        // Do the magic...
+        try {
+            byte[] mId = magicId.getBytes("UTF-8");
+            byte[] fId = id.getBytes("UTF-8");
+            byte[] hexId = new byte[mId.length * 2 + fId.length];
+
+            // Build secure ID base: [MAGIC_ID][FOLDER_ID][MAGIC_ID]
+            System.arraycopy(mId, 0, hexId, 0, mId.length);
+            System.arraycopy(fId, 0, hexId, mId.length - 1, fId.length);
+            System.arraycopy(mId, 0, hexId, mId.length + fId.length - 2,
+                    mId.length);
+            new String(Util.encodeHex(Util.md5(hexId)));
+        } catch (UnsupportedEncodingException e) {
+            throw (IllegalStateException) new IllegalStateException(
+                    "Fatal problem: UTF-8 encoding not found").initCause(e);
+        }
+    }
 }
