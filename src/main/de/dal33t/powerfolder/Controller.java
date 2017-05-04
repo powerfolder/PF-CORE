86743699e43312bb722cd0376f9101b60715600e
Maximilian Krickl <krickl@powerfolder.com>
2017-05-04 15:29:30 +0200

    PFS-2363 11.4.424

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -87,7 +87,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 4;
-    private static final int REVISION_VERSION = 423;
+    private static final int REVISION_VERSION = 424;
 
     /**
      * Program version.
