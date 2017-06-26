8909cd943be3486eb1bac76db3a2ad1ceda0bcb6
Christian Sprajc <sprajc@powerfolder.com>
2017-06-26 17:02:53 +0200

    PFC-2956 #comment set reuse address

--- src/main/de/dal33t/powerfolder/net/ConnectionListener.java
+++ src/main/de/dal33t/powerfolder/net/ConnectionListener.java
@@ -151,6 +151,7 @@ public class ConnectionListener extends PFComponent implements Runnable {
             }
             serverSocket = new ServerSocket(port,
                 Constants.MAX_INCOMING_CONNECTIONS, bAddress);
+            serverSocket.setReuseAddress(true);
         } catch (Exception e) {
             throw new ConnectionException(Translation.get(
                 "dialog.unable_to_open_port", port + ""), e);
