3f9def7fc6fb26bb68fb60f858770790db477fc9
Christian Oberdörfer <oberdoerfer@powerfolder.com>
2017-10-26 14:48:18 +0200

    PF-849 19

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -93,7 +93,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 14;
     private static final int MINOR_VERSION = 0;
-    private static final int REVISION_VERSION = 18;
+    private static final int REVISION_VERSION = 19;
 
     /**
      * Program version.
