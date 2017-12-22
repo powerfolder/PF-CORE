793bc9ef26708d6e411b783ac98e25477a8541c1
Jan Wiegmann <wiegmann@powerfolder.com>
2017-12-22 14:03:09 +0100

    PF-615 / PF-102 11.5.618

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -91,7 +91,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 5;
-    private static final int REVISION_VERSION = 617;
+    private static final int REVISION_VERSION = 618;
 
     /**
      * Program version.
