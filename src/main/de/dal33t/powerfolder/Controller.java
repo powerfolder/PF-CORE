39241543742bb3750cdb0cf7b3dc79f1cba7762f
Jan Wiegmann <wiegmann@powerfolder.com>
2018-03-27 14:34:18 +0200

    11.6.705 #comment Account activation date bug fix for account API call setActivationDateAll

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -91,7 +91,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 6;
-    private static final int REVISION_VERSION = 704;
+    private static final int REVISION_VERSION = 705;
 
     /**
      * Program version.
