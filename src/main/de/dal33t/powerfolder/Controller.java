8a94949b74bb05833a77f6dc14990fa80aa2dc9f
Jan Wiegmann <wiegmann@powerfolder.com>
2018-11-28 14:41:53 +0100

    Client/Server connection with self signed SSL certs

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -91,7 +91,6 @@ public class Controller extends PFComponent {
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 7;
     private static final int REVISION_VERSION = 766;
-    private static final int REVISION_VERSION = 765;
     private static final int SPRINT_NUMBER = 2;
 
     /**
