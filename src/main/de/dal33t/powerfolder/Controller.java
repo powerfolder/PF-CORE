7881b594a3b79573368a6f09320c346b9e1d3ac3
Christian Oberdörfer <oberdoerfer@powerfolder.com>
2019-01-02 13:11:21 +0100

    Increased revision number to 769 and sprint number to 4 to mark increment
    # Conflicts:
    #	src/main/de/dal33t/powerfolder/Controller.java

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -90,8 +90,8 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 7;
-    private static final int REVISION_VERSION = 768;
-    private static final int SPRINT_NUMBER = 3;
+    private static final int REVISION_VERSION = 769;
+    private static final int SPRINT_NUMBER = 4;
 
     /**
      * Program version.
