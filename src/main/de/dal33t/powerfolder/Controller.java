6e3bd3c33faa411126dc921343d5a161a5f1540d
Christian Oberdörfer <oberdoerfer@powerfolder.com>
2019-03-11 12:14:25 +0100

    Release of version 14.3.0

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -89,8 +89,8 @@ public class Controller extends PFComponent {
         .getName());
 
     private static final int MAJOR_VERSION = 14;
-    private static final int MINOR_VERSION = 2;
-    private static final int REVISION_VERSION = 12;
+    private static final int MINOR_VERSION = 3;
+    private static final int REVISION_VERSION = 0;
     private static final int SPRINT_NUMBER = 8;
 
     /**
