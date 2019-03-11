ece9aa79bfdc43f9f03e49d9800bd751b0cb23bc
Christian Oberdörfer <oberdoerfer@powerfolder.com>
2019-03-11 17:39:22 +0100

    Revert "Increased revision number to 1 and sprint number to 9 to mark increment"
    This reverts commit b17ec91f89577a5a45e833234ad6ee6c797cbe56.

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -90,8 +90,8 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 14;
     private static final int MINOR_VERSION = 3;
-    private static final int REVISION_VERSION = 1;
-    private static final int SPRINT_NUMBER = 9;
+    private static final int REVISION_VERSION = 0;
+    private static final int SPRINT_NUMBER = 8;
 
     /**
      * Program version.
