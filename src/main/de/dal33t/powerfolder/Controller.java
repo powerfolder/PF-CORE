5901b682e4a3647c93aed8666e3f61466c3f5b06
Christian Oberdörfer <oberdoerfer@powerfolder.com>
2019-04-18 16:08:38 +0200

    Increased revision number to 2 and sprint number to 10 to mark increment

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -90,8 +90,8 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 11;
     private static final int MINOR_VERSION = 8;
-    private static final int REVISION_VERSION = 1;
-    private static final int SPRINT_NUMBER = 6;
+    private static final int REVISION_VERSION = 2;
+    private static final int SPRINT_NUMBER = 10;
 
     /**
      * Program version.
