3b07906c885aa251018a5cc1a1a57813cb287acf
Christian Oberdörfer <oberdoerfer@powerfolder.com>
2019-06-11 10:36:20 +0200

    Increased revision number to 6 after hotfix to mark increment

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -90,7 +90,7 @@ public class Controller extends PFComponent {
 
     private static final int MAJOR_VERSION = 14;
     private static final int MINOR_VERSION = 4;
-    private static final int REVISION_VERSION = 4;
+    private static final int REVISION_VERSION = 6;
     private static final int SPRINT_NUMBER = 13;
 
     /**
