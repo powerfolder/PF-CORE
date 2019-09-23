92f7d16a9c02ac2c346a73a39b1419baeb4a3a88
Christian Sprajc <sprajc@powerfolder.com>
2019-09-23 12:17:50 +0200

    Corrected sprint number

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -91,7 +91,7 @@ public class Controller extends PFComponent {
     private static final int MAJOR_VERSION = 14;
     private static final int MINOR_VERSION = 8;
     private static final int REVISION_VERSION = 0;
-    private static final int SPRINT_NUMBER = 18;
+    private static final int SPRINT_NUMBER = 17;
 
     /**
      * Program version.
