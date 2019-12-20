e1474a54c525f56683e175f0d585734892a5e6df
Christian Sprajc <sprajc@powerfolder.com>
2019-12-20 22:39:28 +0100

    Logging

--- src/main/de/dal33t/powerfolder/transfer/TransferManager.java
+++ src/main/de/dal33t/powerfolder/transfer/TransferManager.java
@@ -1237,7 +1237,7 @@ public class TransferManager extends PFComponent {
             return null;
         }
         if (!folder.hasReadPermission(from)) {
-            logWarning("No Read permission: " + from + " on " + folder);
+            logWarning("No Read permission for " + from.getAccountInfo() + " device " + from + " on " + folder);
             return null;
         }
 
