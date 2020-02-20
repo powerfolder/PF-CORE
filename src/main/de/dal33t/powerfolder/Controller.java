aeeb7eab318b406392ad0a73f4b3f86d6966fcef
Christian Sprajc <sprajc@powerfolder.com>
2020-02-20 21:55:55 +0100

    15.1.1 DEV

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -88,8 +88,8 @@ public class Controller extends PFComponent {
     private static final Logger log = Logger.getLogger(Controller.class.getName());
 
     private static final int MAJOR_VERSION = 15;
-    private static final int MINOR_VERSION = 0;
-    private static final int REVISION_VERSION = 101;
+    private static final int MINOR_VERSION = 1;
+    private static final int REVISION_VERSION = 1;
     private static final int SPRINT_NUMBER = 22;
 
     /**
