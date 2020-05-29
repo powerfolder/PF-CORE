96131c430bf0a70b0236e0f3346e3b4a1626ea27
Christian Sprajc <sprajc@powerfolder.com>
2020-05-29 23:54:44 +0200

    logging

--- src/main/de/dal33t/powerfolder/disk/FileArchiver.java
+++ src/main/de/dal33t/powerfolder/disk/FileArchiver.java
@@ -119,11 +119,7 @@ public class FileArchiver {
 
         if (Files.exists(target)) {
             // PFS-1794: Happens 2136x
-            if (log.isLoggable(Level.WARNING)
-                    && !fileInfo.inSyncWithDisk(target)) {
-                log.warning("File " + fileInfo.toDetailString()
-                        + " seems to be archived already, doing nothing.");
-            } else if (log.isLoggable(Level.FINE)) {
+            if (log.isLoggable(Level.FINE) {
                 log.fine("File " + fileInfo.toDetailString()
                         + " seems to be archived already, doing nothing.");
             }
