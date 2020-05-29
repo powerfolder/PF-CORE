96c60301fb126db68c3123d3b24acf37ed6795cb
Christian Sprajc <sprajc@powerfolder.com>
2020-05-30 00:19:46 +0200

    logging

--- src/main/de/dal33t/powerfolder/util/StackDump.java
+++ src/main/de/dal33t/powerfolder/util/StackDump.java
@@ -1,10 +1,17 @@
 package de.dal33t.powerfolder.util;
 
+import java.util.Date;
+
 public class StackDump extends RuntimeException {
     private static final long serialVersionUID = 100L;
+    private Date created = new Date();
 
     public StackDump() {
         super();
+    }
+
+    public Date getCreated() {
+        return created;
     }
 
     public StackDump(String message) {
