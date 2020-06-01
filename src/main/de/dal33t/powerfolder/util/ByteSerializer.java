36d480378e74eef39879c4fefc4343ab03cb97c9
Christian Sprajc <sprajc@powerfolder.com>
2020-06-01 19:14:30 +0200

    logging

--- src/main/de/dal33t/powerfolder/util/ByteSerializer.java
+++ src/main/de/dal33t/powerfolder/util/ByteSerializer.java
@@ -275,14 +275,14 @@ public class ByteSerializer extends Loggable {
             } catch (StreamCorruptedException e2) {
                 LOG.log(Level.WARNING, "While deserializing "
                     + (expectCompression ? "   compressed" : "uncompressed")
-                    + ": " + e, e);
+                    + ": " + e);
                 if (!e2.toString().toLowerCase()
                     .contains("invalid stream header: 1f8b0800"))
                 {
                     LOG.log(Level.WARNING, "While deserializing "
                         + (!expectCompression
                             ? "   compressed"
-                            : "uncompressed") + ": " + e2, e2);
+                            : "uncompressed") + ": " + e2);
                 }
                 throw e2;
             } catch (InvalidClassException e2) {
