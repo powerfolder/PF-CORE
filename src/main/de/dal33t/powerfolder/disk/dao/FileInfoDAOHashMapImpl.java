8e3e66cf90f4387febc48e30de0dfbfbaa82ed46
Christian Sprajc <sprajc@powerfolder.com>
2020-06-02 12:11:26 +0200

    PFS-3616 more logging

--- src/main/de/dal33t/powerfolder/disk/dao/FileInfoDAOHashMapImpl.java
+++ src/main/de/dal33t/powerfolder/disk/dao/FileInfoDAOHashMapImpl.java
@@ -6,6 +6,7 @@ import de.dal33t.powerfolder.light.DirectoryInfo;
 import de.dal33t.powerfolder.light.FileHistory;
 import de.dal33t.powerfolder.light.FileInfo;
 import de.dal33t.powerfolder.util.Reject;
+import de.dal33t.powerfolder.util.StackDump;
 import de.dal33t.powerfolder.util.StringUtils;
 import de.dal33t.powerfolder.util.Util;
 import de.dal33t.powerfolder.util.logging.Loggable;
@@ -273,14 +274,21 @@ public class FileInfoDAOHashMapImpl extends Loggable implements FileInfoDAO {
 
             if (fileInfo.isFile()) {
                 if (isInfo()) {
-                    logInfo("Storing file: " + fileInfo.toDetailString());
+                    if (fileInfo.getModifiedByAccount() != null) {
+                        logInfo("Storing file: " + fileInfo.toDetailString());
+                    } else {
+                        logWarning("Storing file without account " + fileInfo.toDetailString(), new StackDump());
+                    }
+
                 }
                 d.files.put(fileInfo, fileInfo);
                 // Make sure not dir is left with name name.
                 d.directories.remove(fileInfo);
             } else {
-                if (isInfo()) {
+                if (fileInfo.getModifiedByAccount() != null) {
                     logInfo("Storing directory: " + fileInfo.toDetailString());
+                } else {
+                    logWarning("Storing directory without account " + fileInfo.toDetailString(), new StackDump());
                 }
                 d.directories.put((DirectoryInfo) fileInfo,
                     (DirectoryInfo) fileInfo);
