d26e414d22cefd1dd8233980f836a936af0e7453
sprajc <sprajc@powerfolder.com>
2020-10-12 21:06:27 +0200

    PFS-3707

--- src/main/de/dal33t/powerfolder/disk/Folder.java
+++ src/main/de/dal33t/powerfolder/disk/Folder.java
@@ -1413,9 +1413,12 @@ public class Folder extends PFComponent {
                         }
                         Member fromDevice = modByDevice.getNode(
                             getController(), false);
-                        AccountInfo modByAccount = fromDevice != null
-                            ? fromDevice.getAccountInfo()
-                            : null;
+                        AccountInfo modByAccount = fInfo.getModifiedByAccount();
+                        if (modByAccount == null) {
+                            modByAccount = fromDevice != null
+                                    ? fromDevice.getAccountInfo()
+                                    : null;
+                        }
                         if (modByAccount == null) {
                             modByAccount = getMySelf().getAccountInfo();
                         }
