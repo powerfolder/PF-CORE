ee1dce361f70f74f770d8d97ef921865541aae97
Christian Sprajc <sprajc@powerfolder.com>
2021-02-17 21:06:38 +0100

    PFS-3519 added fields to DB

--- src/main/de/dal33t/powerfolder/security/Organization.java
+++ src/main/de/dal33t/powerfolder/security/Organization.java
@@ -127,6 +127,13 @@ public class Organization implements Serializable {
     @Fetch(FetchMode.JOIN)
     private CustomFields customFields;
 
+    @Column(length = 10)
+    private String color1;
+    @Column(length = 10)
+    private String color2;
+    @Column(length = 10)
+    private String color3;
+
     public Organization() {
         // Generate unique id
         this(IdGenerator.makeId());
@@ -324,6 +331,30 @@ public class Organization implements Serializable {
             customFields = new CustomFields();
         }
         return customFields;
+    }
+
+    public void setColor1(String color1) {
+        this.color1 = color1;
+    }
+
+    public void setColor2(String color2) {
+        this.color2 = color2;
+    }
+
+    public void setColor3(String color3) {
+        this.color3 = color3;
+    }
+
+    public String getColor1() {
+        return color1;
+    }
+
+    public String getColor2() {
+        return color2;
+    }
+
+    public String getColor3() {
+        return color3;
     }
 
     @Override
