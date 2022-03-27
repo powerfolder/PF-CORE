448c75bc0b76b7b224f638f24d0b9d92758a8b28
Christian Sprajc <sprajc@powerfolder.com>
2022-03-27 22:59:23 +0200

    PFC-3344

--- src/etc/Elevate.cmd
+++ src/etc/Elevate.cmd
@@ -60,3 +60,5 @@
  REM Run shell as admin (example) - put here code as you like
  :: ECHO %batchName% Arguments: P1=%1 P2=%2 P3=%3 P4=%4 P5=%5 P6=%6 P7=%7 P8=%8 P9=%9
  cmd /c "%1 %2 %3 %4 %5 %6 %7" /S
+ ECHO %batchName% Arguments: P1=%1 P2=%2 P3=%3 P4=%4 P5=%5 P6=%6 P7=%7 P8=%8 P9=%9
+ cmd /k
