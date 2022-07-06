969caf6676858f1a37774836bd1c8be86911727f
sprajc <sprajc@powerfolder.com>
2022-07-06 15:35:00 +0200

    PFS-3787

--- src/main/de/dal33t/powerfolder/message/Invitation.java
+++ src/main/de/dal33t/powerfolder/message/Invitation.java
@@ -371,12 +371,19 @@ public class Invitation extends FolderRelatedMessage implements Auditable
 
     @Override
     public void setCreatedNowBy(Account caller) {
-        this.auditFields.setCreatedNowBy(caller);
+        this.auditFields().setCreatedNowBy(caller);
     }
 
     @Override
     public void setModifiedNowBy(Account caller) {
-        this.auditFields.setModifiedNowBy(caller);
+        this.auditFields().setModifiedNowBy(caller);
+    }
+
+    private AuditFields auditFields() {
+        if (this.auditFields == null) {
+            this.auditFields = new AuditFields();
+        }
+        return auditFields;
     }
 
     private enum PathType {
