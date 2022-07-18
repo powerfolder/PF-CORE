0381e2261e66125d263c4f240c356d218819eb9b
sprajc <sprajc@powerfolder.com>
2022-07-18 10:50:13 +0200

    Fixed NPE for existing file

--- src/main/de/dal33t/powerfolder/disk/Folder.java
+++ src/main/de/dal33t/powerfolder/disk/Folder.java
@@ -881,9 +881,9 @@ public class Folder extends PFComponent {
                                 }
                             }
                             arch.archive(oldLocalFileInfo, targetFile, false);
+                            fInfo.setPreviousSize(oldLocalFileInfo.getSize());
                         }
                         logFileOperation("UPDATED", oldLocalFileInfo, fInfo);
-                        fInfo.setPreviousSize(oldLocalFileInfo.getSize());
                     } catch (IOException e) {
                         // Same behavior as below, on failure drop out
                         // TODO Maybe raise folder-problem....
