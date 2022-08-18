ad1a6e35159d7f00c13366f702147a6019c92b2c
sprajc <sprajc@powerfolder.com>
2022-08-18 23:46:47 +0200

    PF-1812 Sync profile

--- src/main/de/dal33t/powerfolder/clientserver/FolderService.java
+++ src/main/de/dal33t/powerfolder/clientserver/FolderService.java
@@ -201,7 +201,7 @@ public interface FolderService {
      *
      * @param profile
      */
-    void setSyncProfile(SyncProfile profile);
+    void setSyncProfileOfAllFolders(SyncProfile profile);
 
     /**
      * TRAC #991
