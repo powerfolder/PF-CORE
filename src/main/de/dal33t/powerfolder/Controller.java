3fe437caf713d1d7223ae3a75340298c4592c0aa
sprajc <sprajc@powerfolder.com>
2022-08-18 23:48:29 +0200

    45

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -90,7 +90,7 @@ public class Controller extends PFComponent {
     private static final int MINOR_VERSION = 4;
     private static final int REVISION_VERSION = 37;
 
-    private static final int SPRINT_NUMBER = 44;
+    private static final int SPRINT_NUMBER = 45;
 
     /**
      * Program version.
