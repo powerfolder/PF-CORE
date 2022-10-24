3c09538d5fc1317068e9b990a8b9e649c18d14de
sprajc <sprajc@powerfolder.com>
2022-10-24 10:58:26 +0200

    INT-673 Solved in output and input

--- src/main/de/dal33t/powerfolder/security/Account.java
+++ src/main/de/dal33t/powerfolder/security/Account.java
@@ -738,7 +738,6 @@ public class Account implements Serializable, D2DObject, Auditable {
      */
 
     public void setLanguage(String lang) {
-        // TODO: PENTEST2022: Check for valid language codes
         this.language = lang;
     }
 
