4c824eb8c35c230a23a07f9ea397fec02958ba34
sprajc <sprajc@powerfolder.com>
2022-11-18 23:47:56 +0100

    PFC-3349

--- src/main/de/dal33t/powerfolder/ui/preferences/GeneralSettingsTab.java
+++ src/main/de/dal33t/powerfolder/ui/preferences/GeneralSettingsTab.java
@@ -257,8 +257,7 @@ public class GeneralSettingsTab extends PFUIComponent implements PreferenceTab {
             builder.add(verboseCB, cc.xy(3, row));
 
             // Start: PFC-2385
-            if (PreferencesEntry.MODE_SELECT.getValueBoolean(getController())
-                    && !PreferencesEntry.WEBDAV_ONLY.getValueBoolean(getController())) {
+            if (PreferencesEntry.MODE_SELECT.getValueBoolean(getController())) {
                 row += 2; builder.appendRelatedComponentsGapRow(); builder.appendRow("pref");
                 builder.add(modeChooser, cc.xy(3, row));
             }
