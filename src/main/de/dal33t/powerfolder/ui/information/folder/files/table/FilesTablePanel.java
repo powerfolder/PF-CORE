f1071fd98ffadfa0cb9645f5ac8993b3f6bce639
sprajc <sprajc@powerfolder.com>
2023-01-20 13:06:05 +0100

    INT-686 Generate file link instead of download link

--- src/main/de/dal33t/powerfolder/ui/information/folder/files/table/FilesTablePanel.java
+++ src/main/de/dal33t/powerfolder/ui/information/folder/files/table/FilesTablePanel.java
@@ -19,34 +19,9 @@
  */
 package de.dal33t.powerfolder.ui.information.folder.files.table;
 
-import java.awt.Cursor;
-import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Collection;
-
-import javax.swing.AbstractAction;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
-import javax.swing.KeyStroke;
-import javax.swing.SwingUtilities;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
-
 import de.dal33t.powerfolder.ConfigurationEntry;
 import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.disk.Folder;
@@ -65,23 +40,27 @@ import de.dal33t.powerfolder.ui.PFUIComponent;
 import de.dal33t.powerfolder.ui.action.BaseAction;
 import de.dal33t.powerfolder.ui.contextmenu.ShareFileNotificationHandler;
 import de.dal33t.powerfolder.ui.information.HasDetailsPanel;
-import de.dal33t.powerfolder.ui.information.folder.files.DirectoryFilter;
-import de.dal33t.powerfolder.ui.information.folder.files.DirectoryFilterListener;
-import de.dal33t.powerfolder.ui.information.folder.files.FileDetailsPanel;
-import de.dal33t.powerfolder.ui.information.folder.files.FilesTab;
-import de.dal33t.powerfolder.ui.information.folder.files.FilteredDirectoryEvent;
-import de.dal33t.powerfolder.ui.information.folder.files.FilteredDirectoryModel;
+import de.dal33t.powerfolder.ui.information.folder.files.*;
 import de.dal33t.powerfolder.ui.information.folder.files.versions.FileVersionsPanel;
 import de.dal33t.powerfolder.ui.util.SwingWorker;
 import de.dal33t.powerfolder.ui.util.UIUtil;
 import de.dal33t.powerfolder.ui.widget.ActionLabel;
 import de.dal33t.powerfolder.ui.widget.ActivityVisualizationWorker;
-import de.dal33t.powerfolder.util.BrowserLauncher;
-import de.dal33t.powerfolder.util.BrowserLauncher.URLProducer;
 import de.dal33t.powerfolder.util.PathUtils;
 import de.dal33t.powerfolder.util.Translation;
 import de.dal33t.powerfolder.util.Util;
 import de.dal33t.powerfolder.util.os.OSUtil;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
 
 public class FilesTablePanel extends PFUIComponent implements HasDetailsPanel,
     DirectoryFilterListener
@@ -864,7 +843,7 @@ public class FilesTablePanel extends PFUIComponent implements HasDetailsPanel,
                         public void run() {
                             String previousEntry = Util.getClipboardContents();
                             String url = getController().getOSClient().
-                                    getFolderService(fileInfo.getFolderInfo()).getDownloadLink(fileInfo);
+                                    getFolderService(fileInfo.getFolderInfo()).getFileLink(fileInfo);
                             Util.setClipboardContents(url);
 
                             ShareFileNotificationHandler handler = new ShareFileNotificationHandler(
