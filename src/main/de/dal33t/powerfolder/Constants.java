8918ab069bc5025c430139395525a4a0e3d99dc5
sprajc <sprajc@powerfolder.com>
2023-01-26 00:28:28 +0100

    PFS-4114 Code cosmetics

--- src/main/de/dal33t/powerfolder/Constants.java
+++ src/main/de/dal33t/powerfolder/Constants.java
@@ -430,12 +430,7 @@ public class Constants {
     public static final String MS_OFFICE_FILENAME_PREFIX = "~$";
     public static final String LIBRE_OFFICE_FILENAME_PREFIX = ".~lock.";
 
-
     public static final List<String> SUPPORTED_LANGUAGES = Arrays.asList("en","de");
-
-    public static final List<String> SUPPORTED_COUNTRIES = Arrays.asList("de","ch","nl","po","it");
-
-
 
     private Constants() {
         // No instance allowed
