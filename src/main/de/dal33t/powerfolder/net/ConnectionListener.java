d66b17efa40512964520d996c35f28b5de259fb2
Christian Sprajc <sprajc@powerfolder.com>
2024-03-27 21:58:55 +0100

    INT-856 Not use SSL on hostname binding for D2D to enable unencrypted websockets between nodes

--- src/main/de/dal33t/powerfolder/net/ConnectionListener.java
+++ src/main/de/dal33t/powerfolder/net/ConnectionListener.java
@@ -149,7 +149,8 @@ public class ConnectionListener extends PFComponent implements Runnable {
                 }
             }
             // Use SSL socket for D2D connections on external interfaces
-            if (useD2D && !(NetworkUtil.LOOPBACK_LOCALHOST_IPv4.equals(bind))) {
+            if (false) {
+            //if (useD2D && !(NetworkUtil.LOOPBACK_LOCALHOST_IPv4.equals(bind))) {
                 // Load trust store
                 KeyStore keyStore = KeyStore.getInstance("JKS");
                 keyStore.load(new FileInputStream(getController().getSslTrustStoreFile().toString()), new char[0]);
