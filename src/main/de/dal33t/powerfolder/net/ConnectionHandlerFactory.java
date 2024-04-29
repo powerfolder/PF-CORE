33bf79b5cff61bcca633e6f088a17d4f76990d66
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2024-04-29 15:29:18 +0200

    feature/PFC-3412 refactored to process ClientWebSocketConnectionHandler

--- src/main/de/dal33t/powerfolder/net/ConnectionHandlerFactory.java
+++ src/main/de/dal33t/powerfolder/net/ConnectionHandlerFactory.java
@@ -20,6 +20,7 @@
 package de.dal33t.powerfolder.net;
 
 import de.dal33t.powerfolder.*;
+import de.dal33t.powerfolder.d2d.ClientWebSocketConnectionHandler;
 import de.dal33t.powerfolder.d2d.D2DSocketConnectionHandler;
 import de.dal33t.powerfolder.light.MemberInfo;
 import de.dal33t.powerfolder.util.StringUtils;
@@ -177,7 +178,7 @@ public class ConnectionHandlerFactory extends PFComponent {
           conHan = new D2DSocketConnectionHandler(
             getController(), socket);
         }
-      else conHan = new PlainSocketConnectionHandler(
+      else conHan = new ClientWebSocketConnectionHandler(
         getController(), socket);
 
       /* Finally init this handler */
