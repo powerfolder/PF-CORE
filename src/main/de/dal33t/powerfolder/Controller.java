7770692bb60c78df614d6c8d078bcae4a3b0fadf
Christian Sprajc <sprajc@powerfolder.com>
2024-10-31 13:14:59 +0100

    22.1.101 PRODUCTION

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -87,10 +87,10 @@ public class Controller extends PFComponent {
     private static final Logger log = Logger.getLogger(Controller.class.getName());
 
     private static final int MAJOR_VERSION = 22;
-    private static final int MINOR_VERSION = 2;
-    private static final int REVISION_VERSION = 7;
+    private static final int MINOR_VERSION = 1;
+    private static final int REVISION_VERSION = 101;
 
-    private static final int SPRINT_NUMBER = 58;
+    private static final int SPRINT_NUMBER = 57;
 
     /**
      * Program version.
