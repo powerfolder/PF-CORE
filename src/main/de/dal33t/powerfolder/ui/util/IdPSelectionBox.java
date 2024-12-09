a97579e61ba709d2c679f9a50efe671edc9b5aff
Christian Sprajc <sprajc@powerfolder.com>
2024-12-09 23:01:20 +0100

    PFC-3435 Refactorings

--- src/main/de/dal33t/powerfolder/ui/util/IdPSelectionBox.java
+++ src/main/de/dal33t/powerfolder/ui/util/IdPSelectionBox.java
@@ -2,30 +2,42 @@ package de.dal33t.powerfolder.ui.util;
 
 import de.dal33t.powerfolder.ConfigurationEntry;
 import de.dal33t.powerfolder.Controller;
+import de.dal33t.powerfolder.PFComponent;
 import de.dal33t.powerfolder.clientserver.ServerClient;
 import de.dal33t.powerfolder.ui.StyledComboBox;
-import de.dal33t.powerfolder.util.Convert;
-import de.dal33t.powerfolder.util.Reject;
-import de.dal33t.powerfolder.util.StringUtils;
-import de.dal33t.powerfolder.util.Translation;
+import de.dal33t.powerfolder.util.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.swing.SwingWorker;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class IdPSelectionBox extends StyledComboBox<String> {
+    private final Controller controller;
+    private List<String> idPList;
     private boolean listLoaded;
 
     public IdPSelectionBox(Controller controller) {
         super(new String[]{Translation.get("general.loading")});
         Reject.ifNull(controller, "Controller");
+        this.controller = controller;
+        this.idPList = new ArrayList<>();
         this.listLoaded = false;
         setEnabled(false);
 
@@ -51,7 +63,6 @@ public class IdPSelectionBox extends StyledComboBox<String> {
                 }
 
                 JSONArray resp = new JSONArray(body.toString());
-                List<String> idPList = new ArrayList<>(resp.length());
 
                 String lastIdP = ConfigurationEntry.SERVER_IDP_LAST_CONNECTED.getValue(controller);
                 boolean lastIdPSet = false;
@@ -106,7 +117,7 @@ public class IdPSelectionBox extends StyledComboBox<String> {
                     ConfigurationEntry.SERVER_IDP_LAST_CONNECTED_ECP.setValue(controller, ServerClient.SAML_EXTERNAL_NON_SAML_USERS);
                 }
 
-                addActionListener(new IdPSelectionAction(controller, idPList));
+                addActionListener(new IdPSelectionAction());
                 setEnabled(true);
                 listLoaded = true;
                 return null;
@@ -118,5 +129,53 @@ public class IdPSelectionBox extends StyledComboBox<String> {
 
     public boolean isListLoaded() {
         return listLoaded;
+    }
+
+    private class IdPSelectionAction implements ActionListener {
+
+        @Override
+        public void actionPerformed(final ActionEvent e) {
+            SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() throws UnsupportedEncodingException {
+                    int index = getSelectedIndex();
+                    String entity = idPList.get(index);
+
+                    ConfigurationEntry.SERVER_IDP_LAST_CONNECTED.setValue(controller, entity);
+                    String externalNames = ConfigurationEntry.SERVER_IDP_EXTERNAL_NAMES.getValue(controller);
+
+                    if (StringUtils.isBlank(entity)) {
+                        return null;
+                    } else if (ServerClient.SAML_EXTERNAL_NON_SAML_USERS.equals(entity)
+                            || (StringUtils.isNotBlank(externalNames) && externalNames.contains(entity)))
+                    {
+                        ConfigurationEntry.SERVER_IDP_LAST_CONNECTED_ECP
+                                .setValue(controller, ServerClient.SAML_EXTERNAL_NON_SAML_USERS);
+                        return null;
+                    }
+
+                    String idpLookupURL = ConfigurationEntry.SERVER_WEB_URL.getValue(controller)
+                            + "/api/idpd?entityID="
+                            + URLEncoder.encode(entity, Convert.UTF8.toString());
+
+                    HttpGet getBindingURL = new HttpGet(idpLookupURL);
+                    // PFC-2669:
+                    HttpClientBuilder builder = Util.createHttpClientBuilder(controller);
+                    HttpClient client = builder.build();
+
+                    try {
+                        HttpResponse httpResponse = client.execute(getBindingURL);
+                        String ecpURL = EntityUtils.toString(httpResponse.getEntity());
+                        ConfigurationEntry.SERVER_IDP_LAST_CONNECTED_ECP.setValue(controller, ecpURL);
+                    } catch (IOException e1) {
+                    }
+
+                    controller.saveConfig();
+                    return null;
+                }
+            };
+
+            worker.execute();
+        }
     }
 }
