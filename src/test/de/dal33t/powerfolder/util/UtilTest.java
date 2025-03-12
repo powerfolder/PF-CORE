dbaee337b9768030ac072f6468a54a3e91ede46a
Christian Sprajc <sprajc@powerfolder.com>
2025-03-12 19:11:53 +0100

    PFC-3448 testCreateHttpBuilderProxyHost skip on linux

--- src/test/de/dal33t/powerfolder/util/UtilTest.java
+++ src/test/de/dal33t/powerfolder/util/UtilTest.java
@@ -26,6 +26,8 @@ import de.dal33t.powerfolder.Member;
 import de.dal33t.powerfolder.light.FileInfo;
 import de.dal33t.powerfolder.light.MemberInfo;
 import de.dal33t.powerfolder.net.ConnectionException;
+import de.dal33t.powerfolder.util.os.OSUtil;
+import de.javasoft.util.OS;
 import junit.framework.TestCase;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.client.methods.HttpGet;
@@ -834,6 +836,9 @@ public class UtilTest extends TestCase {
 
 
     public void testCreateHttpBuilderProxyHost() throws IOException {
+        if (OSUtil.isLinux()) {
+            return;
+        }
         File file = new File("build/test/first.config");
         file.getParentFile().mkdirs();
         file.createNewFile();
