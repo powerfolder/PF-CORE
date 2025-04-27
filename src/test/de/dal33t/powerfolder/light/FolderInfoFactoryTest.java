d35e7da73f1ddfb103f816cc161f8abd6fd125e8
Christian Sprajc <sprajc@powerfolder.com>
2025-04-27 23:36:21 +0200

    PFS-4514 Tests

---
+++ src/test/de/dal33t/powerfolder/light/FolderInfoFactoryTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2004 - 2025 Christian Sprajc. All rights reserved.
+ *
+ * This file is part of PowerFolder.
+ *
+ * PowerFolder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * PowerFolder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerFolder. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.dal33t.powerfolder.light;
+
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+
+public class FolderInfoFactoryTest extends TestCase {
+
+
+    public void testParentFolder() {
+        FolderInfo top = FolderInfoFactory.newTopFolder("top");
+        DirectoryInfo topRoot = FileInfoFactory.lookupDirectory(top, "");
+        FolderInfo subDirect = FolderInfoFactory.newFolder("subDirect", topRoot);
+
+        assertEquals(top, subDirect.getParent().getFolderInfo());
+        assertEquals("", subDirect.getParent().getRelativeName());
+        assertEquals("subDirect", subDirect.getLocation().getRelativeName());
+
+        DirectoryInfo topDeepDir = FileInfoFactory.lookupDirectory(top, "this/is/in/a/deep/structure");
+        FolderInfo subDeep = FolderInfoFactory.newFolder("subDeep", topDeepDir);
+
+        assertEquals(top, subDeep.getParent().getFolderInfo());
+        assertEquals("this/is/in/a/deep/structure", subDeep.getParent().getRelativeName());
+        assertEquals("this/is/in/a/deep/structure/subDeep", subDeep.getLocation().getRelativeName());
+
+        subDirect = FolderInfoFactory.rename(subDirect, "subDirectRENAMED");
+        assertEquals(top, subDirect.getParent().getFolderInfo());
+        assertEquals("", subDirect.getParent().getRelativeName());
+    }
+
+
+
+}
