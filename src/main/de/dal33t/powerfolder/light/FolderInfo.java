b5fea60d242c737d4a58ced8444783c57aeaad0c
Christian Sprajc <sprajc@powerfolder.com>
2025-06-04 19:19:25 +0200

    PFS-4529 Logging

--- src/main/de/dal33t/powerfolder/light/FolderInfo.java
+++ src/main/de/dal33t/powerfolder/light/FolderInfo.java
@@ -285,7 +285,6 @@ public class FolderInfo implements Serializable, Cloneable, D2DObject {
 
     public FolderInfo intern(boolean force) {
         if (force) {
-            LOG.info(this + ": rename (forced internalize)");
             return INTERNALIZER.rename(this);
         } else {
             return INTERNALIZER.intern(this);
