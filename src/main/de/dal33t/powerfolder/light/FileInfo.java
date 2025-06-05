bad04b2ef46c378a7729d81d168cf65cbf13a557
Christian Sprajc <sprajc@powerfolder.com>
2025-06-05 11:01:06 +0200

    PFS-4515 lock file now includes modify account. ignore missing account info for meta folders

--- src/main/de/dal33t/powerfolder/light/FileInfo.java
+++ src/main/de/dal33t/powerfolder/light/FileInfo.java
@@ -180,7 +180,11 @@ public class FileInfo implements Serializable, DiskItem, Cloneable, D2DObject {
         this.reupload = false;
 
         if (Feature.FILEINFO_LOG_MISSING_MODIFIED_BY_ACCOUNT.isEnabled()) {
-            if (modifiedByAccount == null && log.isLoggable(Level.WARNING) && !folderInfo.getName().endsWith("server_maintenance")) {
+            if (modifiedByAccount == null
+                    && log.isLoggable(Level.WARNING)
+                    && !folderInfo.isMetaFolder()
+                    && !folderInfo.getName().endsWith("server_maintenance")) {
+
                 log.log(Level.INFO, this.toDetailString() + ": Missing account information", new StackDump());
             }
         }
