8e0371c4518e7f2bf0f809bf7a5bac14608695cd
Christian Sprajc <sprajc@powerfolder.com>
2025-06-27 23:56:02 +0200

    PFC-3477

--- src/main/de/dal33t/powerfolder/disk/Folder.java
+++ src/main/de/dal33t/powerfolder/disk/Folder.java
@@ -1058,6 +1058,13 @@ public class Folder extends PFComponent {
             logFine(getName() + ": Already shutdown: Not scanLocalFiles");
             return false;
         }
+        if (currentInfo.isSubFolder()) {
+            Folder parentFolder = currentInfo.getParent().getFolder(getController().getFolderRepository());
+            if (parentFolder != null) {
+                logInfo(this + ": Skipping scan of local filesystem. is handled by parent " + parentFolder);
+                return false;
+            }
+        }
         checkIfDeviceDisconnected();
         ScanResult result;
         FolderScanner scanner = getController().getFolderRepository()
@@ -1897,7 +1904,6 @@ public class Folder extends PFComponent {
                 logInfo(this + ": Using OWN DAO for subfolder. Parent folder not here.");
                 dao = new FileInfoDAOHashMapImpl(getMySelf().getId(), diskItemFilter);
             }
-
         }
     }
 
