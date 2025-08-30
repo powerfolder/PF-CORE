4daf53b8769c65350078f166c2b11244a1473265
YOUNES EL-HAITI <elhaiti.younes@gmail.com>
2025-08-30 19:57:58 +0100

    INT-1084 add change storage size

--- src/main/de/dal33t/powerfolder/clientserver/OrganizationService.java
+++ src/main/de/dal33t/powerfolder/clientserver/OrganizationService.java
@@ -30,7 +30,15 @@ public interface OrganizationService {
      * update max user count
      *
      * @param organizationId the organization id
-     * @param newMaxUsers the organization id
+     * @param newMaxUsers new Users count
      */
     void changeMaxUsers(String organizationId, int newMaxUsers);
+
+    /**
+     * update Storage Size
+     *
+     * @param organizationId the organization id
+     * @param newMaxStorageSize new max storage
+     */
+    void changeStorageSize(String organizationId, long newMaxStorageSize);
 }
