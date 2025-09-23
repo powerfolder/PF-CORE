492c95215869aa3b29b1867e6cad4864338e0e75
Christian Sprajc <sprajc@powerfolder.com>
2025-09-23 13:38:40 +0200

    Text

--- src/main/de/dal33t/powerfolder/disk/Folder.java
+++ src/main/de/dal33t/powerfolder/disk/Folder.java
@@ -5505,7 +5505,7 @@ public class Folder extends PFComponent {
     public Folder share(DirectoryInfo subDirInfo) {
         Reject.ifNull(subDirInfo, "Subdirectory");
         Reject.ifFalse(subDirInfo.getFolderInfo().equals(currentInfo), "Folder mismatch");
-        Reject.ifTrue(getInfo().isSubFolder(), "Folder is Subfolder. Sharing subfolder only allowed on top level folder");
+        Reject.ifTrue(getInfo().isSubFolder(), "Folder is Subfolder. Sharing subfolder only allowed from top level folder");
 
         Path subDirPath = subDirInfo.getDiskFile(getController().getFolderRepository());
         Folder subFolder = getController().getFolderRepository().findExistingFolder(subDirPath);
