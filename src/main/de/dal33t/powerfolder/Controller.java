034f3c7701b5fc05e812337c0de16db8a60b74de
Christian Sprajc <sprajc@powerfolder.com>
2026-01-02 16:23:01 +0100

    7

--- src/main/de/dal33t/powerfolder/Controller.java
+++ src/main/de/dal33t/powerfolder/Controller.java
@@ -86,9 +86,9 @@ import static org.quartz.CronScheduleBuilder.dailyAtHourAndMinute;
 public class Controller extends PFComponent {
     private static final Logger log = Logger.getLogger(Controller.class.getName());
 
-    private static final int MAJOR_VERSION = 26;
+    private static final int MAJOR_VERSION = 27;
     private static final int MINOR_VERSION = 1;
-    private static final int REVISION_VERSION = 7;
+    private static final int REVISION_VERSION = 8;
 
     private static final int SPRINT_NUMBER = 65;
 
