b93572271d0574cc6787d19509f5c8bb7fe8e509
Christian Sprajc <sprajc@powerfolder.com>
2026-01-18 23:57:41 +0100

    PFS-4731

--- src/main/de/dal33t/powerfolder/clientserver/SecurityService.java
+++ src/main/de/dal33t/powerfolder/clientserver/SecurityService.java
@@ -187,21 +187,6 @@ public interface SecurityService {
 
     /**
      * @param foInfo
-     * @return the Anonyoumus permission for the given folder.
-     * @deprecated Use {@link de.dal33t.powerfolder.domain.FileLink}s instead
-     */
-    FolderPermission getWebPermission(FolderInfo foInfo);
-
-    /**
-     * Sets the Anonyoumus permission for the given folder.
-     *
-     * @param foInfo
-     * @param permission
-     */
-    void setWebPermission(FolderInfo foInfo, FolderPermission permission);
-
-    /**
-     * @param foInfo
      * @return the permissions on the folder.
      */
     Map<AccountInfo, FolderPermission> getFolderPermissions(FolderInfo foInfo);
